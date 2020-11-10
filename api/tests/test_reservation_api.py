0ca4786852bdfc2d08c746df6fc13e06d8dbb355
Oskari Tiala <oskari.tiala@haltu.fi>
2020-11-10 11:16:56 +0200

    Add test

--- api/tests/test_reservation_api.py
+++ api/tests/test_reservation_api.py
@@ -4,7 +4,7 @@ from reservations.models import Reservation
 
 
 @pytest.mark.django_db
-def test_foobar(user_api_client, valid_reservation_data):
+def test_reservation_create(user_api_client, valid_reservation_data):
     assert Reservation.objects.count() == 0
     response = user_api_client.post(reverse("reservation-list"), valid_reservation_data)
     assert response.status_code == 201
