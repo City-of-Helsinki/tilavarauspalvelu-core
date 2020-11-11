4c7d4bdeeeaab3eed3328690448ca9926df88433
Oskari Tiala <oskari.tiala@haltu.fi>
2020-11-11 10:40:44 +0200

    Change read_only_fields

--- api/reservations_api.py
+++ api/reservations_api.py
@@ -1,13 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, serializers
 from api.base import BaseNestedSerializer
 from reservations.models import Reservation
 from .reservation_units_api import ReservationUnitSerializer
 
 
 class ReservationSerializer(BaseNestedSerializer):
-    reservation_unit = ReservationUnitSerializer(
-        read_only=True, many=True, exclude_detail_fields=True
-    )
+    # reservation_unit = ReservationUnitSerializer(many=True, exclude_detail_fields=True)
 
     class Meta:
         model = Reservation
@@ -23,6 +21,15 @@ class ReservationSerializer(BaseNestedSerializer):
             "reservation_unit",
             "recurring_reservation",
         ]
+        read_only_fields = ["state", "priority", "user"]
+
+    # def validate(self, data):
+    #     for reservation_unit in data["reservation_unit"]:
+    #         if reservation_unit.check_reservation_overlap(data["begin"], data["end"]):
+    #             raise serializers.ValidationError(
+    #                 "Overlapping reservations are not allowed"
+    #             )
+    #     return data
 
 
 class ReservationViewSet(viewsets.ModelViewSet):
@@ -32,3 +39,6 @@ class ReservationViewSet(viewsets.ModelViewSet):
         .prefetch_related("reservation_unit")
         .select_related("recurring_reservation")
     )
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
