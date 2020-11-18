6ac285638d9c4969158a4047c7d406db1537d46d
Tommi Mantila <tommi@manbytes.fi>
2020-11-18 14:25:49 +0200

    Hierarchy and types for districts. Also changes area fields to surface_area to describe the field better.

--- spaces/models.py
+++ spaces/models.py
@@ -3,10 +3,36 @@ from django.utils.translation import gettext_lazy as _
 
 
 class District(models.Model):
+    TYPE_MAJOR_DISTRICT = "major_district"
+    TYPE_DISTRICT = "district"
+    TYPE_SUB_DISTRICT = "sub_district"
+    TYPE_NEIGHBORHOOD = "neighborhood"
+
+    TYPE_CHOICES = (
+        (TYPE_MAJOR_DISTRICT, _("Major district")),
+        (TYPE_DISTRICT, _("District")),
+        (TYPE_SUB_DISTRICT, _("Sub district")),
+        (TYPE_NEIGHBORHOOD, _("Neighborhood")),
+    )
+
+    district_type = models.CharField(
+        verbose_name=_("Type"),
+        max_length=30,
+        choices=TYPE_CHOICES,
+        default=TYPE_DISTRICT,
+    )
     name = models.CharField(verbose_name=_("Name"), max_length=255)
+    parent = models.ForeignKey(
+        "self",
+        verbose_name=_("Parent"),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="children",
+    )
 
     def __str__(self):
-        return "{}".format(self.name)
+        return "{} ({})".format(self.name, self.parent.name if self.parent else "")
 
 
 class RealEstate(models.Model):
@@ -14,13 +40,18 @@ class RealEstate(models.Model):
     district = models.ForeignKey(
         District,
         verbose_name=_("District"),
+        related_name="real_estates",
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
     )
 
-    area = models.DecimalField(
-        verbose_name=_("Area"), max_digits=10, decimal_places=2, blank=True, null=True
+    surface_area = models.DecimalField(
+        verbose_name=_("Surface area"),
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True,
     )
     location = models.OneToOneField(
         "Location",
@@ -40,6 +71,7 @@ class Building(models.Model):
     district = models.ForeignKey(
         District,
         verbose_name=_("District"),
+        related_name="buildings",
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
@@ -48,13 +80,18 @@ class Building(models.Model):
     real_estate = models.ForeignKey(
         RealEstate,
         verbose_name=_("Real estate"),
+        related_name="buildings",
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
     )
 
-    area = models.DecimalField(
-        verbose_name=_("Area"), max_digits=10, decimal_places=2, blank=True, null=True
+    surface_area = models.DecimalField(
+        verbose_name=_("Surface area"),
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True,
     )
     location = models.OneToOneField(
         "Location",
@@ -79,6 +116,15 @@ class Space(models.Model):
         null=True,
         blank=True,
     )
+    district = models.ForeignKey(
+        District,
+        verbose_name=_("District"),
+        related_name="spaces",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
     building = models.ForeignKey(
         Building,
         verbose_name=_("Building"),
@@ -87,8 +133,12 @@ class Space(models.Model):
         null=True,
         blank=True,
     )
-    area = models.DecimalField(
-        verbose_name=_("Area"), max_digits=10, decimal_places=2, blank=True, null=True
+    surface_area = models.DecimalField(
+        verbose_name=_("Surface area"),
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True,
     )
     location = models.OneToOneField(
         "Location",
@@ -97,6 +147,9 @@ class Space(models.Model):
         blank=True,
         related_name="space",
         on_delete=models.CASCADE,
+    )
+    max_persons = models.fields.PositiveIntegerField(
+        verbose_name=_("Maximum number of persons"), null=True, blank=True
     )
 
     def __str__(self):
