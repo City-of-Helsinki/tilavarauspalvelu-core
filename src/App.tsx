5103dbbbb7253118bcea06136e6586060a7cb35d
Sami Siren <siren@foofactory.fi>
2020-11-24 21:13:10 +0200

    rename dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Home from './frontpage/Home';
+import Home from './home/Home';
 import PageWrapper from './component/PageWrapper';
 
 function App(): JSX.Element {
