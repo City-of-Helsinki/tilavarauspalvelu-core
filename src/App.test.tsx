54e7926b8dfc525c78f3c23d57c709bc92231172
Sami Siren <siren@foofactory.fi>
2020-12-01 15:39:17 +0200

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Etusivu/i);
+  const linkElement = screen.getByText(/Vakiovuorot/i);
   expect(linkElement).toBeInTheDocument();
 });
