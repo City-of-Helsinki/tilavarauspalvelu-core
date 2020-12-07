deab3d5d43b572e81ba34c8c7e0ac637d0a1a22d
Sami Siren <siren@foofactory.fi>
2020-12-07 13:29:36 +0200

    do not use card on home, make Card look more default

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, IconSearch } from 'hds-react';
 import { useHistory } from 'react-router-dom';
-import Card from '../component/Card';
 import MainContainer from '../component/MainContainer';
 import Head from './Head';
 import ApplicationPeriods from './ApplicationPeriodList';
@@ -16,27 +15,29 @@ const Home = (): JSX.Element => {
     <>
       <Head heading={t('home.head.heading')} text={t('home.head.text')} />
       <MainContainer>
-        <Card
-          heading="Hakeminen"
-          text="Vakiovuoroja haetaan yleisen haun kautta. Voit selata tiloja, mutta
-          hakemuksen voi ainoastaan jättää silloin kun hakuaika on käynnissä.">
-          <div className={styles.buttonContainer}>
-            <Button
-              variant="secondary"
-              onClick={() => history.push('/search')}
-              iconLeft={<IconSearch />}>
-              Selaa kaikkia tiloja
-            </Button>
-            <Button variant="secondary">Lue lisää hakuprosessista</Button>
-          </div>
-        </Card>
-        <Card
-          heading="Hakuajat"
-          text="Vakiovuoroihin on hakuaika kaksi kertaa vuodessa. Ajankohta vaihtelee
+        <h2 className="heading-l">Hakeminen</h2>
+        <p className="text-lg">
+          Vakiovuoroja haetaan yleisen haun kautta. Voit selata tiloja, mutta
+          hakemuksen voi ainoastaan jättää silloin kun hakuaika on käynnissä.
+        </p>
+        <div className={styles.buttonContainer}>
+          <Button
+            variant="secondary"
+            onClick={() => history.push('/search')}
+            iconLeft={<IconSearch />}>
+            Selaa kaikkia tiloja
+          </Button>
+          <Button variant="secondary">Lue lisää hakuprosessista</Button>
+        </div>
+        <h2 className="heading-l" style={{ marginTop: 'var(--spacing-xl)' }}>
+          Hakuajat
+        </h2>
+        <p className="text-lg">
+          Vakiovuoroihin on hakuaika kaksi kertaa vuodessa. Ajankohta vaihtelee
           palvelusta ja toimialasta riippuen. Voit tilata sähköpostimuistutuksen
-          tuleviin hakuihin.">
-          <ApplicationPeriods />
-        </Card>
+          tuleviin hakuihin.
+        </p>
+        <ApplicationPeriods />
       </MainContainer>
     </>
   );
