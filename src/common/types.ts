f6a2e72d24439dde24224391b443e2df3ca200bc
Sami Siren <siren@foofactory.fi>
2020-12-07 20:33:48 +0200

    add new api types

--- src/common/types.ts
+++ src/common/types.ts
@@ -8,3 +8,38 @@ export type ApplicationPeriod = {
   reservationPeriodEnd: string;
   purposes: number[];
 };
+
+export type Space = {
+  id: number;
+  locationType: 'fixed';
+  name: string;
+  parent: number;
+  building: number;
+  surfaceArea: null;
+};
+
+export type Resource = {
+  id: number;
+  name: string;
+  locationType: 'fixed';
+  space: number;
+  bufferTimeBefore: string;
+  bufferTimeAfter: string;
+};
+
+export type Service = {
+  id: number;
+  name: string;
+  serviceType: 'introduction';
+  bufferTimeBefore: string;
+  bufferTimeAfter: string;
+};
+
+export type ReservationUnit = {
+  id: number;
+  name: string;
+  spaces: Space[];
+  resources: Resource[];
+  services: Service[];
+  requireINtroduction: boolean;
+};
