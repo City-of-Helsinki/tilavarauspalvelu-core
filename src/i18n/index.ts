9ded51217ee4e300f393777ef8e677cc1aa343ce
Sami Siren <siren@foofactory.fi>
2020-12-08 17:10:38 +0200

    remove cruft

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,13 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-// import fi from './messages/fi';
-// import sv from './messages/sv';
-// import en from './messages/en';
 import messages from './messages/messages';
 
-console.log(JSON.stringify(messages, null, 2));
-
-// const resources = { en, fi, sv };
 i18n.use(initReactI18next).init({
   resources: messages,
   lng: 'fi',
