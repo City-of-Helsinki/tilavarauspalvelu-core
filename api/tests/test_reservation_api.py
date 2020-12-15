80ea3b3ed688106fcea9f6e5c9e37a207cd9aad9
Anttipekka Renfors <anttipekka.renfors@anders.com>
2020-12-15 14:47:59 +0200

    TILA-95 Application model refinements.
    TILA-95 Nested serializers for application.
    
    TILA-95 Ability group and age group endpoints.
    
    TILA-95 More test cases.
    
    TILA-95 Remove unique constraint from unit identifier.
    
    TILA-95 Set user in serializer, not in the viewset. Allow null values in user for now since we are not enforcing logins.
    
    Rebase fix.

--- api/tests/test_reservation_api.py
+++ api/tests/test_reservation_api.py
@@ -3,7 +3,7 @@ import datetime
 import pytest
 from django.urls import reverse
 
-from reservations.models import Reservation
+from reservations.models import AbilityGroup, AgeGroup, Reservation
 
 
 @pytest.mark.django_db
@@ -28,3 +28,53 @@ def test_reservation_overlapping(user_api_client, reservation, valid_reservation
         reverse("reservation-list"), data=valid_reservation_data, format="json"
     )
     assert response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_age_group_create(user_api_client):
+    assert AgeGroup.objects.count() == 0
+    response = user_api_client.post(
+        reverse("age_group-list"), data={"minimum": 20, "maximum": 40}, format="json"
+    )
+    assert response.status_code == 201
+    assert AgeGroup.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_age_group_create_invalid_maximum(user_api_client):
+    assert AgeGroup.objects.count() == 0
+    response = user_api_client.post(
+        reverse("age_group-list"), data={"minimum": 20, "maximum": 10}, format="json"
+    )
+    assert response.status_code == 400
+    assert (
+        "Maximum age should be larger than minimum age"
+        in response.data["non_field_errors"]
+    )
+
+
+@pytest.mark.django_db
+def test_age_group_fetch(user_api_client, ten_to_15_age_group):
+    response = user_api_client.get(reverse("age_group-list"))
+    assert response.status_code == 200
+    assert len(response.data) == 1
+    assert response.data[0].get("minimum") == 10
+    assert response.data[0].get("maximum") == 15
+
+
+@pytest.mark.django_db
+def test_ability_group_create(user_api_client):
+    assert AbilityGroup.objects.count() == 0
+    response = user_api_client.post(
+        reverse("ability_group-list"), data={"name": "new group"}, format="json"
+    )
+    assert response.status_code == 201
+    assert AbilityGroup.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_ability_group_fetch(user_api_client, hobbyist_ability_group):
+    response = user_api_client.get(reverse("ability_group-list"))
+    assert response.status_code == 200
+    assert len(response.data) == 1
+    assert response.data[0].get("name") == "Hobbyist level"
