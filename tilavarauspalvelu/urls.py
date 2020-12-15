c1a3b324c2766c0d926bbbf331bbf54c147e99eb
indigane <indigane@users.noreply.github.com>
2020-12-15 13:11:27 +0200

    Add initial AutoSchema generation

--- tilavarauspalvelu/urls.py
+++ tilavarauspalvelu/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 from graphene_django.views import GraphQLView
+from rest_framework.schemas import get_schema_view
 
 from api.urls import router as api_router
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("v1/", include(api_router.urls)),
+    path("openapi/", get_schema_view()),
     path("graphql", GraphQLView.as_view(graphiql=True)),
 ]
