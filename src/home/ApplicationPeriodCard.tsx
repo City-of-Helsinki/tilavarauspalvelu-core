a885efc31c86a55ba0d16ab0084ba44ef95c2a3d
Sami Siren <siren@foofactory.fi>
2020-12-21 18:41:38 +0200

    use button

--- src/home/ApplicationPeriodCard.tsx
+++ src/home/ApplicationPeriodCard.tsx
@@ -35,14 +35,12 @@ const ApplicationPeriodCard = ({ applicationPeriod }: Props): JSX.Element => {
                 ),
               })}
         </div>
-        <div className={styles.linkContainer}>
-          <span className={styles.linkIcon}>
-            <IconArrowRight />
-          </span>
-          <span className={styles.linkText}>
-            {t('ApplicationPeriodCard.criteria')}
-          </span>
-        </div>
+        <Button
+          id={styles.noLeftMargin}
+          variant="supplementary"
+          iconLeft={<IconArrowRight />}>
+          {t('ApplicationPeriodCard.criteria')}
+        </Button>
       </Container>
       {active ? (
         <Button className={styles.button} disabled>
