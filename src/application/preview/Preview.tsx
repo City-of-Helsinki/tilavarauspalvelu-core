70a4e066977727f0b1d6f4b31e53f9b48d52dc04
Sami Siren <siren@foofactory.fi>
2020-12-21 16:13:20 +0200

    work on preview page

--- src/application/preview/Preview.tsx
+++ src/application/preview/Preview.tsx
@@ -1,11 +1,13 @@
 import { Button, IconArrowLeft, IconArrowRight } from 'hds-react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './Preview.module.scss';
-import { Application as ApplicationType } from '../../common/types';
+import { Application, ReservationUnit, Parameter } from '../../common/types';
+import { formatDate } from '../../common/util';
+import { getParameters, getReservationUnits } from '../../common/api';
 
 type Props = {
-  application: ApplicationType;
+  application: Application;
   onNext: () => void;
 };
 
@@ -14,7 +16,7 @@ const LabelValue = ({
   value,
 }: {
   label: string;
-  value: string | number | null;
+  value: any;
 }): JSX.Element | null => (
   <div>
     <div className={styles.label}>{label}</div>
@@ -22,16 +24,56 @@ const LabelValue = ({
   </div>
 );
 
+const mapArrayById = (array: { id: number }[]): { [key: number]: any } => {
+  return array.reduce((prev, current) => {
+    // eslint-disable-next-line no-param-reassign
+    prev[current.id] = current;
+    return prev;
+  }, {} as { [key: number]: any });
+};
+
 const Preview = ({ onNext, application }: Props): JSX.Element | null => {
+  const [ready, setReady] = useState(false);
+
+  const [ageGroupOptions, setAgeGroupOptions] = useState<{
+    [key: number]: Parameter;
+  }>({});
+  const [purposeOptions, setPurposeOptions] = useState<{
+    [key: number]: Parameter;
+  }>({});
+  const [abilityGroupOptions, setAbilityGroupOptions] = useState<{
+    [key: number]: Parameter;
+  }>({});
+
+  const [reservationUnits, setReservationUnits] = useState<{
+    [key: number]: ReservationUnit;
+  }>({});
+
+  useEffect(() => {
+    async function fetchData() {
+      // there's no api to get reservation units with multiple ids so we're getting them all :)  a.k.a. FIXME
+      const units = await getReservationUnits({ search: undefined });
+      setReservationUnits(mapArrayById(units));
+
+      const fetchedAbilityGroupOptions = await getParameters('ability_group');
+      setAbilityGroupOptions(mapArrayById(fetchedAbilityGroupOptions));
+      const fetchedAgeGroupOptions = await getParameters('age_group');
+      setAgeGroupOptions(mapArrayById(fetchedAgeGroupOptions));
+      const fetchedPurposeOptions = await getParameters('purpose');
+      setPurposeOptions(mapArrayById(fetchedPurposeOptions));
+      setReady(true);
+    }
+    fetchData();
+  }, []);
+
   const { t } = useTranslation();
 
   const onSubmit = () => {
-    // eslint-disable-next-line no-param-reassign
-    application.status = 'review';
+    Object.assign(application.status, 'review');
     onNext();
   };
 
-  return application.contactPerson != null ? (
+  return ready ? (
     <>
       <div className={styles.subHeadLine}>
         {t('Application.preview.basicInfoSubHeading')}
@@ -39,15 +81,15 @@ const Preview = ({ onNext, application }: Props): JSX.Element | null => {
       <div className={styles.twoColumnContainer}>
         <LabelValue
           label={t('Application.preview.firstName')}
-          value={application.contactPerson.firstName}
+          value={application.contactPerson?.firstName}
         />
         <LabelValue
           label={t('Application.preview.lastName')}
-          value={application.contactPerson.lastName}
+          value={application.contactPerson?.lastName}
         />
         <LabelValue
           label={t('Application.preview.email')}
-          value={application.contactPerson.email}
+          value={application.contactPerson?.email}
         />
       </div>
       {application.applicationEvents.map((applicationEvent) => (
@@ -64,15 +106,19 @@ const Preview = ({ onNext, application }: Props): JSX.Element | null => {
             />
             <LabelValue
               label={t('Application.preview.applicationEvent.ageGroup')}
-              value={applicationEvent.ageGroupId}
+              value={`${
+                ageGroupOptions[applicationEvent.ageGroupId || 0].minimum
+              } - ${ageGroupOptions[applicationEvent.ageGroupId || 0].maximum}`}
             />
             <LabelValue
               label={t('Application.preview.applicationEvent.abilityGroup')}
-              value={applicationEvent.abilityGroupId}
+              value={
+                abilityGroupOptions[applicationEvent.abilityGroupId || 0].name
+              }
             />
             <LabelValue
               label={t('Application.preview.applicationEvent.purpose')}
-              value={applicationEvent.purposeId}
+              value={purposeOptions[applicationEvent.purposeId || 0].name}
             />
             <LabelValue
               label={t('Application.preview.applicationEvent.additionalInfo')}
@@ -80,11 +126,11 @@ const Preview = ({ onNext, application }: Props): JSX.Element | null => {
             />
             <LabelValue
               label={t('Application.preview.applicationEvent.begin')}
-              value={applicationEvent.begin}
+              value={formatDate(applicationEvent.begin || '')}
             />
             <LabelValue
               label={t('Application.preview.applicationEvent.end')}
-              value={applicationEvent.end}
+              value={formatDate(applicationEvent.end || '')}
             />
             <LabelValue
               label={t('Application.preview.applicationEvent.eventsPerWeek')}
@@ -92,16 +138,48 @@ const Preview = ({ onNext, application }: Props): JSX.Element | null => {
             />
             <LabelValue
               label={t('Application.preview.applicationEvent.biweekly')}
-              value={String(applicationEvent.biweekly)}
+              value={t(`common.${applicationEvent.biweekly}`)}
             />
-            {applicationEvent.eventReservationUnits.map((reservationUnit) => (
-              <LabelValue
-                key={reservationUnit.reservationUnit}
-                label={t(
-                  'Application.preview.applicationEvent.reeservationUnit'
-                )}
-                value={reservationUnit.reservationUnit}
-              />
+            {applicationEvent.eventReservationUnits.map(
+              (reservationUnit, index) => (
+                <LabelValue
+                  key={reservationUnit.reservationUnit}
+                  label={t(
+                    'Application.preview.applicationEvent.reservationUnit',
+                    { order: index + 1 }
+                  )}
+                  value={reservationUnits[reservationUnit.reservationUnit].name}
+                />
+              )
+            )}
+          </div>
+          <hr className={styles.ruler} />
+          <div className={styles.smallSubHeadLine}>
+            {t('Application.preview.applicationEventSchedules')}
+          </div>
+          <div className={styles.twoColumnContainer}>
+            {[
+              'monday',
+              'tuesday',
+              'wednesday',
+              'thursday',
+              'friday',
+              'saturday',
+              'sunday',
+            ].map((day, index) => (
+              <div key={day}>
+                <LabelValue
+                  label={t(`calendar.${day}`)}
+                  value={applicationEvent.applicationEventSchedules
+                    .filter((s) => s.day === index)
+                    .map((s, i) => (
+                      <span>
+                        {i !== 0 ? ', ' : ''}
+                        {s.begin.substring(0, 5)} - {s.end.substring(0, 5)}
+                      </span>
+                    ))}
+                />
+              </div>
             ))}
           </div>
         </div>
@@ -112,7 +190,7 @@ const Preview = ({ onNext, application }: Props): JSX.Element | null => {
           {t('common.prev')}
         </Button>
         <Button iconRight={<IconArrowRight />} onClick={() => onSubmit()}>
-          {t('common.next')}
+          {t('common.submit')}
         </Button>
       </div>
     </>
