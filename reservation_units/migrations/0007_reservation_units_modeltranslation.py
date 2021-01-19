b1f6454258572724aeeaf22a8f66d66494c89572
Oskari Tiala <oskari.tiala@haltu.fi>
2021-01-19 09:18:04 +0200

    Fix conflicting migrations

--- reservation_units/migrations/0007_reservation_units_modeltranslation.py
+++ reservation_units/migrations/0007_reservation_units_modeltranslation.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('reservation_units', '0004_reservation_unit_type'),
+        ('reservation_units', '0006_added_equipment_and_terms_of_use'),
     ]
 
     operations = [
