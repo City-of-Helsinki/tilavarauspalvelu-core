178fc5500e2ced9fe586a247a0011442483c7281
Nico Virkki <nico.virkki@anders.com>
2021-01-28 08:42:29 +0200

    Add UnitImporter for importing Units from desired json api
    TILA-281

---
+++ spaces/importers/units.py
@@ -0,0 +1,104 @@
+import requests
+from django.contrib.gis.geos import Point
+from django.db import transaction
+
+from spaces.models import Location, Unit
+
+
+class UnitImporter:
+    """Imports units from given json data source url.
+
+    Unit importer uses field map dict to map django db fields
+    and the data source's fields. Field map also should define the default values
+    to be used for missing values.
+    Field map can be given as a kwarg in __init__ and should be formatted like:
+        field_map = {
+            "unit": {
+                "<unit model field>": "<data source field>",
+                ...
+            },
+            "location": {
+                "<location model field>": "<data source field>",
+                ...
+            },
+        }
+
+    """
+
+    # Field map is used to map to django model fields to api data.
+    field_map = {
+        "unit": {
+            "service_map_id": "id",
+            "name": "name_fi",
+            "description": "desc_fi",
+            "web_page": "www_fi",
+            "email": "email",
+            "phone": "phone",
+        },
+        "location": {
+            "address_street": "street_address_fi",
+            "address_zip": "address_zip",
+            "address_city": "address_city_fi",
+            "lat": "latitude",
+            "lon": "longitude",
+        },
+        # These values we default to.
+        "defaults": {
+            "service_map_id": None,
+            "name": None,
+            "description": "",
+            "web_page": "",
+            "email": "",
+            "phone": "",
+            "address_street": None,
+            "address_zip": None,
+            "address_city": None,
+            "lat": None,
+            "lon": None,
+        },
+    }
+
+    def __init__(self, url: str, field_map: dict = None):
+        self.url = url
+        if field_map:
+            self.field_map = field_map
+
+    @transaction.atomic
+    def import_units(self):
+        resp = requests.get(self.url)
+        resp.raise_for_status()
+        unit_data = resp.json()
+
+        for row in unit_data:
+            self.create_unit(row)
+
+    def create_unit(self, importer_data: dict):
+        """Creates or updates an Unit object"""
+        unit_data = {}
+        for model_field, data_field in self.field_map["unit"].items():
+            unit_data[model_field] = importer_data.get(
+                data_field, self.field_map["defaults"].get(model_field)
+            )
+
+        unit, _ = Unit.objects.update_or_create(
+            service_map_id=importer_data.get("id"), defaults=unit_data
+        )
+
+        location_data = {}
+        for model_field, data_field in self.field_map["location"].items():
+            location_data[model_field] = importer_data.get(
+                data_field, self.field_map["defaults"].get(model_field)
+            )
+        location_data["unit"] = unit
+
+        point = None
+        lon = location_data.pop("lon", self.field_map["defaults"].get("lon"))
+        lat = location_data.pop("lat", self.field_map["defaults"].get("lat"))
+        if lon and lat:
+            point = Point(lon, lat)
+
+        location_data["coordinates"] = point
+
+        location, _ = Location.objects.update_or_create(
+            unit=unit, defaults=location_data
+        )
