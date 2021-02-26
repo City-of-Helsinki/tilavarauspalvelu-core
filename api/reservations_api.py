e6f0bcf2222b781040f59ba267d56fb44eb33af7
Nico Virkki <nico.virkki@anders.com>
2021-02-26 06:19:32 +0200

    Fix optional AgeGroupSerializer display option
    The display option was before *args so it was applied as first argument
    not as kwarg => caused it to be displa=True when it shouldn't

--- api/reservations_api.py
+++ api/reservations_api.py
@@ -140,7 +140,7 @@ class AgeGroupSerializer(serializers.ModelSerializer):
             },
         }
 
-    def __init__(self, display=False, *args, **kwargs):
+    def __init__(self, *args, display=False, **kwargs):
         super(AgeGroupSerializer, self).__init__(*args, **kwargs)
         if display:
             self.fields.pop("id")
