821d657bdf8575a3f1e7fc8442b222c2de73e2c0
Nico Virkki <nico.virkki@anders.com>
2021-03-22 17:34:49 +0200

    Add location's street to translated fields
    The street address can be either Finnish or Swedish
    
    TILA-112

--- spaces/translation.py
+++ spaces/translation.py
@@ -1,6 +1,6 @@
 from modeltranslation.translator import TranslationOptions, translator
 
-from .models import District, ServiceSector, Space, Unit
+from .models import District, Location, ServiceSector, Space, Unit
 
 
 class SpaceTranslationOptions(TranslationOptions):
@@ -15,11 +15,16 @@ class ServiceSectorTranslationOptions(TranslationOptions):
     fields = ["name"]
 
 
-class DistrictTranslationoptions(TranslationOptions):
+class DistrictTranslationOptions(TranslationOptions):
     fields = ["name"]
+
+
+class LocationTranslationOption(TranslationOptions):
+    fields = ["address_street"]
 
 
 translator.register(Space, SpaceTranslationOptions)
 translator.register(Unit, UnitTranslationOptions)
 translator.register(ServiceSector, SpaceTranslationOptions)
-translator.register(District, DistrictTranslationoptions)
+translator.register(District, DistrictTranslationOptions)
+translator.register(Location, LocationTranslationOption)
