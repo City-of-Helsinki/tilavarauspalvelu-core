73ab663e3ac445af9c6edba9b5cb67a650e6867a
Anttipekka Renfors <anttipekka.renfors@anders.com>
2021-03-31 11:26:07 +0300

    Force pipeline.

--- tilavarauspalvelu/settings.py
+++ tilavarauspalvelu/settings.py
@@ -54,6 +54,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
+
 # Application definition
 
 INSTALLED_APPS = [
