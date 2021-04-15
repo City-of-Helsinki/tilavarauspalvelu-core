ae09bb777e355027c0787cc2b95ab05d77c49b08
Sami Siren <siren@foofactory.fi>
2021-04-15 11:32:08 +0300

    add accessibility statement

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -40,6 +40,8 @@ const Footer = (): JSX.Element => {
         <HDSFooter.Item
           href={t(`Footer.Base.Item.accessibilityStatement.href`)}
           label={t(`Footer.Base.Item.accessibilityStatement.label`)}
+          target="_blank"
+          rel="noopener noreferrer"
         />
       </HDSFooter.Base>
     </HDSFooter>
