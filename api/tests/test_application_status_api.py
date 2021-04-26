ce51bfd2d94d7161d2eb39f432c5231f9de05a0c
Nico Virkki <nico.virkki@anders.com>
2021-04-26 09:50:15 +0300

    Add tests for application status api
    TILA-545

---
+++ api/tests/test_application_status_api.py
@@ -0,0 +1,300 @@
+import datetime
+
+import pytest
+from assertpy import assert_that
+from django.contrib.auth import get_user_model
+from django.test.testcases import TestCase
+from freezegun import freeze_time
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
+
+from applications.models import ApplicationEventStatus, ApplicationStatus
+from applications.tests.factories import (
+    ApplicationEventFactory,
+    ApplicationEventScheduleFactory,
+    ApplicationEventScheduleResultFactory,
+    ApplicationFactory,
+    ApplicationRoundFactory,
+    EventReservationUnitFactory,
+)
+from permissions.models import (
+    ServiceSectorRole,
+    ServiceSectorRoleChoice,
+    UnitRole,
+    UnitRoleChoice,
+)
+from reservation_units.tests.factories import ReservationUnitFactory
+from reservations.models import STATE_CHOICES, RecurringReservation, Reservation
+from reservations.tests.factories import ReservationFactory
+from spaces.tests.factories import SpaceFactory
+from tilavarauspalvelu.utils.date_util import next_or_current_matching_weekday
+
+User = get_user_model()
+
+
+@freeze_time("2021-05-03")
+@pytest.mark.django_db
+class ApplicationStatusBaseTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        date = datetime.date.today()
+        cls.round = ApplicationRoundFactory(
+            reservation_period_begin=date,
+            reservation_period_end=date + datetime.timedelta(weeks=4),
+        )
+        cls.application = ApplicationFactory(application_round=cls.round)
+        cls.application_event = ApplicationEventFactory(
+            application=cls.application,
+            events_per_week=1,
+            begin=date,
+            end=date + datetime.timedelta(weeks=4),
+        )
+        event_res_unit = EventReservationUnitFactory(
+            application_event=cls.application_event
+        )
+        cls.schedule = ApplicationEventScheduleFactory(
+            application_event=cls.application_event
+        )
+        space = SpaceFactory()
+        cls.result = ApplicationEventScheduleResultFactory(
+            application_event_schedule=cls.schedule,
+            allocated_reservation_unit=ReservationUnitFactory(spaces=[space]),
+        )
+        cls.service_sector_manager = User.objects.create(
+            username="service_sector_person",
+            first_name="ser",
+            last_name="vice",
+            email="ser.vice.person@foo.com",
+        )
+
+        ServiceSectorRole.objects.create(
+            user=cls.service_sector_manager,
+            role=ServiceSectorRoleChoice.objects.get(code="application_manager"),
+            service_sector=cls.application.application_round.service_sector,
+        )
+        cls.manager_api_client = APIClient()
+        cls.manager_api_client.force_authenticate(cls.service_sector_manager)
+        cls.applicant_user = User.objects.create(
+            username="applicant",
+            first_name="app",
+            last_name="licant",
+            email="app.licant.person@foo.com",
+        )
+        cls.applicant_api_client = APIClient()
+        cls.applicant_api_client.force_authenticate(cls.applicant_user)
+        ApplicationStatus.objects.create(
+            status=ApplicationStatus.IN_REVIEW,
+            user=cls.service_sector_manager,
+            application=cls.application,
+        )
+        cls.unit_handler_user = User.objects.create(
+            username="unhandler",
+            first_name="hand",
+            last_name="ler",
+            email="hand.ler.person@foo.com",
+        )
+        UnitRole.objects.create(
+            user=cls.unit_handler_user,
+            role=UnitRoleChoice.objects.get(code="manager"),
+            unit=event_res_unit.reservation_unit.unit,
+        )
+        cls.unit_handler_client = APIClient()
+        cls.unit_handler_client.force_authenticate(cls.unit_handler_user)
+
+
+@pytest.mark.django_db
+class ApplicationStatusApiPermissionsTestCase(ApplicationStatusBaseTestCase):
+    def test_service_sector_manager_cant_modify_status(self):
+        response = self.manager_api_client.post(
+            reverse("application_status-detail", kwargs={"pk": self.application.id}),
+            data={
+                "status": ApplicationStatus.REVIEW_DONE,
+                "application_id": self.application.id,
+            },
+        )
+        assert_that(response.status_code).is_equal_to(405)
+
+    def test_user_cant_create_status(self):
+        response = self.applicant_api_client.post(
+            reverse("application_status-list"),
+            data={
+                "status": ApplicationStatus.IN_REVIEW,
+                "application_id": self.application.id,
+            },
+        )
+        assert_that(response.status_code).is_equal_to(403)
+
+    def test_service_sector_manager_can_create_status(self):
+        response = self.manager_api_client.post(
+            reverse("application_status-list"),
+            data={
+                "status": ApplicationStatus.REVIEW_DONE,
+                "application_id": self.application.id,
+            },
+        )
+        assert_that(response.status_code).is_equal_to(201)
+        assert_that(ApplicationStatus.objects.latest("id").status).is_equal_to(
+            ApplicationStatus.REVIEW_DONE
+        )
+
+
+@pytest.mark.django_db
+class ApplicationEventStatusApiPermissionsTestCase(ApplicationStatusBaseTestCase):
+    def test_service_sector_manager_cant_modify_status(self):
+        response = self.manager_api_client.post(
+            reverse(
+                "application_event_status-detail",
+                kwargs={"pk": self.application_event.id},
+            ),
+            data={
+                "status": ApplicationEventStatus.VALIDATED,
+                "application_event_id": self.application_event.id,
+            },
+        )
+        assert_that(response.status_code).is_equal_to(403)
+
+    def test_user_cant_create_status(self):
+        response = self.applicant_api_client.post(
+            reverse("application_event_status-list"),
+            data={
+                "status": ApplicationEventStatus.VALIDATED,
+                "application_event_id": self.application_event.id,
+            },
+        )
+        assert_that(response.status_code).is_equal_to(403)
+
+    def test_service_sector_manager_cant_create_created_status(self):
+        response = self.manager_api_client.post(
+            reverse("application_event_status-list"),
+            data={
+                "status": ApplicationEventStatus.CREATED,
+                "application_event_id": self.application_event.id,
+            },
+        )
+        assert_that(response.status_code).is_equal_to(403)
+        assert_that(ApplicationEventStatus.objects.latest("id").status).is_equal_to(
+            ApplicationEventStatus.CREATED
+        )
+
+    def test_service_sector_manager_cant_create_validated_status(self):
+        response = self.manager_api_client.post(
+            reverse("application_event_status-list"),
+            data={
+                "status": ApplicationEventStatus.VALIDATED,
+                "application_event_id": self.application_event.id,
+            },
+        )
+        assert_that(response.status_code).is_equal_to(403)
+
+    def test_unit_handler_can_create_validated_status(self):
+        response = self.unit_handler_client.post(
+            reverse("application_event_status-list"),
+            data={
+                "status": ApplicationEventStatus.VALIDATED,
+                "application_event_id": self.application_event.id,
+            },
+        )
+        assert_that(response.status_code).is_equal_to(201)
+        assert_that(ApplicationEventStatus.objects.latest("id").status).is_equal_to(
+            ApplicationEventStatus.VALIDATED
+        )
+
+    def test_service_sector_manager_can_create_approved_status(self):
+        response = self.manager_api_client.post(
+            reverse("application_event_status-list"),
+            data={
+                "status": ApplicationEventStatus.APPROVED,
+                "application_event_id": self.application_event.id,
+            },
+        )
+        assert_that(response.status_code).is_equal_to(201)
+        assert_that(ApplicationEventStatus.objects.latest("id").status).is_equal_to(
+            ApplicationEventStatus.APPROVED
+        )
+
+
+@pytest.mark.django_db
+class ReservationCreationOnStatusCreationTestCase(ApplicationStatusBaseTestCase):
+    def test_creating_approved_status_creates_reservations(self):
+        response = self.manager_api_client.post(
+            reverse("application_event_status-list"),
+            data={
+                "status": ApplicationEventStatus.APPROVED,
+                "application_event_id": self.application_event.id,
+            },
+        )
+        assert_that(response.status_code).is_equal_to(201)
+        assert_that(RecurringReservation.objects.count()).is_equal_to(1)
+        assert_that(Reservation.objects.count()).is_greater_than(0)
+
+    def test_correct_amount_of_reservations_are_created_when_weekly(self):
+        response = self.manager_api_client.post(
+            reverse("application_event_status-list"),
+            data={
+                "status": ApplicationEventStatus.APPROVED,
+                "application_event_id": self.application_event.id,
+            },
+        )
+        assert_that(response.status_code).is_equal_to(201)
+        assert_that(RecurringReservation.objects.count()).is_equal_to(1)
+        assert_that(Reservation.objects.count()).is_equal_to(4)
+
+    def test_correct_amount_of_reservations_are_created_when_bi_weekly(self):
+        self.application_event.biweekly = True
+        self.application_event.save()
+        response = self.manager_api_client.post(
+            reverse("application_event_status-list"),
+            data={
+                "status": ApplicationEventStatus.APPROVED,
+                "application_event_id": self.application_event.id,
+            },
+        )
+        assert_that(response.status_code).is_equal_to(201)
+        assert_that(RecurringReservation.objects.count()).is_equal_to(1)
+        assert_that(Reservation.objects.count()).is_equal_to(2)
+
+    def test_correct_dates_are_used_for_reservations(self):
+        response = self.manager_api_client.post(
+            reverse("application_event_status-list"),
+            data={
+                "status": ApplicationEventStatus.APPROVED,
+                "application_event_id": self.application_event.id,
+            },
+        )
+        assert_that(response.status_code).is_equal_to(201)
+        reservations = Reservation.objects.filter(
+            recurring_reservation__application=self.application
+        ).order_by("begin")
+        schedule_result = (
+            self.application_event.application_event_schedules.first().application_event_schedule_result
+        )
+        for reservation in reservations:
+            assert_that(reservation.begin.weekday()).is_equal_to(
+                schedule_result.allocated_day
+            )
+
+    def test_reservation_gets_denied_status_when_overlapping(self):
+        res_date = next_or_current_matching_weekday(
+            self.application_event.begin, self.result.allocated_day
+        )
+        res_dt = datetime.datetime.combine(res_date, self.result.allocated_begin)
+        ReservationFactory(
+            begin=res_dt,
+            end=res_dt + datetime.timedelta(hours=12),
+            reservation_unit=[self.result.allocated_reservation_unit],
+        )
+        response = self.manager_api_client.post(
+            reverse("application_event_status-list"),
+            data={
+                "status": ApplicationEventStatus.APPROVED,
+                "application_event_id": self.application_event.id,
+            },
+        )
+        assert_that(response.status_code).is_equal_to(201)
+
+        reservations = Reservation.objects.filter(
+            recurring_reservation__application=self.application
+        ).order_by("begin")
+        assert_that(reservations[0].state).is_equal_to(STATE_CHOICES.DENIED)
+        for reservation in reservations[1:4]:
+            assert_that(reservation.state).is_equal_to(STATE_CHOICES.CREATED)
---
+++ applications/tests/factories.py
@@ -0,0 +1,211 @@
+import datetime
+
+from factory import SubFactory, post_generation
+from factory.django import DjangoModelFactory
+from factory.fuzzy import FuzzyChoice, FuzzyDate, FuzzyDateTime, FuzzyInteger, FuzzyText
+from pytz import UTC
+
+from applications.models import (
+    Application,
+    ApplicationEventStatus,
+    ApplicationRound,
+    ApplicationStatus,
+    Organisation,
+)
+from reservation_units.tests.factories import ReservationUnitFactory
+from spaces.tests.factories import ServiceSectorFactory
+
+
+class AddressFactory(DjangoModelFactory):
+    class Meta:
+        model = "applications.Address"
+
+    street_address = FuzzyText()
+    post_code = FuzzyText(length=5)
+    city = "Helsinki"
+
+
+class OrganisationFactory(DjangoModelFactory):
+    class Meta:
+        model = "applications.Organisation"
+
+    name = FuzzyText()
+    identifier = FuzzyText()
+    year_established = FuzzyInteger(low=1, high=datetime.date.today().year)
+    address = SubFactory(AddressFactory)
+    active_members = FuzzyInteger(low=1, high=500)
+    organisation_type = FuzzyChoice(
+        choices=(
+            Organisation.COMPANY,
+            Organisation.REGISTERED_ASSOCIATION,
+            Organisation.PUBLIC_ASSOCIATION,
+            Organisation.UNREGISTERED_ASSOCIATION,
+            Organisation.MUNICIPALITY_CONSORTIUM,
+            Organisation.RELIGIOUS_COMMUNITY,
+        )
+    )
+    email = FuzzyText(suffix="@testingisbesthing.com")
+
+
+class PersonFactory(DjangoModelFactory):
+    class Meta:
+        model = "applications.Person"
+
+
+class ApplicationRoundFactory(DjangoModelFactory):
+    class Meta:
+        model = "applications.ApplicationRound"
+
+    name = FuzzyText()
+    target_group = FuzzyChoice(
+        choices=(
+            ApplicationRound.TARGET_GROUP_INTERNAL,
+            ApplicationRound.TARGET_GROUP_PUBLIC,
+            ApplicationRound.TARGET_GROUP_ALL,
+        )
+    )
+    service_sector = SubFactory(ServiceSectorFactory)
+    application_period_begin = FuzzyDateTime(
+        start_dt=datetime.datetime.now(tz=UTC),
+        end_dt=datetime.datetime.now(tz=UTC),
+    )
+    application_period_end = FuzzyDateTime(
+        start_dt=(datetime.datetime.now(tz=UTC) + datetime.timedelta(weeks=4)),
+        end_dt=(datetime.datetime.now(tz=UTC) + datetime.timedelta(weeks=4)),
+    )
+    reservation_period_begin = FuzzyDate(
+        start_date=datetime.date.today(), end_date=datetime.date.today()
+    )
+    reservation_period_end = FuzzyDate(
+        start_date=datetime.date.today(),
+        end_date=(datetime.date.today() + datetime.timedelta(weeks=4)),
+    )
+    public_display_begin = FuzzyDateTime(
+        start_dt=datetime.datetime.now(tz=UTC),
+        end_dt=datetime.datetime.now(tz=UTC),
+    )
+    public_display_end = FuzzyDateTime(
+        start_dt=(datetime.datetime.now(tz=UTC) + datetime.timedelta(weeks=4)),
+        end_dt=(datetime.datetime.now(tz=UTC) + datetime.timedelta(weeks=4)),
+    )
+
+    @post_generation
+    def purposes(self, create, purposes, **kwargs):
+        if not create or not purposes:
+            return
+
+        for purpose in purposes:
+            self.purposes.add(purpose)
+
+    @post_generation
+    def reservation_units(self, create, reservation_units, **kwargs):
+        if not create or not reservation_units:
+            return
+
+        for reservation_unit in reservation_units:
+            self.reservation_units.add(reservation_unit)
+
+
+class ApplicationFactory(DjangoModelFactory):
+    class Meta:
+        model = "applications.Application"
+
+    applicant_type = FuzzyChoice(
+        choices=(
+            Application.APPLICANT_TYPE_COMPANY,
+            Application.APPLICANT_TYPE_ASSOCIATION,
+            Application.APPLICANT_TYPE_COMMUNITY,
+            Application.APPLICANT_TYPE_COMPANY,
+        )
+    )
+
+    organisation = SubFactory(OrganisationFactory)
+    contact_person = SubFactory(PersonFactory)
+    application_round = SubFactory(ApplicationRoundFactory)
+
+
+class ApplicationEventFactory(DjangoModelFactory):
+    class Meta:
+        model = "applications.ApplicationEvent"
+
+    name = FuzzyText()
+    application = SubFactory(ApplicationFactory)
+    min_duration = "01:00"
+    max_duration = "02:00"
+    events_per_week = FuzzyInteger(low=1, high=4)
+
+    @post_generation
+    def declined_reservation_units(self, create, declined_reservation_units, **kwargs):
+        if not create or not declined_reservation_units:
+            return
+
+        for reservation_unit in declined_reservation_units:
+            self.declined_reservation_units.add(reservation_unit)
+
+    @post_generation
+    def set_dates(self, *args, **kwargs):
+        self.begin = self.application.application_round.reservation_period_begin
+        self.end = self.application.application_round.reservation_period_end
+
+
+class EventReservationUnitFactory(DjangoModelFactory):
+    class Meta:
+        model = "applications.EventReservationUnit"
+
+    application_event = SubFactory(ApplicationEventFactory)
+    priority = FuzzyInteger(low=0, high=1000, step=100)
+    reservation_unit = SubFactory(ReservationUnitFactory)
+
+
+class ApplicationEventScheduleFactory(DjangoModelFactory):
+    class Meta:
+        model = "applications.ApplicationEventSchedule"
+
+    day = FuzzyInteger(low=0, high=6)
+    begin = datetime.time(12, 0)
+    end = datetime.time(14, 0)
+    application_event = SubFactory(ApplicationEventFactory)
+
+
+class ApplicationEventScheduleResultFactory(DjangoModelFactory):
+    class Meta:
+        model = "applications.ApplicationEventScheduleResult"
+
+    application_event_schedule = SubFactory(ApplicationEventScheduleFactory)
+    allocated_reservation_unit = SubFactory(ReservationUnitFactory)
+    allocated_duration = "01:00"
+    allocated_day = FuzzyInteger(low=0, high=6)
+    allocated_begin = datetime.time(12, 0)
+    allocated_end = datetime.time(14, 0)
+
+
+class ApplicationStatusFactory(DjangoModelFactory):
+    class Meta:
+        model = "applications.ApplicationStatus"
+
+    status = FuzzyChoice(
+        choices=[
+            ApplicationStatus.DRAFT,
+            ApplicationStatus.IN_REVIEW,
+            ApplicationStatus.REVIEW_DONE,
+            ApplicationStatus.DECLINED,
+            ApplicationStatus.CANCELLED,
+        ]
+    )
+    application = SubFactory(ApplicationFactory)
+
+
+class ApplicationEventStatusFactory(DjangoModelFactory):
+    class Meta:
+        model = "applications.ApplicationEventStatus"
+
+    status = FuzzyChoice(
+        choices=[
+            ApplicationEventStatus.CREATED,
+            ApplicationEventStatus.ALLOCATED,
+            ApplicationEventStatus.VALIDATED,
+            ApplicationEventStatus.APPROVED,
+            ApplicationEventStatus.DECLINED,
+        ]
+    )
+    application_event = SubFactory(ApplicationEventFactory)
