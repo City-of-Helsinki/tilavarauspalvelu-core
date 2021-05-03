10fddf38cea0b1905f797e2d75ed09f08cf78931
Nico Virkki <nico.virkki@anders.com>
2021-05-03 15:06:58 +0300

    Add reservation scheduler and use it in reservation creation
    The scheduler checks the opening hours of the reservation_unit from
    HAUKI and possibly shortens the reservation time if have to (not open)
    
    TILA-572

--- applications/utils/reservation_creation.py
+++ applications/utils/reservation_creation.py
@@ -3,6 +3,7 @@ import logging
 
 from django.db import Error
 
+from opening_hours.hours import get_opening_hours
 from reservations.models import (
     STATE_CHOICES,
     RecurringReservation,
@@ -36,26 +37,38 @@ def create_reservation_from_schedule_result(result, application_event):
 
     interval = 14 if application_event.biweekly else 7
     while reservation_date < application_event.end:
-        start = datetime.datetime.combine(
+        res_start = datetime.datetime.combine(
             reservation_date,
             result.allocated_begin,
             tzinfo=result.allocated_begin.tzinfo,
         )
-        end = datetime.datetime.combine(
+        res_end = datetime.datetime.combine(
             reservation_date,
             result.allocated_end,
             tzinfo=result.allocated_end.tzinfo,
         )
         is_overlapping = result.allocated_reservation_unit.check_reservation_overlap(
-            start, end
+            res_start, res_end
         )
+
+        reservation_scheduler = ReservationScheduler(
+            result.allocated_reservation_unit.id, res_start, res_end
+        )
+        (
+            start,
+            end,
+        ) = reservation_scheduler.get_reservation_times_based_on_opening_hours()
+        is_unit_closed = start is None
+
         try:
             reservation = Reservation.objects.create(
-                state=STATE_CHOICES.DENIED if is_overlapping else STATE_CHOICES.CREATED,
+                state=STATE_CHOICES.DENIED
+                if is_overlapping or is_unit_closed
+                else STATE_CHOICES.CREATED,
                 priority=result.application_event_schedule.priority,
                 user=application_event.application.user,
-                begin=start,
-                end=end,
+                begin=start if not is_unit_closed else res_start,
+                end=end if not is_unit_closed else res_end,
                 recurring_reservation=recurring_reservation,
                 num_persons=application_event.num_persons,
             )
@@ -67,3 +80,79 @@ def create_reservation_from_schedule_result(result, application_event):
         except Error:
             logger.exception("Error while creating reservation")
         reservation_date = reservation_date + datetime.timedelta(days=interval)
+
+
+class ReservationScheduler:
+    def __init__(self, reservation_unit_id, begin, end):
+        self.reservation_unit_id = reservation_unit_id
+        self.begin = begin
+        self.end = end
+        self.dates = []
+
+        self.is_multiple_days_reservation = self.begin.date() < self.end.date()
+        if self.is_multiple_days_reservation:
+            loop_date = self.begin.date()
+            while loop_date <= self.end.date():
+                self.dates.append(loop_date)
+                loop_date += datetime.timedelta(days=1)
+
+    def get_reservation_times_based_on_opening_hours(self):
+        """Returns amount of time that the reservation unit is open on selected datetime range."""
+        opening_hours = self.get_opening_hours()
+        for opening_hour in opening_hours:
+            opening_hour_date = opening_hour["date"]
+            for time in opening_hour["times"]:
+                if (
+                    opening_hour_date == self.begin.date()
+                    and not self.is_multiple_days_reservation
+                ):
+                    return self._get_datetimes_for_the_day(opening_hour_date, time)
+
+                # Assume opening hours are sorted properly.
+                if (
+                    self.is_multiple_days_reservation
+                    and opening_hour_date in self.dates
+                ):
+                    if not time.end_time == datetime.time(
+                        23, 59, tzinfo=time.end_time.tzinfo
+                    ):
+                        return self._get_datetimes_for_the_day(self.begin.date(), time)
+                    for date in self.dates[1:]:
+                        if self.can_continue_to_next_day(date, time):
+                            continue
+                        _, end = self._get_datetimes_for_the_day(date, time)
+                        return self.begin, end
+        return None, None
+
+    def _get_datetimes_for_the_day(self, date, time):
+        start_dt = datetime.datetime.combine(
+            date, time.start_time, tzinfo=time.start_time.tzinfo
+        )
+        end_dt = datetime.datetime.combine(
+            date, time.end_time, tzinfo=time.end_time.tzinfo
+        )
+
+        starts_in_opening_hours = start_dt <= self.begin
+        ends_in_opening_hours = end_dt >= self.end
+        if starts_in_opening_hours and ends_in_opening_hours:
+            return self.begin, self.end
+        if starts_in_opening_hours and not ends_in_opening_hours:
+            return self.begin, end_dt
+        if not starts_in_opening_hours and ends_in_opening_hours:
+            return start_dt, self.end
+
+        return None, None
+
+    def can_continue_to_next_day(self, date, time):
+        last_date = len(self.dates) - 1
+        return (
+            self.dates.index(date) < last_date and time.hour == 23 and time.hour == 59
+        )
+
+    def get_opening_hours(self):
+        # TODO: Cache opening hours per unit.
+        return get_opening_hours(
+            self.reservation_unit_id,
+            start_date=self.begin.date(),
+            end_date=self.end.date(),
+        )
