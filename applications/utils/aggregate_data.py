3b5de713ed0ddb5ada3d36e77d30cf1ef289ac2e
Nico Virkki <nico.virkki@anders.com>
2021-05-06 19:55:56 +0300

    Add method to create application event aggregate data
    Also creator class which inherits Thread to do it in own thread for
    performance
    
    TILA-571

---
+++ applications/utils/aggregate_data.py
@@ -0,0 +1,10 @@
+from threading import Thread
+
+
+class EventAggregateDataCreator(Thread):
+    def __init__(self, event, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.event = event
+
+    def run(self) -> None:
+        return self.event.create_aggregate_data()
