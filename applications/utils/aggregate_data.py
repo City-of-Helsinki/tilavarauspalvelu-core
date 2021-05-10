cb6e847885245b9f7c338060a22413d4862cfa59
Nico Virkki <nico.virkki@anders.com>
2021-05-10 08:00:45 +0300

    Create schedule result aggregate data in result mapper
    Mapper starts new thread for the aggregate data creation
    
    TILA-592

--- applications/utils/aggregate_data.py
+++ applications/utils/aggregate_data.py
@@ -8,3 +8,12 @@ class EventAggregateDataCreator(Thread):
 
     def run(self) -> None:
         return self.event.create_aggregate_data()
+
+
+class ApplicationEventScheduleResultAggregateDataCreator(Thread):
+    def __init__(self, event, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.event = event
+
+    def run(self) -> None:
+        return self.event.create_schedule_result_aggregated_data()
