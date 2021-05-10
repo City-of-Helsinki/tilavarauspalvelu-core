3f1b77dea7ce45a18e0d002ec3d2b00ac352be1b
Sami Siren <siren@foofactory.fi>
2021-05-10 13:55:07 +0300

    fix url

--- src/common/auth/configuration.ts
+++ src/common/auth/configuration.ts
@@ -9,7 +9,7 @@ const configuration = {
   client_id: oidcClientId,
   redirect_uri: `${base}/login/helsinki/return`,
   response_type: 'id_token token',
-  post_logout_redirect_uri: base,
+  post_logout_redirect_uri: `${base}/?logout`,
   scope: oidcScope,
   authority: 'https://api.hel.fi/sso/',
   silent_redirect_uri: `${base}/login/helsinki/silent`,
