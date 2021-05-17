b544e04ee3e563a4d48f0e034ee529192f4dc64c
Nico Virkki <nico.virkki@anders.com>
2021-05-17 13:48:14 +0300

    Add utility function to get total opening hours per resource
    Request to HAUKi and returns dict with recourse_id as key and total
    opening hour in given period as value
    
    TILA-502

--- opening_hours/utils.py
+++ opening_hours/utils.py
@@ -22,3 +22,30 @@ def get_resources_total_hours(resource_ids, period_start, period_end):
             total_opening_hours += time.end_time.hour - time.start_time.hour
 
     return total_opening_hours
+
+
+def get_resources_total_hours_per_resource(resource_ids, period_start, period_end):
+    opening_hours = get_opening_hours(
+        resource_ids,
+        period_start,
+        period_end,
+    )
+    hours_in_day = 24
+    total_opening_hours = {}
+    for opening_hour in opening_hours:
+        total_open = 0
+        for time in opening_hour["times"]:
+            if time.full_day:
+                total_opening_hours += hours_in_day
+                continue
+
+            if time.end_time_on_next_day:
+                total_opening_hours += hours_in_day - time.start_time.hour
+                continue
+
+            total_open += time.end_time.hour - time.start_time.hour
+
+        prev_tot = total_opening_hours.get(opening_hour["resource_id"], 0)
+        total_opening_hours[opening_hour["resource_id"]] = total_open + prev_tot
+
+    return total_opening_hours
