ceb6cd9bb4183f4f0244331ef420ca9f01395eab
Nico Virkki <nico.virkki@anders.com>
2021-05-19 13:48:51 +0300

    Change capacity method tests for using uuid
    TILA-628

--- api/tests/test_reservation_unit_capacity.py
+++ api/tests/test_reservation_unit_capacity.py
@@ -1,8 +1,8 @@
 import datetime
 from unittest import mock
 
-import pytest
 from assertpy import assert_that
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test.testcases import TestCase
 from rest_framework.reverse import reverse
@@ -10,14 +10,17 @@ from rest_framework.test import APIClient
 
 from opening_hours.hours import TimeElement
 from permissions.models import GeneralRole, GeneralRoleChoice
+from reservation_units.models import ReservationUnit
 from reservation_units.tests.factories import ReservationUnitFactory
+from reservations.models import STATE_CHOICES
 from reservations.tests.factories import ReservationFactory
 
 
 def get_mocked_opening_hours():
+    resource_id = f"{settings.HAUKI_ORIGIN_ID}:{ReservationUnit.objects.first().uuid}"
     return [
         {
-            "resource_id": 123,
+            "resource_id": resource_id,
             "date": datetime.datetime.strptime("2021-01-01", "%Y-%m-%d").date(),
             "times": [
                 TimeElement(
@@ -28,7 +31,7 @@ def get_mocked_opening_hours():
             ],
         },
         {
-            "resource_id": 123,
+            "resource_id": resource_id,
             "date": datetime.datetime.strptime("2021-01-02", "%Y-%m-%d").date(),
             "times": [
                 TimeElement(
@@ -41,18 +44,16 @@ def get_mocked_opening_hours():
     ]
 
 
-@mock.patch(
-    "opening_hours.utils.get_opening_hours", return_value=get_mocked_opening_hours()
-)
-@pytest.mark.skip
+@mock.patch("opening_hours.utils.get_opening_hours")
 class ReservationUnitCapacityTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.reservation_unit = ReservationUnitFactory(id=123)
+        cls.reservation_unit = ReservationUnitFactory()
         cls.reservation = ReservationFactory(
             reservation_unit=[cls.reservation_unit],
             begin=datetime.datetime(2020, 5, 5, 12),
             end=datetime.datetime(2020, 5, 5, 14),
+            state=STATE_CHOICES.CONFIRMED,
         )
 
         general_admin = get_user_model().objects.create(
@@ -71,12 +72,13 @@ class ReservationUnitCapacityTestCase(TestCase):
         cls.api_client.force_authenticate(general_admin)
 
     def test_hour_capacity(self, mock):
+        mock.return_value = get_mocked_opening_hours()
         response = self.api_client.get(
             reverse(
                 "reservationunit-capacity",
             ),
             data={
-                "reservation_unit": "123",
+                "reservation_unit": str(self.reservation_unit.id),
                 "period_start": "2020-01-01",
                 "period_end": "2022-01-01",
             },
@@ -87,12 +89,13 @@ class ReservationUnitCapacityTestCase(TestCase):
         assert_that(response.data[0].get("hour_capacity")).is_equal_to(24)
 
     def test_reservation_duration_total_in_period(self, mock):
+        mock.return_value = get_mocked_opening_hours()
         response = self.api_client.get(
             reverse(
                 "reservationunit-capacity",
             ),
             data={
-                "reservation_unit": "123",
+                "reservation_unit": str(self.reservation_unit.id),
                 "period_start": "2020-01-01",
                 "period_end": "2022-01-01",
             },
@@ -103,17 +106,19 @@ class ReservationUnitCapacityTestCase(TestCase):
         assert_that(response.data[0].get("reservation_duration_total")).is_equal_to(2)
 
     def test_reservation_duration_total_when_reservation_out_of_period(self, mock):
+        mock.return_value = get_mocked_opening_hours()
         ReservationFactory(
             reservation_unit=[self.reservation_unit],
             begin=datetime.datetime(2022, 5, 5, 12),
             end=datetime.datetime(2022, 5, 5, 14),
+            state=STATE_CHOICES.CONFIRMED,
         )
         response = self.api_client.get(
             reverse(
                 "reservationunit-capacity",
             ),
             data={
-                "reservation_unit": "123",
+                "reservation_unit": str(self.reservation_unit.id),
                 "period_start": "2020-01",
                 "period_end": "2022-01-01",
             },
