3e854f98142aa7ae26848cdfaa5b2c970a62b3da
Sami Siren <siren@foofactory.fi>
2021-06-11 12:04:48 +0300

    date formattting

--- tilavarauspalvelu-ui-nextjs/pages/_app.jsx
+++ tilavarauspalvelu-ui-nextjs/pages/_app.jsx
@@ -1,14 +1,14 @@
 import { appWithTranslation } from "next-i18next";
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 import SessionLost from "../components/common/SessionLost";
 import PageWrapper from "../components/common/PageWrapper";
 import { authEnabled, isBrowser } from "../modules/const";
 import LoggingIn from "../components/common/LoggingIn";
-import { CenterSpinner } from '../components/common/common';
-import oidcConfiguration from '../modules/auth/configuration'
+import { CenterSpinner } from "../components/common/common";
+import oidcConfiguration from "../modules/auth/configuration";
+import nextI18NextConfig from "../next-i18next.config.js";
 import "../styles/global.scss";
-
-
+import { format } from 'date-fns';
 
 function MyApp({ Component, pageProps }) {
   if (!isBrowser) {
@@ -19,13 +19,11 @@ function MyApp({ Component, pageProps }) {
     );
   }
 
-
   const AuthenticationProvider = dynamic(() =>
-    import('@axa-fr/react-oidc-context').then((mod) => mod.AuthenticationProvider)
-  )
-
-
-  console.log('rendering with config', oidcConfiguration)
+    import("@axa-fr/react-oidc-context").then(
+      (mod) => mod.AuthenticationProvider
+    )
+  );
 
   return (
     <AuthenticationProvider
@@ -34,15 +32,22 @@ function MyApp({ Component, pageProps }) {
       sessionLostComponent={SessionLost}
       configuration={oidcConfiguration}
       isEnabled={authEnabled}
-      callbackComponentOverride={LoggingIn}>
+      callbackComponentOverride={LoggingIn}
+    >
       <PageWrapper>
         <Component {...pageProps} />
       </PageWrapper>
-
     </AuthenticationProvider>
   );
-
-
 }
 
-export default appWithTranslation(MyApp);
+export default appWithTranslation(MyApp, {
+  ...nextI18NextConfig,
+  interpolation: {
+    format: (value, fmt) => {
+      if (value instanceof Date) return format(value, fmt || "dd.MM.YY");
+      return value;
+    },
+    escapeValue: false,
+  },
+});
