4f95599bf6f3426da2ebeaf92eebb09a9094e567
Nico Virkki <nico.virkki@anders.com>
2021-08-23 10:18:10 +0300

    Add space "code" field to gql api
    also some validation fine tuning => making optional fields optional.
    
    TILA-746

--- api/graphql/spaces/space_serializers.py
+++ api/graphql/spaces/space_serializers.py
@@ -1,3 +1,5 @@
+from rest_framework import serializers
+
 from api.graphql.base_serializers import (
     PrimaryKeySerializer,
     PrimaryKeyUpdateSerializer,
@@ -6,7 +8,17 @@ from api.space_api import SpaceSerializer
 
 
 class SpaceCreateSerializer(SpaceSerializer, PrimaryKeySerializer):
-    pass
+    max_persons = serializers.IntegerField(required=False)
+    code = serializers.CharField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["building_id"].required = False
+        self.fields["district_id"].required = False
+        self.fields["parent_id"].required = False
+
+    class Meta(SpaceSerializer.Meta):
+        fields = SpaceSerializer.Meta.fields + ["max_persons", "code"]
 
 
 class SpaceUpdateSerializer(SpaceSerializer, PrimaryKeyUpdateSerializer):
