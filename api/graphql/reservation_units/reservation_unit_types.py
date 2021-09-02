e5f0e9f0b373394b14acc9d6cd7aff6f221563ca
Nico Virkki <nico.virkki@anders.com>
2021-09-02 14:56:05 +0300

    Add reservation unit by pk type to have opening hours available
    TILA-738

--- api/graphql/reservation_units/reservation_unit_types.py
+++ api/graphql/reservation_units/reservation_unit_types.py
@@ -9,6 +9,7 @@ from graphene_permissions.mixins import AuthNode
 from graphene_permissions.permissions import AllowAny
 
 from api.graphql.base_type import PrimaryKeyObjectType
+from api.graphql.opening_hours.opening_hours_types import OpeningHoursMixin
 from api.graphql.resources.resource_types import ResourceType
 from api.graphql.services.service_types import ServiceType
 from api.graphql.spaces.space_types import LocationType, SpaceType
@@ -201,7 +202,7 @@ class ReservationUnitType(AuthNode, PrimaryKeyObjectType):
         return duration.time()
 
 
-class ReservationUnitByPkType(ReservationUnitType):
+class ReservationUnitByPkType(ReservationUnitType, OpeningHoursMixin):
     next_available_slot = graphene.DateTime()
 
     class Meta:
