83af543127a74edaec8658fd66771aa56e05d878
Nico Virkki <nico.virkki@anders.com>
2021-09-08 09:59:02 +0300

    Add base validation logic to AuthDeleteMutation
    TILA-773

--- api/graphql/base_mutations.py
+++ api/graphql/base_mutations.py
@@ -1,4 +1,5 @@
 import graphene
+from django.core.exceptions import ValidationError
 from graphene_permissions.mixins import AuthMutation
 from rest_framework.generics import get_object_or_404
 
@@ -14,13 +15,36 @@ class AuthSerializerMutation(AuthMutation):
 
 class AuthDeleteMutation(AuthMutation):
     deleted = graphene.Boolean()
+    errors = graphene.String()
 
     class Input:
         pk = graphene.ID()
 
     @classmethod
     def mutate_and_get_payload(cls, root, info, **input):
-        if cls.has_permission(root, info, input):
-            object = get_object_or_404(cls.model, pk=input["pk"])
-            object.delete()
-            return cls(deleted=True)
+        if not cls.has_permission(root, info, input):
+            return cls(errors="No permissions to perform delete.", deleted=False)
+
+        try:
+            if cls.is_valid(root, info, **input):
+                object = get_object_or_404(cls.model, pk=input["pk"])
+                object.delete()
+                return cls(deleted=True)
+        except ValidationError as ve:
+            message = ve.message
+
+        return cls(errors=message, deleted=False)
+
+    @classmethod
+    def validate(self, root, info, **input):
+        """Classes that ought to be overriding this method should raise django's
+        ValidationError if there's some validation problem. It gets caught in
+        `mutate_and_get_payload` method and returned as an error.
+        """
+        return None
+
+    @classmethod
+    def is_valid(cls, root, info, **input):
+        errors = cls.validate(root, info, **input)
+        is_valid = False if errors else True
+        return is_valid
