637b33b84a3c6a2a03d0748f8c98454683c7a7b1
Nico Virkki <nico.virkki@anders.com>
2021-09-08 15:18:15 +0300

    Make fields not required on updating the unit via gql
    TILA-769

--- api/graphql/units/unit_serializers.py
+++ api/graphql/units/unit_serializers.py
@@ -3,5 +3,15 @@ from api.units_api.serializers import UnitSerializer
 
 
 class UnitUpdateSerializer(UnitSerializer, PrimaryKeyUpdateSerializer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["tprek_id"].required = False
+        self.fields["name"].required = False
+        self.fields["description"].required = False
+        self.fields["short_description"].required = False
+        self.fields["web_page"].required = False
+        self.fields["email"].required = False
+        self.fields["phone"].required = False
+
     class Meta(UnitSerializer.Meta):
         fields = UnitSerializer.Meta.fields + ["pk"]
