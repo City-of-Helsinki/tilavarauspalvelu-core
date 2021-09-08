c3a9ff9102a5c767c71b2e012023bc41125d0d17
Nico Virkki <nico.virkki@anders.com>
2021-09-08 09:59:47 +0300

    Add validation logic to SpaceDeleteMutation
    TILA-773

--- api/graphql/spaces/space_mutations.py
+++ api/graphql/spaces/space_mutations.py
@@ -1,8 +1,10 @@
 import graphene
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from graphene import ClientIDMutation
 from graphene_django.rest_framework.mutation import SerializerMutation
 from graphene_permissions.permissions import AllowAny
+from rest_framework.generics import get_object_or_404
 
 from api.graphql.base_mutations import AuthDeleteMutation, AuthSerializerMutation
 from api.graphql.spaces.space_serializers import (
@@ -10,6 +12,7 @@ from api.graphql.spaces.space_serializers import (
     SpaceUpdateSerializer,
 )
 from api.graphql.spaces.space_types import SpaceType
+from applications.models import ApplicationRound, ApplicationRoundStatus
 from permissions.api_permissions.graphene_permissions import SpacePermission
 from spaces.models import Space
 
@@ -45,3 +48,18 @@ class SpaceDeleteMutation(AuthDeleteMutation, ClientIDMutation):
         (SpacePermission,) if not settings.TMP_PERMISSIONS_DISABLED else (AllowAny,)
     )
     model = Space
+
+    @classmethod
+    def validate(self, root, info, **input):
+        space = get_object_or_404(Space, pk=input.get("pk", None))
+        in_active_round = (
+            ApplicationRound.objects.exclude(
+                statuses__status=ApplicationRoundStatus.APPROVED
+            )
+            .filter(reservation_units__spaces=space)
+            .exists()
+        )
+        if in_active_round:
+            raise ValidationError("Space occurs in active application round.")
+
+        return None
