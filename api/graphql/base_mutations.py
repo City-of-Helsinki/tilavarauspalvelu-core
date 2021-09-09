485b4e633a346bd797ac425d30ed35dc9922f45d
Nico Virkki <nico.virkki@anders.com>
2021-09-09 10:02:13 +0300

    Raise Permission denied when has not permission
    In AuthSerializerMutation
    
    TILA-769

--- api/graphql/base_mutations.py
+++ api/graphql/base_mutations.py
@@ -1,5 +1,6 @@
 import graphene
 from graphene_permissions.mixins import AuthMutation
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import get_object_or_404
 
 
@@ -7,7 +8,7 @@ class AuthSerializerMutation(AuthMutation):
     @classmethod
     def mutate_and_get_payload(cls, root, info, **input):
         if not cls.has_permission(root, info, input):
-            return None
+            raise PermissionDenied("No permission to mutate")
 
         return super().mutate_and_get_payload(root, info, **input)
 
