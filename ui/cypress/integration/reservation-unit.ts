561c371dd94a76a78877003d865fce72e990a7ce
Mikko Sartanen <sartanen@gmail.com>
2021-09-09 16:13:32 +0300

    cleanup

--- ui/cypress/integration/reservation-unit.ts
+++
@@ -1,9 +0,0 @@
-describe("Tilavaraus ui reservation unit page", () => {
-  beforeEach(() => {
-    cy.visit("/reservation-unit/36");
-  });
-
-  it("displays default elements", () => {
-    cy.get("h1").should("contain", "Pukinmäen nuorisotalon keittiö");
-  });
-});
--- ui/mocks/handlers.ts
+++ ui/mocks/handlers.ts
@@ -1,12 +1,8 @@
-import { graphql, rest } from "msw";
+import { graphql } from "msw";
 import { ReservationUnit } from "../modules/types";
 
 type Variables = {
   pk: number;
-};
-
-type Vars = {
-  id: string;
 };
 
 type ReturnType = {
@@ -26,20 +22,6 @@ export const handlers = [
 
       const reservationUnit = await response;
       return res(ctx.data({ reservationUnit }));
-    }
-  ),
-  rest.get<ReturnType, Vars>(
-    "*/v1/reservation_unit/:id",
-    async (req, res, ctx) => {
-      const { id } = req.params;
-
-      const url = id
-        ? `../cypress/fixtures/rest/reservationUnit/${id}.json`
-        : null;
-      const response = id ? await import(url) : Promise.resolve();
-
-      const reservationUnit = await response;
-      return res(ctx.json(reservationUnit));
     }
   ),
 ];
