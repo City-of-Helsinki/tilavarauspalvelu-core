08c803baca1b4db9ad5f0bc20683b8dca7af7b92
Mikko Sartanen <sartanen@gmail.com>
2021-09-17 10:21:03 +0300

    TILA-784 WIP

---
+++ ui/components/single-search/SearchForm.tsx
@@ -0,0 +1,172 @@
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "next-i18next";
+import { Select, TextInput, Button as HDSButton, IconSearch } from "hds-react";
+import { useForm } from "react-hook-form";
+import styled from "styled-components";
+import { breakpoint } from "../../modules/style";
+import { getApplicationRounds, getParameters } from "../../modules/api";
+import { mapOptions, getSelectedOption } from "../../modules/util";
+import { emptyOption, participantCountOptions } from "../../modules/const";
+import { OptionType } from "../../modules/types";
+
+type Props = {
+  onSearch: (search: Record<string, string>) => void;
+  formValues: { [key: string]: string };
+};
+
+const Button = styled(HDSButton)`
+  margin-left: var(--spacing-m);
+`;
+
+const Container = styled.div`
+  @media (max-width: ${breakpoint.xl}) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (max-width: ${breakpoint.m}) {
+    grid-template-columns: 1fr;
+  }
+
+  margin-top: var(--spacing-s);
+  max-width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: var(--spacing-m);
+  font-size: var(--fontsize-body-m);
+`;
+
+const Hr = styled.hr`
+  margin-top: var(--spacing-l);
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: var(--spacing-l);
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const SearchForm = ({ onSearch, formValues }: Props): JSX.Element | null => {
+  const { t, i18n } = useTranslation();
+  const [ready, setReady] = useState<boolean>(false);
+  const [reservationUnitTypeOptions, setReservationUnitTypeOptions] = useState<
+    OptionType[]
+  >([]);
+  const [applicationPeriodOptions, setApplicationPeriodOptions] = useState<
+    OptionType[]
+  >([]);
+
+  const { register, handleSubmit, setValue, getValues } = useForm();
+
+  useEffect(() => {
+    register({ name: "purpose" });
+    register({ name: "district" });
+    register({ name: "applicationRound" });
+    register({ name: "maxPersons" });
+    register({ name: "reservationUnitType" });
+  }, [register]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedApplicationPeriods = await getApplicationRounds();
+      setApplicationPeriodOptions(
+        mapOptions(fetchedApplicationPeriods, t("common:select"), i18n.language)
+      );
+      const fetchedReservationUnitTypes = await getParameters(
+        "reservation_unit_type"
+      );
+      setReservationUnitTypeOptions(
+        mapOptions(
+          fetchedReservationUnitTypes,
+          t("common:select"),
+          i18n.language
+        )
+      );
+      setReady(true);
+    }
+    fetchData();
+  }, [i18n, t]);
+
+  useEffect(() => {
+    Object.keys(formValues).forEach((p) => setValue(p, formValues[p]));
+  }, [formValues, setValue]);
+
+  const search = (criteria: Record<string, string>) => {
+    onSearch(criteria);
+  };
+
+  if (!ready) {
+    return null;
+  }
+
+  return (
+    <>
+      <Container>
+        <TextInput
+          id="search"
+          name="search"
+          label="&nbsp;"
+          ref={register()}
+          placeholder={t("searchForm:searchTermPlaceholder")}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSubmit(search)();
+            }
+          }}
+          defaultValue={formValues.search}
+        />
+        <Select
+          id="applicationRound"
+          placeholder={t("common:select")}
+          options={applicationPeriodOptions}
+          onChange={(selection: OptionType): void => {
+            setValue("applicationRound", selection.value);
+          }}
+          defaultValue={getSelectedOption(
+            getValues("applicationRound"),
+            applicationPeriodOptions
+          )}
+          label={t("searchForm:roundLabel")}
+        />
+        <Select
+          id="participantCountFilter"
+          placeholder={t("common:select")}
+          options={[emptyOption(t("common:select"))].concat(
+            participantCountOptions
+          )}
+          label={t("searchForm:participantCountLabel")}
+          onChange={(selection: OptionType): void => {
+            setValue("maxPersons", selection.value);
+          }}
+          defaultValue={getSelectedOption(
+            getValues("maxPersons"),
+            participantCountOptions
+          )}
+        />
+        <Select
+          placeholder={t("common:select")}
+          options={reservationUnitTypeOptions}
+          label={t("searchForm:typeLabel")}
+          onChange={(selection: OptionType): void => {
+            setValue("reservationUnitType", selection.value);
+          }}
+          defaultValue={getSelectedOption(
+            getValues("reservationUnitType"),
+            reservationUnitTypeOptions
+          )}
+        />
+      </Container>
+      <Hr />
+      <ButtonContainer>
+        <Button
+          id="searchButton"
+          onClick={handleSubmit(search)}
+          iconLeft={<IconSearch />}
+        >
+          {t("searchForm:searchButton")}
+        </Button>
+      </ButtonContainer>
+    </>
+  );
+};
+
+export default SearchForm;
---
+++ ui/pages/single/search.tsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from "react";
+import { Koros } from "hds-react";
+import { useTranslation } from "next-i18next";
+import styled from "styled-components";
+import queryString from "query-string";
+import { useRouter } from "next/router";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Container from "../../components/common/Container";
+import Breadcrumb from "../../components/common/Breadcrumb";
+import SearchForm from "../../components/single-search/SearchForm";
+import SearchResultList from "../../components/single-search/SearchResultList";
+import {
+  getReservationUnits,
+  ReservationUnitsParameters,
+} from "../../modules/api";
+import { ReservationUnit } from "../../modules/types";
+import { searchUrl } from "../../modules/util";
+import { isBrowser, searchPrefix } from "../../modules/const";
+import { CenterSpinner } from "../../components/common/common";
+
+const style = {
+  fontSize: "var(--fontsize-heading-l)",
+} as React.CSSProperties;
+
+const HeadContainer = styled.div`
+  background-color: white;
+  padding-top: var(--spacing-layout-xs);
+`;
+
+const StyledKoros = styled(Koros)`
+  fill: white;
+`;
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const getServerSideProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale)),
+    },
+  };
+};
+
+const Search = (): JSX.Element => {
+  const { t } = useTranslation();
+
+  const [values, setValues] = useState({} as Record<string, string>);
+  const [state, setState] = useState<"loading" | "done" | "error">("done");
+
+  const [reservationUnits, setReservationUnits] = useState<
+    ReservationUnit[] | null
+  >(null);
+
+  const searchParams = isBrowser ? window.location.search : "";
+
+  useEffect(() => {
+    if (searchParams) {
+      const parsed = queryString.parse(searchParams);
+
+      const newValues = Object.keys(parsed).reduce((p, key) => {
+        if (parsed[key]) {
+          return { ...p, [key]: parsed[key]?.toString() } as Record<
+            string,
+            string
+          >;
+        }
+        return p;
+      }, {} as Record<string, string>);
+
+      setValues(newValues);
+      setState("loading");
+      getReservationUnits(newValues)
+        .then((v) => {
+          setReservationUnits(v);
+          setState("done");
+        })
+        .catch(() => {
+          setState("error");
+          setReservationUnits(null);
+        });
+    }
+  }, [searchParams, setReservationUnits]);
+
+  const history = useRouter();
+
+  const onSearch = async (criteria: ReservationUnitsParameters) => {
+    history.replace(searchUrl(criteria));
+  };
+
+  return (
+    <>
+      <HeadContainer>
+        <Container>
+          <Breadcrumb current={{ label: "search", linkTo: searchPrefix }} />
+          <h1 style={style}>{t("search:heading")}</h1>
+          <span className="text-lg">{t("search:text")}</span>
+          <SearchForm onSearch={onSearch} formValues={values} />
+        </Container>
+      </HeadContainer>
+      <StyledKoros type="wave" className="koros" flipHorizontal />
+      {state === "loading" ? (
+        <CenterSpinner />
+      ) : (
+        <SearchResultList
+          error={state === "error"}
+          reservationUnits={reservationUnits}
+        />
+      )}
+    </>
+  );
+};
+
+export default Search;
