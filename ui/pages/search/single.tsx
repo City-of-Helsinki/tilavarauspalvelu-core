84571462ba8782e583d9e4de3940c872a91857ec
Mikko Sartanen <sartanen@gmail.com>
2022-02-04 11:12:43 +0200

    TILA-1205 change search parameter value types
    commit 6dd8c9d225238557978d1d9ecc2c26f6b272b872
    Merge: 42f71a4 16e86cf
    Author: Mikko Sartanen <sartanen@gmail.com>
    Date:   Fri Feb 4 10:47:06 2022 +0200
    
        Merge branch 'main' into TILA-1205
        * main:
          TILA-1206 search for multiple units fix
    
    commit 42f71a41cfb1956c1c09badf1a2de1a5c84cf18c
    Author: Mikko Sartanen <sartanen@gmail.com>
    Date:   Fri Feb 4 09:19:47 2022 +0200
    
        TILA-1205 change search parameter value types

--- ui/pages/search/single.tsx
+++ ui/pages/search/single.tsx
@@ -95,7 +95,23 @@ const SearchSingle = (): JSX.Element => {
     QueryReservationUnitsArgs
   >(RESERVATION_UNITS, {
     variables: {
-      ...omit(values, ["order", "sort", "unit"]),
+      ...omit(values, [
+        "order",
+        "sort",
+        "minPersons",
+        "maxPersons",
+        "purposes",
+        "unit",
+      ]),
+      ...(values.minPersons && {
+        minPersons: parseInt(values.minPersons, 10),
+      }),
+      ...(values.maxPersons && {
+        maxPersons: parseInt(values.maxPersons, 10),
+      }),
+      ...(values.purposes && {
+        purposes: values.purposes.split(","),
+      }),
       ...(values.unit && {
         unit: values.unit.split(","),
       }),
