9e55a5649c032c6818a1a3581e3c3f9656d40744
Nico Virkki <nico.virkki@anders.com>
2022-02-16 12:30:40 +0200

    Add tprek update for units to django admin
    This to units list where you can choose the units to be updated from
    tprek
    
    TILA-1257

--- spaces/admin.py
+++ spaces/admin.py
@@ -1,4 +1,6 @@
-from django.contrib import admin
+from django.conf import settings
+from django.contrib import admin, messages
+from django.core.management import call_command
 from mptt.admin import MPTTModelAdmin
 
 from .models import (
@@ -54,6 +56,21 @@ class SpaceAdmin(MPTTModelAdmin):
 class UnitAdmin(admin.ModelAdmin):
     model = Unit
     inlines = [LocationInline]
+    actions = ["update_from_tprek"]
+
+    @admin.action
+    def update_from_tprek(self, request, queryset):
+        ids = queryset.filter(tprek_id__isnull=False).values_list("tprek_id", flat=True)
+        try:
+            output = call_command(
+                "import_units", settings.TPREK_UNIT_URL, "--ids", *ids
+            )
+        except Exception as e:
+            self.message_user(
+                request, f"Error while importing units: {e}", level=messages.ERROR
+            )
+        else:
+            self.message_user(request, output, level=messages.SUCCESS)
 
 
 @admin.register(UnitGroup)
