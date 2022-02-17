7c432907d2b75adea93789b6d3319e5725bfe6f2
Sami Siren <siren@foofactory.fi>
2022-02-17 14:29:18 +0200

    tweak

--- ui/next.config.js
+++ ui/next.config.js
@@ -29,7 +29,7 @@ const sentryWebpackPluginOptions = {
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
 
-  dryRun: process.env.ENABLE_SENTRY_PROJECT_REGISTRATION !== "true",
+  dryRun: process.env.ENABLE_SENTRY_RELEASE_REGISTRATION !== "true",
   silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
