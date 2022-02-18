9ada5f2f5124d6dfcf991fc9d4ca23855d40b2a8
Mikko Sartanen <sartanen@gmail.com>
2022-02-18 15:25:41 +0200

    TILA-1217 restore quota tests

--- ui/cypress/integration/reservation-unit-single.ts
+++ ui/cypress/integration/reservation-unit-single.ts
@@ -464,6 +464,59 @@ describe("Tilavaraus ui reservation unit page (single)", () => {
     });
   });
 
+  describe("with reservation quota notification", () => {
+    it("should display apt notification if quota is set and full", () => {
+      cy.window().then(() => {
+        sessionStorage.setItem(
+          `oidc.apiToken.${Cypress.env("API_SCOPE")}`,
+          "foobar"
+        );
+      });
+
+      cy.visit("/reservation-unit/single/901");
+
+      gotoCalendarButton().should("exist");
+
+      calendarWrapper().should("exist");
+
+      reservationSubmitButton().should("not.exist");
+
+      reservationQuotaNotification()
+        .invoke("text")
+        .should(
+          "match",
+          /^Sinulla on jo \d+ varausta tähän tilaan. Et voi tehdä uusia varauksia.$/
+        );
+
+      cy.window().then(() => {
+        sessionStorage.removeItem(`oidc.apiToken.${Cypress.env("API_SCOPE")}`);
+      });
+    });
+
+    it("should display apt notification if quota is set but not full", () => {
+      cy.window().then(() => {
+        sessionStorage.setItem(
+          `oidc.apiToken.${Cypress.env("API_SCOPE")}`,
+          "foobar"
+        );
+      });
+
+      cy.visit("/reservation-unit/single/902");
+
+      gotoCalendarButton().should("exist");
+
+      calendarWrapper().should("exist");
+
+      reservationQuotaNotification()
+        .invoke("text")
+        .should("match", /^Sinulla on jo \d+\/\d+ varausta tähän tilaan.$/);
+
+      cy.window().then(() => {
+        sessionStorage.removeItem(`oidc.apiToken.${Cypress.env("API_SCOPE")}`);
+      });
+    });
+  });
+
   describe("with metadataset", () => {
     Cypress.config("defaultCommandTimeout", 20000);
     beforeEach(() => {
