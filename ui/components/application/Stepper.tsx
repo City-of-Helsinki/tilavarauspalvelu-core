ffdd14681c62c05b128d529e2a6ba40925811a14
Sami Siren <siren@foofactory.fi>
2022-03-04 13:25:55 +0200

    change stepper styles/labels

--- ui/components/application/Stepper.tsx
+++ ui/components/application/Stepper.tsx
@@ -1,49 +1,88 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import Link from "next/link";
 import styled from "styled-components";
+import { IconCheck } from "hds-react";
+import { useRouter } from "next/router";
 import { Application } from "../../modules/types";
 
-const NavigationContainer = styled.nav`
-  font-size: var(--fontsize-body-l);
+const StepContainer = styled.div<{ $disabled: boolean }>`
+  ${({ $disabled }) =>
+    $disabled
+      ? `background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='80' width='100'%3E%3Cg fill='none' stroke='rgb(120,120,120)' stroke-width='3'%3E%3Cpath  d='M24 0 l0 80' /%3E%3C/g%3E%3C/svg%3E");`
+      : `background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='80' width='100'%3E%3Cg fill='none' stroke='rgb(0,0,191)' stroke-width='3'%3E%3Cpath  d='M24 0 l0 80' /%3E%3C/g%3E%3C/svg%3E");`}
 
-  ul {
-    padding: 0;
-    list-style-type: none;
-    font-family: var(--font-bold);
-    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='80' width='100'%3E%3Cg fill='none' stroke='rgb(0,0,191)' stroke-width='3'%3E%3Cpath stroke-dasharray='4,4' d='M12 0 l0 80' /%3E%3C/g%3E%3C/svg%3E");
-    background-repeat: repeat-y;
-
-    li {
-      margin-top: 1.5em;
-    }
-  }
-
-  span {
-    color: var(--color-black-90);
-    display: flex;
-    align-items: center;
-  }
-
-  a {
-    text-decoration: none;
-
-    :focus {
-      outline: 2px solid var(--color-coat-of-arms);
-    }
+  background-repeat: repeat-y;
+  height: 70px;
+  padding: 0;
+  :last-of-type {
+    height: auto;
   }
 `;
-
-const Number = styled.div`
-  background-color: var(--color-bus);
+const Container = styled.nav`
+  margin: var(--spacing-m) 0;
+  font-size: var(--fontsize-body-l);
+  padding: 0;
+  list-style-type: none;
+  font-family: var(--font-reqular);
+  font-family: var(--font-bold);
+`;
+const Number = styled.div<{ $current: boolean; $disabled: boolean }>`
   border-radius: 50%;
-  width: 24px;
-  height: 24px;
-  color: var(--color-white);
+  width: 32px;
+  height: 32px;
   margin-right: 1em;
   text-align: center;
-  line-height: 1.3;
   align-self: flex-end;
+  border: 2px solid var(--color-bus);
+  outline: 2px solid;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--color-black-40);
+  ${({ $current }) =>
+    $current
+      ? "outline-color: var(--color-coat-of-arms);"
+      : "outline-color: var(--tilavaraus-gray);"}
+
+  ${({ $disabled }) =>
+    $disabled
+      ? "border-color: var(--color-black-20);"
+      : `
+      color: var(--color-bus);
+      `}
+
+${({ $disabled, $current }) =>
+    !$disabled && !$current
+      ? "background: var(--color-bus); color: white;"
+      : ``}
+`;
+
+const Step = styled.button<{ $clickable: boolean }>`
+  border: none;
+  background: none;
+  width: 100%;
+  display: flex;
+  line-height: 1;
+  align-content: flex-start;
+  align-items: center;
+  background: var(--tilavaraus-gray);
+
+  ${({ $clickable }) => ($clickable ? "cursor: pointer;" : "cursor: normal;")}
+`;
+
+const Name = styled.div<{ $disabled: boolean; $current: boolean }>`
+  text-decoration: underline;
+
+  ${({ $current }) =>
+    $current
+      ? `text-decoration: none;
+      color: var(--color-black-90);`
+      : `color: var(--color-bus);`}
+  ${({ $disabled }) =>
+    $disabled
+      ? `      text-decoration: none;
+      color: var(--color-black-40);`
+      : ``}
 `;
 
 type Props = {
@@ -52,6 +91,7 @@ type Props = {
 
 const Stepper = ({ application }: Props): JSX.Element => {
   const { t } = useTranslation();
+  const { asPath, push } = useRouter();
   let maxPage = -1;
   if (application) {
     if (
@@ -72,29 +112,36 @@ const Stepper = ({ application }: Props): JSX.Element => {
   const pages = ["page1", "page2", "page3", "preview"];
 
   return (
-    <NavigationContainer aria-label={t("common:applicationNavigationName")}>
-      <ul>
-        {pages.map((page, index) => (
-          <li key={page}>
-            {maxPage >= index ? (
-              <Link href={`/application/${application.id}/${page}`}>
-                <a>
-                  <span>
-                    <Number>{index + 1}</Number>
-                    {t(`application:navigation.${page}`)}
-                  </span>
-                </a>
-              </Link>
-            ) : (
-              <span>
-                <Number>{index + 1}</Number>
-                {t(`application:navigation.${page}`)}
-              </span>
-            )}
-          </li>
-        ))}
-      </ul>
-    </NavigationContainer>
+    <Container aria-label={t("common:applicationNavigationName")}>
+      <>
+        {pages.map((page, index) => {
+          const isCurrent = asPath.indexOf(page) !== -1;
+          const isDisabled = index > maxPage;
+          const isReady = !isDisabled && !isCurrent;
+          return (
+            <StepContainer $disabled={maxPage <= index} key={page}>
+              <Step
+                key={page}
+                onClick={() => {
+                  if (!isCurrent) {
+                    push(`/application/${application.id}/${page}`);
+                  }
+                }}
+                $clickable={!isDisabled && !isCurrent}
+                disabled={isDisabled || isCurrent}
+              >
+                <Number $disabled={isDisabled} $current={isCurrent}>
+                  {isReady ? <IconCheck /> : index + 1}
+                </Number>
+                <Name $current={isCurrent} $disabled={isDisabled}>
+                  {index + 1}. {t(`application:navigation.${page}`)}
+                </Name>
+              </Step>
+            </StepContainer>
+          );
+        })}
+      </>
+    </Container>
   );
 };
 export default Stepper;
