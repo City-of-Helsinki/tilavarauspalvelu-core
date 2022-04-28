88547ea3ce982346837cd856fd318f897904c8f2
Mikko Sartanen <sartanen@gmail.com>
2022-04-28 09:51:36 +0300

    TILA-1465 hotjar script

--- ui/components/ExternalScripts.tsx
+++ ui/components/ExternalScripts.tsx
@@ -52,6 +52,25 @@ d.parentNode.insertBefore(e,d);
   `,
         }}
       />
+      <Script
+        id="hotjar"
+        strategy="afterInteractive"
+        data-consent="analytics"
+        type="text/plain"
+        dangerouslySetInnerHTML={{
+          // eslint-disable-next-line
+          __html: `
+(function(h,o,t,j,a,r){
+  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+  h._hjSettings={hjid:2913591,hjsv:6};
+  a=o.getElementsByTagName('head')[0];
+  r=o.createElement('script');r.async=1;
+  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+        }}
+      />
     </>
   );
 };
