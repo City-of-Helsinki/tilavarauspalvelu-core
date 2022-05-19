2e913ce4579d8139f11176afe9c211a69ae71c0b
Ian Niilola <ian.niilola@anders.com>
2022-05-19 11:48:10 +0300

    Add order by to Applications
    * Order by filter to applications GraphQL query
    * Tests for the different ordering types
    
    Refs TILA-1609

--- api/graphql/applications/application_filtersets.py
+++ api/graphql/applications/application_filtersets.py
@@ -1,3 +1,7 @@
+from django.db.models import Case, CharField, F
+from django.db.models import Value as V
+from django.db.models import When
+from django.db.models.functions import Concat
 from django_filters import rest_framework as filters
 
 from applications.models import Application, ApplicationRound, User
@@ -14,9 +18,38 @@ class ApplicationFilterSet(filters.FilterSet):
     )
     user = filters.ModelChoiceFilter(field_name="user", queryset=User.objects.all())
 
+    order_by = filters.OrderingFilter(fields=("pk", "applicant"))
+
     class Meta:
         model = Application
         fields = ("application_round", "status", "unit", "user")
+
+    def filter_queryset(self, queryset):
+        queryset = queryset.alias(
+            applicant=Case(
+                When(organisation__isnull=False, then=F("organisation__name")),
+                When(
+                    contact_person__isnull=False,
+                    then=Concat(
+                        "contact_person__first_name",
+                        V(" "),
+                        "contact_person__last_name",
+                    ),
+                ),
+                When(
+                    user__isnull=False,
+                    then=Concat(
+                        "user__first_name",
+                        V(" "),
+                        "user__last_name",
+                    ),
+                ),
+                default=V(""),
+                output_field=CharField(),
+            )
+        )
+
+        return super().filter_queryset(queryset)
 
     def filter_by_possible_units(self, qs, property, value):
         if not value:
