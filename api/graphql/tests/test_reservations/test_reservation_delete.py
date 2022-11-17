d76147fcd80f357d0d3f2a154b070428df1a7421
Nico Virkki <nico.virkki@anders.com>
2022-11-17 13:37:35 +0200

    Adds mutation for deleting the reservation
    Reservation can be deleted when it is in CREATED state or
    WAITING_FOR_PAYMENT
    
    Refs TILA-2023

---
+++ api/graphql/tests/test_reservations/test_reservation_delete.py
@@ -0,0 +1,157 @@
+import datetime
+import json
+
+from assertpy import assert_that
+from django.contrib.auth import get_user_model
+from django.utils.timezone import get_default_timezone
+
+from api.graphql.tests.test_reservations.base import ReservationTestCaseBase
+from reservations.models import STATE_CHOICES as ReservationState
+from reservations.models import Reservation
+from reservations.tests.factories import ReservationFactory
+
+
+class ReservationDeleteTestCase(ReservationTestCaseBase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+        cls.reservation = ReservationFactory(
+            reservation_unit=[cls.reservation_unit],
+            begin=datetime.datetime.now(tz=get_default_timezone()),
+            end=(
+                datetime.datetime.now(tz=get_default_timezone())
+                + datetime.timedelta(hours=1)
+            ),
+            state=ReservationState.CREATED,
+            user=cls.regular_joe,
+            reservee_email="email@reservee",
+        )
+
+    def get_delete_query(self):
+        return """
+            mutation deleteReservation($input: ReservationDeleteMutationInput!) {
+                deleteReservation(input: $input){
+                    deleted
+                    errors
+                }
+            }
+        """
+
+    def get_delete_input_data(self):
+        return {"pk": self.reservation.id}
+
+    def test_general_admin_can_delete(self):
+        self.client.force_login(self.general_admin)
+
+        response = self.query(
+            self.get_delete_query(), input_data=self.get_delete_input_data()
+        )
+        assert_that(response.status_code).is_equal_to(200)
+
+        content = json.loads(response.content)
+        assert_that(content.get("errors")).is_none()
+        assert_that(
+            content.get("data").get("deleteReservation").get("errors")
+        ).is_none()
+        assert_that(
+            content.get("data").get("deleteReservation").get("deleted")
+        ).is_true()
+
+        assert_that(
+            Reservation.objects.filter(pk=self.reservation.pk).exists()
+        ).is_false()
+
+    def test_user_can_delete(self):
+        self.client.force_login(self.regular_joe)
+
+        response = self.query(
+            self.get_delete_query(), input_data=self.get_delete_input_data()
+        )
+        assert_that(response.status_code).is_equal_to(200)
+
+        content = json.loads(response.content)
+        assert_that(content.get("errors")).is_none()
+        assert_that(
+            content.get("data").get("deleteReservation").get("errors")
+        ).is_none()
+        assert_that(
+            content.get("data").get("deleteReservation").get("deleted")
+        ).is_true()
+
+        assert_that(
+            Reservation.objects.filter(pk=self.reservation.pk).exists()
+        ).is_false()
+
+    def test_waiting_for_payment_can_be_deleted(self):
+        self.reservation.state = ReservationState.WAITING_FOR_PAYMENT
+        self.reservation.save()
+        self.client.force_login(self.regular_joe)
+
+        response = self.query(
+            self.get_delete_query(), input_data=self.get_delete_input_data()
+        )
+        assert_that(response.status_code).is_equal_to(200)
+
+        content = json.loads(response.content)
+        assert_that(content.get("errors")).is_none()
+        assert_that(
+            content.get("data").get("deleteReservation").get("errors")
+        ).is_none()
+        assert_that(
+            content.get("data").get("deleteReservation").get("deleted")
+        ).is_true()
+
+        assert_that(
+            Reservation.objects.filter(pk=self.reservation.pk).exists()
+        ).is_false()
+
+    def test_cannot_delete_when_status_not_created_nor_waiting_for_payment(self):
+        self.client.force_login(self.general_admin)
+
+        self.reservation.state = ReservationState.CONFIRMED
+        self.reservation.save()
+        response = self.query(
+            self.get_delete_query(), input_data=self.get_delete_input_data()
+        )
+        assert_that(response.status_code).is_equal_to(200)
+
+        content = json.loads(response.content)
+        assert_that(content.get("errors")).is_none()
+        assert_that(
+            content.get("data").get("deleteReservation").get("errors")
+        ).is_not_none()
+        assert_that(
+            content.get("data").get("deleteReservation").get("deleted")
+        ).is_false()
+
+        assert_that(
+            Reservation.objects.filter(pk=self.reservation.pk).exists()
+        ).is_true()
+
+    def test_other_user_cannot_delete(self):
+        other_guy = get_user_model().objects.create(
+            username="other",
+            first_name="oth",
+            last_name="er",
+            email="oth.er@foo.com",
+        )
+        self.client.force_login(other_guy)
+
+        response = self.query(
+            self.get_delete_query(), input_data=self.get_delete_input_data()
+        )
+        assert_that(response.status_code).is_equal_to(200)
+
+        content = json.loads(response.content)
+        assert_that(content.get("errors")).is_none()
+        assert_that(
+            content.get("data").get("deleteReservation").get("errors")
+        ).is_not_none()
+        assert_that(
+            content.get("data").get("deleteReservation").get("deleted")
+        ).is_false()
+
+        assert_that(
+            Reservation.objects.filter(pk=self.reservation.pk).exists()
+        ).is_true()
