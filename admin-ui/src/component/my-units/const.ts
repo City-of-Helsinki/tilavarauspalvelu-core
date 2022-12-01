4bc68aab0b75cfe23647de65e01f777a121dba4e
huru-ukko <t.pesola@gmail.com>
2022-12-01 11:54:46 +0200

    Adds UI changes to display draft resources differrently from non-draft ones

--- admin-ui/src/component/my-units/const.ts
+++ admin-ui/src/component/my-units/const.ts
@@ -1,1 +1,3 @@
 export const CELL_BORDER = "1px solid var(--color-black-20)";
+export const CELL_BORDER_LEFT = "2px solid var(--color-black-10)";
+export const CELL_BORDER_LEFT_ALERT = "2px dashed var(--color-alert-dark)";
