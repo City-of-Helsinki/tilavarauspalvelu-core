a8f6c9b1fae6aff1f349b2255d096a952854638d
huru-ukko <t.pesola@gmail.com>
2022-12-14 09:17:07 +0200

    Tabs logic moved to its own generic component to lessen the clutter in page view

--- admin-ui/src/component/my-units/MyUnitView.tsx
+++ admin-ui/src/component/my-units/MyUnitView.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { H1 } from "common/src/common/typography";
-import { Tabs } from "hds-react";
 import { useTranslation } from "react-i18next";
-import styled from "styled-components";
-import { useHistory, useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { LocationType, Query, QueryUnitsArgs } from "common/types/gql-types";
 import { publicUrl } from "../../common/const";
 import { parseAddress } from "../../common/util";
@@ -15,28 +13,17 @@ import withMainMenu from "../withMainMenu";
 import { UNIT_QUERY } from "./queries";
 import ReservationUnitCalendarView from "./ReservationUnitCalendarView";
 import UnitReservationsView from "./UnitReservationsView";
+import { TabPanel, Tabs } from "../Tabs";
 
 type Params = {
   unitId: string;
   reservationUnitId: string;
 };
 
-const TabPanel = styled(Tabs.TabPanel)`
-  margin-top: var(--spacing-s);
-  padding-block: var(--spacing-m);
-`;
-
-const ExistingTabs = ["#unit-reservations", "#reservation-unit"];
-
 const MyUnitView = () => {
   const { notifyError } = useNotification();
   const { unitId } = useParams<Params>();
-  const history = useHistory();
-  const { hash } = useLocation();
   const { t } = useTranslation();
-  const initialSelectedTab = ExistingTabs.includes(hash)
-    ? ExistingTabs.indexOf(hash)
-    : undefined;
 
   const { loading, data: unitData } = useQuery<Query, QueryUnitsArgs>(
     UNIT_QUERY,
@@ -53,10 +40,6 @@ const MyUnitView = () => {
 
   const unit = unitData?.units?.edges[0];
 
-  const onTabClick = (tabName: string) => {
-    history.push(`#${tabName}`);
-  };
-
   if (loading) return null;
 
   return (
@@ -70,19 +53,19 @@ const MyUnitView = () => {
           <H1>{unit?.node?.nameFi}</H1>
           <p>{parseAddress(unit?.node?.location as LocationType)}</p>
         </div>
-        <Tabs initiallyActiveTab={initialSelectedTab}>
-          <Tabs.TabList>
-            <Tabs.Tab onClick={() => onTabClick("unit-reservations")}>
-              {t("MyUnits.UnitCalendar.tab")}
-            </Tabs.Tab>
-            <Tabs.Tab onClick={() => onTabClick("reservation-unit")}>
-              {t("MyUnits.Calendar.tab")}
-            </Tabs.Tab>
-          </Tabs.TabList>
-          <TabPanel>
+        <Tabs
+          headers={[
+            {
+              key: "unit-reservations",
+              label: `${t("MyUnits.UnitCalendar.tab")}`,
+            },
+            { key: "reservation-unit", label: `${t("MyUnits.Calendar.tab")}` },
+          ]}
+        >
+          <TabPanel key="unit-reservations">
             <UnitReservationsView />
           </TabPanel>
-          <TabPanel>
+          <TabPanel key="reservation-unit">
             <ReservationUnitCalendarView />
           </TabPanel>
         </Tabs>
