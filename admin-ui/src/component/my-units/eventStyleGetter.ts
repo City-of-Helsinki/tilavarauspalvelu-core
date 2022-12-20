1bc724d98ed481ed037e54a65428648c3bc01a48
huru-ukko <t.pesola@gmail.com>
2022-12-20 15:50:31 +0200

    Adds key value for event legends so they can be filtered

--- admin-ui/src/component/my-units/eventStyleGetter.ts
+++ admin-ui/src/component/my-units/eventStyleGetter.ts
@@ -4,6 +4,23 @@ import {
   ReservationType,
 } from "common/types/gql-types";
 
+type EventKey =
+  | "CONFIRMED"
+  | "UNCONFIRMED"
+  | "STAFF_RESERVATION"
+  | "INTERSECTING_RESERVATION_UNIT"
+  | "PAUSE"
+  | "CLOSED"
+  | "WAITING_PAYMENT"
+  | "RESERVATION_UNIT_RELEASED"
+  | "RESERVATION_UNIT_DRAFT";
+
+type EventStyle = {
+  key: EventKey;
+  label: string;
+  style: Record<string, string>;
+};
+
 const CONFIRMED = {
   style: {
     borderColor: "var(--color-success)",
@@ -81,34 +98,72 @@ const CLOSED = {
   },
 };
 
-export const legend = [
+const RESERVATION_UNIT_RELEASED = {
+  style: {
+    borderColor: "var(--color-black-20)",
+    borderStyle: "solid",
+    borderWidth: "0px 0px 0px 2px",
+    background: "var(--color-white)",
+    color: "black",
+  },
+};
+
+const RESERVATION_UNIT_DRAFT = {
+  style: {
+    borderColor: "var(--color-alert-dark)",
+    borderStyle: "dashed",
+    borderWidth: "0px 0px 0px 2px",
+    background: "var(--color-white)",
+    color: "black",
+    width: "4px",
+  },
+};
+
+export const legend: EventStyle[] = [
   {
+    key: "CONFIRMED",
     label: "MyUnits.Calendar.legend.confirmed",
     style: CONFIRMED.style,
   },
   {
+    key: "WAITING_PAYMENT",
     label: "MyUnits.Calendar.legend.waitingPayment",
     style: WAITING_PAYMENT.style,
   },
   {
+    key: "UNCONFIRMED",
     label: "MyUnits.Calendar.legend.unconfirmed",
     style: UNCONFIRMED.style,
   },
   {
+    key: "STAFF_RESERVATION",
     label: "MyUnits.Calendar.legend.staffReservation",
     style: STAFF_RESERVATION.style,
   },
   {
+    key: "INTERSECTING_RESERVATION_UNIT",
     label: "MyUnits.Calendar.legend.intersecting",
     style: INTERSECTING_RESERVATION_UNIT.style,
   },
   {
+    key: "PAUSE",
     label: "MyUnits.Calendar.legend.pause",
     style: POST_PAUSE.style,
   },
   {
+    key: "CLOSED",
     label: "MyUnits.Calendar.legend.closed",
     style: CLOSED.style,
+  },
+  {
+    key: "RESERVATION_UNIT_RELEASED",
+    label: "MyUnits.Calendar.legend.reservationUnitReleased",
+    style: RESERVATION_UNIT_RELEASED.style,
+  },
+  {
+    key: "RESERVATION_UNIT_DRAFT",
+    label: "MyUnits.Calendar.legend.reservationUnitDraft",
+    style: RESERVATION_UNIT_DRAFT.style,
   },
 ];
 
