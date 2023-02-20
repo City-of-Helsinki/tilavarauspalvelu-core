fb32fe1ea319c8e940dcbd50324a256cec3c8090
Nico Virkki <nico.virkki@anders.com>
2023-02-20 12:10:16 +0200

    Add municipality code city model
    Refs TILA-1983

--- applications/models.py
+++ applications/models.py
@@ -507,6 +507,9 @@ class ApplicationRoundAggregateData(AggregateDataBase):
 
 class City(models.Model):
     name = models.CharField(verbose_name=_("Name"), max_length=100)
+    municipality_code = models.CharField(
+        verbose_name=_("Municipality code"), default="", max_length=30
+    )
 
     def __str__(self):
         return self.name
