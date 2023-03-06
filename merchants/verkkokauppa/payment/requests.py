a6bae0c7b421aaaca6f872224903a2eff8120d2b
Mika Hiltunen <github@haaska.net>
2023-03-06 08:25:01 +0200

    Add client for webshop refunds
    Refs TILA-2380

--- merchants/verkkokauppa/payment/requests.py
+++ merchants/verkkokauppa/payment/requests.py
@@ -6,13 +6,20 @@ from uuid import UUID
 from django.conf import settings
 from requests import RequestException
 from requests import get as _get
+from requests import post as _post
+from sentry_sdk import capture_exception, capture_message, push_scope
 
 from utils.metrics import ExternalServiceMetric
 
 from ..constants import METRIC_SERVICE_NAME, REQUEST_TIMEOUT_SECONDS
 from ..exceptions import VerkkokauppaConfigurationError
-from .exceptions import GetPaymentError, ParsePaymentError
-from .types import Payment
+from .exceptions import (
+    GetPaymentError,
+    ParsePaymentError,
+    ParseRefundError,
+    RefundPaymentError,
+)
+from .types import Payment, Refund
 
 
 def _get_base_url():
@@ -60,3 +67,49 @@ def get_payment(order_id: UUID, namespace: str, get=_get) -> Optional[Payment]:
         return Payment.from_json(json)
     except (RequestException, JSONDecodeError, ParsePaymentError) as e:
         raise GetPaymentError("Payment retrieval failed") from e
+
+
+def refund_order(order_id: UUID, post=_post) -> Optional[Refund]:
+    try:
+        with ExternalServiceMetric(
+            METRIC_SERVICE_NAME, "POST", f"/refund/instant/{order_id}"
+        ) as metric:
+            response = post(
+                url=urljoin(_get_base_url(), f"refund/instant/{order_id}"),
+                headers={
+                    "api-key": settings.VERKKOKAUPPA_API_KEY,
+                    "namespace": settings.VERKKOKAUPPA_NAMESPACE,
+                },
+                timeout=REQUEST_TIMEOUT_SECONDS,
+            )
+            metric.response_status = response.status_code
+
+        if response.status_code > 200:
+            capture_message(
+                f"Call to Payment Experience API refund endpoint failed with status {response.status_code}. "
+                + f"Response body: {response.text}",
+                level="error",
+            )
+            raise RefundPaymentError(
+                "Payment refund failed: problem with upstream service"
+            )
+        json = response.json()
+        refund_count = len(json["refunds"]) if "refunds" in json else 0
+        if refund_count == 1:
+            return Refund.from_json(json["refunds"][0])
+        else:
+            capture_message(
+                "Call to Payment Experience API refund endpoint failed. "
+                + f"Response contains {refund_count} refunds instead of one. "
+                + f"Response body: {response.text}",
+                level="error",
+            )
+            raise RefundPaymentError(
+                f"Refund response refund count expected to be 1 but was {refund_count}"
+            )
+    except (RequestException, JSONDecodeError, ParseRefundError) as err:
+        with push_scope() as scope:
+            scope.set_extra("details", "Payment refund failed")
+            scope.set_extra("order-id", order_id)
+            capture_exception(err)
+        raise RefundPaymentError(f"Payment refund failed: {str(err)}") from err
--- merchants/verkkokauppa/payment/types.py
+++ merchants/verkkokauppa/payment/types.py
@@ -5,7 +5,9 @@ from enum import Enum
 from typing import Any, Dict, Optional
 from uuid import UUID
 
-from ..payment.exceptions import ParsePaymentError
+from sentry_sdk import capture_exception, push_scope
+
+from ..payment.exceptions import ParsePaymentError, ParseRefundError
 
 
 class PaymentStatus(Enum):
@@ -65,3 +67,43 @@ class Payment:
     @classmethod
     def _parse_datetime(cls, string: str) -> datetime:
         return datetime.strptime(string, "%Y%m%d-%H%M%S")
+
+
+@dataclass(frozen=True)
+class Refund:
+    refund_id: UUID
+    order_id: UUID
+    namespace: str
+    user: str
+    created_at: datetime
+    status: str
+    customer_first_name: Optional[str]
+    customer_last_name: Optional[str]
+    customer_email: Optional[str]
+    customer_phone: Optional[str]
+    refund_reason: Optional[str]
+
+    @classmethod
+    def from_json(cls, json: Dict[str, Any]) -> "Refund":
+        from ..helpers import parse_datetime
+
+        try:
+            return Refund(
+                refund_id=UUID(json["refundId"]),
+                order_id=UUID(json["orderId"]),
+                namespace=json["namespace"],
+                user=json["user"],
+                created_at=parse_datetime(json["createdAt"]),
+                status=json["status"],
+                customer_first_name=json.get("customerFirstName"),
+                customer_last_name=json.get("customerLastName"),
+                customer_email=json.get("customerEmail"),
+                customer_phone=json.get("customerPhone"),
+                refund_reason=json.get("refundReason"),
+            )
+        except (KeyError, ValueError) as err:
+            with push_scope() as scope:
+                scope.set_extra("details", "Parsing refund failed")
+                scope.set_extra("json", json)
+                capture_exception(err)
+            raise ParseRefundError(f"Could not parse refund: {str(err)}") from err
