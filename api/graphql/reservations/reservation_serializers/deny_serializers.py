2b6d2996ba6cde06b65068c26af30cd8594cfef7
Nico Virkki <nico.virkki@anders.com>
2023-03-15 17:28:27 +0200

    Correct typo in denySerializer comment
    Refs TILA-2162

--- api/graphql/reservations/reservation_serializers/deny_serializers.py
+++ api/graphql/reservations/reservation_serializers/deny_serializers.py
@@ -46,7 +46,7 @@ class ReservationDenySerializer(PrimaryKeySerializer):
         validated_data = super().validated_data
         validated_data["state"] = STATE_CHOICES.DENIED
         validated_data["handled_at"] = datetime.datetime.now(tz=DEFAULT_TIMEZONE)
-        # For now we wan't to copy the handling details to working memo. In future perhaps not.
+        # For now, we want to copy the handling details to working memo. In future perhaps not.
         validated_data["working_memo"] = validated_data["handling_details"]
         return validated_data
 
