8c4b5bf21c58bdd0677534e242f1e735e178a8ef
Joonatan Kuosa <35558218+joonatank@users.noreply.github.com>
2023-03-20 11:04:50 +0200

    TILA-2385: fix query user permissions also (#565)

--- admin-ui/src/context/queries.ts
+++ admin-ui/src/context/queries.ts
@@ -4,31 +4,40 @@ export const CURRENT_USER = gql`
   query currentUser {
     currentUser {
       isSuperuser
+      username
       unitRoles {
         pk
-
         role {
           code
+          verboseNameFi
         }
         units {
           pk
+          nameFi
+        }
+        permissions {
+          permission
         }
       }
       serviceSectorRoles {
         pk
-        role {
-          code
-        }
         serviceSector {
           pk
+          nameFi
+        }
+        permissions {
+          permission
         }
       }
       generalRoles {
         pk
         role {
           code
+          verboseNameFi
         }
-        pk
+        permissions {
+          permission
+        }
       }
     }
   }
