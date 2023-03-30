64fb3bf2e26eb73837dccb1965c8767b8f36cb9b
Mikko Sartanen <sartanen@gmail.com>
2023-03-30 11:05:25 +0300

    TILA-2413 validate adjacent time slots (#583)

--- ui/modules/application/__tests__/application.test.ts
+++ ui/modules/application/__tests__/application.test.ts
@@ -2,7 +2,7 @@ import { ApplicationEvent, Cell } from "common";
 import {
   getApplicationEventsWhichMinDurationsIsNotFulfilled,
   getListOfApplicationEventTitles,
-  getSelectedHours,
+  getLongestChunks,
 } from "../application";
 
 jest.mock("next/config", () => () => ({
@@ -16,43 +16,294 @@ jest.mock("next-i18next", () => ({
   },
 }));
 
-describe("getSelectedHours", () => {
+describe("getLongestChunks", () => {
   test("with some data", () => {
-    const selectorData = [
+    const selectorData: Cell[][][] = [
       [
-        [{ state: 200 }, { state: false }],
-        [{ state: 300 }, { state: 200 }],
+        [
+          {
+            key: "7-1",
+            hour: 7,
+            label: "7 - 8",
+            state: false,
+          },
+          {
+            key: "8-1",
+            hour: 8,
+            label: "8 - 9",
+            state: 300,
+          },
+          {
+            key: "9-1",
+            hour: 9,
+            label: "9 - 10",
+            state: 300,
+          },
+          {
+            key: "10-1",
+            hour: 10,
+            label: "10 - 11",
+            state: 300,
+          },
+          {
+            key: "11-1",
+            hour: 11,
+            label: "11 - 12",
+            state: false,
+          },
+          {
+            key: "12-1",
+            hour: 12,
+            label: "12 - 13",
+            state: false,
+          },
+          {
+            key: "13-1",
+            hour: 13,
+            label: "13 - 14",
+            state: 300,
+          },
+          {
+            key: "14-1",
+            hour: 14,
+            label: "14 - 15",
+            state: 300,
+          },
+          {
+            key: "15-1",
+            hour: 15,
+            label: "15 - 16",
+            state: false,
+          },
+          {
+            key: "16-1",
+            hour: 16,
+            label: "16 - 17",
+            state: false,
+          },
+          {
+            key: "17-1",
+            hour: 17,
+            label: "17 - 18",
+            state: false,
+          },
+          {
+            key: "18-1",
+            hour: 18,
+            label: "18 - 19",
+            state: false,
+          },
+          {
+            key: "19-1",
+            hour: 19,
+            label: "19 - 20",
+            state: 300,
+          },
+          {
+            key: "20-1",
+            hour: 20,
+            label: "20 - 21",
+            state: false,
+          },
+          {
+            key: "21-1",
+            hour: 21,
+            label: "21 - 22",
+            state: false,
+          },
+          {
+            key: "22-1",
+            hour: 22,
+            label: "22 - 23",
+            state: false,
+          },
+          {
+            key: "23-1",
+            hour: 23,
+            label: "23 - 24",
+            state: false,
+          },
+        ],
+      ],
+      [
+        [
+          {
+            key: "7-1",
+            hour: 7,
+            label: "7 - 8",
+            state: false,
+          },
+          {
+            key: "8-1",
+            hour: 8,
+            label: "8 - 9",
+            state: false,
+          },
+          {
+            key: "9-1",
+            hour: 9,
+            label: "9 - 10",
+            state: false,
+          },
+          {
+            key: "10-1",
+            hour: 10,
+            label: "10 - 11",
+            state: false,
+          },
+          {
+            key: "11-1",
+            hour: 11,
+            label: "11 - 12",
+            state: 300,
+          },
+          {
+            key: "12-1",
+            hour: 12,
+            label: "12 - 13",
+            state: 300,
+          },
+          {
+            key: "13-1",
+            hour: 13,
+            label: "13 - 14",
+            state: false,
+          },
+          {
+            key: "14-1",
+            hour: 14,
+            label: "14 - 15",
+            state: false,
+          },
+          {
+            key: "15-1",
+            hour: 15,
+            label: "15 - 16",
+            state: false,
+          },
+          {
+            key: "16-1",
+            hour: 16,
+            label: "16 - 17",
+            state: false,
+          },
+          {
+            key: "17-1",
+            hour: 17,
+            label: "17 - 18",
+            state: 200,
+          },
+          {
+            key: "18-1",
+            hour: 18,
+            label: "18 - 19",
+            state: 200,
+          },
+          {
+            key: "19-1",
+            hour: 19,
+            label: "19 - 20",
+            state: 200,
+          },
+          {
+            key: "20-1",
+            hour: 20,
+            label: "20 - 21",
+            state: 200,
+          },
+          {
+            key: "21-1",
+            hour: 21,
+            label: "21 - 22",
+            state: false,
+          },
+          {
+            key: "22-1",
+            hour: 22,
+            label: "22 - 23",
+            state: false,
+          },
+          {
+            key: "23-1",
+            hour: 23,
+            label: "23 - 24",
+            state: false,
+          },
+        ],
       ],
     ];
-    expect(getSelectedHours(selectorData as Cell[][][])).toEqual([3]);
+    expect(getLongestChunks(selectorData)).toEqual([3, 4]);
   });
 
   test("with some data", () => {
     const selectorData = [
       [
-        [{ state: 200 }, { state: false }],
-        [{ state: 300 }, { state: 200 }],
+        [
+          {
+            key: "20-1",
+            hour: 20,
+            label: "20 - 21",
+            state: 200,
+          },
+          {
+            key: "21-1",
+            hour: 21,
+            label: "21 - 22",
+            state: false,
+          },
+        ],
+        [
+          {
+            key: "11-1",
+            hour: 11,
+            label: "11 - 12",
+            state: 300,
+          },
+          {
+            key: "12-1",
+            hour: 12,
+            label: "12 - 13",
+            state: 300,
+          },
+        ],
       ],
-      [[{ state: 200 }, { state: false }]],
+      [
+        [
+          {
+            key: "10-1",
+            hour: 10,
+            label: "10 - 11",
+            state: 300,
+          },
+          {
+            key: "11-1",
+            hour: 11,
+            label: "11 - 12",
+            state: false,
+          },
+        ],
+      ],
     ];
-    expect(getSelectedHours(selectorData as Cell[][][])).toEqual([3, 1]);
+    expect(getLongestChunks(selectorData as Cell[][][])).toEqual([2, 1]);
   });
 
   test("with some data", () => {
     const selectorData = [[[{ state: false }]], [[{ state: false }]]];
-    expect(getSelectedHours(selectorData as Cell[][][])).toEqual([0, 0]);
+    expect(getLongestChunks(selectorData as Cell[][][])).toEqual([0, 0]);
   });
 
   test("with no data", () => {
     const selectorData = [[[{ state: false }]]];
-    expect(getSelectedHours(selectorData as Cell[][][])).toEqual([0]);
+    expect(getLongestChunks(selectorData as Cell[][][])).toEqual([0]);
   });
 });
 
 describe("getApplicationEventsWhichMinDurationsIsNotFulfilled", () => {
   test("with enough data", () => {
     const applicationEvents = [{ minDuration: "01:00:00" }];
-    const selectorData = [[[{ state: 300 }]]];
+    const selectorData: Cell[][][] = [
+      [[{ key: "7-1", hour: 7, label: "7 - 8", state: 300 }]],
+    ];
     expect(
       getApplicationEventsWhichMinDurationsIsNotFulfilled(
         applicationEvents as ApplicationEvent[],
@@ -63,7 +314,9 @@ describe("getApplicationEventsWhichMinDurationsIsNotFulfilled", () => {
 
   test("with missing data", () => {
     const applicationEvents = [{ minDuration: "01:30:00" }];
-    const selectorData = [[[{ state: 200 }]]];
+    const selectorData: Cell[][][] = [
+      [[{ key: "7-1", hour: 7, label: "7 - 8", state: 200 }]],
+    ];
     expect(
       getApplicationEventsWhichMinDurationsIsNotFulfilled(
         applicationEvents as ApplicationEvent[],
@@ -78,8 +331,19 @@ describe("getApplicationEventsWhichMinDurationsIsNotFulfilled", () => {
       { minDuration: "01:30:00" },
     ];
     const selectorData = [
-      [[{ state: 200 }, { state: 300 }]],
-      [[{ state: 200 }]],
+      [
+        [
+          { key: "7-1", hour: 7, label: "7 - 8", state: 200 },
+          { key: "8-1", hour: 8, label: "8 - 9", state: 200 },
+          { key: "9-1", hour: 8, label: "9 - 10", state: 200 },
+        ],
+      ],
+      [
+        [
+          { key: "7-1", hour: 7, label: "7 - 8", state: 200 },
+          { key: "7-1", hour: 7, label: "7 - 8", state: 200 },
+        ],
+      ],
     ];
     expect(
       getApplicationEventsWhichMinDurationsIsNotFulfilled(
--- ui/modules/application/application.ts
+++ ui/modules/application/application.ts
@@ -1,24 +1,36 @@
 import { convertHMSToSeconds } from "common/src/common/util";
 import { ApplicationEvent, Cell } from "common/types/common";
-import { getReadableList } from "../util";
+import { cellsToApplicationEventSchedules, getReadableList } from "../util";
 
-export const getSelectedHours = (selectorData: Cell[][][]): number[] =>
-  selectorData.map((n) =>
-    n.reduce((acc, cur) => acc + cur.filter((nn) => nn.state).length, 0)
-  );
+export const getLongestChunks = (selectorData: Cell[][][]): number[] =>
+  selectorData.map((n) => {
+    const primarySchedules = cellsToApplicationEventSchedules(
+      n.map((nn) => nn.filter((nnn) => nnn.state === 300))
+    );
+    const secondarySchedules = cellsToApplicationEventSchedules(
+      n.map((nn) => nn.filter((nnn) => nnn.state === 200))
+    );
+
+    return [...primarySchedules, ...secondarySchedules].reduce((acc, cur) => {
+      const start = parseInt(cur.begin, 10);
+      const end = cur.end === "00:00" ? 24 : parseInt(cur.end, 10);
+      const length = end - start;
+      return length > acc ? length : acc;
+    }, 0);
+  });
 
 export const getApplicationEventsWhichMinDurationsIsNotFulfilled = (
   applicationEvents: ApplicationEvent[],
   selectorData: Cell[][][]
 ): number[] => {
-  const selectedHours = getSelectedHours(selectorData);
+  const selectedHours = getLongestChunks(selectorData);
   return applicationEvents
-    .map((applicationEvent, index) =>
-      selectedHours[index] <
-      convertHMSToSeconds(applicationEvent.minDuration) / 3600
+    .map((applicationEvent, index) => {
+      return selectedHours[index] <
+        convertHMSToSeconds(applicationEvent.minDuration) / 3600
         ? index
-        : null
-    )
+        : null;
+    })
     .filter((n) => n !== null);
 };
 
