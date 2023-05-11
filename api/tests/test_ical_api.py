5be80a48c36161dc3dd54d425b36db7c87294e56
Mika Hiltunen <github@haaska.net>
2023-05-11 13:46:25 +0300

    Update icalendar test
    Looks like the newer version if icalendar does not
    use typing anymore. Also, removed pytz.
    
    Refs TILA-2582

--- api/tests/test_ical_api.py
+++ api/tests/test_ical_api.py
@@ -1,8 +1,8 @@
 import io
 import uuid
+from datetime import timezone
 
 import pytest
-import pytz
 from assertpy import assert_that
 from rest_framework.reverse import reverse
 
@@ -64,10 +64,10 @@ def test_getting_reservation_unit_calendar(
         io.BytesIO(b"".join(response.streaming_content)).read().decode("utf-8")
     )
 
-    expected_start = f"DTSTART;VALUE=DATE-TIME:{reservation.begin.astimezone(pytz.UTC).strftime('%Y%m%dT%H%M%SZ')}"
+    expected_start = f"DTSTART:{reservation.begin.astimezone(timezone.utc).strftime('%Y%m%dT%H%M%SZ')}"
     unexpected_start = (
-        f"DTSTART;VALUE=DATE-TIME:"
-        f"{reservation_in_second_unit.begin.astimezone(pytz.UTC).strftime('%Y%m%dT%H%M%SZ')}"
+        f"DTSTART:"
+        f"{reservation_in_second_unit.begin.astimezone(timezone.utc).strftime('%Y%m%dT%H%M%SZ')}"
     )
 
     assert_that(expected_start in zip_content).is_true()
@@ -180,10 +180,10 @@ def test_getting_reservation_calendar(
     expected_description = "DESCRIPTION:" + reservation.get_ical_description().replace(
         "\n", "\\n"
     )
-    expected_start = f"DTSTART;VALUE=DATE-TIME:{reservation.begin.astimezone(pytz.UTC).strftime('%Y%m%dT%H%M%SZ')}"
+    expected_start = f"DTSTART:{reservation.begin.astimezone(timezone.utc).strftime('%Y%m%dT%H%M%SZ')}"
     unexpected_start = (
-        f"DTSTART;VALUE=DATE-TIME:"
-        f"{reservation_in_second_unit.begin.astimezone(pytz.UTC).strftime('%Y%m%dT%H%M%SZ')}"
+        f"DTSTART:"
+        f"{reservation_in_second_unit.begin.astimezone(timezone.utc).strftime('%Y%m%dT%H%M%SZ')}"
     )
 
     assert_that(zip_content).contains(expected_summary)
