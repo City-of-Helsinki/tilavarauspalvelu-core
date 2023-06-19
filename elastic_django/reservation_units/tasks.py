a5e6be2e1b87ed9a5cea87004681a53723a22dae
Nico Virkki <nico.virkki@anders.com>
2023-06-19 10:47:22 +0300

    Adds create and delete tasks for elastic index
    Refs TILA-2458

--- elastic_django/reservation_units/tasks.py
+++ elastic_django/reservation_units/tasks.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from elasticsearch_django.index import update_index
+from elasticsearch_django.index import create_index, delete_index, update_index
 
 from tilavarauspalvelu.celery import app
 
@@ -8,3 +8,15 @@ from tilavarauspalvelu.celery import app
 def update_reservation_unit_elastic_index() -> None:
     index = list(settings.SEARCH_SETTINGS["indexes"].keys())[0]
     update_index(index)
+
+
+@app.task(name="Create ReservationUnit Elastic index")
+def create_reservation_unit_elastic_index() -> None:
+    index = list(settings.SEARCH_SETTINGS["indexes"].keys())[0]
+    create_index(index)
+
+
+@app.task(name="Delete ReservationUnit Elastic index")
+def delete_reservation_unit_elastic_index() -> None:
+    index = list(settings.SEARCH_SETTINGS["indexes"].keys())[0]
+    delete_index(index)
