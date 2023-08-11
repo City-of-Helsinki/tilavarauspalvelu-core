beef865ec08f41dd0727cf5fa6fb05d9e4db0d4e
Matti Lamppu <matti.lamppu@vincit.fi>
2023-08-11 10:53:03 +0300

    Add handling_details to reservation schema

--- permissions/api_permissions/graphene_field_decorators.py
+++ permissions/api_permissions/graphene_field_decorators.py
@@ -1,7 +1,14 @@
+from functools import wraps
+from typing import Any, Callable, ParamSpec, TypeVar
+
 from graphene_permissions.permissions import BasePermission
 from rest_framework.exceptions import PermissionDenied
 
 from permissions.helpers import can_view_recurring_reservation, can_view_reservation
+
+T = TypeVar("T")
+P = ParamSpec("P")
+Decorator = Callable[[Callable[P, T]], Callable[P, T]] | Callable[P, T]
 
 
 def reservation_non_public_field(func: callable):
@@ -14,16 +21,26 @@ def reservation_non_public_field(func: callable):
     return permission_check
 
 
-def reservation_staff_field(func: callable):
-    def permission_check(*args, **kwargs):
-        if can_view_reservation(
-            args[1].context.user, args[0], needs_staff_permissions=True
-        ):
-            return func(*args, **kwargs)
+def reservation_staff_field(default: Any = None) -> Decorator:
+    def decorator(func: Callable[P, T]) -> Callable[P, T]:
+        @wraps(func)
+        def permission_check(*args: P.args, **kwargs: P.kwargs) -> T:
+            if can_view_reservation(
+                args[1].context.user, args[0], needs_staff_permissions=True
+            ):
+                return func(*args, **kwargs)
 
-        return None
+            return default
 
-    return permission_check
+        return permission_check
+
+    # If used without parentheses: '@reservation_staff_field'
+    if callable(default):
+        f = decorator(default)
+        default = None
+        return f
+
+    return decorator
 
 
 def recurring_reservation_non_public_field(func: callable):
