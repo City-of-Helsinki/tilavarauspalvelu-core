9c86738df541cc87ec0d241ddf215ce8b858e6a4
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-08-11 14:44:20 +0300

    fix: remove top margin of search
    Refs TILA-2712

--- admin-ui/src/component/Spaces/SpacesList.tsx
+++ admin-ui/src/component/Spaces/SpacesList.tsx
@@ -5,7 +5,6 @@ import { uniq } from "lodash";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import { H1, Strong } from "common/src/common/typography";
-import { breakpoints } from "common/src/common/style";
 import { useDebounce } from "react-use";
 import { useQuery, ApolloError } from "@apollo/client";
 import { Query, SpaceType } from "common/types/gql-types";
@@ -19,30 +18,7 @@ import BreadcrumbWrapper from "../BreadcrumbWrapper";
 import { useNotification } from "../../context/NotificationContext";
 import { spaceUrl } from "../../common/urls";
 import { Container } from "../../styles/layout";
-
-const SearchContainer = styled.div`
-  display: grid;
-  align-items: center;
-  margin-top: var(--spacing-layout-xl);
-  margin-bottom: var(--spacing-layout-m);
-  position: relative;
-  width: 100%;
-
-  &&& input {
-    padding-right: var(--spacing-2-xl);
-    padding-left: var(--spacing-xl);
-  }
-
-  .searchIcon {
-    position: absolute;
-    left: 0;
-    z-index: 1;
-  }
-
-  @media (min-width: ${breakpoints.s}) {
-    width: 20rem;
-  }
-`;
+import SearchContainer from "../SearchContainer";
 
 const StyledInput = styled(TextInput).attrs({
   style: {
