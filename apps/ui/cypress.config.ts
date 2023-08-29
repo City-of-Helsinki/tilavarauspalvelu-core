3bf362bd6724413ecfadca7b083c566831eef07f
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-08-29 07:36:54 +0300

    fix: cypress tests
    Downgrade and pin NextJs.
    Add cypress workflow.
    
    WIP: one test set is broken.

--- apps/ui/cypress.config.ts
+++ apps/ui/cypress.config.ts
@@ -7,17 +7,7 @@ export default defineConfig({
     openMode: 0,
   },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      on("before:browser:launch", (browser, launchOptions) => {
-        if (browser.name === "chrome") {
-          launchOptions.args.push("--user-data-dir=/tmp/foo");
-          launchOptions.args.push("--ignore-certificate-errors");
-          return launchOptions;
-        }
-        return launchOptions;
-      });
       // eslint-disable-next-line import/extensions
       return require("./cypress/plugins/index.js")(on, config);
     },
