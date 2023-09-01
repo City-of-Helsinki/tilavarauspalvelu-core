c99720cdace516ddb017a1241a777eedcd5d7b60
Matti Lamppu <matti.lamppu@vincit.fi>
2023-09-01 14:18:52 +0300

    Replace broken reservation unit csv export tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,3 +11,16 @@ P = ParamSpec("P")
 @pytest.fixture()
 def graphql() -> GraphQLClient:
     return GraphQLClient()
+
+
+@pytest.fixture()
+def disable_elasticsearch(settings):
+    """Disable syncing to Elasticsearch for the duration of the test.
+    Use with '@pytest.mark.usefixtures' decorator.
+    """
+    original = settings.SEARCH_SETTINGS["settings"]["auto_sync"]
+    try:
+        settings.SEARCH_SETTINGS["settings"]["auto_sync"] = False
+        yield
+    finally:
+        settings.SEARCH_SETTINGS["settings"]["auto_sync"] = original
