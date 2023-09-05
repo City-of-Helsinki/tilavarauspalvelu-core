57e60c0bf7bc728c2af56d4064f04c8bb7f96efe
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-09-05 21:55:06 +0300

    add (admin): drop extra headers on api rest calls

--- admin-ui/src/pages/api/v1/[...slug].ts
+++ admin-ui/src/pages/api/v1/[...slug].ts
@@ -5,17 +5,37 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { slug } = req.query;
+  const { slug, params } = req.query;
   const path = Array.isArray(slug) ? slug.join("/") : slug;
   const url = `${apiBaseUrl}/v1/${path}`;
   // TODO do some safety checks here
-  // TODO we should modify headers here instead of in the client call (api.ts)
-  // setting the authorization
-  res.redirect(url);
+  const { method } = req;
+  const reqUrl = `${url}?${params ?? ""}`;
+  try {
+    const result = await fetch(reqUrl, {
+      method,
+      body: req.body,
+      headers: {
+        Authorization: req.headers.authorization as string,
+      },
+    })
+      .then((x) => (x.ok ? x : Promise.reject(x)))
+      .then((x) => x.json())
+      .catch((err) => {
+        return Promise.reject(err);
+      });
+    return res.status(200).json(result);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 }
 
 export const config = {
   api: {
     bodyParser: false,
+    // We use api routes to rewrite backend calls without headers and cookies
+    // it's more important to rewrite them than to control the size
+    // remove this if we are comfortable that they are smaller than 4MB
+    responseLimit: false,
   },
 };
