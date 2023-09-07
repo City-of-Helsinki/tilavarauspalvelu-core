4401cc4664de9131dd2d951d6f0fe4d2a395ad40
Matti Lamppu <matti.lamppu@vincit.fi>
2023-09-07 15:50:54 +0300

    Reactor Helauth to support multiple redirect hosts

--- tilavarauspalvelu/urls.py
+++ tilavarauspalvelu/urls.py
@@ -7,9 +7,29 @@ from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+from helusers import views
 
 from api.urls import router as api_router
 from api.urls import urlpatterns as other_patterns  # GraphQL and GDPR api.
+
+
+class TVPLogoutView(views.LogoutView):
+    success_url_allowed_hosts = settings.SOCIAL_AUTH_TUNNISTAMO_ALLOWED_REDIRECT_HOSTS
+
+
+class TVPLogoutCompleteView(views.LogoutCompleteView):
+    success_url_allowed_hosts = settings.SOCIAL_AUTH_TUNNISTAMO_ALLOWED_REDIRECT_HOSTS
+
+
+helusers_pattern = (
+    [
+        path("login/", views.LoginView.as_view(), name="auth_login"),
+        path("logout/", TVPLogoutView.as_view(), name="auth_logout"),
+        path("logout/complete/", TVPLogoutCompleteView.as_view(), name="auth_logout_complete"),
+    ],
+    "helusers",
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -18,7 +38,7 @@ urlpatterns = [
     path("swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema")),
     path("redoc/", SpectacularRedocView.as_view(url_name="schema")),
     path("pysocial/", include("social_django.urls", namespace="social")),
-    path("helauth/", include("helusers.urls")),
+    path("helauth/", include(helusers_pattern)),
     path("tinymce/", include("tinymce.urls")),
     path("", include("django_prometheus.urls")),
 ]
