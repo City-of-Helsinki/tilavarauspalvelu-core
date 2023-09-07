fb75172b74fc78dc37d8c7542e90c94e0b37cfe8
Matti Lamppu <matti.lamppu@vincit.fi>
2023-09-07 15:45:57 +0300

    Add get_nested utility function

---
+++ common/utils.py
@@ -0,0 +1,32 @@
+from typing import Any, Optional
+
+__all__ = [
+    "get_nested",
+]
+
+
+def get_nested(obj: Optional[dict | list], /, *args: str | int, default: Any = None) -> Any:
+    """Get value from a nested structure containing dicts with string keys or lists,
+    where the keys and list indices might not exist.
+
+    1) `data["foo"][0]["bar"]["baz"]`
+     - Might raise a `KeyError` or `IndexError` if any of the keys or indices don't exist.
+
+    2) `get_nested(data, "foo", 0, "bar", "baz")`
+     - Will return `None` (default) if any of the keys or indices don't exist.
+    """
+    if not args:
+        return obj if obj is not None else default
+
+    arg, args = args[0], args[1:]
+
+    if isinstance(arg, int):
+        obj = obj or []
+        try:
+            obj = obj[arg]
+        except IndexError:
+            obj = None
+        return get_nested(obj, *args, default=default)
+
+    obj = obj or {}
+    return get_nested(obj.get(arg), *args, default=default)
