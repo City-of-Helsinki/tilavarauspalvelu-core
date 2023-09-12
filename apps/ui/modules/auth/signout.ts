d27c2b000ef79a4a574bd4a1adf0af90352a5f35
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-09-12 12:05:17 +0300

    fix: missing nullcheck

--- apps/ui/modules/auth/signout.ts
+++ apps/ui/modules/auth/signout.ts
@@ -6,6 +6,6 @@ export default function signOut({ session }: { session: Session }) {
   const origin = typeof window !== 'undefined' && window.location.origin
           ? window.location.origin
           : '';
-  const callbackUrl = `${origin}${authenticationLogoutApiRoute}?token=${session.accessToken}`
+  const callbackUrl = `${origin}${authenticationLogoutApiRoute}${session != null ? `?token=${session.accessToken}` : ""}`
   return signOutAuth({ callbackUrl });
 };
