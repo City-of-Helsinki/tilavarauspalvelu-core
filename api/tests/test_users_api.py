3bc91e955751b610dea69728c32bf21eb622375b
Eemeli Ranta <eemeli.ranta@vincit.fi>
2023-09-13 19:20:10 +0300

    Remove automatic database usage fixture

--- api/tests/test_users_api.py
+++ api/tests/test_users_api.py
@@ -27,6 +27,7 @@ def test_user_can_view_current_user(user_api_client, user):
     assert response.data["id"] == user.id
 
 
+@pytest.mark.django_db
 def test_general_admin_can_view_all_users(general_admin_api_client, general_admin, user, user_2):
     response = general_admin_api_client.get(
         reverse("user-list"),
@@ -39,6 +40,7 @@ def test_general_admin_can_view_all_users(general_admin_api_client, general_admi
     assert_that(user_id_list).contains_only(user.id, user_2.id, general_admin.id)
 
 
+@pytest.mark.django_db
 def test_roles_are_included_in_user_data(service_sector_admin, service_sector_admin_api_client):
     response = service_sector_admin_api_client.get(
         reverse("user-current"),
@@ -58,6 +60,7 @@ def test_unauthenticated_cannot_get_user_data(unauthenticated_api_client):
     assert response.status_code == 401
 
 
+@pytest.mark.django_db
 def test_normal_user_does_not_see_notification_field(user_api_client):
     response = user_api_client.get(
         reverse("user-list"),
@@ -67,6 +70,7 @@ def test_normal_user_does_not_see_notification_field(user_api_client):
     assert response.data[0]["reservation_notification"] is None
 
 
+@pytest.mark.django_db
 def test_staff_user_sees_notification_field(staff_user_api_client):
     response = staff_user_api_client.get(
         reverse("user-list"),
