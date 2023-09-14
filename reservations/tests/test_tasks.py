53cdfa70c22069bbc010bf3ffc892af50b2e23c8
Eemeli Ranta <eemeli.ranta@vincit.fi>
2023-09-14 21:19:46 +0300

    Don't test for things that never fail
    - `refund_paid_reservation_task` does not return a value, therefore testing if its return value is None is pointless

--- reservations/tests/test_tasks.py
+++ reservations/tests/test_tasks.py
@@ -12,13 +12,11 @@ from ..tasks import refund_paid_reservation_task
 
 class RefundPaidReservationTestCase(TestCase):
     def test_does_not_fail_when_reservation_is_missing(self):
-        result = refund_paid_reservation_task(0)
-        assert_that(result).is_none()
+        refund_paid_reservation_task(0)
 
     def test_does_not_fail_when_order_is_missing(self):
         reservation = ReservationFactory.create()
-        result = refund_paid_reservation_task(reservation.pk)
-        assert_that(result).is_none()
+        refund_paid_reservation_task(reservation.pk)
 
     @mock.patch("reservations.tasks.refund_order")
     def test_updates_payment_order_on_success(self, mock_refund_order):
@@ -29,9 +27,8 @@ class RefundPaidReservationTestCase(TestCase):
         refund.refund_id = uuid4()
         mock_refund_order.return_value = refund
 
-        result = refund_paid_reservation_task(reservation.pk)
+        refund_paid_reservation_task(reservation.pk)
         assert_that(mock_refund_order.called).is_true()
-        assert_that(result).is_none()
 
         order.refresh_from_db()
         assert_that(order.refund_id).is_equal_to(refund.refund_id)
