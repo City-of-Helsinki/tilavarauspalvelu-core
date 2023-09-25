a4131c656d16a4c1fa1b74f42c4cc2a2689fd911
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-09-25 13:53:40 +0300

    fix: edit delete single notification from cache

--- apps/admin-ui/src/component/notifications/page.tsx
+++ apps/admin-ui/src/component/notifications/page.tsx
@@ -325,6 +325,7 @@ const NotificationForm = ({
   >(BANNER_NOTIFICATIONS_CREATE, {
     update(cache) {
       deleteQueryFromCache(cache, "bannerNotifications");
+      deleteQueryFromCache(cache, "bannerNotification");
     },
   });
   const [updateMutation] = useMutation<
@@ -333,6 +334,7 @@ const NotificationForm = ({
   >(BANNER_NOTIFICATIONS_UPDATE, {
     update(cache) {
       deleteQueryFromCache(cache, "bannerNotifications");
+      deleteQueryFromCache(cache, "bannerNotification");
     },
   });
 
