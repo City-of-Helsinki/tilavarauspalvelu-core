3de4ec034d1fd627163ce66bffee9edecf60d040
Matti Lamppu <matti.lamppu@vincit.fi>
2023-10-04 15:14:32 +0300

    Enable celery worker in production entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "Starting uWSGI server..."
-exec uwsgi --yaml /tvp/docker/uwsgi.yml
+echo "Starting Celery worker and uWSGI server..."
+exec celery -A tilavarauspalvelu worker --detach & uwsgi --yaml /tvp/docker/uwsgi.yml
