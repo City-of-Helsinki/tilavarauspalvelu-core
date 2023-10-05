17627b34af63285c1b491d5ef29e64446650ad94
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-10-05 15:57:51 +0300

    remove (admin): pdf rendering

--- apps/admin-ui/src/component/pdf/Logo.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Image } from "@react-pdf/renderer";
-import { LOGO_IMAGE_URL } from "app/common/const";
-
-const Logo = (): JSX.Element => (
-  <Image style={{ width: 82 }} src={LOGO_IMAGE_URL} />
-);
-export default Logo;
