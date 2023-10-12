96d10a09bea8bfb7c0fa6bf7dca6308288713014
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-10-12 10:15:58 +0300

    remove: @tanstack-query

--- apps/admin-ui/src/pages/_app.tsx
+++ apps/admin-ui/src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "hds-core/lib/base.css";
 import "../index.scss";
 import { isBrowser } from "app/common/const";
@@ -8,15 +7,11 @@ import { ApolloProvider } from "@apollo/client";
 import apolloClient from "app/common/apolloClient";
 import ExternalScripts from "../common/ExternalScripts";
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <ApolloProvider client={apolloClient}>
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
+        <Component {...pageProps} />
       </ApolloProvider>
       {isBrowser && <ExternalScripts />}
     </>
