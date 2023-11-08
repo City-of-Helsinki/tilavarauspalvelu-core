fe5b309789e657a83dca926ba90146d3c703ef11
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-11-08 17:51:53 +0200

    fix: non strict null checks

--- packages/common/src/helpers.ts
+++ packages/common/src/helpers.ts
@@ -3,7 +3,7 @@ import type { Maybe } from "../types/gql-types";
 export function filterNonNullable<T>(
   arr: Maybe<Maybe<T>[]> | undefined
 ): NonNullable<T>[] {
-  return arr?.filter((n): n is NonNullable<T> => n !== null) ?? [];
+  return arr?.filter((n): n is NonNullable<T> => n != null) ?? [];
 }
 
 export const toMondayFirstUnsafe = (day: number) => {
