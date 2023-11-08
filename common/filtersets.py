b1bfa3cacd8dba06b2771c70839139c96626d03b
Matti Lamppu <matti.lamppu@vincit.fi>
2023-11-08 13:04:31 +0200

    Add `combination_methods` config to Filterset.Meta-class
    This can be used to combine two filters into one method, which can then
    used for example for makeing an OR filter between them.

--- common/filtersets.py
+++ common/filtersets.py
@@ -2,6 +2,7 @@ from typing import Any
 
 import django_filters
 from django.db import models
+from django_filters import Filter
 from django_filters.filterset import FILTER_FOR_DBFIELD_DEFAULTS
 
 from common.fields.forms import EnumChoiceField, EnumMultipleChoiceField, IntChoiceField, IntMultipleChoiceField
@@ -65,3 +66,33 @@ class BaseModelFilterSet(django_filters.FilterSet):
     FILTER_DEFAULTS[models.OneToOneRel] = {"filter_class": IntChoiceFilter}
     FILTER_DEFAULTS[models.ManyToOneRel] = {"filter_class": IntMultipleChoiceFilter}
     FILTER_DEFAULTS[models.ManyToManyRel] = {"filter_class": IntMultipleChoiceFilter}
+
+    def filter_queryset(self, queryset: models.QuerySet) -> models.QuerySet:
+        combination_methods: list[str] = getattr(self.Meta, "combination_methods", [])
+        combined_values: dict[str, dict[str, Any]] = {key: {} for key in combination_methods}
+        combined_filters: dict[str, Filter] = {}
+
+        name: str
+        value: Any
+        for name, value in self.form.cleaned_data.items():
+            field_filter: Filter = self.filters[name]
+            method: str = field_filter._method  # type: ignore
+            if method in combination_methods:
+                combined_values[method][name] = value
+                combined_filters[method] = field_filter
+                continue
+
+            queryset = field_filter.filter(queryset, value)
+            self._verify_that_queryset(queryset)
+
+        for key, values in combined_values.items():
+            queryset = combined_filters[key].filter(queryset, values)
+            self._verify_that_queryset(queryset)
+
+        return queryset
+
+    @staticmethod
+    def _verify_that_queryset(queryset: Any) -> None:
+        if not isinstance(queryset, models.QuerySet):
+            msg = f"Expected to receive a QuerySet from filters, but got {type(queryset).__name__} instead."
+            raise TypeError(msg)
