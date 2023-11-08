667cb5f68926ae4eabcf9a3a34f7557d6a963267
Matti Lamppu <matti.lamppu@vincit.fi>
2023-11-08 09:42:06 +0200

    Fix saving closed application round timeslots in admin panel

--- reservation_units/admin/forms/reservation_unit.py
+++ reservation_units/admin/forms/reservation_unit.py
@@ -138,8 +138,15 @@ class ReservationUnitAdminForm(forms.ModelForm):
 
 
 class TimeslotForm(forms.Form):
-    begin = forms.CharField(validators=[validate_string_time])
-    end = forms.CharField(validators=[validate_string_time])
+    begin = forms.CharField(validators=[validate_string_time], required=False)
+    end = forms.CharField(validators=[validate_string_time], required=False)
+
+
+def remove_empty_timeslots(timeslots: list[dict[str, str]]) -> None:
+    # Iterate in reverse order so that items can be deleted without affecting the loop
+    for i, timeslot in enumerate(reversed(timeslots)):
+        if timeslot == {"begin": "", "end": ""}:
+            del timeslots[i]
 
 
 class ApplicationRoundTimeSlotForm(forms.ModelForm):
@@ -150,6 +157,7 @@ class ApplicationRoundTimeSlotForm(forms.ModelForm):
     reservable_times = DynamicArrayField(
         subfield=NestedFormField(subform=TimeslotForm),
         help_text=_("Timeslots when the reservation unit is reservable"),
+        validators=[remove_empty_timeslots],
     )
 
     class Meta:
