a88b10400f369e9b6c26ee2e63fa6b5dc42a608c
Matti Lamppu <matti.lamppu@vincit.fi>
2023-11-13 14:49:03 +0200

    Remove celery worker from core container
    No need for this anymore, since worker pod can get celery tasks from
    redis.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "Starting Celery worker and uWSGI server..."
-exec celery \
-    -A tilavarauspalvelu \
-    worker \
-    --loglevel="${CELERY_LOG_LEVEL:-INFO}" \
-    --logfile=/broker/worker.log \
-    --detach \
-    & uwsgi --yaml /tvp/docker/uwsgi.yml
+echo "Starting uWSGI server..."
+uwsgi --yaml /tvp/docker/uwsgi.yml
