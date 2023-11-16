259102efce230150e8ac0bf742c2bd9e9fc40a42
Matti Lamppu <matti.lamppu@vincit.fi>
2023-11-16 09:15:36 +0200

    Move gdpr and helauth views and urls to the api package

---
+++ api/helauth/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from api.helauth.views import TVPLoginView, TVPLogoutCompleteView, TVPLogoutView
+
+urlpatterns = [
+    path("login/", TVPLoginView.as_view(), name="auth_login"),
+    path("logout/", TVPLogoutView.as_view(), name="auth_logout"),
+    path("logout/complete/", TVPLogoutCompleteView.as_view(), name="auth_logout_complete"),
+]
