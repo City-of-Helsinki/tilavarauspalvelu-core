5a9b0942d04eb3294cbfc1d9e5990f2e32954550
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2023-11-27 15:19:52 +0200

    fix: broken application because of workingmemo
    Fragment the queries because admin query reguires specific permissions.

--- packages/common/src/queries/application.ts
+++ packages/common/src/queries/application.ts
@@ -1,138 +1,159 @@
 import { gql } from "@apollo/client";
 
-// TODO make into fragments and combine them with other queries
-// a lot of things like name translations are only needed in the client side (not admin)
-// also a lot of the deep hierarchy is only needed in the client side
-export const APPLICATION_QUERY = gql`
-  query getApplications($pk: [Int]) {
-    applications(pk: $pk) {
-      edges {
-        node {
+// TODO fragment this futher
+// ex. admin side doesn't need nameEn / nameSv
+// a lot of the deep hierarchy is only needed in the client side
+// other uncommon fields ?
+const APPLICATION_FRAGMENT = gql`
+  fragment ApplicationCommon on ApplicationNode {
+    pk
+    status
+    applicantType
+    lastModifiedDate
+    applicant {
+      name
+      email
+      dateOfBirth
+    }
+    applicationRound {
+      pk
+      nameFi
+      serviceSector {
+        pk
+        nameFi
+      }
+      reservationUnits {
+        pk
+        nameFi
+        nameSv
+        nameEn
+        minPersons
+        maxPersons
+        images {
+          imageType
+          mediumUrl
+        }
+        unit {
           pk
-          status
-          applicantType
-          workingMemo
-          lastModifiedDate
-          applicant {
-            name
-            email
-            dateOfBirth
-          }
-          applicationRound {
-            pk
-            nameFi
-            serviceSector {
-              pk
-              nameFi
-            }
-            reservationUnits {
-              pk
-              nameFi
-              nameSv
-              nameEn
-              minPersons
-              maxPersons
-              images {
-                imageType
-                mediumUrl
-              }
-              unit {
-                pk
-                nameFi
-                nameSv
-                nameEn
-              }
-            }
-            applicationPeriodBegin
-            applicationPeriodEnd
-            reservationPeriodBegin
-            reservationPeriodEnd
-            status
-            applicationsCount
-            reservationUnitCount
-            statusTimestamp
-          }
-          contactPerson {
-            pk
-            firstName
-            lastName
-            email
-            phoneNumber
-          }
-          additionalInformation
-          organisation {
-            pk
-            name
-            identifier
-            organisationType
-            coreBusiness
-            address {
-              postCode
-              streetAddress
-              city
-            }
-          }
-          homeCity {
+          nameFi
+          nameSv
+          nameEn
+        }
+      }
+      applicationPeriodBegin
+      applicationPeriodEnd
+      reservationPeriodBegin
+      reservationPeriodEnd
+      status
+      applicationsCount
+      reservationUnitCount
+      statusTimestamp
+    }
+    contactPerson {
+      pk
+      firstName
+      lastName
+      email
+      phoneNumber
+    }
+    additionalInformation
+    organisation {
+      pk
+      name
+      identifier
+      organisationType
+      coreBusiness
+      address {
+        postCode
+        streetAddress
+        city
+      }
+    }
+    homeCity {
+      pk
+      nameFi
+      nameEn
+      nameSv
+    }
+    billingAddress {
+      pk
+      postCode
+      streetAddress
+      city
+    }
+    applicationEvents {
+      pk
+      name
+      begin
+      end
+      status
+      eventsPerWeek
+      minDuration
+      maxDuration
+      numPersons
+      purpose {
+        pk
+        nameFi
+        nameSv
+        nameEn
+      }
+      ageGroup {
+        pk
+        minimum
+        maximum
+      }
+      abilityGroup {
+        pk
+        name
+      }
+      applicationEventSchedules {
+        pk
+        begin
+        end
+        day
+        priority
+      }
+      eventReservationUnits {
+        pk
+        preferredOrder
+        reservationUnit {
+          pk
+          nameFi
+          nameEn
+          nameSv
+          unit {
             pk
             nameFi
             nameEn
             nameSv
           }
-          billingAddress {
-            pk
-            postCode
-            streetAddress
-            city
-          }
-          applicationEvents {
-            pk
-            name
-            begin
-            end
-            status
-            eventsPerWeek
-            minDuration
-            maxDuration
-            numPersons
-            purpose {
-              pk
-              nameFi
-              nameSv
-              nameEn
-            }
-            ageGroup {
-              pk
-              minimum
-              maximum
-            }
-            abilityGroup {
-              pk
-              name
-            }
-            applicationEventSchedules {
-              pk
-              begin
-              end
-              day
-              priority
-            }
-            eventReservationUnits {
-              pk
-              preferredOrder
-              reservationUnit {
-                pk
-                nameFi
-                nameEn
-                nameSv
-                unit {
-                  pk
-                  nameFi
-                  nameEn
-                  nameSv
-                }
-              }
-            }
-          }
+        }
+      }
+    }
+  }
+`;
+
+export const APPLICATION_ADMIN_QUERY = gql`
+  ${APPLICATION_FRAGMENT}
+  query getApplications($pk: [Int]) {
+    applications(pk: $pk) {
+      edges {
+        node {
+          ...ApplicationCommon
+          workingMemo
+        }
+      }
+    }
+  }
+`;
+
+export const APPLICATION_QUERY = gql`
+  ${APPLICATION_FRAGMENT}
+  query getApplications($pk: [Int]) {
+    applications(pk: $pk) {
+      edges {
+        node {
+          ...ApplicationCommon
         }
       }
     }
