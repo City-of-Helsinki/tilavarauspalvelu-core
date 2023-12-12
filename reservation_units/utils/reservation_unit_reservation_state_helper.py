6297cedeb82034111a28a03c536b7e20e2a51c23
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2023-12-12 09:11:35 +0200

    Refactor reservation state helper and its tests

--- reservation_units/utils/reservation_unit_reservation_state_helper.py
+++ reservation_units/utils/reservation_unit_reservation_state_helper.py
@@ -18,13 +18,22 @@ class QueryState:
 class ReservationUnitReservationStateHelper:
     @classmethod
     def __is_scheduled_reservation(cls, reservation_unit: ReservationUnit) -> bool:
-        """Returns True if reservation unit has reservation_begins set in to the future with no ending date."""
+        """
+        Support following cases in regard to reservation_begins and reservation_ends:
+
+                          |---------
+        ---------|        |---------
+        -------------|    |---------
+                     ↑ now
+        """
         now = datetime.datetime.now(tz=get_default_timezone())
 
-        return (reservation_unit.reservation_begins and now < reservation_unit.reservation_begins) and (
-            reservation_unit.reservation_ends is None
-            or reservation_unit.reservation_ends <= now
-            or (now < reservation_unit.reservation_ends < reservation_unit.reservation_begins)
+        return (
+            # Reservations begin in the future
+            reservation_unit.reservation_begins is not None and reservation_unit.reservation_begins > now
+        ) and (
+            # Reservations don't end, or have ended somewhere in the past
+            reservation_unit.reservation_ends is None or reservation_unit.reservation_ends <= now
         )
 
     @classmethod
@@ -32,29 +41,24 @@ class ReservationUnitReservationStateHelper:
         now = datetime.datetime.now(tz=get_default_timezone())
 
         return QueryState(
-            filters=Q(
-                Q(
-                    reservation_begins__isnull=False,
-                    reservation_begins__gt=now,
-                )
-                & Q(Q(reservation_ends__isnull=True) | Q(reservation_ends__lt=now))
-            ),
+            filters=Q(reservation_begins__gt=now) & (Q(reservation_ends__isnull=True) | Q(reservation_ends__lte=now)),
         )
 
     @classmethod
     def __is_scheduled_period(cls, reservation_unit: ReservationUnit) -> bool:
         """
-        Returns True if reservation unit has both reservation_begins
-        and reservations_ends dates set to the future.
+        Support following cases in regard to reservation_begins and reservation_ends:
+
+                          |---------|
+                    ↑ now
         """
         now = datetime.datetime.now(tz=get_default_timezone())
 
+        # Reservation begins and ends in the future, and begins before it ends
         return (
-            reservation_unit.reservation_begins
-            and reservation_unit.reservation_begins > now
-            and reservation_unit.reservation_ends
-            and reservation_unit.reservation_ends > now
-            and reservation_unit.reservation_begins < reservation_unit.reservation_ends
+            reservation_unit.reservation_begins is not None
+            and reservation_unit.reservation_ends is not None
+            and now < reservation_unit.reservation_begins < reservation_unit.reservation_ends
         )
 
     @classmethod
@@ -63,161 +67,194 @@ class ReservationUnitReservationStateHelper:
 
         return QueryState(
             filters=Q(
-                reservation_begins__isnull=False,
                 reservation_begins__gt=now,
-                reservation_ends__isnull=False,
                 reservation_ends__gt=now,
                 reservation_begins__lt=F("reservation_ends"),
             ),
         )
 
     @classmethod
-    def __is_reservable(cls, reservation_unit: ReservationUnit) -> bool:
-        now = datetime.datetime.now(tz=get_default_timezone())
+    def __active_pricing_type_alias(cls) -> dict[str, Subquery]:
+        return {
+            "active_pricing_type": Subquery(
+                queryset=(
+                    ReservationUnitPricing.objects.filter(
+                        reservation_unit=OuterRef("pk"),
+                        status=PricingStatus.PRICING_STATUS_ACTIVE,
+                    ).values("pricing_type")[:1]
+                ),
+            ),
+        }
 
+    @classmethod
+    def __has_valid_pricing(cls, reservation_unit: ReservationUnit) -> bool:
         active_price = ReservationUnitPricingHelper.get_active_price(reservation_unit)
 
         return (
-            (
-                reservation_unit.reservation_ends is None
-                and (
-                    reservation_unit.reservation_begins
-                    and reservation_unit.reservation_begins <= now
-                    or reservation_unit.reservation_begins is None
-                )
+            # There is an active pricing
+            active_price is not None
+            and (
+                # Pricing is paid and there is a payment product, or pricing is free
+                (active_price.pricing_type == PricingType.PAID and reservation_unit.payment_product is not None)
+                or active_price.pricing_type == PricingType.FREE
             )
+        )
+
+    @classmethod
+    def __has_valid_pricing_query(cls) -> Q:
+        return Q(active_pricing_type=PricingType.PAID, payment_product__isnull=False) | Q(
+            active_pricing_type=PricingType.FREE
+        )
+
+    @classmethod
+    def __is_reservable(cls, reservation_unit: ReservationUnit) -> bool:
+        """
+        Support following cases in regard to reservation_begins and reservation_ends:
+
+        -------------------------------
+                  |--------------------
+                        |--------------
+        ------|   |--------------------
+        ------|         |--------------
+                        ↑ now
+        """
+        now = datetime.datetime.now(tz=get_default_timezone())
+
+        return cls.__is_reservable_period(now, reservation_unit) and cls.__has_valid_pricing(reservation_unit)
+
+    @classmethod
+    def __is_reservable_period(cls, now: datetime.datetime, reservation_unit: ReservationUnit) -> bool:
+        return (
+            # Reservations don't begin or end
+            (reservation_unit.reservation_ends is None and reservation_unit.reservation_begins is None)
+            # Reservations don't end, but begin somewhere in the past (or right now)
             or (
-                reservation_unit.reservation_ends <= now
-                and reservation_unit.reservation_begins
-                and now >= reservation_unit.reservation_begins > reservation_unit.reservation_ends
+                reservation_unit.reservation_ends is None
+                and reservation_unit.reservation_begins is not None
+                and reservation_unit.reservation_begins <= now
             )
-        ) and (
-            (
-                active_price is not None
-                and active_price.pricing_type == PricingType.PAID
-                and reservation_unit.payment_product is not None
+            # Reservations begin somewhere in the past (or right now),
+            # and end in the past, but begin before they end
+            or (
+                reservation_unit.reservation_ends is not None
+                and reservation_unit.reservation_begins is not None
+                and reservation_unit.reservation_ends < reservation_unit.reservation_begins <= now
             )
-            or (active_price is not None and active_price.pricing_type == PricingType.FREE)
         )
 
     @classmethod
     def __get_is_reservable_query(cls) -> QueryState:
+        return QueryState(
+            aliases=cls.__active_pricing_type_alias(),
+            filters=(cls.__is_reservable_period_query() & cls.__has_valid_pricing_query()),
+        )
+
+    @classmethod
+    def __is_reservable_period_query(cls) -> Q:
         now = datetime.datetime.now(tz=get_default_timezone())
 
-        return QueryState(
-            aliases={
-                "active_pricing_type": Subquery(
-                    queryset=(
-                        ReservationUnitPricing.objects.filter(
-                            reservation_unit=OuterRef("pk"),
-                            status=PricingStatus.PRICING_STATUS_ACTIVE,
-                        ).values("pricing_type")[:1]
-                    ),
-                ),
-            },
-            filters=(
-                (
-                    Q(reservation_ends__isnull=True)
-                    & (Q(reservation_begins__lte=now) | Q(reservation_begins__isnull=True))
-                )
-                | Q(
-                    reservation_ends__lte=now,
-                    reservation_begins__lte=now,
-                    reservation_begins__gt=F("reservation_ends"),
-                )
-                & (
-                    Q(
-                        active_pricing_type=PricingType.PAID,
-                        payment_product__isnull=False,
-                    )
-                    | Q(
-                        active_pricing_type=PricingType.FREE,
-                    )
-                )
-            ),
+        return (
+            Q(reservation_begins__isnull=True, reservation_ends__isnull=True)
+            | Q(reservation_ends__isnull=True, reservation_begins__lte=now)
+            | Q(reservation_begins__lte=now, reservation_begins__gt=F("reservation_ends"))
         )
 
     @classmethod
     def __is_scheduled_closing(cls, reservation_unit: ReservationUnit) -> bool:
         """
-        Returns True if reservation unit has reservation_begins set in the past
-        and reservation_ends in the future.
+        Support following cases in regard to reservation_begins and reservation_ends:
+
+        ----------------------|
+                 |------------------|
+                       |------------|
+        ----------------------|     |------------
+                       ↑ now
+        """
+        now = datetime.datetime.now(tz=get_default_timezone())
+
+        return cls.__is_scheduled_closing_period(now, reservation_unit) and cls.__has_valid_pricing(reservation_unit)
+
+    @classmethod
+    def __is_scheduled_closing_period(cls, now: datetime.datetime, reservation_unit: ReservationUnit) -> bool:
+        return (
+            # Reservations end in the future
+            reservation_unit.reservation_ends is not None
+            and reservation_unit.reservation_ends > now
+            and (
+                # Reservations don't begin
+                reservation_unit.reservation_begins is None
+                # Reservations begin in the past
+                or (reservation_unit.reservation_begins is not None and reservation_unit.reservation_begins <= now)
+                # Reservations being in the future after they end
+                or (
+                    reservation_unit.reservation_begins is not None
+                    and now < reservation_unit.reservation_ends < reservation_unit.reservation_begins
+                )
+            )
+        )
+
+    @classmethod
+    def __get_is_scheduled_closing_query(cls) -> QueryState:
+        return QueryState(
+            aliases=cls.__active_pricing_type_alias(),
+            filters=(cls.__is_scheduled_closing_period_query() & cls.__has_valid_pricing_query()),
+        )
+
+    @classmethod
+    def __is_scheduled_closing_period_query(cls) -> Q:
+        now = datetime.datetime.now(tz=get_default_timezone())
+        return Q(reservation_ends__gt=now) & (
+            Q(reservation_begins__isnull=True)
+            | Q(reservation_begins__lte=now)
+            | Q(reservation_ends__gt=now, reservation_ends__lt=F("reservation_begins"))
+        )
+
+    @classmethod
+    def __is_reservation_closed(cls, reservation_unit: ReservationUnit) -> bool:
+        """
+        Support following cases in regard to reservation_begins and reservation_ends:
+
+
+        -------------|
+        ----------------|
+                |----|
+                |-------|
+                |
+                        |
+                                |
+                        ↑ now
         """
         now = datetime.datetime.now(tz=get_default_timezone())
 
         active_price = ReservationUnitPricingHelper.get_active_price(reservation_unit)
 
         return (
-            reservation_unit.reservation_ends
-            and reservation_unit.reservation_ends > now
-            and (
+            (
+                # Reservations don't begin, but end in the past (or right now)
                 reservation_unit.reservation_begins is None
-                or (reservation_unit.reservation_begins and reservation_unit.reservation_begins <= now)
-            )
-            and (
-                (
-                    active_price is not None
-                    and active_price.pricing_type == PricingType.PAID
-                    and reservation_unit.payment_product is not None
-                )
-                or (active_price is not None and active_price.pricing_type == PricingType.FREE)
-            )
-        )
-
-    @classmethod
-    def __get_is_scheduled_closing_query(cls) -> QueryState:
-        now = datetime.datetime.now(tz=get_default_timezone())
-
-        return QueryState(
-            aliases={
-                "active_pricing_type": Subquery(
-                    queryset=(
-                        ReservationUnitPricing.objects.filter(
-                            reservation_unit=OuterRef("pk"),
-                            status=PricingStatus.PRICING_STATUS_ACTIVE,
-                        ).values("pricing_type")[:1]
-                    ),
-                ),
-            },
-            filters=(
-                Q(reservation_ends__gt=now)
-                & (Q(reservation_begins__isnull=True) | Q(reservation_begins__lte=now))
-                & (
-                    Q(
-                        active_pricing_type=PricingType.PAID,
-                        payment_product__isnull=False,
-                    )
-                    | Q(
-                        active_pricing_type=PricingType.FREE,
-                    )
-                )
-            ),
-        )
-
-    @classmethod
-    def __is_reservation_closed(cls, reservation_unit: ReservationUnit) -> bool:
-        """
-        Returns True if reservation begins is in the past or none and if the reservation ends is in the past.
-        Meaning the reservation is not scheduled to open anymore.
-        """
-        now = datetime.datetime.now(tz=get_default_timezone())
-
-        return (
-            reservation_unit.reservation_ends
-            and reservation_unit.reservation_ends <= now
-            and (
-                reservation_unit.reservation_begins is None
-                or (
-                    reservation_unit.reservation_begins <= now
-                    and reservation_unit.reservation_begins <= reservation_unit.reservation_ends
-                )
+                and reservation_unit.reservation_ends is not None
+                and reservation_unit.reservation_ends <= now
             )
             or (
-                reservation_unit.reservation_begins
-                and reservation_unit.reservation_ends
-                and reservation_unit.reservation_ends > now
-                and reservation_unit.reservation_begins > now
-                and reservation_unit.reservation_begins == reservation_unit.reservation_ends
+                # Reservations begin in the past, and end in the past (or right now),
+                # but begin before they end (or at the same time)
+                reservation_unit.reservation_begins is not None
+                and reservation_unit.reservation_ends is not None
+                and reservation_unit.reservation_begins <= reservation_unit.reservation_ends <= now
+            )
+            # Reservation period would be zero-length
+            or reservation_unit.reservation_begins == reservation_unit.reservation_ends
+            or (
+                # Reservation period would be for reservable or scheduled closing based on reservable period,
+                # but there is no active pricing or pricing is paid and there is no payment product
+                (
+                    cls.__is_reservable_period(now, reservation_unit)
+                    or cls.__is_scheduled_closing_period(now, reservation_unit)
+                )
+                and (
+                    active_price is None
+                    or (active_price.pricing_type == PricingType.PAID and reservation_unit.payment_product is None)
+                )
             )
         )
 
@@ -226,26 +263,23 @@ class ReservationUnitReservationStateHelper:
         now = datetime.datetime.now(tz=get_default_timezone())
 
         return QueryState(
+            aliases=cls.__active_pricing_type_alias(),
             filters=(
-                Q(
-                    Q(
-                        Q(reservation_ends__lte=now)
-                        & (
-                            Q(Q(reservation_begins__lte=now) & Q(reservation_begins__lte=F("reservation_ends")))
-                            | Q(reservation_begins__isnull=True)
-                        )
-                    )
-                    | Q(
-                        reservation_ends__gt=now,
-                        reservation_begins__gt=now,
-                        reservation_begins=F("reservation_ends"),
+                Q(reservation_ends__lte=now, reservation_ends__gte=F("reservation_begins"))
+                | Q(reservation_begins__isnull=True, reservation_ends__lte=now)
+                | Q(reservation_ends=F("reservation_begins"))
+                | (
+                    (cls.__is_reservable_period_query() | cls.__is_scheduled_closing_period_query())
+                    & (
+                        Q(active_pricing_type=None)
+                        | (Q(active_pricing_type=PricingType.PAID) & Q(payment_product__isnull=True))
                     )
                 )
             ),
         )
 
     @classmethod
-    def get_state(cls, reservation_unit: ReservationUnit) -> ReservationState | None:
+    def get_state(cls, reservation_unit: ReservationUnit) -> ReservationState:
         if cls.__is_scheduled_reservation(reservation_unit):
             return ReservationState.SCHEDULED_RESERVATION
         if cls.__is_scheduled_period(reservation_unit):
@@ -256,11 +290,11 @@ class ReservationUnitReservationStateHelper:
             return ReservationState.SCHEDULED_CLOSING
         if cls.__is_reservation_closed(reservation_unit):
             return ReservationState.RESERVATION_CLOSED
-        return None
+
+        raise ValueError(f"Unknown reservation state for reservation unit: {reservation_unit.pk}")
 
     @classmethod
     def get_state_query(cls, state: str) -> QueryState:
-        """Get matching filter query based on the Reservation Unit state (as string)"""
         state = ReservationState(state)
         if state == ReservationState.SCHEDULED_RESERVATION:
             return cls.__get_is_scheduled_reservation_query()
@@ -273,4 +307,4 @@ class ReservationUnitReservationStateHelper:
         elif state == ReservationState.RESERVATION_CLOSED:
             return cls.__get_is_reservation_closed_query()
 
-        raise ValueError("Unknown ReservationState")
+        raise ValueError(f"Unknown reservation state: {state}")
