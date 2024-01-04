bdbb7dc91e196d6264cf659673dda85866c82a60
Matti Lamppu <matti.lamppu@vincit.com>
2024-01-04 13:31:14 +0200

    Add unit count and better selector for unit groups and service sectors

--- spaces/admin.py
+++ spaces/admin.py
@@ -1,9 +1,14 @@
+from typing import Any
+
 from adminsortable2.admin import SortableAdminMixin
 from django.conf import settings
 from django.contrib import admin, messages
 from django.core.management import call_command
+from django.forms import IntegerField, ModelForm
+from django.utils.translation import gettext_lazy as _
 from mptt.admin import MPTTModelAdmin
 
+from common.fields.forms import ModelMultipleChoiceFilteredField
 from spaces.models import Building, Location, RealEstate, ServiceSector, Space, Unit, UnitGroup
 
 
@@ -54,9 +59,43 @@ class UnitAdmin(SortableAdminMixin, admin.ModelAdmin):
             self.message_user(request, output, level=messages.SUCCESS)
 
 
+class UnitGroupForm(ModelForm):
+    units = ModelMultipleChoiceFilteredField(
+        Unit.objects.all(),
+        help_text=_("Selected units for this unit group."),
+    )
+
+    selected_units = IntegerField(
+        required=False,
+        disabled=True,
+        help_text=_("Previously selected number of units for this unit group."),
+    )
+
+    class Meta:
+        model = UnitGroup
+        fields = [
+            "name_fi",
+            "name_en",
+            "name_sv",
+            "units",
+        ]
+        help_texts = {
+            "name_fi": _("Name of the unit group in Finnish."),
+            "name_en": _("Name of the unit group in English."),
+            "name_sv": _("Name of the unit group in Swedish."),
+        }
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        instance: UnitGroup | None = kwargs.get("instance", None)
+        if instance is not None:
+            self.fields["selected_units"].initial = instance.units.count()
+
+
 @admin.register(UnitGroup)
 class UnitGroupAdmin(admin.ModelAdmin):
     model = UnitGroup
+    form = UnitGroupForm
     search_fields = ["name"]
     list_display = ("__str__", "number_of_units")
 
@@ -65,9 +104,43 @@ class UnitGroupAdmin(admin.ModelAdmin):
         return obj.units.count()
 
 
+class ServiceSectorForm(ModelForm):
+    units = ModelMultipleChoiceFilteredField(
+        Unit.objects.all(),
+        help_text=_("Selected units for this service sector."),
+    )
+
+    selected_units = IntegerField(
+        required=False,
+        disabled=True,
+        help_text=_("Previously selected number of units for this service sector."),
+    )
+
+    class Meta:
+        model = UnitGroup
+        fields = [
+            "name_fi",
+            "name_en",
+            "name_sv",
+            "units",
+        ]
+        help_texts = {
+            "name_fi": _("Name of the service sector in Finnish."),
+            "name_en": _("Name of the service sector in English."),
+            "name_sv": _("Name of the service sector in Swedish."),
+        }
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        instance: ServiceSector | None = kwargs.get("instance", None)
+        if instance is not None:
+            self.fields["selected_units"].initial = instance.units.count()
+
+
 @admin.register(ServiceSector)
 class ServiceSectorAdmin(admin.ModelAdmin):
     model = ServiceSector
+    form = ServiceSectorForm
     list_display = ("__str__", "number_of_units")
 
     @staticmethod
