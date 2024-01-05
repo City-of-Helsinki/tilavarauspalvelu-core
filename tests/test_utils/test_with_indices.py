0890b748e8c5f55ac3444c9535fbd0cb7e2ae513
Matti Lamppu <matti.lamppu@vincit.com>
2024-01-05 23:03:36 +0200

    Add `with_indices` generator

---
+++ tests/test_utils/test_with_indices.py
@@ -0,0 +1,14 @@
+from common.utils import with_indices
+
+
+def test_with_indices():
+    test_list = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
+
+    for i, item in (gen := with_indices(test_list)):
+        if item % 2 == 0:
+            del test_list[i]
+            gen.item_deleted = True
+        if item % 3 == 0:
+            test_list.append(10)
+
+    assert test_list == [1, 1, 3, 3, 5, 5, 7, 7, 9, 9]
