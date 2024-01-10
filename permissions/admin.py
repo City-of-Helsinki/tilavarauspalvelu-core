bce88963f5ec7a8f8a415bbfc376790ddf3971a7
Matti Lamppu <matti.lamppu@vincit.fi>
2024-01-10 10:53:55 +0200

    Add filters and new columns for unit roles

--- permissions/admin.py
+++ permissions/admin.py
@@ -46,7 +46,8 @@ class GeneralRoleChoiceAdmin(admin.ModelAdmin):
 @admin.register(UnitRole)
 class UnitRoleAdmin(admin.ModelAdmin):
     model = UnitRole
-    list_filter = ["role"]
+    list_display = ["__str__", "unit_names", "unit_group_names", "service_sector_names"]
+    list_filter = ["role", "unit", "unit_group"]
     search_fields = [
         "user__username",
         "user__email",
@@ -54,6 +55,42 @@ class UnitRoleAdmin(admin.ModelAdmin):
         "user__last_name",
     ]
     autocomplete_fields = ["user", "unit", "unit_group"]
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .prefetch_related(
+                "unit",
+                "unit_group",
+                "unit__service_sectors",
+                "unit_group__units__service_sectors",
+            )
+        )
+
+    @staticmethod
+    def unit_names(obj: UnitRole) -> str:
+        return ", ".join([unit.name for unit in obj.unit.all()])
+
+    @staticmethod
+    def unit_group_names(obj: UnitRole) -> str:
+        return ", ".join([unit.name for unit in obj.unit_group.all()])
+
+    @staticmethod
+    def service_sector_names(obj: UnitRole) -> str:
+        return ", ".join(
+            sorted(
+                set(
+                    [service_sector.name for unit in obj.unit.all() for service_sector in unit.service_sectors.all()]
+                    + [
+                        service_sector.name
+                        for unit in obj.unit_group.all()
+                        for unit in unit.units.all()
+                        for service_sector in unit.service_sectors.all()
+                    ]
+                )
+            )
+        )
 
 
 @admin.register(GeneralRole)
