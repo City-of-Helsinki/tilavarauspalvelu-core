c992fbd80d007e75dd56d479500eb87afdc693f6
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2024-01-13 08:12:52 +0200

    fix: login buttons flickering
    We don't want to cache the current user, but use the previous result
    while the request is in progress.

--- apps/ui/hooks/user.tsx
+++ apps/ui/hooks/user.tsx
@@ -1,18 +1,18 @@
 import { Query, UserType } from "common/types/gql-types";
 import { ApolloError, useQuery } from "@apollo/client";
-import { CURRENT_USER } from "../modules/queries/user";
+import { CURRENT_USER } from "@/modules/queries/user";
 
 export const useCurrentUser = (): {
   currentUser?: UserType;
   error: ApolloError | undefined;
   loading: boolean;
 } => {
-  const { data, error, loading } = useQuery<Query>(CURRENT_USER, {
-    fetchPolicy: "no-cache",
+  const { data, error, previousData, loading } = useQuery<Query>(CURRENT_USER, {
+    fetchPolicy: "network-only",
   });
 
   return {
-    currentUser: data?.currentUser ?? undefined,
+    currentUser: data?.currentUser ?? previousData?.currentUser ?? undefined,
     error,
     loading,
   };
