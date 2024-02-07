1984ee09bb7762de2f4685bfbb1a4906661bba64
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-02-08 00:45:33 +0200

    Convert StateHelper staticmethods used as classmethods to classmethods

--- reservation_units/tests/test_reservation_unit_state_helper.py
+++ reservation_units/tests/test_reservation_unit_state_helper.py
@@ -4,9 +4,7 @@ from django.test.testcases import TestCase
 
 from reservation_units.enums import ReservationUnitState
 from reservation_units.models import ReservationUnit
-from reservation_units.utils.reservation_unit_state_helper import (
-    ReservationUnitStateHelper as Helper,
-)
+from reservation_units.utils.reservation_unit_state_helper import ReservationUnitStateHelper
 from tests.factories import ReservationUnitFactory
 
 
@@ -19,24 +17,24 @@ class ReservationUnitStateHelperTestCase(TestCase):
 
     def test_get_state_with_archived(self):
         self.reservation_unit.is_archived = True
-        assert Helper.get_state(self.reservation_unit) == ReservationUnitState.ARCHIVED
+        assert ReservationUnitStateHelper.get_state(self.reservation_unit) == ReservationUnitState.ARCHIVED
 
     def test_get_state_with_archived_and_draft(self):
         self.reservation_unit.is_archived = True
         self.reservation_unit.is_draft = True
-        assert Helper.get_state(self.reservation_unit) == ReservationUnitState.ARCHIVED
+        assert ReservationUnitStateHelper.get_state(self.reservation_unit) == ReservationUnitState.ARCHIVED
 
     def test_get_state_with_draft(self):
         self.reservation_unit.is_archived = False
         self.reservation_unit.is_draft = True
-        assert Helper.get_state(self.reservation_unit) == ReservationUnitState.DRAFT
+        assert ReservationUnitStateHelper.get_state(self.reservation_unit) == ReservationUnitState.DRAFT
 
     def test_get_state_with_scheduled_publishing_and_publish_begins(self):
         one_hour = datetime.timedelta(hours=1)
         self.reservation_unit.is_archived = False
         self.reservation_unit.is_draft = False
         self.reservation_unit.publish_begins = self.now + one_hour
-        assert Helper.get_state(self.reservation_unit) == ReservationUnitState.SCHEDULED_PUBLISHING
+        assert ReservationUnitStateHelper.get_state(self.reservation_unit) == ReservationUnitState.SCHEDULED_PUBLISHING
 
     def test_get_state_with_scheduled_publishing_when_ends_in_past_begins_future(self):
         one_hour = datetime.timedelta(hours=1)
@@ -44,37 +42,37 @@ class ReservationUnitStateHelperTestCase(TestCase):
         self.reservation_unit.is_draft = False
         self.reservation_unit.publish_begins = self.now + one_hour
         self.reservation_unit.publish_ends = self.now - one_hour
-        assert Helper.get_state(self.reservation_unit) == ReservationUnitState.SCHEDULED_PUBLISHING
+        assert ReservationUnitStateHelper.get_state(self.reservation_unit) == ReservationUnitState.SCHEDULED_PUBLISHING
 
     def test_get_state_with_published(self):
         self.reservation_unit.is_archived = False
         self.reservation_unit.is_draft = False
-        assert Helper.get_state(self.reservation_unit) == ReservationUnitState.PUBLISHED
+        assert ReservationUnitStateHelper.get_state(self.reservation_unit) == ReservationUnitState.PUBLISHED
 
     def test_get_state_with_hidden(self):
         self.reservation_unit.is_archived = False
         self.reservation_unit.is_draft = False
         self.reservation_unit.publish_begins = self.now - datetime.timedelta(hours=2)
         self.reservation_unit.publish_ends = self.now - datetime.timedelta(hours=1)
-        assert Helper.get_state(self.reservation_unit) == ReservationUnitState.HIDDEN
+        assert ReservationUnitStateHelper.get_state(self.reservation_unit) == ReservationUnitState.HIDDEN
 
     def test_get_state_with_scheduled_hiding(self):
         self.reservation_unit.is_archived = False
         self.reservation_unit.is_draft = False
         self.reservation_unit.publish_begins = self.now - datetime.timedelta(hours=1)
         self.reservation_unit.publish_ends = self.now + datetime.timedelta(hours=1)
-        assert Helper.get_state(self.reservation_unit) == ReservationUnitState.SCHEDULED_HIDING
+        assert ReservationUnitStateHelper.get_state(self.reservation_unit) == ReservationUnitState.SCHEDULED_HIDING
 
     def test_get_state_with_scheduled_period(self):
         self.reservation_unit.is_archived = False
         self.reservation_unit.is_draft = False
         self.reservation_unit.publish_begins = self.now + datetime.timedelta(hours=1)
         self.reservation_unit.publish_ends = self.now + datetime.timedelta(days=1)
-        assert Helper.get_state(self.reservation_unit) == ReservationUnitState.SCHEDULED_PERIOD
+        assert ReservationUnitStateHelper.get_state(self.reservation_unit) == ReservationUnitState.SCHEDULED_PERIOD
 
     def test_state_is_hidden_when_publish_end_and_begins_in_the_past_and_the_same(self):
         self.reservation_unit.is_archived = False
         self.reservation_unit.is_draft = False
         self.reservation_unit.publish_begins = self.now - datetime.timedelta(hours=1)
         self.reservation_unit.publish_ends = self.reservation_unit.publish_begins
-        assert Helper.get_state(self.reservation_unit) == ReservationUnitState.HIDDEN
+        assert ReservationUnitStateHelper.get_state(self.reservation_unit) == ReservationUnitState.HIDDEN
