0cda7fba9f6fbdc84e9e5d3c2e60b95525267795
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2024-02-09 11:32:42 +0200

    refactor: move common css variables
    First step towards unifying the CSS variables should between the apps.

--- apps/admin-ui/src/pages/_app.tsx
+++ apps/admin-ui/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { AppProps } from "next/app";
 import "hds-core/lib/base.css";
+import "common/styles/variables.css";
 import "../index.scss";
 import { ApolloProvider } from "@apollo/client";
 import { createClient } from "@/common/apolloClient";
--- apps/ui/pages/_app.tsx
+++ apps/ui/pages/_app.tsx
@@ -14,6 +14,7 @@ import { DataContextProvider } from "../context/DataContext";
 import { createApolloClient } from "../modules/apolloClient";
 import { TrackingWrapper } from "../modules/tracking";
 import nextI18NextConfig from "../next-i18next.config";
+import "common/styles/variables.css";
 import "../styles/global.scss";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
