523c8656ae9b34fe4b1f5aa9f6cb50b69f974ad7
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-02-09 18:22:16 +0200

    Enable all Ruff-specific rules

--- elastic_django/reservation_units/tasks.py
+++ elastic_django/reservation_units/tasks.py
@@ -6,17 +6,17 @@ from tilavarauspalvelu.celery import app
 
 @app.task(name="Update ReservationUnit Elastic index")
 def update_reservation_unit_elastic_index() -> None:
-    index = list(settings.SEARCH_SETTINGS["indexes"].keys())[0]
+    index = next(iter(settings.SEARCH_SETTINGS["indexes"].keys()))
     update_index(index)
 
 
 @app.task(name="Create ReservationUnit Elastic index")
 def create_reservation_unit_elastic_index() -> None:
-    index = list(settings.SEARCH_SETTINGS["indexes"].keys())[0]
+    index = next(iter(settings.SEARCH_SETTINGS["indexes"].keys()))
     create_index(index)
 
 
 @app.task(name="Delete ReservationUnit Elastic index")
 def delete_reservation_unit_elastic_index() -> None:
-    index = list(settings.SEARCH_SETTINGS["indexes"].keys())[0]
+    index = next(iter(settings.SEARCH_SETTINGS["indexes"].keys()))
     delete_index(index)
