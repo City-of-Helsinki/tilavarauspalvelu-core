456052b14b93059bf7e7d8ae6b839ed23bd4b93b
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2024-02-19 09:20:24 +0200

    fix: use cache for user query

--- apps/ui/hooks/user.tsx
+++ apps/ui/hooks/user.tsx
@@ -2,13 +2,16 @@ import { Query, UserType } from "common/types/gql-types";
 import { ApolloError, useQuery } from "@apollo/client";
 import { CURRENT_USER } from "@/modules/queries/user";
 
+// TODO this should be replaced with SSR current user in most cases.
+// Causes a flash of unauthenticated content on page load.
 export const useCurrentUser = (): {
   currentUser?: UserType;
   error: ApolloError | undefined;
   loading: boolean;
 } => {
   const { data, error, previousData, loading } = useQuery<Query>(CURRENT_USER, {
-    fetchPolicy: "network-only",
+    fetchPolicy: "cache-and-network",
+    nextFetchPolicy: "cache-first",
   });
 
   return {
