8781da97e15de228784f6821203962a329d3dc4a
Matti Lamppu <matti.lamppu@vincit.fi>
2024-02-23 11:28:31 +0200

    Add translations

--- applications/admin/forms/suitable_time_range.py
+++ applications/admin/forms/suitable_time_range.py
@@ -37,6 +37,6 @@ class SuitableTimeRangeInlineAdminForm(forms.ModelForm):
         help_texts = {
             "priority": _("Priority of the time range."),
             "day_of_the_week": _("Requested day of the week."),
-            "begin_time": _("Requested start time of event."),
-            "end_time": _("Requested end time of event."),
+            "begin_time": _("Requested start time for allocations."),
+            "end_time": _("Requested end time for allocations."),
         }
