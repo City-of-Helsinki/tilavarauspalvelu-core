3bc5d804ec323b80b74623cf7111cb3c3734cd58
Matti Lamppu <matti.lamppu@vincit.fi>
2024-02-26 08:40:19 +0200

    Remove buffer modification from staff reservation modify endpoint

--- tests/test_graphql_api/test_reservation/test_update_staff.py
+++ tests/test_graphql_api/test_reservation/test_update_staff.py
@@ -15,7 +15,7 @@ from tests.factories import (
     SpaceFactory,
     UserFactory,
 )
-from tests.test_graphql_api.test_reservation.helpers import ADJUST_STAFF_MUTATION, UPDATE_STAFF_MUTATION
+from tests.test_graphql_api.test_reservation.helpers import ADJUST_STAFF_MUTATION
 
 DEFAULT_TIMEZONE = get_default_timezone()
 
@@ -24,44 +24,6 @@ pytestmark = [
     pytest.mark.django_db,
     pytest.mark.usefixtures("_disable_reservation_email_sending"),
 ]
-
-
-@freezegun.freeze_time("2021-01-01")
-def test_modify_staff_reservation__reservation_block_whole_day__ignore_given_buffers(graphql):
-    reservation_unit = ReservationUnitFactory.create(
-        origin_hauki_resource=OriginHaukiResourceFactory(id=999),
-        reservation_block_whole_day=True,
-        spaces=[SpaceFactory.create()],
-    )
-    ReservableTimeSpanFactory.create(
-        resource=reservation_unit.origin_hauki_resource,
-        start_datetime=datetime(2023, 1, 1, 6, tzinfo=DEFAULT_TIMEZONE),
-        end_datetime=datetime(2023, 1, 1, 22, tzinfo=DEFAULT_TIMEZONE),
-    )
-    reservation = ReservationFactory.create_for_reservation_unit(
-        name="foo",
-        reservation_unit=reservation_unit,
-        begin=datetime(2023, 1, 1, 12, tzinfo=DEFAULT_TIMEZONE),
-        end=datetime(2023, 1, 1, 13, tzinfo=DEFAULT_TIMEZONE),
-        state=ReservationStateChoice.CONFIRMED.value,
-    )
-
-    user = UserFactory.create_with_unit_permissions(reservation_unit.unit, perms=["can_create_staff_reservations"])
-    graphql.force_login(user)
-
-    input_data = {
-        "pk": reservation.pk,
-        "bufferTimeBefore": 3600,
-        "bufferTimeAfter": 3600,
-    }
-
-    response = graphql(UPDATE_STAFF_MUTATION, input_data=input_data)
-    assert response.has_errors is False, response.errors
-
-    reservation: Reservation | None = Reservation.objects.filter(name="foo").first()
-    assert reservation is not None
-    assert reservation.buffer_time_before == timedelta(hours=12)
-    assert reservation.buffer_time_after == timedelta(hours=11)
 
 
 @freezegun.freeze_time("2021-01-01")
