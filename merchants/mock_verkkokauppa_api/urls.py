0ea2c639d784e90ba20e96ec95739761d00ea974
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-03-01 16:12:09 +0200

    Exclude MockVerkkokauppaView CSRF excemption from SONAR checks

--- merchants/mock_verkkokauppa_api/urls.py
+++ merchants/mock_verkkokauppa_api/urls.py
@@ -3,7 +3,9 @@ from django.views.decorators.csrf import csrf_exempt
 
 from merchants.mock_verkkokauppa_api.views import MockVerkkokauppaView
 
+mock_verkkokauppa_view = csrf_exempt(MockVerkkokauppaView.as_view())  # NOSONAR
+
 urlpatterns = [
-    path("<str:order_uuid>/", csrf_exempt(MockVerkkokauppaView.as_view()), name="mock_verkkokauppa"),
-    path("<str:order_uuid>/paymentmethod", csrf_exempt(MockVerkkokauppaView.as_view()), name="mock_verkkokauppa_2"),
+    path("<str:order_uuid>/", mock_verkkokauppa_view, name="mock_verkkokauppa"),
+    path("<str:order_uuid>/paymentmethod", mock_verkkokauppa_view, name="mock_verkkokauppa_paymentmethod"),
 ]
