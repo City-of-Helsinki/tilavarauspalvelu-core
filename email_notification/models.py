a73352012ddd60b34ed55a3e5d30c6211236a556
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-03-11 15:39:40 +0200

    Add new EmailTemplate types choices
    - APPLICATION_HANDLED
    - APPLICATION_IN_HANDLING
    - APPLICATION_RECEIVED

--- email_notification/models.py
+++ email_notification/models.py
@@ -9,17 +9,20 @@ __all__ = [
 
 
 class EmailType(models.TextChoices):
-    RESERVATION_CONFIRMED = "reservation_confirmed"
-    RESERVATION_WITH_PIN_CONFIRMED = "reservation_with_pin_confirmed"
+    ACCESS_CODE_FOR_RESERVATION = "access_code_for_reservation"
+    APPLICATION_HANDLED = "application_handled"
+    APPLICATION_IN_HANDLING = "application_in_handling"
+    APPLICATION_RECEIVED = "application_received"
     HANDLING_REQUIRED_RESERVATION = "handling_required_reservation"
+    RESERVATION_CANCELLED = "reservation_cancelled"
+    RESERVATION_CONFIRMED = "reservation_confirmed"
     RESERVATION_HANDLED_AND_CONFIRMED = "reservation_handled_and_confirmed"
     RESERVATION_MODIFIED = "reservation_modified"
-    RESERVATION_REJECTED = "reservation_rejected"
-    RESERVATION_CANCELLED = "reservation_cancelled"
-    ACCESS_CODE_FOR_RESERVATION = "access_code_for_reservation"
     RESERVATION_NEEDS_TO_BE_PAID = "reservation_needs_to_be_paid"
-    STAFF_NOTIFICATION_RESERVATION_REQUIRES_HANDLING = "staff_notification_reservation_requires_handling"
+    RESERVATION_REJECTED = "reservation_rejected"
+    RESERVATION_WITH_PIN_CONFIRMED = "reservation_with_pin_confirmed"
     STAFF_NOTIFICATION_RESERVATION_MADE = "staff_notification_reservation_made"
+    STAFF_NOTIFICATION_RESERVATION_REQUIRES_HANDLING = "staff_notification_reservation_requires_handling"
 
 
 class EmailTemplate(models.Model):
