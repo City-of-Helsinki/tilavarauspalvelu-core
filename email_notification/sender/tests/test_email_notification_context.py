d1c149111aecfb8b14afc7dd2cc35c25de5c3caa
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-03-14 04:13:29 +0200

    Refactor: `EmailNotification`-app tests from class based to functional

--- email_notification/sender/tests/test_email_notification_context.py
+++ email_notification/sender/tests/test_email_notification_context.py
@@ -1,189 +1,202 @@
 import datetime
 from decimal import Decimal
 
+import pytest
 from django.contrib.auth import get_user_model
-from django.test import TestCase
 from django.utils.timezone import get_default_timezone
 
 from email_notification.sender.email_notification_context import EmailNotificationContext
+from reservation_units.models import ReservationUnit
 from reservations.choices import CustomerTypeChoice
+from reservations.models import Reservation
+from spaces.models import Location
 from tests.factories import (
     LocationFactory,
     ReservationCancelReasonFactory,
     ReservationDenyReasonFactory,
     ReservationFactory,
     ReservationUnitFactory,
-    UnitFactory,
 )
 
+pytestmark = [
+    pytest.mark.django_db,
+]
 
-class EmailNotificationContextTestCase(TestCase):
-    def setUp(self) -> None:
-        self.unit = UnitFactory.create(name="Test unit", name_sv="Svensk test namn", name_en="English test name")
-        self.location = LocationFactory.create(
-            unit=self.unit,
-            address_street="Street",
-            address_zip="0100",
-            address_city="Rovaniemi",
-        )
-        self.reservation_unit = ReservationUnitFactory.create(
-            name_fi="ReservationUnit Name FI",
-            name_sv="ReservationUnit Name SV",
-            name_en="ReservationUnit Name EN",
-            unit=self.unit,
-        )
-        self.deny_reason = ReservationDenyReasonFactory.create(
-            reason_fi="Reason FI",
-            reason_sv="Reason SV",
-            reason_en=None,  # None should use fi as fallback
-        )
-        self.cancel_reason = ReservationCancelReasonFactory.create(
-            reason_fi="Reason FI",
-            reason_sv="Reason SV",
-            reason_en="",  # empty string should use fi as fallback
-        )
-        self.reservation = ReservationFactory.create(
-            reservation_unit=[self.reservation_unit],
-            deny_reason=self.deny_reason,
-            cancel_reason=self.cancel_reason,
-            reservee_type=CustomerTypeChoice.INDIVIDUAL,
-        )
 
-    def test_with_mock_data(self):
-        context = EmailNotificationContext.with_mock_data()
-        assert context.reservee_name == "Email Test"
-        assert context.begin_datetime == (datetime.datetime(2100, 1, 1, 12, 00))
-        assert context.end_datetime == (datetime.datetime(2100, 1, 1, 13, 15))
-        assert context.reservation_number == 1234567
-        assert context.unit_location == "Testikatu 99999 Korvatunturi"
-        assert context.unit_name == "TOIMIPISTE"
-        assert context.reservation_name == "TESTIVARAUS"
-        assert context.reservation_unit_name == "VARAUSYKSIKKÖ"
-        assert context.price == (Decimal("12.30"))
-        assert context.non_subsidised_price == (Decimal("15.00"))
-        assert context.subsidised_price == (Decimal("5.00"))
-        assert context.tax_percentage == 24
-        assert context.confirmed_instructions == {
-            "fi": "[lisäohje: hyväksytty]",
-            "sv": "[mer information: bekräftats]",
-            "en": "[additional info: confirmed]",
-        }
+@pytest.fixture()
+def reservation() -> Reservation:
+    reservation_unit = ReservationUnitFactory.create(
+        name_fi="ReservationUnit Name FI",
+        name_sv="ReservationUnit Name SV",
+        name_en="ReservationUnit Name EN",
+        unit__name="Test unit",
+        unit__name_sv="Svensk test namn",
+        unit__name_en="English test name",
+    )
+    LocationFactory.create(
+        unit=reservation_unit.unit,
+        address_street="Street",
+        address_zip="0100",
+        address_city="Rovaniemi",
+    )
+    deny_reason = ReservationDenyReasonFactory.create(
+        reason_fi="Reason FI",
+        reason_sv="Reason SV",
+        reason_en=None,  # None should use fi as fallback
+    )
+    cancel_reason = ReservationCancelReasonFactory.create(
+        reason_fi="Reason FI",
+        reason_sv="Reason SV",
+        reason_en="",  # empty string should use fi as fallback
+    )
+    return ReservationFactory.create_for_reservation_unit(
+        reservation_unit=reservation_unit,
+        name="Test reservation",
+        deny_reason=deny_reason,
+        cancel_reason=cancel_reason,
+        reservee_type=CustomerTypeChoice.INDIVIDUAL,
+    )
 
-        assert context.pending_instructions == {
-            "fi": "[lisäohje: käsittelyssä]",
-            "sv": "[mer information: kräver hantering]",
-            "en": "[additional info: requires handling]",
-        }
 
-        assert context.cancelled_instructions == {
-            "fi": "[lisäohje: peruttu]",
-            "sv": "[mer information: avbokad]",
-            "en": "[additional info: cancelled]",
-        }
+def test_with_mock_data():
+    context = EmailNotificationContext.with_mock_data()
 
-        assert context.deny_reason == {
-            "fi": "[syy]",
-            "sv": "[orsak]",
-            "en": "[reason]",
-        }
+    assert context.reservee_name == "Email Test"
+    assert context.begin_datetime == (datetime.datetime(2100, 1, 1, 12, 00))
+    assert context.end_datetime == (datetime.datetime(2100, 1, 1, 13, 15))
+    assert context.reservation_number == 1234567
+    assert context.unit_location == "Testikatu 99999 Korvatunturi"
+    assert context.unit_name == "TOIMIPISTE"
+    assert context.reservation_name == "TESTIVARAUS"
+    assert context.reservation_unit_name == "VARAUSYKSIKKÖ"
+    assert context.price == (Decimal("12.30"))
+    assert context.non_subsidised_price == (Decimal("15.00"))
+    assert context.subsidised_price == (Decimal("5.00"))
+    assert context.tax_percentage == 24
+    assert context.confirmed_instructions == {
+        "fi": "[lisäohje: hyväksytty]",
+        "sv": "[mer information: bekräftats]",
+        "en": "[additional info: confirmed]",
+    }
+    assert context.pending_instructions == {
+        "fi": "[lisäohje: käsittelyssä]",
+        "sv": "[mer information: kräver hantering]",
+        "en": "[additional info: requires handling]",
+    }
+    assert context.cancelled_instructions == {
+        "fi": "[lisäohje: peruttu]",
+        "sv": "[mer information: avbokad]",
+        "en": "[additional info: cancelled]",
+    }
+    assert context.deny_reason == {
+        "fi": "[syy]",
+        "sv": "[orsak]",
+        "en": "[reason]",
+    }
+    assert context.cancel_reason == {
+        "fi": "[syy]",
+        "sv": "[orsak]",
+        "en": "[reason]",
+    }
 
-        assert context.cancel_reason == {
-            "fi": "[syy]",
-            "sv": "[orsak]",
-            "en": "[reason]",
-        }
+    assert (len(context.__dict__.keys())) == 17
 
-        assert (len(context.__dict__.keys())) == 17
 
-    def test_from_reservation(self):
-        context = EmailNotificationContext.from_reservation(self.reservation)
+def test_from_reservation(reservation):
+    context = EmailNotificationContext.from_reservation(reservation)
+    reservation_unit: ReservationUnit = reservation.reservation_unit.first()
 
-        assert context.reservee_name == f"{self.reservation.reservee_first_name} {self.reservation.reservee_last_name}"
+    assert context.reservee_name == f"{reservation.reservee_first_name} {reservation.reservee_last_name}"
 
-        assert context.reservation_name == self.reservation.name
-        assert context.begin_datetime == self.reservation.begin.astimezone(get_default_timezone())
-        assert context.end_datetime == self.reservation.end.astimezone(get_default_timezone())
-        assert context.reservation_number == self.reservation.id
-        assert context.unit_location == (
-            f"{self.location.address_street}, {self.location.address_zip} {self.location.address_city}"
-        )
-        assert context.unit_name == self.unit.name
-        assert context.price == self.reservation.price
-        assert context.non_subsidised_price == self.reservation.non_subsidised_price
-        assert context.subsidised_price == self.reservation.price
-        assert context.tax_percentage == self.reservation.tax_percentage_value
-        assert context.reservee_language == self.reservation.reservee_language
-        assert context.confirmed_instructions["fi"] == self.reservation_unit.reservation_confirmed_instructions_fi
-        assert context.confirmed_instructions["sv"] == self.reservation_unit.reservation_confirmed_instructions_sv
-        assert context.confirmed_instructions["en"] == self.reservation_unit.reservation_confirmed_instructions_en
-        assert context.pending_instructions["fi"] == self.reservation_unit.reservation_pending_instructions_fi
-        assert context.pending_instructions["sv"] == self.reservation_unit.reservation_pending_instructions_sv
-        assert context.pending_instructions["en"] == self.reservation_unit.reservation_pending_instructions_en
-        assert context.cancelled_instructions["fi"] == self.reservation_unit.reservation_cancelled_instructions_fi
-        assert context.cancelled_instructions["sv"] == self.reservation_unit.reservation_cancelled_instructions_sv
-        assert context.cancelled_instructions["en"] == self.reservation_unit.reservation_cancelled_instructions_en
-        assert context.deny_reason["fi"] == self.deny_reason.reason_fi
-        assert context.deny_reason["sv"] == self.deny_reason.reason_sv
-        assert self.deny_reason.reason_en is None
-        assert context.deny_reason["en"] == self.deny_reason.reason_fi
-        assert context.cancel_reason["fi"] == self.cancel_reason.reason_fi
-        assert context.cancel_reason["sv"] == self.cancel_reason.reason_sv
-        assert self.cancel_reason.reason_en == ""
-        assert context.cancel_reason["en"] == self.cancel_reason.reason_fi
+    assert context.reservation_name == reservation.name
+    assert context.begin_datetime == reservation.begin.astimezone(get_default_timezone())
+    assert context.end_datetime == reservation.end.astimezone(get_default_timezone())
+    assert context.reservation_number == reservation.id
+    assert context.unit_location == str(reservation_unit.unit.location)
+    assert context.unit_name == reservation_unit.unit.name
+    assert context.price == reservation.price
+    assert context.non_subsidised_price == reservation.non_subsidised_price
+    assert context.subsidised_price == reservation.price
+    assert context.tax_percentage == reservation.tax_percentage_value
+    assert context.reservee_language == reservation.reservee_language
 
-    def test_from_reservation_organisation_name(self):
-        self.reservation.reservee_type = CustomerTypeChoice.BUSINESS
-        self.reservation.reservee_organisation_name = "Business"
-        self.reservation.save()
+    assert context.confirmed_instructions["fi"] == reservation_unit.reservation_confirmed_instructions_fi
+    assert context.confirmed_instructions["sv"] == reservation_unit.reservation_confirmed_instructions_sv
+    assert context.confirmed_instructions["en"] == reservation_unit.reservation_confirmed_instructions_en
+    assert context.pending_instructions["fi"] == reservation_unit.reservation_pending_instructions_fi
+    assert context.pending_instructions["sv"] == reservation_unit.reservation_pending_instructions_sv
+    assert context.pending_instructions["en"] == reservation_unit.reservation_pending_instructions_en
+    assert context.cancelled_instructions["fi"] == reservation_unit.reservation_cancelled_instructions_fi
+    assert context.cancelled_instructions["sv"] == reservation_unit.reservation_cancelled_instructions_sv
+    assert context.cancelled_instructions["en"] == reservation_unit.reservation_cancelled_instructions_en
 
-        context = EmailNotificationContext.from_reservation(self.reservation)
-        assert context.reservee_name == self.reservation.reservee_organisation_name
+    assert context.deny_reason["fi"] == reservation.deny_reason.reason_fi
+    assert context.deny_reason["sv"] == reservation.deny_reason.reason_sv
+    assert reservation.deny_reason.reason_en is None
+    assert context.deny_reason["en"] == reservation.deny_reason.reason_fi
 
-    def test_from_reservation_no_location(self):
-        self.location.delete()
-        context = EmailNotificationContext.from_reservation(self.reservation)
-        assert context.unit_location == ""
+    assert context.cancel_reason["fi"] == reservation.cancel_reason.reason_fi
+    assert context.cancel_reason["sv"] == reservation.cancel_reason.reason_sv
+    assert reservation.cancel_reason.reason_en == ""
+    assert context.cancel_reason["en"] == reservation.cancel_reason.reason_fi
 
-    def test_from_reservation_unit_name_is_reservee_langauge(self):
-        self.reservation.reservee_language = "sv"
-        self.reservation.save()
 
-        context = EmailNotificationContext.from_reservation(self.reservation)
-        assert context.unit_name == self.unit.name_sv
+def test_from_reservation_organisation_name(reservation):
+    reservation.reservee_type = CustomerTypeChoice.BUSINESS
+    reservation.reservee_organisation_name = "Business"
+    reservation.save()
 
-    def test_from_reservation_unit_name_uses_reservation_user_preferred_language(self):
-        user = get_user_model().objects.create(
-            username="test",
-            first_name="test",
-            last_name="user",
-            email="test.user@localhost",
-            preferred_language="sv",
-        )
-        self.reservation.user = user
-        self.reservation.save()
+    context = EmailNotificationContext.from_reservation(reservation)
+    assert context.reservee_name == reservation.reservee_organisation_name
 
-        context = EmailNotificationContext.from_reservation(self.reservation)
-        assert context.unit_name == self.unit.name_sv
 
-    def test_from_reservation_reservation_unit_name_is_reservee_langauge(self):
-        self.reservation.reservee_language = "sv"
-        self.reservation.save()
+def test_from_reservation_no_location(reservation):
+    Location.objects.all().delete()
+    context = EmailNotificationContext.from_reservation(reservation)
+    assert context.unit_location == ""
 
-        context = EmailNotificationContext.from_reservation(self.reservation)
-        assert context.reservation_unit_name == self.reservation_unit.name_sv
 
-    def test_from_reservation_reservation_unit_name_uses_reservation_user_preferred_language(
-        self,
-    ):
-        user = get_user_model().objects.create(
-            username="test",
-            first_name="test",
-            last_name="user",
-            email="test.user@localhost",
-            preferred_language="sv",
-        )
-        self.reservation.user = user
-        self.reservation.save()
+def test_from_reservation_unit_name_is_reservee_langauge(reservation):
+    reservation.reservee_language = "sv"
+    reservation.save()
 
-        context = EmailNotificationContext.from_reservation(self.reservation)
-        assert context.reservation_unit_name == self.reservation_unit.name_sv
+    context = EmailNotificationContext.from_reservation(reservation)
+    assert context.unit_name == reservation.reservation_unit.first().unit.name_sv
+
+
+def test_from_reservation_unit_name_uses_reservation_user_preferred_language(reservation):
+    user = get_user_model().objects.create(
+        username="test",
+        first_name="test",
+        last_name="user",
+        email="test.user@localhost",
+        preferred_language="sv",
+    )
+    reservation.user = user
+    reservation.save()
+
+    context = EmailNotificationContext.from_reservation(reservation)
+    assert context.unit_name == reservation.reservation_unit.first().unit.name_sv
+
+
+def test_from_reservation_reservation_unit_name_is_reservee_langauge(reservation):
+    reservation.reservee_language = "sv"
+    reservation.save()
+
+    context = EmailNotificationContext.from_reservation(reservation)
+    assert context.reservation_unit_name == reservation.reservation_unit.first().name_sv
+
+
+def test_from_reservation_reservation_unit_name_uses_reservation_user_preferred_language(reservation):
+    user = get_user_model().objects.create(
+        username="test",
+        first_name="test",
+        last_name="user",
+        email="test.user@localhost",
+        preferred_language="sv",
+    )
+    reservation.user = user
+    reservation.save()
+
+    context = EmailNotificationContext.from_reservation(reservation)
+    assert context.reservation_unit_name == reservation.reservation_unit.first().name_sv
--- email_notification/sender/tests/test_validators.py
+++ email_notification/sender/tests/test_validators.py
@@ -1,71 +1,69 @@
 from unittest import mock
 
 import pytest
-from assertpy import assert_that
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.test import override_settings
-from django.test.testcases import TestCase
 
-from email_notification.sender.email_notification_builder import EmailTemplateValidator
+from email_notification.sender.email_notification_validator import EmailTemplateValidator
 
 
 @override_settings(EMAIL_HTML_MAX_FILE_SIZE=150000)
-class HTMLFileValidatorTestCase(TestCase):
-    def setUp(self) -> None:
-        self.validator = EmailTemplateValidator()
+def test_raises_validation_error_on_invalid_file_extension():
+    mock_field_file = mock.MagicMock()
+    mock_field_file.name = "/tmp/mock_template.jpg"
+    mock_field_file.size = settings.EMAIL_HTML_MAX_FILE_SIZE
 
-    def test_raises_validation_error_on_invalid_file_extension(self):
-        mock_field_file = mock.MagicMock()
-        mock_field_file.name = "/tmp/mock_template.jpg"
-        mock_field_file.size = settings.EMAIL_HTML_MAX_FILE_SIZE
+    msg = "Unsupported file extension .jpg. Only .html files are allowed"
+    with pytest.raises(ValidationError, match=msg):
+        EmailTemplateValidator().validate_html_file(mock_field_file)
 
-        with pytest.raises(ValidationError) as err:
-            self.validator.validate_html_file(mock_field_file)
-        assert_that(err.value.message).is_equal_to("Unsupported file extension .jpg. Only .html files are allowed")
 
-    def test_raises_validation_error_on_zero_size_file(self):
-        mock_field_file = mock.MagicMock()
-        mock_field_file.name = "/tmp/mock_template.html"
-        mock_field_file.size = 0
+@override_settings(EMAIL_HTML_MAX_FILE_SIZE=150000)
+def test_raises_validation_error_on_zero_size_file():
+    mock_field_file = mock.MagicMock()
+    mock_field_file.name = "/tmp/mock_template.html"
+    mock_field_file.size = 0
 
-        with pytest.raises(ValidationError) as err:
-            self.validator.validate_html_file(mock_field_file)
-        assert_that(err.value.message).is_equal_to(
-            f"Invalid HTML file size. Allowed file size: 1-{settings.EMAIL_HTML_MAX_FILE_SIZE} bytes"
-        )
+    msg = f"Invalid HTML file size. Allowed file size: 1-{settings.EMAIL_HTML_MAX_FILE_SIZE} bytes"
+    with pytest.raises(ValidationError, match=msg):
+        EmailTemplateValidator().validate_html_file(mock_field_file)
 
-    def test_raises_validation_error_on_big_file(self):
-        mock_field_file = mock.MagicMock()
-        mock_field_file.name = "/tmp/mock_template.html"
-        mock_field_file.size = settings.EMAIL_HTML_MAX_FILE_SIZE + 1
 
-        with pytest.raises(ValidationError) as err:
-            self.validator.validate_html_file(mock_field_file)
-        assert_that(err.value.message).is_equal_to(
-            f"Invalid HTML file size. Allowed file size: 1-{settings.EMAIL_HTML_MAX_FILE_SIZE} bytes"
-        )
+@override_settings(EMAIL_HTML_MAX_FILE_SIZE=150000)
+def test_raises_validation_error_on_big_file():
+    mock_field_file = mock.MagicMock()
+    mock_field_file.name = "/tmp/mock_template.html"
+    mock_field_file.size = settings.EMAIL_HTML_MAX_FILE_SIZE + 1
 
-    def test_raises_validation_error_on_unsupported_tag(self):
-        mock_file = mock.MagicMock()
-        mock_file.read.return_value = b"<html>{{invalid_tag}}</html>"
+    msg = f"Invalid HTML file size. Allowed file size: 1-{settings.EMAIL_HTML_MAX_FILE_SIZE} bytes"
+    with pytest.raises(ValidationError, match=msg):
+        EmailTemplateValidator().validate_html_file(mock_field_file)
 
-        mock_field_file = mock.MagicMock()
-        mock_field_file.name = "/tmp/mock_template.html"
-        mock_field_file.size = settings.EMAIL_HTML_MAX_FILE_SIZE
-        mock_field_file.open.return_value = mock_file
 
-        with pytest.raises(ValidationError) as err:
-            self.validator.validate_html_file(mock_field_file)
-        assert_that(err.value.message).is_equal_to("Tag invalid_tag not supported")
+@override_settings(EMAIL_HTML_MAX_FILE_SIZE=150000)
+def test_raises_validation_error_on_unsupported_tag():
+    mock_file = mock.MagicMock()
+    mock_file.read.return_value = b"<html>{{invalid_tag}}</html>"
 
-    def valid_file_raises_no_exceptions(self):
-        mock_file = mock.MagicMock()
-        mock_file.read.return_value = b"<html></html>"
+    mock_field_file = mock.MagicMock()
+    mock_field_file.name = "/tmp/mock_template.html"
+    mock_field_file.size = settings.EMAIL_HTML_MAX_FILE_SIZE
+    mock_field_file.open.return_value = mock_file
 
-        mock_field_file = mock.MagicMock()
-        mock_field_file.name = "/tmp/mock_template.html"
-        mock_field_file.size = settings.EMAIL_HTML_MAX_FILE_SIZE
-        mock_field_file.open.return_value = mock_file
+    msg = "Tag 'invalid_tag' is not supported"
+    with pytest.raises(ValidationError, match=msg):
+        EmailTemplateValidator().validate_html_file(mock_field_file)
 
-        self.validator.validate_html_file(mock_field_file)
+
+@override_settings(EMAIL_HTML_MAX_FILE_SIZE=150000)
+def valid_file_raises_no_exceptions():
+    mock_file = mock.MagicMock()
+    mock_file.read.return_value = b"<html></html>"
+
+    mock_field_file = mock.MagicMock()
+    mock_field_file.name = "/tmp/mock_template.html"
+    mock_field_file.size = settings.EMAIL_HTML_MAX_FILE_SIZE
+    mock_field_file.open.return_value = mock_file
+
+    EmailTemplateValidator().validate_html_file(mock_field_file)
