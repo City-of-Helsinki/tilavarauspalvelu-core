34b087359c76f88626f26e4c39199c4c39919f7d
Matti Lamppu <matti.lamppu@vincit.fi>
2024-03-25 10:28:24 +0200

    Remove custom connection fields

--- api/graphql/types/reservation_unit_cancellation_rule/field.py
+++
@@ -1,7 +0,0 @@
-from graphene_permissions.mixins import AuthFilter
-
-from api.graphql.types.reservation_unit_cancellation_rule.permissions import ReservationUnitCancellationRulePermission
-
-
-class ReservationUnitCancellationRulesFilter(AuthFilter):
-    permission_classes = (ReservationUnitCancellationRulePermission,)
--- api/graphql/types/tax_percentage/field.py
+++
@@ -1,7 +0,0 @@
-from graphene_permissions.mixins import AuthFilter
-
-from api.graphql.types.tax_percentage.permissions import TaxPercentagePermission
-
-
-class TaxPercentageFilter(AuthFilter):
-    permission_classes = (TaxPercentagePermission,)
