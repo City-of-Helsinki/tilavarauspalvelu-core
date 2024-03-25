7b6855ed0fd3d654418339c6bb9b4947a0d04432
Matti Lamppu <matti.lamppu@vincit.fi>
2024-03-25 10:28:24 +0200

    Remove custom connection fields

--- api/graphql/types/units/fields.py
+++
@@ -1,8 +0,0 @@
-import django_filters
-from graphene_permissions.mixins import AuthFilter
-
-from api.graphql.types.units.permissions import UnitPermission
-
-
-class UnitsFilter(AuthFilter, django_filters.FilterSet):
-    permission_classes = (UnitPermission,)
