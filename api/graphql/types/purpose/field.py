41fc15ce303750c8723a5f5ba2b783b47cdb13bd
Matti Lamppu <matti.lamppu@vincit.fi>
2024-03-25 10:28:24 +0200

    Remove custom connection fields

--- api/graphql/types/purpose/field.py
+++
@@ -1,7 +0,0 @@
-from graphene_permissions.mixins import AuthFilter
-
-from api.graphql.types.purpose.permissions import PurposePermission
-
-
-class PurposeFilter(AuthFilter):
-    permission_classes = (PurposePermission,)
