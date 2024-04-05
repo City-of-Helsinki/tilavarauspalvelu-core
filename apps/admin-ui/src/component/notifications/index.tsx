ba49e4ff247c47f190ccea22e085256573ca44d6
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2024-04-05 16:32:04 +0300

    fix: notifications sorting

--- apps/admin-ui/src/component/notifications/index.tsx
+++ apps/admin-ui/src/component/notifications/index.tsx
@@ -5,22 +5,29 @@ import { type TFunction } from "i18next";
 import styled from "styled-components";
 import { Button } from "hds-react";
 import { BANNER_NOTIFICATIONS_ADMIN_LIST } from "common/src/components/BannerNotificationsQuery";
-import type {
-  Query,
-  BannerNotificationNode,
-  QueryBannerNotificationsArgs,
+import {
+  type BannerNotificationNode,
+  type Query,
+  type QueryBannerNotificationsArgs,
+  BannerNotificationOrderingChoices,
 } from "common/types/gql-types";
 import { H1 } from "common/src/common/typography";
-import { Container } from "app/styles/layout";
-import BreadcrumbWrapper from "app/component/BreadcrumbWrapper";
-import Loader from "app/component/Loader";
-import { ButtonLikeLink } from "app/component/ButtonLikeLink";
-import { valueForDateInput, valueForTimeInput } from "app/helpers";
-import { GQL_MAX_RESULTS_PER_QUERY } from "app/common/const";
+import { Container } from "@/styles/layout";
+import BreadcrumbWrapper from "@/component/BreadcrumbWrapper";
+import Loader from "@/component/Loader";
+import { ButtonLikeLink } from "@/component/ButtonLikeLink";
+import { valueForDateInput, valueForTimeInput } from "@/helpers";
+import { GQL_MAX_RESULTS_PER_QUERY } from "@/common/const";
 import { CustomTable, TableLink } from "@/component/Table";
 import { filterNonNullable } from "common/src/helpers";
 
 const notificationUrl = (pk: number) => `/messaging/notifications/${pk}`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 // Tila, Nimi, Voimassa alk, Voimassa asti, Kohderyhmä, Tyyppi
 const getColConfig = (t: TFunction) => [
@@ -46,7 +53,7 @@ const getColConfig = (t: TFunction) => [
   },
   {
     headerName: t("Notifications.headings.activeFrom"),
-    key: "activeFrom",
+    key: "starts",
     isSortable: true,
     transform: (notification: NonNullable<BannerNotificationNode>) =>
       notification.activeFrom
@@ -57,7 +64,7 @@ const getColConfig = (t: TFunction) => [
   },
   {
     headerName: t("Notifications.headings.activeUntil"),
-    key: "activeUntil",
+    key: "ends",
     isSortable: true,
     transform: (notification: NonNullable<BannerNotificationNode>) =>
       notification.activeUntil
@@ -68,7 +75,7 @@ const getColConfig = (t: TFunction) => [
   },
   {
     headerName: t("Notifications.headings.targetGroup"),
-    key: "targetGroup",
+    key: "target",
     isSortable: true,
     transform: (notification: NonNullable<BannerNotificationNode>) =>
       t(`Notifications.target.${notification.target ?? "noTarget"}`),
@@ -82,48 +89,17 @@ const getColConfig = (t: TFunction) => [
   },
 ];
 
-type Sort = {
-  field: string;
-  order: "asc" | "desc";
-};
-
-// Transform the one sort key to the format that the table component expects
-// TODO proper way of doing this is to remap the Table sort keys in the ColConfig
-// so allow different index keys and sort keys for a column (requires refactoring Table).
-const transformSortKey = (key: string) => {
-  if (key === "targetGroup") {
-    return "target";
-  }
-  if (key === "activeFrom") {
-    return "starts";
-  }
-  if (key === "activeUntil") {
-    return "ends";
-  }
-  return key;
-};
-const transformToTableKey = (key: string) => {
-  if (key === "target") {
-    return "targetGroup";
-  }
-  if (key === "starts") {
-    return "activeFrom";
-  }
-  if (key === "ends") {
-    return "activeUntil";
-  }
-  return key;
-};
-
-const NotificationsTable = ({
+function NotificationsTable({
   notifications,
   onSortChanged,
-  sortKey,
+  sort,
+  isLoading,
 }: {
   notifications: BannerNotificationNode[];
   onSortChanged: (key: string) => void;
-  sortKey: Sort;
-}) => {
+  sort: string;
+  isLoading: boolean;
+}) {
   const { t } = useTranslation();
   const cols = getColConfig(t);
 
@@ -133,58 +109,52 @@ const NotificationsTable = ({
 
   return (
     <CustomTable
-      setSort={(sortBy) => onSortChanged(transformSortKey(sortBy))}
+      setSort={(sortBy) => onSortChanged(sortBy)}
       indexKey="pk"
       rows={notifications}
       cols={cols}
-      initialSortingColumnKey={transformToTableKey(sortKey.field)}
-      initialSortingOrder={sortKey.order}
+      initialSortingColumnKey={sort.startsWith("-") ? sort.slice(1) : sort}
+      initialSortingOrder={sort.startsWith("-") ? "desc" : "asc"}
+      isLoading={isLoading}
     />
   );
-};
-
-const HeaderContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
+}
 
 /// @brief this is the listing page for all notifications.
 function Page() {
   // TODO the default sort should be ["state", "-ends"] but the frontend sort doesn't support multiple options
   // so either leave it with just state or do some custom magic for the initial sort
-  const [sortKey, setSortKey] = useState<Sort>({
-    field: "state",
-    order: "asc" as const,
-  });
+  const [sort, setSort] = useState<string>("state");
+  const orderBy = transformSortString(sort);
 
   const {
     data,
     loading: isLoading,
+    previousData,
     fetchMore,
   } = useQuery<Query, QueryBannerNotificationsArgs>(
     BANNER_NOTIFICATIONS_ADMIN_LIST,
     {
       variables: {
         first: GQL_MAX_RESULTS_PER_QUERY,
-        // offset: 0,
-        // FIXME orderby using enum
-        // orderBy: `${sortKey.order === "desc" ? "-" : ""}${sortKey.field}`,
+        orderBy,
       },
     }
   );
 
+  const { bannerNotifications } = data ?? previousData ?? {};
   const notifications = filterNonNullable(
-    data?.bannerNotifications?.edges?.map((edge) => edge?.node)
+    bannerNotifications?.edges?.map((edge) => edge?.node)
   );
   const totalCount = data?.bannerNotifications?.totalCount ?? 0;
 
   const { t } = useTranslation();
 
-  const handleSortChange = (key: string) => {
-    if (sortKey.field === key && sortKey.order === "asc") {
-      setSortKey({ field: key, order: "desc" });
+  const handleSortChange = (sortField: string) => {
+    if (sort === sortField) {
+      setSort(`-${sortField}`);
     } else {
-      setSortKey({ field: key, order: "asc" });
+      setSort(sortField);
     }
   };
 
@@ -201,14 +171,15 @@ function Page() {
         </ButtonLikeLink>
       </HeaderContainer>
       <p>{t("Notifications.pageDescription")}</p>
-      {isLoading ? (
+      {isLoading && notifications.length === 0 ? (
         <Loader />
       ) : (
         <>
           <NotificationsTable
             notifications={notifications}
             onSortChanged={handleSortChange}
-            sortKey={sortKey}
+            sort={sort}
+            isLoading={isLoading}
           />
           {totalCount > notifications.length && (
             <Button
@@ -228,6 +199,56 @@ function Page() {
       )}
     </>
   );
+}
+
+function transformOrderBy(
+  orderBy: string,
+  desc: boolean
+): BannerNotificationOrderingChoices | null {
+  switch (orderBy) {
+    case "pk":
+      return desc
+        ? BannerNotificationOrderingChoices.PkDesc
+        : BannerNotificationOrderingChoices.PkAsc;
+    case "state":
+      return desc
+        ? BannerNotificationOrderingChoices.StateDesc
+        : BannerNotificationOrderingChoices.StateAsc;
+    case "name":
+      return desc
+        ? BannerNotificationOrderingChoices.NameDesc
+        : BannerNotificationOrderingChoices.NameAsc;
+    case "starts":
+      return desc
+        ? BannerNotificationOrderingChoices.StartsDesc
+        : BannerNotificationOrderingChoices.StartsAsc;
+    case "ends":
+      return desc
+        ? BannerNotificationOrderingChoices.EndsDesc
+        : BannerNotificationOrderingChoices.EndsAsc;
+    case "target":
+      return desc
+        ? BannerNotificationOrderingChoices.TargetDesc
+        : BannerNotificationOrderingChoices.TargetAsc;
+    case "level":
+      return desc
+        ? BannerNotificationOrderingChoices.LevelDesc
+        : BannerNotificationOrderingChoices.LevelAsc;
+    default:
+      return null;
+  }
+}
+
+function transformSortString(
+  sort: string | null
+): BannerNotificationOrderingChoices[] {
+  if (!sort) {
+    return [BannerNotificationOrderingChoices.StateDesc];
+  }
+  const desc = sort.startsWith("-");
+  const sortKey = desc ? sort.slice(1) : sort;
+  const orderBy = transformOrderBy(sortKey, desc);
+  return orderBy ? [orderBy] : [BannerNotificationOrderingChoices.StateDesc];
 }
 
 // We don't have proper layouts yet, so just separate the container stuff here
