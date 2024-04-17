a5250984640e68670ffaf2c87c48850552aef415
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-04-17 22:57:22 +0300

    ruff: Add `base64` to `banned-from` imports

--- users/helauth/utils.py
+++ users/helauth/utils.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
+import base64
 import json
-from base64 import urlsafe_b64decode
 from typing import TYPE_CHECKING, Any
 
 from django.core.handlers.wsgi import WSGIRequest
@@ -21,7 +21,7 @@ __all__ = [
 def get_jwt_payload(json_web_token: str) -> dict[str, Any]:
     payload_part: str = json_web_token.split(".")[1]  # Get the payload part of the id token
     payload_part += "=" * divmod(len(payload_part), 4)[1]  # Add padding to the payload if needed
-    payload: str = urlsafe_b64decode(payload_part).decode()  # Decode the payload
+    payload: str = base64.urlsafe_b64decode(payload_part).decode()  # Decode the payload
     return json.loads(payload)  # Return the payload as a dict
 
 
