d5bc9e8712a71685ca5bbc0aefc35ac8eb98d0e0
Matti Lamppu <matti.lamppu@vincit.fi>
2024-04-17 11:38:03 +0300

    Remove `get_nested` util.
    This is available from `graphene_django_extensions`

--- common/utils.py
+++ common/utils.py
@@ -9,41 +9,12 @@ __all__ = [
     "comma_sep_str",
     "get_attr_by_language",
     "get_field_to_related_field_mapping",
-    "get_nested",
     "get_translation_fields",
     "with_indices",
 ]
 
 
 T = TypeVar("T")
-
-
-def get_nested(obj: dict | list | None, /, *args: str | int, default: Any = None) -> Any:
-    """
-    Get value from a nested structure containing dicts with string keys or lists,
-    where the keys and list indices might not exist.
-
-    1) `data["foo"][0]["bar"]["baz"]`
-     - Might raise a `KeyError` or `IndexError` if any of the keys or indices don't exist.
-
-    2) `get_nested(data, "foo", 0, "bar", "baz")`
-     - Will return `None` (default) if any of the keys or indices don't exist.
-    """
-    if not args:
-        return obj if obj is not None else default
-
-    arg, args = args[0], args[1:]
-
-    if isinstance(arg, int):
-        obj = obj or []
-        try:
-            obj = obj[arg]
-        except IndexError:
-            obj = None
-        return get_nested(obj, *args, default=default)
-
-    obj = obj or {}
-    return get_nested(obj.get(arg), *args, default=default)
 
 
 def comma_sep_str(values: Sequence[str]) -> str:
