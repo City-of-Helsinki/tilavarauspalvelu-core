29abaa218c9d8720b5de11097b57c7a8571a1138
Matti Lamppu <matti.lamppu@vincit.fi>
2024-04-18 09:05:13 +0300

    Add tests for middleware

---
+++ tests/test_utils/test_query_logging_middleware.py
@@ -0,0 +1,57 @@
+import pytest
+from graphene_django_extensions.testing import build_query
+
+from common.models import SQLLog
+from tests.factories import SpaceFactory
+
+# Applied to all tests
+pytestmark = [
+    pytest.mark.django_db,
+    pytest.mark.usefixtures("_celery_synchronous"),
+]
+
+
+def test_query_logging_middleware(graphql, settings):
+    settings.QUERY_LOGGING_ENABLED = True
+
+    SpaceFactory.create()
+
+    assert SQLLog.objects.count() == 0
+
+    query = build_query("spaces", connection=True)
+    graphql.login_with_superuser()
+    response = graphql(query)
+
+    assert response.has_errors is False, response
+
+    # Check created logs
+    logs = list(SQLLog.objects.all())
+    # Might have 1 extra query for session, doesn't always happen for some reason
+    if len(logs) == 4:
+        logs = logs[1:]
+
+    assert len(logs) == 3
+
+    # Fetching user
+    assert logs[0].path == "/graphql/"
+    assert logs[0].succeeded is True
+    assert 'FROM "user"' in logs[0].sql
+
+    # Counting spaces for pagination
+    assert logs[1].path == "/graphql/"
+    assert logs[1].succeeded is True
+    assert logs[1].sql == 'SELECT COUNT(*) AS "__count" FROM "space"'
+
+    # Fetching spaces
+    assert logs[2].path == "/graphql/"
+    assert logs[2].succeeded is True
+    assert logs[2].sql == 'SELECT "space"."id" FROM "space" ORDER BY "space"."tree_id" ASC, "space"."lft" ASC LIMIT 1'
+
+    # All logs are for the same request
+    assert logs[0].request_id == logs[1].request_id
+    assert logs[0].request_id == logs[2].request_id
+
+    # All logs have the same body saved
+    assert logs[0].body == "query { spaces { edges { node { pk } } } }"
+    assert logs[0].body == logs[1].body
+    assert logs[0].body == logs[2].body
