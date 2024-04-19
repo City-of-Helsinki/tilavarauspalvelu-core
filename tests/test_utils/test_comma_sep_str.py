c212978d75122d838289d47f1a2be9b1e166a65d
Matti Lamppu <matti.lamppu@vincit.fi>
2024-04-19 08:37:57 +0300

    Improve `comma_sep_str` util
    Make it work with all iterables. Add tests.

---
+++ tests/test_utils/test_comma_sep_str.py
@@ -0,0 +1,19 @@
+import pytest
+
+from common.utils import comma_sep_str
+
+
+@pytest.mark.parametrize(
+    ("values", "result"),
+    [
+        (["foo", "bar", "baz"], "foo, bar & baz"),
+        (["foo", "bar"], "foo & bar"),
+        (["foo"], "foo"),
+        ([""], ""),
+        ([], ""),
+        ([1, 2, 3], "1, 2 & 3"),
+        ((i for i in ["foo", "bar", "baz"]), "foo, bar & baz"),
+    ],
+)
+def test_comma_sep_str(values, result):
+    assert comma_sep_str(values) == result
