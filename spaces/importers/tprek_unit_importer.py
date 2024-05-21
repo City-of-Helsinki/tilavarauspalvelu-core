ea9728701c73aa97ce597703f25e1ba3c7fd13c1
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-05-21 21:50:12 +0300

    Fetch Hauki Resources for units imported from TPREK

--- spaces/importers/tprek_unit_importer.py
+++ spaces/importers/tprek_unit_importer.py
@@ -4,6 +4,9 @@ from dataclasses import asdict
 from django.db.models import QuerySet
 from django.db.transaction import atomic
 
+from opening_hours.models import OriginHaukiResource
+from opening_hours.utils.hauki_api_client import HaukiAPIClient
+from opening_hours.utils.hauki_api_types import HaukiAPIResource
 from spaces.importers.tprek_api_client import TprekAPIClient, TprekLocationData, TprekUnitData
 from spaces.models import Location, Unit
 from utils.sentry import SentryLogger
@@ -14,6 +17,7 @@ logger = logging.getLogger(__name__)
 class TprekUnitImporter:
     def __init__(self) -> None:
         self.updated_units_count = 0
+        self.units_for_hauki_import = []
 
     @atomic
     def update_unit_from_tprek(self, units: list[Unit] | QuerySet[Unit], force_update: bool = False) -> None:
@@ -46,6 +50,10 @@ class TprekUnitImporter:
 
         logger.info(f"Updated {self.updated_units_count} from TPREK.")
 
+        if self.units_for_hauki_import:
+            tprek_hauki_resource_importer = TprekUnitHaukiResourceIdImporter()
+            tprek_hauki_resource_importer.import_hauki_resources_for_units(self.units_for_hauki_import)
+
     def _update_unit(self, unit: Unit, tprek_unit_data: TprekUnitData, tprek_location_data: TprekLocationData) -> None:
         """Updates a Unit and its location from the given TPREK data."""
         for field, value in asdict(tprek_unit_data).items():
@@ -55,3 +63,57 @@ class TprekUnitImporter:
         Location.objects.update_or_create(unit=unit, defaults=asdict(tprek_location_data))
 
         self.updated_units_count += 1
+        if unit.origin_hauki_resource is None:
+            self.units_for_hauki_import.append(unit)
+
+
+class TprekUnitHaukiResourceIdImporter:
+    @classmethod
+    @atomic
+    def import_hauki_resources_for_units(cls, units: list[Unit]) -> None:
+        hauki_resource_id_map = cls._fetch_hauki_resource_ids(units)
+
+        logger.info(f"Importing Hauki resources for {len(units)} units...")
+
+        updated_units_count = 0
+        created_resources_count = 0
+        for unit in units:
+            hauki_resource_id = hauki_resource_id_map.get(str(unit.tprek_id))
+            if hauki_resource_id:
+                origin_hauki_resource, created = OriginHaukiResource.objects.get_or_create(id=hauki_resource_id)
+                unit.origin_hauki_resource = origin_hauki_resource
+                unit.save()
+                updated_units_count += 1
+                if created:
+                    created_resources_count += 1
+
+        logger.info(
+            f"Saved Hauki resources for {updated_units_count}/{len(units)} units. "
+            f"Created {created_resources_count} new OriginHaukiResources."
+        )
+
+    @classmethod
+    def _fetch_hauki_resource_ids(cls, units: list[Unit]) -> dict[str, str]:
+        # Fetch only units that don't have a Hauki resource yet
+        tprek_ids: list[str] = [unit.tprek_id for unit in units if unit.origin_hauki_resource is None]
+
+        if not len(tprek_ids):
+            return {}
+
+        hauki_resources: list[HaukiAPIResource] = HaukiAPIClient.get_resources_all_pages(
+            hauki_resource_ids=[f"tprek:{tprek_id}" for tprek_id in tprek_ids],
+            data_source="tprek",
+            origin_id_exists=True,
+            page_size=50000,
+        )
+
+        hauki_resource_id_map: dict[str, str | int] = {}
+
+        # For each Hauki resource, find the origin with data_source=tprek and add the origin id to the map
+        for hauki_resource in hauki_resources:
+            for resource_origin in hauki_resource["origins"]:
+                if resource_origin["data_source"]["id"] == "tprek":
+                    hauki_resource_id_map[resource_origin["origin_id"]] = str(hauki_resource["id"])
+                    break
+
+        return hauki_resource_id_map
