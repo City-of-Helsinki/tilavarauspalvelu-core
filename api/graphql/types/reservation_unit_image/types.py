4d3669824925fa9f073690f7e5bda9c6bd85cc88
Matti Lamppu <matti.lamppu@vincit.fi>
2024-05-23 08:31:47 +0300

    Optimize reservation unit image urls

--- api/graphql/types/reservation_unit_image/types.py
+++ api/graphql/types/reservation_unit_image/types.py
@@ -1,5 +1,7 @@
 import graphene
+from django.db import models
 from graphene_django_extensions import DjangoNode
+from query_optimizer import AnnotatedField
 
 from common.typing import GQLInfo
 from reservation_units.models import ReservationUnitImage
@@ -8,9 +10,10 @@ from .permissions import ReservationUnitImagePermission
 
 
 class ReservationUnitImageNode(DjangoNode):
-    image_url = graphene.String()
-    medium_url = graphene.String()
+    image_url = AnnotatedField(graphene.String, expression=models.F("image"))
+
     small_url = graphene.String()
+    medium_url = graphene.String()
     large_url = graphene.String()
 
     class Meta:
@@ -26,9 +29,14 @@ class ReservationUnitImageNode(DjangoNode):
         permission_classes = [ReservationUnitImagePermission]
 
     def resolve_image_url(root: ReservationUnitImage, info: GQLInfo) -> str | None:
-        if not root.image:
+        # Annotating a ThumbnailerImageField annotates the name of the file (if it exists).
+        image_name: str | None = getattr(root, "image_url", None)
+        if not image_name:
             return None
-        return info.context.build_absolute_uri(root.image.url)
+
+        # Build the name into the absolute URL for the image.
+        url = root.image.storage.url(image_name)
+        return info.context.build_absolute_uri(url)
 
     def resolve_large_url(root: ReservationUnitImage, info: GQLInfo) -> str | None:
         if not root.large_url:
