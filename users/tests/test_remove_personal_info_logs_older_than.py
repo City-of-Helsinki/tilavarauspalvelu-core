d18b03d2ae1db460b00f0671f4bbbe23e9e6fe61
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-05-28 13:36:03 +0300

    Refactor all `users`-app UnitTests to functional tests

--- users/tests/test_remove_personal_info_logs_older_than.py
+++ users/tests/test_remove_personal_info_logs_older_than.py
@@ -1,47 +1,37 @@
-from django.test import TestCase
+import pytest
 from freezegun import freeze_time
 
 from tests.factories import UserFactory
 from users.models import PersonalInfoViewLog
 from users.tasks import remove_old_personal_info_view_logs, remove_personal_info_view_logs_older_than
 
+pytestmark = [
+    pytest.mark.django_db,
+]
 
-class RemovePersonalInfoViewLogsOlderThanTestCase(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        super().setUpTestData()
 
-        cls.user = UserFactory.create(
-            username="testuser",
-            email="test@localhost",
-            first_name="Test",
-            last_name="User",
+@pytest.mark.parametrize(
+    "remove_logs_function",
+    [
+        remove_personal_info_view_logs_older_than,
+        remove_old_personal_info_view_logs,
+    ],
+)
+def test_remove_personal_info_view_logs_functions(remove_logs_function):
+    user = UserFactory.create()
+    with freeze_time("2021-02-21 10:00"):
+        PersonalInfoViewLog.objects.create(
+            user=user,
+            viewer_user=user,
+            viewer_username=user.username,
+            field="some field",
+            viewer_user_full_name=user.get_full_name(),
+            viewer_user_email=user.email,
         )
 
-        with freeze_time("2021-02-21 10:00"):
-            PersonalInfoViewLog.objects.create(
-                user=cls.user,
-                viewer_user=cls.user,
-                viewer_username=cls.user.username,
-                field="some field",
-                viewer_user_full_name=cls.user.get_full_name(),
-                viewer_user_email=cls.user.email,
-            )
+    assert PersonalInfoViewLog.objects.exists() is True
 
-    @freeze_time("2023-02-22 06:00")
-    def test_remove_personal_info_view_logs_older_than_removes_older_than_two_years(
-        self,
-    ):
-        assert PersonalInfoViewLog.objects.exists() is True
+    with freeze_time("2023-02-22 06:00"):
+        remove_logs_function()
 
-        remove_personal_info_view_logs_older_than()
-
-        assert PersonalInfoViewLog.objects.exists() is False
-
-    @freeze_time("2023-02-22 06:00")
-    def test_remove_old_personal_info_view_logs_task_removes_older_than_two_years(self):
-        assert PersonalInfoViewLog.objects.exists() is True
-
-        remove_old_personal_info_view_logs()
-
-        assert PersonalInfoViewLog.objects.exists() is False
+    assert PersonalInfoViewLog.objects.exists() is False
--- users/tests/test_save_personal_info_view_log_task.py
+++ users/tests/test_save_personal_info_view_log_task.py
@@ -1,40 +1,33 @@
-from django.test import TestCase
+import pytest
 
 from tests.factories import UserFactory
 from users.models import PersonalInfoViewLog
 from users.tasks import save_personal_info_view_log
 
+pytestmark = [
+    pytest.mark.django_db,
+]
 
-class SavePersonalInfoViewLogTaskTestCase(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = UserFactory.create(
-            username="test_user",
-            first_name="Test",
-            last_name="User",
-            email="test.user@localhost",
-        )
 
-    def test_save_personal_info_view_log_as_own_user_does_not_save(self):
-        save_personal_info_view_log(self.user.id, self.user.id, "first_name")
+def test_save_personal_info_view_log__as_own_user_does_not_save():
+    user = UserFactory.create()
 
-        assert PersonalInfoViewLog.objects.exists() is False
+    save_personal_info_view_log(user.id, user.id, "first_name")
 
-    def test_save_personal_info_view_log_as_other_user_saves(self):
-        other = UserFactory.create(
-            username="other_user",
-            first_name="Other",
-            last_name="User",
-            email="other.user@localhost",
-        )
+    assert PersonalInfoViewLog.objects.exists() is False
 
-        save_personal_info_view_log(self.user.id, other.id, "first_name")
 
-        assert PersonalInfoViewLog.objects.exists() is True
-        view_log = PersonalInfoViewLog.objects.first()
+def test_save_personal_info_view_log__as_other_user_saves():
+    user = UserFactory.create()
+    other_user = UserFactory.create()
 
-        assert view_log.user == self.user
-        assert view_log.viewer_user == other
-        assert view_log.viewer_username == other.username
-        assert view_log.field == "first_name"
-        assert view_log.viewer_user_full_name == other.get_full_name()
+    save_personal_info_view_log(user.id, other_user.id, "first_name")
+
+    assert PersonalInfoViewLog.objects.exists() is True
+
+    view_log = PersonalInfoViewLog.objects.first()
+    assert view_log.user == user
+    assert view_log.viewer_user == other_user
+    assert view_log.viewer_username == other_user.username
+    assert view_log.field == "first_name"
+    assert view_log.viewer_user_full_name == other_user.get_full_name()
