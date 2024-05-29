85c1fc5a55bdbcff8d92fad79a550f626bbe1c1c
Matti Lamppu <matti.lamppu@vincit.fi>
2024-05-29 10:13:53 +0300

    Add translations
    Also remove Swedish translations.

--- tilavarauspalvelu/hooks/translations_done.py
+++ tilavarauspalvelu/hooks/translations_done.py
@@ -1,27 +1,88 @@
+from __future__ import annotations
+
 import os
 from collections import defaultdict
 from contextlib import contextmanager
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Literal
+from typing import TYPE_CHECKING
 
 import django
 import polib
+from django.conf import settings
 from django.core.management import call_command
 
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
 BASE_PATH = Path(__file__).resolve().parent.parent.parent
-LangType = Literal["fi", "sv"]
+
+
+@dataclass
+class TranslationData:
+    lang: str
+    path: str
+    contents_before: polib.POFile
 
 
 @dataclass
 class MissingTranslations:
-    missing_languages: set[LangType] = field(default_factory=set)
-    not_included_previous: set[LangType] = field(default_factory=set)
-    fuzzy: set[LangType] = field(default_factory=set)
+    missing_languages: set[str] = field(default_factory=set)
+    not_included_previous: set[str] = field(default_factory=set)
+    fuzzy: set[str] = field(default_factory=set)
+
+
+def main() -> int:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tilavarauspalvelu.settings")
+    django.setup()
+
+    items = _get_current_translations()
+    _run_translation_command()
+    missing = _find_missing_translations(items)
+
+    if missing:
+        _print_errors(missing)
+        return 1
+    return 0
+
+
+def _get_languages() -> Generator[str, None, None]:
+    # Enables adding more languages
+    yield from ["fi"]
+
+
+def _find_missing_translations(items: list[TranslationData]) -> dict[str, MissingTranslations]:
+    missing: dict[str, MissingTranslations] = defaultdict(MissingTranslations)
+    for item in items:
+        pofile = polib.pofile(item.path)
+        for entry in pofile:
+            key = entry.msgid
+            if entry.msgctxt is not None:
+                key = f"{entry.msgctxt} | {key}"
+
+            if entry.msgstr == "":
+                missing[key].missing_languages.add(item.lang)
+            if entry not in item.contents_before:
+                missing[key].not_included_previous.add(item.lang)
+            if "fuzzy" in entry.flags:
+                missing[key].fuzzy.add(item.lang)
+    return missing
+
+
+def _get_current_translations() -> list[TranslationData]:
+    return [
+        TranslationData(
+            lang=lang,
+            path=(path := str(locale_path / lang / "LC_MESSAGES" / "django.po")),
+            contents_before=polib.pofile(path),
+        )
+        for locale_path in settings.LOCALE_PATHS
+        for lang in _get_languages()
+    ]
 
 
 @contextmanager
-def working_directory(directory: Path):
+def working_directory(directory: Path) -> Generator[None, None, None]:
     current_path = Path.cwd()
     try:
         os.chdir(directory)
@@ -30,61 +91,28 @@ def working_directory(directory: Path):
         os.chdir(current_path)
 
 
-def main() -> int:
-    django.setup()
-    path_fi = BASE_PATH / "locale" / "fi" / "LC_MESSAGES" / "django.po"
-    # path_sv = BASE_PATH / "locale" / "sv" / "LC_MESSAGES" / "django.po"  # noqa: ERA001
-
-    missing: dict[str, MissingTranslations] = defaultdict(MissingTranslations)
-
-    contents_before_fi = polib.pofile(str(path_fi))
-    # contents_before_sv = polib.pofile(str(path_sv))  # noqa: ERA001
-
+def _run_translation_command() -> None:
     with working_directory(BASE_PATH):
         call_command(
             "maketranslations",
-            "-l",
-            "fi",
-            # "-l",
-            # "sv",
+            *(arg for lang in _get_languages() for arg in ["-l", lang]),
             "--no-obsolete",
             "--omit-header",
             "--add-location",
             "file",
         )
 
-    items: list[tuple[LangType, str, polib.POFile]] = [
-        ("fi", str(path_fi), contents_before_fi),
-        # ("sv", str(path_sv), contents_before_sv),  # noqa: ERA001
-    ]
 
-    for lang, path, contents_before in items:
-        pofile = polib.pofile(path)
-        for entry in pofile:
-            key = entry.msgid
-            if entry.msgctxt is not None:
-                key = f"{entry.msgctxt} | {key}"
-
-            if entry.msgstr == "":
-                missing[key].missing_languages.add(lang)
-            if entry not in contents_before:
-                missing[key].not_included_previous.add(lang)
-            if "fuzzy" in entry.flags:
-                missing[key].fuzzy.add(lang)
-
-    if missing:
-        print("\nIncomplete translations:")  # noqa: T201
-        for msg, info in missing.items():
-            print(f"  {msg}")  # noqa: T201
-            if len(info.missing_languages) > 0:
-                print("    ↳ Empty:", ", ".join(sorted(info.missing_languages)))  # noqa: T201
-            if len(info.not_included_previous) > 0:
-                print("    ↳ New:", ", ".join(sorted(info.not_included_previous)))  # noqa: T201
-            if len(info.fuzzy) > 0:
-                print("    ↳ Fuzzy:", ", ".join(sorted(info.fuzzy)))  # noqa: T201
-
-        return 1
-    return 0
+def _print_errors(missing: dict[str, MissingTranslations]) -> None:
+    print("\nIncomplete translations:")  # noqa: T201
+    for msg, info in missing.items():
+        print(f"  {msg}")  # noqa: T201
+        if len(info.missing_languages) > 0:
+            print("    ↳ Empty:", ", ".join(sorted(info.missing_languages)))  # noqa: T201
+        if len(info.not_included_previous) > 0:
+            print("    ↳ New:", ", ".join(sorted(info.not_included_previous)))  # noqa: T201
+        if len(info.fuzzy) > 0:
+            print("    ↳ Fuzzy:", ", ".join(sorted(info.fuzzy)))  # noqa: T201
 
 
 if __name__ == "__main__":
