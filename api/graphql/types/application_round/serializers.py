a464feeecd21fe4a9a2d953e8ed689268a5c900f
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-05-30 16:02:24 +0300

    Require all applications to be handled when marking round as handled

--- api/graphql/types/application_round/serializers.py
+++ api/graphql/types/application_round/serializers.py
@@ -1,10 +1,11 @@
 from typing import Any
 
 from graphene_django_extensions import NestingModelSerializer
+from lookup_property import L
 from rest_framework.exceptions import ValidationError
 
 from api.graphql.extensions import error_codes
-from applications.choices import ApplicationRoundStatusChoice
+from applications.choices import ApplicationRoundStatusChoice, ApplicationStatusChoice
 from applications.models import ApplicationRound
 from applications.tasks import generate_reservation_series_from_allocations
 from common.date_utils import local_datetime
@@ -21,8 +22,13 @@ class SetApplicationRoundHandledSerializer(NestingModelSerializer):
 
     def validate(self, attrs: dict[str, Any]) -> dict[str, Any]:
         if self.instance.status != ApplicationRoundStatusChoice.IN_ALLOCATION:
-            msg = "Application round is not in allocation state."
+            msg = "Application round is not in allocation status."
             raise ValidationError(msg, code=error_codes.APPLICATION_ROUND_NOT_IN_ALLOCATION)
+
+        if self.instance.applications.filter(L(status=ApplicationStatusChoice.IN_ALLOCATION)).exists():
+            msg = "Application round has applications still in allocation."
+            raise ValidationError(msg, code=error_codes.APPLICATION_ROUND_HAS_UNHANDLED_APPLICATIONS)
+
         return attrs
 
     def save(self, **kwargs: Any) -> ApplicationRound:
