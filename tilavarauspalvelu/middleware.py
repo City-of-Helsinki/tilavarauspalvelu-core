f061eec54656abb1e714b593a56bce1320a0d777
Matti Lamppu <matti.lamppu@vincit.fi>
2024-06-03 11:48:35 +0300

    Update middlewares
    - Fix issue in QueryLogging middleware if running synchronously
    - Add ProfilerMiddleware for profiling
    - Add GraphQLErrorLoggingMiddleware for logging GraphQL errors
      (remove stderr logging from GraphQLSentryMiddleware)

--- tilavarauspalvelu/middleware.py
+++ tilavarauspalvelu/middleware.py
@@ -3,13 +3,17 @@ import logging
 import time
 import traceback
 from collections.abc import Callable, Generator
+from functools import partial
 from typing import Any
 
 from django.conf import settings
 from django.core.handlers.wsgi import WSGIRequest
 from django.db import connection
 from django.http import HttpResponse
+from pyinstrument import Profiler
+from pyinstrument.renderers import HTMLRenderer, SpeedscopeRenderer
 
+from common.date_utils import local_datetime
 from common.typing import QueryInfo
 from utils.sentry import SentryLogger
 
@@ -51,8 +55,16 @@ class GraphQLSentryMiddleware:
         try:
             return next_(root, info, **kwargs)
         except Exception as err:
-            logger.info(traceback.format_exc())
             SentryLogger.log_exception(err, "Error in GraphQL query")
+            return err
+
+
+class GraphQLErrorLoggingMiddleware:
+    def resolve(self, next_, root, info, **kwargs):
+        try:
+            return next_(root, info, **kwargs)
+        except Exception as err:
+            logger.info(traceback.format_exc())
             return err
 
 
@@ -61,7 +73,6 @@ class QueryLoggingMiddleware:
 
     def __init__(self, get_response: Callable[[WSGIRequest], HttpResponse]) -> None:
         self.get_response = get_response
-        self.query_log: list[QueryInfo] = []
 
     def __call__(self, request: WSGIRequest) -> HttpResponse:
         skipped_route = any(request.path.startswith(path) for path in settings.QUERY_LOGGING_SKIP_ROUTES)
@@ -73,17 +84,19 @@ class QueryLoggingMiddleware:
 
     @contextlib.contextmanager
     def query_logger(self, request: WSGIRequest) -> Generator[None, None, None]:
-        self.query_log = []
+        query_log = []
         start = time.perf_counter_ns()
         try:
-            with connection.execute_wrapper(self.log):
+            with connection.execute_wrapper(partial(self.log, query_log=query_log)):
                 yield
         finally:
             try:
                 from common.tasks import save_sql_queries_from_request
 
+                # TODO: instead of saving all queries to graphql, only save when optimizer
+                #  counted different number of queries than what was actually executed.
                 save_sql_queries_from_request.delay(
-                    queries=self.query_log,
+                    queries=query_log,
                     path=request.path,
                     body=request.body,
                     duration_ms=(time.perf_counter_ns() - start) // 1_000_000,
@@ -98,9 +111,10 @@ class QueryLoggingMiddleware:
         params: tuple[Any, ...],
         many: bool,
         context: dict[str, Any],
+        query_log: list[QueryInfo],
     ) -> Any:
         query_info = QueryInfo(sql=sql, duration_ns=0, succeeded=True)
-        self.query_log.append(query_info)
+        query_log.append(query_info)
 
         start = time.perf_counter_ns()
         try:
@@ -112,3 +126,31 @@ class QueryLoggingMiddleware:
             query_info["duration_ns"] = time.perf_counter_ns() - start
 
         return result
+
+
+class ProfilerMiddleware:
+    def __init__(self, get_response: Callable[[WSGIRequest], HttpResponse]) -> None:
+        self.get_response = get_response
+
+    def __call__(self, request: WSGIRequest) -> HttpResponse:
+        if not request.path.startswith("/graphql") or not bool(request.headers.get("X-Profiler", "")):
+            return self.get_response(request)
+
+        profiler = Profiler()
+        sc_renderer = SpeedscopeRenderer()
+        html_renderer = HTMLRenderer()
+
+        profiler.start()
+        response = self.get_response(request)
+        profile_session = profiler.stop()
+
+        output_sc = sc_renderer.render(profile_session)
+        output_html = html_renderer.render(profile_session)
+
+        t = local_datetime().strftime("%Y-%m-%dT%H-%M-%S")
+        with open(f"graphql-{t}.{sc_renderer.output_file_extension}", mode="w", encoding="utf-8") as file:
+            file.write(output_sc)
+        with open(f"graphql-{t}.{html_renderer.output_file_extension}", mode="w", encoding="utf-8") as file:
+            file.write(output_html)
+
+        return response
