0855c2430c200f587081a50208dc84e99dfd1ac4
Matti Lamppu <matti.lamppu@vincit.fi>
2024-06-03 11:37:39 +0300

    Add some local env settings to example file

--- local_settings_example.py
+++ local_settings_example.py
@@ -7,6 +7,9 @@ os.environ.setdefault("DJANGO_SETTINGS_ENVIRONMENT", "Local")
 class LocalMixin:
     """Add custom local settings here."""
 
+    SOCIAL_AUTH_TUNNISTAMO_ALLOWED_REDIRECT_HOSTS = ["localhost:3000", "localhost:3001"]
+    CELERY_TASK_ALWAYS_EAGER = True
+
 
 class DockerMixin:
     """Add custom docker settings here."""
