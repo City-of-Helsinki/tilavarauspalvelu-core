352bbb861c9791506dbedc7e0c0fa1982f694ca6
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-06-05 20:41:26 +0300

    Add `ApplicationSectionStatusChoice.REJECTED`
    refs. TILA-3380

--- tests/test_factories/test_application_section.py
+++ tests/test_factories/test_application_section.py
@@ -1,6 +1,8 @@
 import pytest
+from lookup_property import L
 
-from applications.choices import ApplicationSectionStatusChoice
+from applications.choices import ApplicationSectionStatusChoice, ApplicationStatusChoice
+from applications.models import ApplicationSection
 from tests.factories import ApplicationSectionFactory
 
 # Applied to all tests
@@ -10,13 +12,17 @@ pytestmark = [
 
 
 @pytest.mark.parametrize(
-    "status",
+    ("status", "application_status"),
     [
-        ApplicationSectionStatusChoice.UNALLOCATED,
-        ApplicationSectionStatusChoice.IN_ALLOCATION,
-        ApplicationSectionStatusChoice.HANDLED,
+        (ApplicationSectionStatusChoice.UNALLOCATED, ApplicationStatusChoice.DRAFT),
+        (ApplicationSectionStatusChoice.IN_ALLOCATION, ApplicationStatusChoice.IN_ALLOCATION),
+        (ApplicationSectionStatusChoice.HANDLED, ApplicationStatusChoice.HANDLED),
+        (ApplicationSectionStatusChoice.REJECTED, ApplicationStatusChoice.HANDLED),
     ],
 )
-def test_application_section_factory_create_in_status(status):
+def test_application_section_factory_create_in_status(status, application_status):
     section = ApplicationSectionFactory.create_in_status(status=status)
     assert section.status == status
+    assert ApplicationSection.objects.filter(L(status=status.value)).exists()
+
+    assert section.application.status == application_status
