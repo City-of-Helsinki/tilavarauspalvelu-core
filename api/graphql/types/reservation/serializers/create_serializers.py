f9bacea46e758057d29260a8cd2d1acc8ffa17d1
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-07-08 15:44:44 +0300

    ruff: Fix lint errors from upgrade

--- api/graphql/types/reservation/serializers/create_serializers.py
+++ api/graphql/types/reservation/serializers/create_serializers.py
@@ -233,9 +233,7 @@ class ReservationCreateSerializer(OldPrimaryKeySerializer, ReservationPriceMixin
             return
 
         if prefill_info is not None:
-            for key, value in prefill_info.items():
-                if value is not None:
-                    data[key] = value
+            data.update({key: value for key, value in prefill_info.items() if value is not None})
 
     def check_sku(self, current_sku, new_sku) -> None:
         if current_sku is not None and current_sku != new_sku:
