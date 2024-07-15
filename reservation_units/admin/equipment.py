958e0e63ace67fd7fd9fe946ac1dcf12c7d126f6
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-07-15 17:43:56 +0300

    Django Admin: Add new columns to Equipment and EquipmentCategory
    refs. TILA-3129

--- reservation_units/admin/equipment.py
+++ reservation_units/admin/equipment.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.core.handlers.wsgi import WSGIRequest
+from django.db import models
 
 from reservation_units.models import Equipment, EquipmentCategory
 
@@ -7,7 +9,33 @@ from reservation_units.models import Equipment, EquipmentCategory
 class EquipmentAdmin(admin.ModelAdmin):
     model = Equipment
 
+    list_display = [
+        "name",
+        "category",
+    ]
+    list_filter = [
+        "category",
+    ]
+
 
 @admin.register(EquipmentCategory)
 class EquipmentCategoryAdmin(admin.ModelAdmin):
     model = EquipmentCategory
+
+    list_display = [
+        "name",
+        "number_of_equipment",
+    ]
+
+    @admin.display(ordering="number_of_equipment")
+    def number_of_equipment(self, obj: EquipmentCategory) -> str:
+        return getattr(obj, "number_of_equipment", -1)
+
+    def get_queryset(self, request: WSGIRequest) -> models.QuerySet:
+        return (
+            super()
+            .get_queryset(request)
+            .annotate(
+                number_of_equipment=models.Count("equipment"),
+            )
+        )
