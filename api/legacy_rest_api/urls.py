b8ca2253e08641f1ff6cf29e50435f67f7136efe
Matti Lamppu <matti.lamppu@vincit.fi>
2024-08-07 09:16:19 +0300

    Remove old legacy API endpoints

--- api/legacy_rest_api/urls.py
+++ api/legacy_rest_api/urls.py
@@ -1,10 +1,6 @@
 from rest_framework import routers
 
-from .views import RecurringReservationViewSet, ReservationIcalViewset, ReservationUnitViewSet, ReservationViewSet
+from .views import ReservationIcalViewset
 
-legacy_outer = routers.DefaultRouter()
-
-legacy_outer.register(r"reservation_unit", ReservationUnitViewSet, "reservationunit")
-legacy_outer.register(r"reservation", ReservationViewSet, "reservation")
-legacy_outer.register(r"recurring_reservation", RecurringReservationViewSet, "recurring_reservation")
-legacy_outer.register(r"reservation_calendar", ReservationIcalViewset, "reservation_calendar")
+legacy_router = routers.DefaultRouter()
+legacy_router.register(r"reservation_calendar", ReservationIcalViewset, "reservation_calendar")
--- api/legacy_rest_api/views.py
+++ api/legacy_rest_api/views.py
@@ -1,40 +1,19 @@
 import hmac
 import io
-from typing import TYPE_CHECKING
+from typing import Any
 
-from dateutil.parser import parse
-from django.conf import settings
-from django.db.models import Prefetch, Q, Sum
 from django.http import FileResponse
-from django_filters import rest_framework as filters
-from rest_framework import filters as drf_filters
-from rest_framework import serializers, viewsets
-from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
-from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework.viewsets import ViewSet
 
-from api.legacy_rest_api.permissions import (
-    RecurringReservationPermission,
-    ReservationPermission,
-    ReservationUnitPermission,
-)
-from opening_hours.utils.summaries import get_resources_total_hours_per_resource
-from permissions.helpers import has_general_permission
-from permissions.models import GeneralPermissionChoices, UnitPermissionChoices
-from reservation_units.models import Equipment, ReservationUnit
-from reservations.models import RecurringReservation, Reservation
-from resources.models import Resource
-from spaces.models import Space
+from reservations.models import Reservation
 
-from .filtersets import RecurringReservationFilter, ReservationFilter, ReservationUnitFilter
-from .serializers import RecurringReservationSerializer, ReservationSerializer, ReservationUnitSerializer
 from .utils import export_reservation_events, get_host, hmac_signature, reservation_unit_calendar
 
-if TYPE_CHECKING:
-    import datetime
-
-    from common.typing import AnyUser
+__all__ = [
+    "ReservationIcalViewset",
+]
 
 
 class ReservationIcalViewset(ViewSet):
@@ -45,7 +24,7 @@ class ReservationIcalViewset(ViewSet):
     def get_object(self):
         return Reservation.objects.filter(pk=self.kwargs["pk"]).prefetch_related("reservation_unit").first()
 
-    def retrieve(self, request, *args, **kwargs):
+    def retrieve(self, request: Request, *args: Any, **kwargs: Any) -> FileResponse:
         hash_pram = request.query_params.get("hash", None)
         if hash_pram is None:
             raise ValidationError("hash is required")
@@ -69,245 +48,3 @@ class ReservationIcalViewset(ViewSet):
         buffer.seek(0)
 
         return FileResponse(buffer, as_attachment=True, filename="reservation_calendar.ics")
-
-
-class RecurringReservationViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = RecurringReservationSerializer
-    permission_classes = [RecurringReservationPermission]
-    queryset = (
-        RecurringReservation.objects.all()
-        .select_related(
-            "age_group",
-            "application_event_schedule__application_event__purpose",
-        )
-        .prefetch_related(
-            Prefetch(
-                "reservations",
-                queryset=(
-                    Reservation.objects.all()
-                    .select_related(
-                        "user",
-                        (
-                            "recurring_reservation__application_event_schedule__"
-                            "application_event__application__organisation"
-                        ),
-                    )
-                    .prefetch_related(
-                        Prefetch(
-                            "reservation_unit",
-                            queryset=(
-                                ReservationUnit.objects.all()
-                                .select_related("reservation_unit_type", "unit")
-                                .prefetch_related(
-                                    "services",
-                                    "images",
-                                    "unit",
-                                    Prefetch(
-                                        "equipments",
-                                        queryset=Equipment.objects.all().only("id"),
-                                    ),
-                                    Prefetch(
-                                        "spaces",
-                                        queryset=Space.objects.all().select_related("building", "location"),
-                                    ),
-                                    Prefetch(
-                                        "resources",
-                                        queryset=Resource.objects.all().select_related("space"),
-                                    ),
-                                )
-                            ),
-                        )
-                    )
-                ),
-            )
-        )
-    )
-    filter_backends = [
-        drf_filters.OrderingFilter,
-        filters.DjangoFilterBackend,
-        drf_filters.SearchFilter,
-    ]
-    filterset_class = RecurringReservationFilter
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-
-        user: AnyUser = self.request.user
-
-        if user.is_anonymous:
-            return queryset.none()
-        if user.is_superuser:
-            return queryset
-        if has_general_permission(user, GeneralPermissionChoices.CAN_VIEW_RESERVATIONS):
-            return queryset
-
-        unit_permission = UnitPermissionChoices.CAN_VIEW_RESERVATIONS.value
-        unit_ids = [pk for pk, perms in user.unit_permissions.items() if unit_permission in perms]
-        unit_group_ids = [pk for pk, perms in user.unit_group_permissions.items() if unit_permission in perms]
-
-        return queryset.filter(
-            Q(user=user)
-            | Q(reservations__reservation_unit__unit__in=unit_ids)
-            | Q(reservations__reservation_unit__unit__unit_groups__in=unit_group_ids)
-        ).distinct()
-
-
-class ReservationUnitViewSet(viewsets.ModelViewSet):
-    serializer_class = ReservationUnitSerializer
-    filter_backends = [
-        drf_filters.OrderingFilter,
-        filters.DjangoFilterBackend,
-        drf_filters.SearchFilter,
-    ]
-    ordering_fields = ["name", "max_persons_sum"]
-    filterset_class = ReservationUnitFilter
-    search_fields = ["name"]
-    permission_classes = [ReservationUnitPermission]
-
-    def get_queryset(self):
-        return (
-            ReservationUnit.objects.annotate(max_persons_sum=Sum("spaces__max_persons"))
-            .all()
-            .select_related("reservation_unit_type", "unit")
-            .prefetch_related(
-                "services",
-                "images",
-                "unit",
-                "purposes",
-                Prefetch("equipments", queryset=Equipment.objects.all().only("id")),
-                Prefetch(
-                    "spaces",
-                    queryset=Space.objects.all().select_related("building", "location"),
-                ),
-                Prefetch("resources", queryset=Resource.objects.all().select_related("space")),
-            )
-        )
-
-    @action(detail=False, methods=["get"])
-    def capacity(self, request, pk=None):
-        reservation_units = request.query_params.get("reservation_unit")
-        period_start: str | None = self.request.query_params.get("period_start")
-        period_end: str | None = self.request.query_params.get("period_end")
-
-        if not (period_start and period_end):
-            raise serializers.ValidationError("Parameters period_start and period_end are required.")
-        try:
-            period_start_date: datetime.date = parse(period_start).date()
-            period_end_date: datetime.date = parse(period_end).date()
-        except (ValueError, OverflowError) as err:
-            raise serializers.ValidationError("Wrong date format. Use YYYY-MM-dd") from err
-
-        if not reservation_units:
-            raise serializers.ValidationError("reservation_unit parameter is required.")
-
-        try:
-            reservation_units = [int(res_unit) for res_unit in reservation_units.split(",")]
-        except ValueError as err:
-            raise serializers.ValidationError("Given reservation unit id is not an integer") from err
-
-        reservation_unit_qs = ReservationUnit.objects.filter(id__in=reservation_units)
-
-        hauki_resource_ids = (
-            reservation_unit_qs.filter(origin_hauki_resource__isnull=False)
-            .values_list("origin_hauki_resource_id", flat=True)
-            .distinct()
-        )
-        total_opening_hours = get_resources_total_hours_per_resource(
-            hauki_resource_ids,
-            period_start_date,
-            period_end_date,
-        )
-
-        result_data = []
-        for res_unit in reservation_unit_qs:
-            total_duration = (
-                Reservation.objects.going_to_occur()
-                .filter(reservation_unit=res_unit)
-                .within_period(period_start=period_start_date, period_end=period_end_date)
-                .total_duration()
-            ).get("total_duration")
-
-            total_duration = total_duration.total_seconds() / 3600 if total_duration else 0
-            resource_id = f"{settings.HAUKI_ORIGIN_ID}:{res_unit.uuid}"
-            result_data.append(
-                {
-                    "id": res_unit.id,
-                    "hour_capacity": total_opening_hours.get(resource_id),
-                    "reservation_duration_total": total_duration,
-                    "period_start": period_start_date,
-                    "period_end": period_end_date,
-                }
-            )
-        return Response(result_data)
-
-
-class ReservationViewSet(viewsets.ModelViewSet):
-    serializer_class = ReservationSerializer
-    permission_classes = [ReservationPermission]
-
-    filter_backends = [
-        drf_filters.OrderingFilter,
-        filters.DjangoFilterBackend,
-        drf_filters.SearchFilter,
-    ]
-
-    filterset_class = ReservationFilter
-    queryset = (
-        Reservation.objects.all()
-        .select_related(
-            "user",
-            "recurring_reservation__application_event_schedule__application_event__application__organisation",
-        )
-        .prefetch_related(
-            Prefetch(
-                "reservation_unit",
-                queryset=(
-                    ReservationUnit.objects.all()
-                    .select_related("reservation_unit_type", "unit")
-                    .prefetch_related(
-                        "services",
-                        "images",
-                        "unit",
-                        Prefetch("equipments", queryset=Equipment.objects.all().only("id")),
-                        Prefetch(
-                            "spaces",
-                            queryset=Space.objects.all().select_related("building", "location"),
-                        ),
-                        Prefetch(
-                            "resources",
-                            queryset=Resource.objects.all().select_related("space"),
-                        ),
-                    )
-                ),
-            )
-        )
-    )
-
-    def perform_create(self, serializer) -> None:
-        serializer.save(user_id=self.request.user.pk)
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-
-        user: AnyUser = self.request.user
-
-        if user.is_anonymous:
-            return queryset.none()
-        if user.is_superuser:
-            return queryset
-        if has_general_permission(user, GeneralPermissionChoices.CAN_VIEW_RESERVATIONS):
-            return queryset
-
-        unit_permission = UnitPermissionChoices.CAN_VIEW_RESERVATIONS.value
-        unit_ids = [pk for pk, perms in user.unit_permissions.items() if unit_permission in perms]
-        unit_group_ids = [pk for pk, perms in user.unit_group_permissions.items() if unit_permission in perms]
-
-        return (
-            queryset.filter(
-                Q(user=user)
-                | Q(reservation_unit__unit__in=unit_ids)
-                | Q(reservation_unit__unit__unit_groups__in=unit_group_ids)
-            )
-            .distinct()
-            .order_by("begin")
-        )
