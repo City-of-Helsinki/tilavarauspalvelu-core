9379d2a6e05349c82d3d326a3b1f2eea774d9020
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-08-13 21:57:13 +0300

    Simplify `update_reservation_unit_pricings` task
    - Refactor `update_reservation_unit_pricings` tests

--- reservation_units/pricing_updates.py
+++ reservation_units/pricing_updates.py
@@ -6,18 +6,11 @@ def update_reservation_unit_pricings(today: date) -> int:
     from reservation_units.models import ReservationUnitPricing
 
     future_pricings = ReservationUnitPricing.objects.filter(status=PricingStatus.PRICING_STATUS_FUTURE, begins=today)
-    num_updated = 0
     for future_pricing in future_pricings:
-        active_pricing = future_pricing.reservation_unit.pricings.filter(
-            status=PricingStatus.PRICING_STATUS_ACTIVE
-        ).first()
-
-        if active_pricing is not None:
-            active_pricing.status = PricingStatus.PRICING_STATUS_PAST
-            active_pricing.save()
+        active_pricings = future_pricing.reservation_unit.pricings.filter(status=PricingStatus.PRICING_STATUS_ACTIVE)
+        active_pricings.update(status=PricingStatus.PRICING_STATUS_PAST)
 
         future_pricing.status = PricingStatus.PRICING_STATUS_ACTIVE
         future_pricing.save()
 
-        num_updated += 1
-    return num_updated
+    return len(future_pricings)
