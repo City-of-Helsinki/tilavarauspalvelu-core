905ff3f477902fa8720b0d3343881f2955856d6b
Matti Lamppu <matti.lamppu@vincit.fi>
2024-08-19 15:21:29 +0300

    Add permissions for roles to GraphQL API

--- api/graphql/types/permissions/types.py
+++ api/graphql/types/permissions/types.py
@@ -1,5 +1,11 @@
+import graphene
+from django.db import models
 from graphene_django_extensions import DjangoNode
+from query_optimizer import ManuallyOptimizedField
+from query_optimizer.optimizer import QueryOptimizer
 
+from common.typing import AnyUser, GQLInfo
+from permissions.enums import UserPermissionChoice
 from permissions.models import GeneralRole, UnitRole
 
 __all__ = [
@@ -7,8 +13,12 @@ __all__ = [
     "UnitRoleNode",
 ]
 
+from spaces.models import Unit, UnitGroup
+
 
 class GeneralRoleNode(DjangoNode):
+    permissions = graphene.List(graphene.Enum.from_enum(UserPermissionChoice))
+
     class Meta:
         model = GeneralRole
         fields = [
@@ -17,10 +27,19 @@ class GeneralRoleNode(DjangoNode):
             "assigner",
             "created",
             "modified",
+            "permissions",
         ]
+
+    def resolve_permissions(root: GeneralRole, info: GQLInfo) -> list[UserPermissionChoice]:
+        user: AnyUser = info.context.user
+        if user.is_anonymous or not user.is_active:
+            return []
+        return user.general_permissions
 
 
 class UnitRoleNode(DjangoNode):
+    permissions = ManuallyOptimizedField(graphene.List(graphene.Enum.from_enum(UserPermissionChoice)))
+
     class Meta:
         model = UnitRole
         fields = [
@@ -31,4 +50,44 @@ class UnitRoleNode(DjangoNode):
             "assigner",
             "created",
             "modified",
+            "permissions",
         ]
+
+    def resolve_permissions(root: UnitRole, info: GQLInfo) -> list[UserPermissionChoice]:
+        user: AnyUser = info.context.user
+        if user.is_anonymous or not user.is_active:
+            return []
+
+        permissions: list[UserPermissionChoice] = []
+        for unit in root.units.all():
+            permissions += user.unit_permissions_map.get(unit.pk, [])
+        for unit_group in root.unit_groups.all():
+            permissions += user.unit_group_permissions_map.get(unit_group.pk, [])
+
+        return sorted(set(permissions))
+
+    @staticmethod
+    def optimize_permissions(queryset: models.QuerySet, optimizer: QueryOptimizer) -> models.QuerySet:
+        unit_optimizer = optimizer.get_or_set_child_optimizer(
+            name="units",
+            optimizer=QueryOptimizer(
+                Unit,
+                optimizer.info,
+                name="units",
+                parent=optimizer,
+            ),
+            set_as="prefetch_related",
+        )
+        unit_optimizer.only_fields.append("pk")
+        unit_group_optimizer = optimizer.get_or_set_child_optimizer(
+            name="unit_groups",
+            optimizer=QueryOptimizer(
+                UnitGroup,
+                optimizer.info,
+                name="unit_groups",
+                parent=optimizer,
+            ),
+            set_as="prefetch_related",
+        )
+        unit_group_optimizer.only_fields.append("pk")
+        return queryset
