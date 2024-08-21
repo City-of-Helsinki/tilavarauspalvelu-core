58c0b7e62a00d268f318c6a070b3cf61d230461f
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-08-21 18:57:56 +0300

    Split merchants app models into separate files

--- api/graphql/types/reservation/serializers/refund_serializers.py
+++ api/graphql/types/reservation/serializers/refund_serializers.py
@@ -6,7 +6,8 @@ from django.utils.timezone import get_default_timezone
 from api.graphql.extensions.serializers import OldPrimaryKeySerializer
 from api.graphql.extensions.validation_errors import ValidationErrorCodes, ValidationErrorWithCode
 from common.utils import comma_sep_str
-from merchants.models import OrderStatus, PaymentOrder
+from merchants.enums import OrderStatus
+from merchants.models import PaymentOrder
 from reservations.enums import ReservationStateChoice
 from reservations.models import Reservation
 from reservations.tasks import refund_paid_reservation_task
