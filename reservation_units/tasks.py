363c79b32e1a67eb11da136b954aea014ef211e5
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-09-02 15:12:10 +0300

    Remove task for updating pricing statuses

--- reservation_units/tasks.py
+++ reservation_units/tasks.py
@@ -12,22 +12,12 @@ from merchants.verkkokauppa.product.exceptions import CreateOrUpdateAccountingEr
 from merchants.verkkokauppa.product.types import CreateOrUpdateAccountingParams, CreateProductParams
 from merchants.verkkokauppa.verkkokauppa_api_client import VerkkokauppaAPIClient
 from reservation_units.enums import PricingStatus, PricingType
-from reservation_units.pricing_updates import update_reservation_unit_pricings
 from reservation_units.utils.reservation_unit_payment_helper import ReservationUnitPaymentHelper
 from tilavarauspalvelu.celery import app
 from utils.image_cache import purge
 from utils.sentry import SentryLogger
 
 logger = logging.getLogger(__name__)
-
-
-@app.task(name="update_reservation_unit_pricings")
-def _update_reservation_unit_pricings() -> None:
-    today = date.today()
-
-    logger.info(f"Updating reservation unit pricing with date {today}")
-    num_updated = update_reservation_unit_pricings(today)
-    logger.info(f"Updated {num_updated} reservation units with date {today}")
 
 
 @app.task(name="update_reservation_unit_pricings_tax_percentage")
--- tests/test_models/test_reservation_unit_pricing_updates.py
+++ tests/test_models/test_reservation_unit_pricing_updates.py
@@ -5,104 +5,15 @@ import pytest
 
 from reservation_units.enums import PricingStatus, PricingType
 from reservation_units.models import ReservationUnitPricing
-from reservation_units.pricing_updates import update_reservation_unit_pricings
 from reservation_units.tasks import update_reservation_unit_pricings_tax_percentage
 from tests.factories import ReservationUnitFactory, ReservationUnitPricingFactory
 from tests.helpers import patch_method
-from tilavarauspalvelu.utils.auditlog_util import AuditLogger
 from utils.sentry import SentryLogger
 
 # Applied to all tests
 pytestmark = [
     pytest.mark.django_db,
 ]
-
-
-####################################
-# update_reservation_unit_pricings #
-####################################
-
-
-def test_reservation_unit__update_pricings__updated(settings):
-    # Enable audit logging for ReservationUnitPricing
-    # This tests for regression for a bug that was found during manual testing,
-    # where this task ran fine in tests but not on the server.
-    settings.AUDIT_LOGGING_ENABLED = True
-    AuditLogger.register(ReservationUnitPricing)
-
-    reservation_unit = ReservationUnitFactory.create(name="Unit that should be updated")
-    ReservationUnitPricingFactory.create(
-        begins=datetime.date(2022, 1, 1),
-        status=PricingStatus.PRICING_STATUS_ACTIVE,
-        reservation_unit=reservation_unit,
-    )
-    ReservationUnitPricingFactory.create(
-        begins=datetime.date(2022, 9, 19),
-        status=PricingStatus.PRICING_STATUS_FUTURE,
-        reservation_unit=reservation_unit,
-    )
-
-    today = datetime.date(2022, 9, 19)
-    num_updated = update_reservation_unit_pricings(today)
-
-    assert num_updated == 1
-
-    active_pricing = reservation_unit.pricings.filter(status=PricingStatus.PRICING_STATUS_ACTIVE).first()
-    future_pricing = reservation_unit.pricings.filter(status=PricingStatus.PRICING_STATUS_FUTURE).first()
-    past_pricing = reservation_unit.pricings.filter(status=PricingStatus.PRICING_STATUS_PAST).first()
-    assert active_pricing.begins == today
-    assert future_pricing is None
-    assert past_pricing.begins == datetime.date(2022, 1, 1)
-
-
-def test_reservation_unit__update_pricings__not_update__non_matching_date():
-    reservation_unit = ReservationUnitFactory.create()
-    ReservationUnitPricingFactory.create(
-        begins=datetime.date(2022, 1, 1),
-        status=PricingStatus.PRICING_STATUS_ACTIVE,
-        reservation_unit=reservation_unit,
-    )
-    ReservationUnitPricingFactory.create(
-        begins=datetime.date(2022, 9, 20),
-        status=PricingStatus.PRICING_STATUS_FUTURE,
-        reservation_unit=reservation_unit,
-    )
-
-    today = datetime.date(2022, 9, 19)
-    num_updated = update_reservation_unit_pricings(today)
-    assert num_updated == 0
-
-    active_pricing = reservation_unit.pricings.filter(status=PricingStatus.PRICING_STATUS_ACTIVE).first()
-    future_pricing = reservation_unit.pricings.filter(status=PricingStatus.PRICING_STATUS_FUTURE).first()
-    past_pricing = reservation_unit.pricings.filter(status=PricingStatus.PRICING_STATUS_PAST).first()
-    assert active_pricing.begins == datetime.date(2022, 1, 1)
-    assert future_pricing.begins == datetime.date(2022, 9, 20)
-    assert past_pricing is None
-
-
-def test_reservation_unit__update_pricings__not_update__no_future_pricing():
-    reservation_unit = ReservationUnitFactory.create()
-    ReservationUnitPricingFactory.create(
-        begins=datetime.date(2022, 1, 1),
-        status=PricingStatus.PRICING_STATUS_ACTIVE,
-        reservation_unit=reservation_unit,
-    )
-
-    today = datetime.date(2022, 9, 19)
-    num_updated = update_reservation_unit_pricings(today)
-    assert num_updated == 0
-
-    active_pricing = reservation_unit.pricings.filter(status=PricingStatus.PRICING_STATUS_ACTIVE).first()
-    future_pricing = reservation_unit.pricings.filter(status=PricingStatus.PRICING_STATUS_FUTURE).first()
-    past_pricing = reservation_unit.pricings.filter(status=PricingStatus.PRICING_STATUS_PAST).first()
-    assert active_pricing.begins == datetime.date(2022, 1, 1)
-    assert future_pricing is None
-    assert past_pricing is None
-
-
-###################################################
-# update_reservation_unit_pricings_tax_percentage #
-###################################################
 
 
 TAX_CHANGE_DATE = datetime.date(2024, 9, 1)
