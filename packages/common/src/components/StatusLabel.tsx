d32160359a757664756363575aa7c0c8748814d3
Mats Eriksson <mats.eriksson@vincit.fi>
2024-09-04 19:59:40 +0300

    add: StatusLabel component into common components
    - our StatusLabel extends the HDS StatusLabel, to also handle the
      “draft” type - and uses tilavaraus colour-scheme

---
+++ packages/common/src/components/StatusLabel.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {
+  StatusLabel as HDSStatusLabel,
+  type StatusLabelType as HDSStatusLabelType,
+} from "hds-react";
+import styled from "styled-components";
+
+export type StatusLabelType = HDSStatusLabelType | "draft";
+
+const handleColorType = ($type: StatusLabelType) => {
+  switch ($type) {
+    case "info":
+      return "var(--color-coat-of-arms-light)";
+    case "alert":
+      return "var(--color-engel-medium-light)";
+    case "success":
+      return "var(--color-tram-light)";
+    case "error":
+      return "var(--color-metro-medium-light)";
+    case "draft":
+      return "var(--color-suomenlinna-medium-light)";
+    case "neutral":
+    default:
+      return "var(--color-silver)";
+  }
+};
+
+const ColoredLabel = styled(HDSStatusLabel)<{
+  $type: StatusLabelType;
+}>`
+  --status-label-background: ${(props) => handleColorType(props.$type)};
+  --status-label-color: var(--color-black);
+  white-space: nowrap;
+`;
+
+/*
+ * StatusLabel component (extends HDS StatusLabel with "draft" type)
+ * @param {StatusLabelWithDraftType} type - StatusLabel type (neutral, info, alert, success, error, draft)
+ * @param {JSX.Element} icon - Icon element
+ * @param {React.ReactNode} children - Label text
+ */
+function StatusLabel({
+  type,
+  icon,
+  dataTestId,
+  children,
+}: {
+  type: StatusLabelType;
+  icon: JSX.Element;
+  dataTestId?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <ColoredLabel
+      type={type === "draft" ? "neutral" : type} // HDS StatusLabel does not support "draft" type - so convert it to "neutral"
+      iconLeft={icon}
+      dataTestId={dataTestId}
+      $type={type}
+    >
+      {children}
+    </ColoredLabel>
+  );
+}
+
+export default StatusLabel;
