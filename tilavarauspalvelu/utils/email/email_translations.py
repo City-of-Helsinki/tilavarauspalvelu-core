eb7a20148b8f73f4eea22665da557a3f9dee13f4
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-10-01 18:14:52 +0300

    Email: Add localisation for text email templates

---
+++ tilavarauspalvelu/utils/email/email_translations.py
@@ -0,0 +1,304 @@
+# ruff: noqa: E501
+from config.utils.commons import LanguageType
+
+EMAIL_TRANSLATIONS: dict[str, dict[LanguageType, str]] = {
+    "subjectConfirmed": {
+        "fi": "Varauksesi on vahvistettu",
+        "en": "Thank you for your booking at Varaamo",
+        "sv": "Din bokning är bekräftad",
+    },
+    "subjectPreliminary": {
+        "fi": "Varauksesi odottaa käsittelyä",
+        "en": "Your booking is waiting for processing",
+        "sv": "Din bokning väntar på att behandlas",
+    },
+    "subjectHandledConfirmed": {
+        "fi": "Varauksesi on vahvistettu",
+        "en": "Your booking is confirmed",
+        "sv": "Din bokning är bekräftad",
+    },
+    "subjectCancelled": {
+        "fi": "Varauksesi on peruttu",
+        "en": "Your booking has been cancelled",
+        "sv": "Din bokning har avbokats",
+    },
+    "subjectRejected": {
+        "fi": "Valitettavasti varaustasi ei voida vahvistaa",
+        "en": "Unfortunately your booking cannot be confirmed",
+        "sv": "Tyvärr kan vi inte bekräfta din bokning",
+    },
+    "subjectUpdated": {
+        "fi": "Varaustasi on muutettu",
+        "en": "Your booking has been updated",
+        "sv": "Din bokning har uppdaterats",
+    },
+    "subjectStaffReservationMade": {
+        "fi": "Toimipisteeseen {{unit_name}} on tehty uusi tilavaraus {{reservation_number}}",
+        "en": "New booking {{reservation_number}} has been made for {{unit_name}}",
+        "sv": "Ny bokning {{reservation_number}} har gjorts för {{unit_name}}",
+    },
+    "subjectStaffReservationPending": {
+        "fi": "Uusi tilavaraus {{reservation_number}} odottaa käsittelyä toimipisteessä {{unit_name}}",
+        "en": "New booking {{reservation_number}} requires handling at unit {{unit_name}}",
+        "sv": "Ny bokningsförfrågan {{reservation_number}} för {{reservation_unit}} väntar på at behandlats",
+    },
+    #
+    "serviceName": {
+        "fi": "Varaamo",
+        "en": "Varaamo",
+        "sv": "Varaamo",
+    },
+    "serviceUrl": {
+        "fi": "varaamo.hel.fi",
+        "en": "varaamo.hel.fi/en/",
+        "sv": "varaamo.hel.fi/sv/",
+    },
+    "handleReservationsUrl": {
+        "fi": "https://tilavaraus.hel.fi/kasittely/reservations/",
+        "en": "https://tilavaraus.hel.fi/kasittely/reservations/",
+        "sv": "https://tilavaraus.hel.fi/kasittely/reservations/",
+    },
+    "helsinkiCity": {
+        "fi": "Helsingin kaupunki",
+        "en": "City of Helsinki",
+        "sv": "Helsingfors stad",
+    },
+    "salutation": {
+        "fi": "Hei",
+        "en": "Hi",
+        "sv": "Hej",
+    },
+    "oClock": {
+        "fi": "klo",
+        "en": "at",
+        "sv": "kl.",
+    },
+    #
+    "vatIncluded": {
+        "fi": "sis. alv",
+        "en": "incl. VAT",
+        "sv": "inkl. moms",
+    },
+    "paymentDue": {
+        "fi": "Eräpäivä",
+        "en": "Due date",
+        "sv": "Förfallodatum",
+    },
+    "payReservation": {
+        "fi": "Maksa varaus",
+        "en": "Pay the booking",
+        "sv": "Betala bokningen",
+    },
+    #
+    "manageReservation": {
+        "fi": "Hallitse varaustasi Varaamossa. Voit perua varauksesi ja tarkistaa varauksen tiedot sekä Varaamon sopimus- ja peruutusehdot",
+        "en": "Manage your booking at Varaamo. You can check the details of your booking and Varaamo's terms of contract and cancellation on the",
+        "sv": "Hantera din bokning på Varaamo. Du kan kontrollera uppgifterna om din bokning samt Varaamos avtals- och avbokningsvillkor på sidan",
+    },
+    "ownReservationsPage": {
+        "fi": "Omat Varaukset -sivulla",
+        "en": "'My bookings' page",
+        "sv": "”Mina bokningar”",
+    },
+    "thankYouForUsing": {
+        "fi": "Kiitos, kun käytit Varaamoa!",
+        "en": "Thank you for choosing Varaamo!",
+        "sv": "Tack för att du använder Varaamo!",
+    },
+    #
+    "withRegards": {
+        "fi": "Ystävällisin terveisin,",
+        "en": "Kind regards",
+        "sv": "Med vänliga hälsningar,",
+    },
+    "automaticMessage": {
+        "fi": "Tämä on automaattinen viesti, johon ei voi vastata.",
+        "en": "This is an automated message, please do not reply.",
+        "sv": "Detta är ett automatiskt meddelande som inte kan besvaras.",
+    },
+    "contactUs": {
+        "fi": "Ota yhteyttä",
+        "en": "Contact us",
+        "sv": "Ta kontakt",
+    },
+    "reserveCityResourcesAt": {
+        "fi": "Varaa kaupungin tiloja ja laitteita käyttöösi helposti osoitteessa",
+        "en": "Book the city's premises and equipment for your use at",
+        "sv": "Boka enkelt stadens lokaler och utrustning för eget bruk på",
+    },
+    #
+    "reservationMadeConfirmed": {
+        "fi": "Olet tehnyt uuden varauksen:",
+        "en": "You have made a new booking:",
+        "sv": "Du har gjort en ny bokning:",
+    },
+    "reservationMadePreliminary": {
+        "fi": "Olet tehnyt alustavan varauksen:",
+        "en": "You have made a new booking request:",
+        "sv": "Du har gjort en ny bokningsförfrågan:",
+    },
+    "reservationHandledConfirmed": {
+        "fi": "Varauksesi on nyt vahvistettu.",
+        "en": "Your booking is now confirmed.",
+        "sv": "Din bokning har bekräftats.",
+    },
+    "reservationHandledSubsidized": {
+        "fi": "Varauksesi on hyväksytty, ja varaukseen on myönnetty seuraava alennus:",
+        "en": "Your booking has been confirmed with the following discount:",
+        "sv": "Din bokning har bekräftats med följande rabatt:",
+    },
+    "reservationNeedsPayment": {
+        "fi": "Varauksesi on hyväksytty, ja sen voi maksaa pankkitunnuksilla.",
+        "en": "Your booking has been confirmed, and can be paid.",
+        "sv": "Din bokning har bekräftats och kan betalas.",
+    },
+    "reservationRejected": {
+        "fi": "Valitettavasti alla olevaa varaustasi ei voida vahvistaa.",
+        "en": "Unfortunately your booking cannot be confirmed.",
+        "sv": "Tyvärr kan vi inte bekräfta din bokning.",
+    },
+    "reservationUpdated": {
+        "fi": "Varaustasi on muutettu:",
+        "en": "Your booking has been updated:",
+        "sv": "Din bokning har uppdaterats:",
+    },
+    "reservationCancelled": {
+        "fi": "Varauksesi on peruttu.",
+        "en": "Your booking has been cancelled.",
+        "sv": "Din bokning har avbokats.",
+    },
+    #
+    "staffReservationMade": {
+        "fi": "Varausyksikköön {{reservation_unit}} on tehty uusi hyväksytty varaus",
+        "en": "A new booking has been confirmed for {{reservation_unit}}",
+        "sv": "En ny bokningsförfrågan för {{reservation_unit}} har bekräftats",
+    },
+    "staffReservationPending": {
+        "fi": "Varausyksikköön {{reservation_unit}} on tehty uusi käsittelyä vaativa varauspyyntö",
+        "en": "A booking request for {{reservation_unit}} is waiting for processing",
+        "sv": "En ny bokningsförfrågan för {{reservation_unit}} väntar på at behandlats",
+    },
+    "staffCheckDetailsMade": {
+        "fi": "Voit tarkistaa varauksen tiedot osoitteessa",
+        "en": "You can view the booking at",
+        "sv": "Du kan se bokningen på",
+    },
+    "staffCheckDetailsPending": {
+        "fi": "Voit tarkistaa ja käsitellä varauksen osoitteessa",
+        "en": "You can view and handle the booking at",
+        "sv": "Du kan se bokningen på",
+    },
+    #
+    "pendingNotification": {
+        "fi": "Saat varausvahvistuksen sähköpostitse, kun varauksesi on käsitelty. Otamme sinuun yhteyttä, jos tarvitsemme lisätietoja varauspyyntöösi liittyen.",
+        "en": "You will receive a confirmation email once your booking has been processed. We will contact you if further information is needed regarding your booking request.",
+        "sv": "Du kommer att få en bekräftelse via e-post när din bokning har behandlats. Vi kommer att kontakta dig om ytterligare information behövs angående din bokningsförfrågan.",
+    },
+    "cancellationReason": {
+        "fi": "Peruutuksen syy",
+        "en": "Your reason for cancellation",
+        "sv": "Din anledning till avbokning",
+    },
+    "rejectionReason": {
+        "fi": "Syy",
+        "en": "Reason",
+        "sv": "Orsak",
+    },
+    "cancellationInstructions": {
+        "fi": "Lisätietoa peruutuksesta",
+        "en": "Additional information about cancellation",
+        "sv": "Mer information om avbokning",
+    },
+    "rejectionInstructions": {
+        "fi": "Lisätietoa",
+        "en": "Additional information",
+        "sv": "Mer information",
+    },
+    "reservationInstruction": {
+        "fi": "Lisätietoa varauksestasi",
+        "en": "Additional information about your booking",
+        "sv": "Mer information om din bokning",
+    },
+    #
+    "unit": {
+        "fi": "Toimipiste",
+        "en": "Unit",
+        "sv": "Enheten",
+    },
+    "reserveeName": {
+        "fi": "Varaajan nimi",
+        "en": "Name",
+        "sv": "Namn",
+    },
+    "reservationBegins": {
+        "fi": "Alkamisaika",
+        "en": "From",
+        "sv": "Börjar",
+    },
+    "reservationEnds": {
+        "fi": "Päättymisaika",
+        "en": "To",
+        "sv": "Slutar",
+    },
+    "reservationPrice": {
+        "fi": "Hinta",
+        "en": "Price",
+        "sv": "Pris",
+    },
+    "reservationNumber": {
+        "fi": "Varausnumero",
+        "en": "Reservation number",
+        "sv": "Bokningsnummer",
+    },
+    #
+    "subjectApplicationReceived": {
+        "fi": "Hakemuksesi on vastaanotettu",
+        "en": "subjectApplicationReceived EN",
+        "sv": "subjectApplicationReceived SV",
+    },
+    "subjectApplicationInHandling": {
+        "fi": "Hakemuksesi on käsiteltävänä",
+        "en": "subjectApplicationInHandling EN",
+        "sv": "subjectApplicationInHandling SV",
+    },
+    "subjectApplicationHandled": {
+        "fi": "Hakemuksesi on käsitelty",
+        "en": "subjectApplicationHandled EN",
+        "sv": "subjectApplicationHandled SV",
+    },
+    "applicationReceived": {
+        "fi": "Kiitos hakemuksestasi.",
+        "en": "applicationReceived EN",
+        "sv": "applicationReceived SVAAAAAAAA",
+    },
+    "applicationInHandling": {
+        "fi": "Hakuaika on päättynyt. Ilmoitamme käsittelyn tuloksesta, kun hakemuksesi on käsitelty.",
+        "en": "applicationInHandling EN",
+        "sv": "applicationInHandling SV",
+    },
+    "applicationHandled": {
+        "fi": "Näet tiedon käsittelyn tuloksesta",
+        "en": "applicationHandled EN",
+        "sv": "applicationHandled SV",
+    },
+    "manageApplication": {
+        "fi": "Voit muuttaa tai täydentää hakemustasi",
+        "en": "manageApplication EN",
+        "sv": "manageApplication SV",
+    },
+    "viewApplication": {
+        "fi": "Voit tarkastella lähettämääsi hakemusta",
+        "en": "viewApplication EN",
+        "sv": "viewApplication SV",
+    },
+    "ownApplicationPage": {
+        "fi": "Omat hakemukset -sivulla",
+        "en": "ownApplicationPage EN",
+        "sv": "ownApplicationPage SV",
+    },
+    "untilApplicationPeriodEnd": {
+        "fi": "hakuajan päättymiseen asti",
+        "en": "untilApplicationPeriodEnd EN",
+        "sv": "untilApplicationPeriodEnd SV",
+    },
+}
