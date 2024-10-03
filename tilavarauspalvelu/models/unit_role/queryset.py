3ef44c5477318217d936921f28e99aacaff7a9e6
Matti Lamppu <matti.lamppu@vincit.fi>
2024-10-03 12:31:10 +0300

    Add task to deactivate permissions for users

--- tilavarauspalvelu/models/unit_role/queryset.py
+++ tilavarauspalvelu/models/unit_role/queryset.py
@@ -1,6 +1,11 @@
 from __future__ import annotations
 
+import datetime
+
+from django.conf import settings
 from django.db import models
+
+from utils.date_utils import local_date
 
 __all__ = [
     "UnitRoleManager",
@@ -8,7 +13,16 @@ __all__ = [
 ]
 
 
-class UnitRoleQuerySet(models.QuerySet): ...
+class UnitRoleQuerySet(models.QuerySet):
+    def deactivate_old_permissions(self) -> None:
+        today = local_date()
+        cutoff = today - datetime.timedelta(days=settings.PERMISSIONS_VALID_FROM_LAST_LOGIN_DAYS)
+
+        self.filter(
+            role_active=True,
+            user__is_active=True,
+            user__last_login__date__lt=cutoff,
+        ).update(role_active=False)
 
 
 class UnitRoleManager(models.Manager.from_queryset(UnitRoleQuerySet)): ...
