7b29f4cbb5b61c4eca820120dfa171f2490e83f2
Matti Lamppu <matti.lamppu@vincit.fi>
2024-10-16 17:17:38 +0300

    Remove email template

--- tilavarauspalvelu/models/email_template/actions.py
+++
@@ -1,9 +0,0 @@
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from .model import EmailTemplate
-
-
-class EmailTemplateActions:
-    def __init__(self, email_template: "EmailTemplate") -> None:
-        self.email_template = email_template
