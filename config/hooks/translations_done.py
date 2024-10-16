da6cec33e88463ac16fa48b590de2f6a2f891318
Matti Lamppu <matti.lamppu@vincit.fi>
2024-10-16 17:10:32 +0300

    Check swedish translations in CI

--- config/hooks/translations_done.py
+++ config/hooks/translations_done.py
@@ -13,7 +13,7 @@ from django.conf import settings
 from django.core.management import call_command
 
 if TYPE_CHECKING:
-    from collections.abc import Generator
+    from collections.abc import Collection, Generator
 
 BASE_PATH = Path(__file__).resolve().parent.parent.parent
 
@@ -22,7 +22,7 @@ BASE_PATH = Path(__file__).resolve().parent.parent.parent
 class TranslationData:
     lang: str
     path: str
-    contents_before: polib.POFile
+    contents: polib.POFile
 
 
 @dataclass
@@ -38,7 +38,7 @@ def main() -> int:
 
     items = _get_current_translations()
     _run_translation_command()
-    missing = _find_missing_translations(items)
+    missing = _find_missing_translations(items, check_swedish_for_contexts=("Email", "ICAL"))
 
     if missing:
         _print_errors(missing)
@@ -48,21 +48,35 @@ def main() -> int:
 
 def _get_languages() -> Generator[str, None, None]:
     # Enables adding more languages
-    yield from ["fi"]
+    yield from ["fi", "sv"]
 
 
-def _find_missing_translations(items: list[TranslationData]) -> dict[str, MissingTranslations]:
+def _find_missing_translations(
+    items: list[TranslationData],
+    *,
+    check_swedish_for_contexts: Collection[str] = (),
+) -> dict[str, MissingTranslations]:
+    """
+    Find missing translations in the current translations.
+
+    :param items: Translation data before running the command for this check.
+    :params check_swedish_for_contexts: When using `pgettext`, these are the 'contexts' that will
+                                        be checked for Swedish translations. Other translations are not checked.
+    """
     missing: dict[str, MissingTranslations] = defaultdict(MissingTranslations)
     for item in items:
         pofile = polib.pofile(item.path)
         for entry in pofile:
+            if item.lang == "sv" and entry.msgctxt not in check_swedish_for_contexts:
+                continue
+
             key = entry.msgid
             if entry.msgctxt is not None:
                 key = f"{entry.msgctxt} | {key}"
 
             if entry.msgstr == "":
                 missing[key].missing_languages.add(item.lang)
-            if entry not in item.contents_before:
+            if entry not in item.contents:
                 missing[key].not_included_previous.add(item.lang)
             if "fuzzy" in entry.flags:
                 missing[key].fuzzy.add(item.lang)
@@ -74,7 +88,7 @@ def _get_current_translations() -> list[TranslationData]:
         TranslationData(
             lang=lang,
             path=(path := str(locale_path / lang / "LC_MESSAGES" / "django.po")),
-            contents_before=polib.pofile(path),
+            contents=polib.pofile(path),
         )
         for locale_path in settings.LOCALE_PATHS
         for lang in _get_languages()
@@ -104,15 +118,14 @@ def _run_translation_command() -> None:
 
 
 def _print_errors(missing: dict[str, MissingTranslations]) -> None:
-    print("\nIncomplete translations:")  # noqa: T201
-    for msg, info in missing.items():
-        print(f"  {msg}")  # noqa: T201
+    print("\nIncomplete translations:")  # noqa: T201, RUF100
+    for info in missing.values():
         if len(info.missing_languages) > 0:
-            print("    ↳ Empty:", ", ".join(sorted(info.missing_languages)))  # noqa: T201
+            print("    ↳ Empty:", ", ".join(sorted(info.missing_languages)))  # noqa: T201, RUF100
         if len(info.not_included_previous) > 0:
-            print("    ↳ New:", ", ".join(sorted(info.not_included_previous)))  # noqa: T201
+            print("    ↳ New:", ", ".join(sorted(info.not_included_previous)))  # noqa: T201, RUF100
         if len(info.fuzzy) > 0:
-            print("    ↳ Fuzzy:", ", ".join(sorted(info.fuzzy)))  # noqa: T201
+            print("    ↳ Fuzzy:", ", ".join(sorted(info.fuzzy)))  # noqa: T201, RUF100
 
 
 if __name__ == "__main__":
