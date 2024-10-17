1e915dfbf8ba4b47a96f5a66310dbd53770d0e3c
Matti Lamppu <matti.lamppu@vincit.fi>
2024-10-17 09:51:10 +0300

    Better email language determination
    Skip invalid languages and use `reservee_language` if available

--- tilavarauspalvelu/integrations/email/find_language.py
+++ tilavarauspalvelu/integrations/email/find_language.py
@@ -4,6 +4,8 @@ from typing import TYPE_CHECKING
 
 from django.conf import settings
 
+from tilavarauspalvelu.enums import Language
+
 if TYPE_CHECKING:
     from tilavarauspalvelu.models import Application, Reservation
     from tilavarauspalvelu.typing import Lang
@@ -15,13 +17,15 @@ __all__ = [
 
 def get_application_email_language(application: Application) -> Lang:
     """Get email notification language for the given application."""
-    if application.user is None:
-        return settings.LANGUAGE_CODE
-    return application.user.get_preferred_language()
+    if getattr(application.user, "preferred_language", None) in Language.values:
+        return application.user.preferred_language  # type: ignore[return-value]
+    return settings.LANGUAGE_CODE
 
 
 def get_reservation_email_language(reservation: Reservation) -> Lang:
     """Get email notification language for the given application."""
-    if reservation.user is None:
-        return settings.LANGUAGE_CODE
-    return reservation.user.get_preferred_language()
+    if getattr(reservation.user, "preferred_language", None) in Language.values:
+        return reservation.user.preferred_language  # type: ignore[return-value]
+    if reservation.reservee_language in Language.values:
+        return reservation.reservee_language  # type: ignore[return-value]
+    return settings.LANGUAGE_CODE
