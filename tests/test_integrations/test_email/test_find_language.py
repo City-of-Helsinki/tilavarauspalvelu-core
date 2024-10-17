a5851a7a9818e383fb196dff5307c511089559da
Matti Lamppu <matti.lamppu@vincit.fi>
2024-10-17 09:51:10 +0300

    Better email language determination
    Skip invalid languages and use `reservee_language` if available

--- tests/test_integrations/test_email/test_find_language.py
+++ tests/test_integrations/test_email/test_find_language.py
@@ -21,10 +21,43 @@ def test_get_application_email_language(preferred_language):
     assert lang == preferred_language
 
 
+def test_get_application_email_language__invalid_preferred_language():
+    application = ApplicationFactory.create(user__preferred_language="uk")
+
+    lang = get_application_email_language(application)
+
+    assert lang == Language.FI.value
+
+
 @pytest.mark.parametrize("preferred_language", Language.values)
 def test_get_reservation_email_language(preferred_language):
-    reservation = ReservationFactory.create(user__preferred_language=preferred_language)
+    reservation = ReservationFactory.create(
+        user__preferred_language=preferred_language,
+        reservee_language="",
+    )
 
     lang = get_reservation_email_language(reservation)
 
     assert lang == preferred_language
+
+
+def test_get_reservation_email_language__use_reservee_language():
+    reservation = ReservationFactory.create(
+        user__preferred_language="",
+        reservee_language=Language.EN.value,
+    )
+
+    lang = get_reservation_email_language(reservation)
+
+    assert lang == Language.EN.value
+
+
+def test_get_reservation_email_language__invalid_languages():
+    reservation = ReservationFactory.create(
+        user__preferred_language="uk",
+        reservee_language="es",
+    )
+
+    lang = get_reservation_email_language(reservation)
+
+    assert lang == Language.FI.value
