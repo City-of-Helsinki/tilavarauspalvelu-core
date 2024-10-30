e8933d5048b7e923445135fc9200707a3d9adb56
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2024-10-30 15:29:31 +0200

    remove: unused components

--- apps/ui/components/form/index.ts
+++ apps/ui/components/form/index.ts
@@ -1,6 +1,5 @@
 export { default as Checkbox } from "./Checkbox";
 export { default as DateRangePicker } from "./DateRangePicker";
-export { default as Dropdown } from "./Dropdown";
 export { default as ScrollIntoViewWithFocus } from "./ScrollIntoViewWithFocus";
 export { default as SearchLabel } from "@/components/form/SearchLabel";
 export { Sorting } from "./Sorting";
