449fe9d7229c7fb462bb9213931505952aba4630
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-10-30 16:25:54 +0200

    Hide old ReservationUnitPricings from the GQL API

--- tilavarauspalvelu/api/graphql/types/reservation_unit_pricing/types.py
+++ tilavarauspalvelu/api/graphql/types/reservation_unit_pricing/types.py
@@ -1,7 +1,10 @@
 import graphene
+from django.db import models
 from graphene_django_extensions import DjangoNode
 
 from tilavarauspalvelu.models import ReservationUnitPricing
+from tilavarauspalvelu.models.reservation_unit_pricing.queryset import ReservationUnitPricingQuerySet
+from tilavarauspalvelu.typing import GQLInfo
 
 
 class ReservationUnitPricingNode(DjangoNode):
@@ -20,3 +23,7 @@ class ReservationUnitPricingNode(DjangoNode):
             "highest_price_net",
             "tax_percentage",
         ]
+
+    @classmethod
+    def filter_queryset(cls, queryset: ReservationUnitPricingQuerySet, info: GQLInfo) -> models.QuerySet:
+        return queryset.exclude_past()
--- tilavarauspalvelu/models/reservation_unit_pricing/queryset.py
+++ tilavarauspalvelu/models/reservation_unit_pricing/queryset.py
@@ -7,8 +7,24 @@ __all__ = [
     "ReservationUnitPricingQuerySet",
 ]
 
+from utils.date_utils import local_date
 
-class ReservationUnitPricingQuerySet(models.QuerySet): ...
+
+class ReservationUnitPricingQuerySet(models.QuerySet):
+    def exclude_past(self) -> models.QuerySet:
+        """Return all currently active and future pricings."""
+        today = local_date()
+
+        return self.alias(
+            reservation_unit_active_pricing_begins_date=models.Subquery(
+                self.model.objects.filter(
+                    reservation_unit=models.OuterRef("reservation_unit"),
+                    begins__lte=today,
+                )
+                .values("begins")
+                .order_by("-begins")[:1]
+            )
+        ).filter(begins__gte=models.F("reservation_unit_active_pricing_begins_date"))
 
 
 class ReservationUnitPricingManager(models.Manager.from_queryset(ReservationUnitPricingQuerySet)): ...
