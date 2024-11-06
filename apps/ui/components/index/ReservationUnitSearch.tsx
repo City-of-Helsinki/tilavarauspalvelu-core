City-of-Helsinki/tilavarauspalvelu-ui

refactor: add url builders to customer ui

apps/ui/components/index/ReservationUnitSearch.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
 import styled from "styled-components";
 import { breakpoints } from "common/src/common/style";
-import { singleSearchPrefix } from "@/modules/const";
+import { getSingleSearchUrl } from "@/modules/urls";
 
 const StyledTextInput = styled(TextInput)`
   position: relative;
@@ -37,7 +37,9 @@ const ReservationUnitSearch = (): JSX.Element => {
 
   const handleSubmit = (event: React.FormEvent | React.MouseEvent) => {
     event.preventDefault();
-    router.push(`${singleSearchPrefix}?textSearch=${searchTerm}`);
+    const params = new URLSearchParams();
+    params.set("textSearch", searchTerm);
+    router.push(getSingleSearchUrl(params));
   };
 
   return (

apps/ui/components/reservation-unit/Head.tsx
@@ -8,12 +8,7 @@ import { fontRegular, H2, H3 } from "common/src/common/typography";
 import { breakpoints } from "common/src/common/style";
 import { ReservationKind, type ReservationUnitPageQuery } from "@gql/gql-types";
 import { Container } from "common";
-import {
-  formatDate,
-  getTranslation,
-  orderImages,
-  singleSearchUrl,
-} from "@/modules/util";
+import { formatDate, getTranslation, orderImages } from "@/modules/util";
 import IconWithText from "../common/IconWithText";
 import { Images } from "./Images";
 import {
@@ -26,6 +21,7 @@ import {
 import BreadcrumbWrapper from "../common/BreadcrumbWrapper";
 import { isReservationStartInFuture } from "@/modules/reservation";
 import { filterNonNullable } from "common/src/helpers";
+import { getSingleSearchUrl } from "@/modules/urls";
 
 type QueryT = NonNullable<ReservationUnitPageQuery["reservationUnit"]>;
 interface PropsType {
@@ -147,8 +143,6 @@ function Head({
 }: PropsType): JSX.Element {
   const { t } = useTranslation();
 
-  const searchUrl = singleSearchUrl();
-
   const minDur = reservationUnit.minReservationDuration ?? 0;
   const maxDur = reservationUnit.maxReservationDuration ?? 0;
   const minReservationDuration = formatDuration(minDur / 60, t, true);
@@ -156,13 +150,11 @@ function Head({
 
   const pricing = getActivePricing(reservationUnit);
   const unitPrice = pricing ? getPriceString({ pricing }) : undefined;
-
   const isPaid = isReservationUnitPaid(reservationUnit.pricings);
   const hasSubventionSuffix = pricing && isPaid && subventionSuffix != null;
-
   const reservationUnitName = getReservationUnitName(reservationUnit);
-
   const unitName = getUnitName(reservationUnit.unit ?? undefined);
+  const searchUrl = getSingleSearchUrl();
 
   const iconsTexts = filterNonNullable([
     reservationUnit.reservationUnitType != null

apps/ui/components/reservation/DeleteCancelled.tsx
@@ -9,7 +9,7 @@ import { Container } from "common";
 import { signOut } from "common/src/browserHelpers";
 import { Paragraph } from "./styles";
 import { LinkButton } from "../../styles/util";
-import { singleSearchUrl } from "../../modules/util";
+import { getSingleSearchUrl } from "@/modules/urls";
 
 type Props = {
   reservationPk: string;
@@ -89,7 +89,7 @@ const DeleteCancelled = ({ reservationPk, error, apiBaseUrl }: Props) => {
               marginTop: "var(--spacing-3-xl)",
             }}
           >
-            <StyledLink href={singleSearchUrl({})}>
+            <StyledLink href={getSingleSearchUrl()}>
               {t("reservations:backToSearch")}
               <IconArrowRight aria-hidden size="m" />
             </StyledLink>

apps/ui/components/reservation/ReservationFail.tsx
@@ -8,8 +8,8 @@ import styled from "styled-components";
 import { Container } from "common";
 import { signOut } from "common/src/browserHelpers";
 import { Paragraph } from "./styles";
-import { singleSearchUrl } from "../../modules/util";
 import { LinkButton } from "../../styles/util";
+import { getSingleSearchUrl } from "@/modules/urls";
 
 type Props = {
   type: "reservation" | "order";
@@ -72,7 +72,7 @@ const ReservationFail = ({ type, apiBaseUrl }: Props) => {
               marginTop: "var(--spacing-3-xl)",
             }}
           >
-            <StyledLink href={singleSearchUrl({})}>
+            <StyledLink href={getSingleSearchUrl()}>
               {t("reservations:backToSearch")}
               <IconArrowRight aria-hidden size="m" />
             </StyledLink>

City-of-Helsinki/tilavarauspalvelu-core

apps/ui/components/index/ReservationUnitSearch.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
 import styled from "styled-components";
 import { breakpoints } from "common/src/common/style";
-import { singleSearchPrefix } from "@/modules/const";
+import { getSingleSearchPath } from "@/modules/urls";
 
 const StyledTextInput = styled(TextInput)`
   position: relative;
@@ -37,7 +37,9 @@ const ReservationUnitSearch = (): JSX.Element => {
 
   const handleSubmit = (event: React.FormEvent | React.MouseEvent) => {
     event.preventDefault();
-    router.push(`${singleSearchPrefix}?textSearch=${searchTerm}`);
+    const params = new URLSearchParams();
+    params.set("textSearch", searchTerm);
+    router.push(getSingleSearchPath(params));
   };
 
   return (

apps/ui/components/reservation-unit/Head.tsx
@@ -8,12 +8,7 @@ import { fontRegular, H2, H3 } from "common/src/common/typography";
 import { breakpoints } from "common/src/common/style";
 import { ReservationKind, type ReservationUnitPageQuery } from "@gql/gql-types";
 import { Container } from "common";
-import {
-  formatDate,
-  getTranslation,
-  orderImages,
-  singleSearchUrl,
-} from "@/modules/util";
+import { formatDate, getTranslation, orderImages } from "@/modules/util";
 import IconWithText from "../common/IconWithText";
 import { Images } from "./Images";
 import {
@@ -26,6 +21,7 @@ import {
 import BreadcrumbWrapper from "../common/BreadcrumbWrapper";
 import { isReservationStartInFuture } from "@/modules/reservation";
 import { filterNonNullable } from "common/src/helpers";
+import { getSingleSearchPath } from "@/modules/urls";
 
 type QueryT = NonNullable<ReservationUnitPageQuery["reservationUnit"]>;
 interface PropsType {
@@ -147,8 +143,6 @@ function Head({
 }: PropsType): JSX.Element {
   const { t } = useTranslation();
 
-  const searchUrl = singleSearchUrl();
-
   const minDur = reservationUnit.minReservationDuration ?? 0;
   const maxDur = reservationUnit.maxReservationDuration ?? 0;
   const minReservationDuration = formatDuration(minDur / 60, t, true);
@@ -156,13 +150,11 @@ function Head({
 
   const pricing = getActivePricing(reservationUnit);
   const unitPrice = pricing ? getPriceString({ t, pricing }) : undefined;
-
   const isPaid = isReservationUnitPaid(reservationUnit.pricings);
   const hasSubventionSuffix = pricing && isPaid && subventionSuffix != null;
-
   const reservationUnitName = getReservationUnitName(reservationUnit);
-
   const unitName = getUnitName(reservationUnit.unit ?? undefined);
+  const searchUrl = getSingleSearchPath();
 
   const iconsTexts = filterNonNullable([
     reservationUnit.reservationUnitType != null

apps/ui/components/reservation/DeleteCancelled.tsx
@@ -9,7 +9,7 @@ import { Container } from "common";
 import { signOut } from "common/src/browserHelpers";
 import { Paragraph } from "./styles";
 import { LinkButton } from "../../styles/util";
-import { singleSearchUrl } from "../../modules/util";
+import { getSingleSearchPath } from "@/modules/urls";
 
 type Props = {
   reservationPk: string;
@@ -89,7 +89,7 @@ const DeleteCancelled = ({ reservationPk, error, apiBaseUrl }: Props) => {
               marginTop: "var(--spacing-3-xl)",
             }}
           >
-            <StyledLink href={singleSearchUrl({})}>
+            <StyledLink href={getSingleSearchPath()}>
               {t("reservations:backToSearch")}
               <IconArrowRight aria-hidden size="m" />
             </StyledLink>

apps/ui/components/reservation/ReservationFail.tsx
@@ -8,8 +8,8 @@ import styled from "styled-components";
 import { Container } from "common";
 import { signOut } from "common/src/browserHelpers";
 import { Paragraph } from "./styles";
-import { singleSearchUrl } from "../../modules/util";
 import { LinkButton } from "../../styles/util";
+import { getSingleSearchPath } from "@/modules/urls";
 
 type Props = {
   type: "reservation" | "order";
@@ -72,7 +72,7 @@ const ReservationFail = ({ type, apiBaseUrl }: Props) => {
               marginTop: "var(--spacing-3-xl)",
             }}
           >
-            <StyledLink href={singleSearchUrl({})}>
+            <StyledLink href={getSingleSearchPath()}>
               {t("reservations:backToSearch")}
               <IconArrowRight aria-hidden size="m" />
             </StyledLink>