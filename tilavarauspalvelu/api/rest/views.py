1a598e0170dfa2ea4e854458bc41bd39dff35e23
Matti Lamppu <matti.lamppu@vincit.fi>
2024-11-11 10:13:10 +0200

    Give json from csrf endpoint if no `redirect_to` is given

--- tilavarauspalvelu/api/rest/views.py
+++ tilavarauspalvelu/api/rest/views.py
@@ -47,7 +47,7 @@ def reservation_ical(request: WSGIRequest, pk: int) -> FileResponse | JsonRespon
 
 @require_GET
 @csrf_exempt  # NOSONAR
-def csrf_view(request: WSGIRequest) -> HttpResponseRedirect:  # NOSONAR
+def csrf_view(request: WSGIRequest) -> HttpResponseRedirect | JsonResponse:  # NOSONAR
     """View for updating the CSRF cookie."""
     # From: https://fractalideas.com/blog/making-react-and-django-play-well-together-single-page-app-model/
     # > You may wonder whether this endpoint creates a security vulnerability.
@@ -55,7 +55,7 @@ def csrf_view(request: WSGIRequest) -> HttpResponseRedirect:  # NOSONAR
     # > the CSRF token on a traditional Django website. The browser's same-origin policy
     # > prevents an attacker from getting access to the token with a cross-origin request.
     # Additionally, our backend's CORS policy only allows cross-origin requests from the frontend.
-    redirect_to: str = request.GET.get("redirect_to", "/")
+    redirect_to: str | None = request.GET.get("redirect_to", None)
     # Set these META-flags to force `django.middleware.csrf.CsrfViewMiddleware` to update the CSRF cookie.
     request.META["CSRF_COOKIE_NEEDS_UPDATE"] = True
     request.META["CSRF_COOKIE"] = get_token(request)
@@ -64,6 +64,8 @@ def csrf_view(request: WSGIRequest) -> HttpResponseRedirect:  # NOSONAR
     # during local development, which is considered a different origin, and thus the CSRF cookie is not
     # shared automatically like it is in production.
     headers = {"NewCSRFToken": request.META["CSRF_COOKIE"]}
+    if redirect_to is None:
+        return JsonResponse(data={"csrfToken": request.META["CSRF_COOKIE"]}, status=200, headers=headers)
     return HttpResponseRedirect(redirect_to=redirect_to, headers=headers)
 
 
