fc50a1e78044963fefe3888d310117ee94c13d43
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2024-11-27 12:25:50 +0200

    remove: unused common components

--- packages/common/src/components/index.ts
+++ packages/common/src/components/index.ts
@@ -5,7 +5,5 @@ export {
 } from "./ShowAllContainer";
 export { default as BannerNotificationsList } from "./BannerNotificationsList";
 export { default as Card } from "./Card";
-export { default as Logo } from "./Logo";
-export { ConfirmationDialog } from "./ConfirmationDialog";
 export { default as StatusLabel } from "./StatusLabel";
 export { default as Tag } from "./Tag";
--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,20 +1,17 @@
-import formatters from "./number-formatters";
+export { default as formatters } from "./number-formatters";
 import {
   getPriceUnitMinutes,
   getReservationPrice,
   getReservationVolume,
   getUnRoundedReservationVolume,
 } from "./reservation-pricing";
-import UserInfo from "./userinfo/UserInfo";
 
 export * from "./common/style";
 export * from "./common/typography";
 
 export {
-  UserInfo,
   getUnRoundedReservationVolume,
   getReservationVolume,
   getPriceUnitMinutes,
   getReservationPrice,
-  formatters,
 };
