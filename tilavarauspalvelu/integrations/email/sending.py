e06eed74a34347fe7526c20b10862f80050f24c5
Eemeli Ranta <eemeli.ranta@vincit.fi>
2024-12-12 15:38:14 +0200

    Use only `EmaiLData` type argument in `send_emails_in_batches_task`

--- tilavarauspalvelu/integrations/email/sending.py
+++ tilavarauspalvelu/integrations/email/sending.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
 from copy import copy
-from dataclasses import asdict
 from itertools import batched
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
@@ -22,25 +21,25 @@ __all__ = [
 
 
 @app.task(name="send_emails_in_batches")
-def send_emails_in_batches_task(email_data: EmailData | dict[str, Any]) -> None:
+def send_emails_in_batches_task(email_data: EmailData) -> None:
     """Sends an email message in batches."""
     if not settings.SEND_EMAILS:
         return
 
     # Celery converts EmailData to a dict so it's serializable. If the method is called directly, convert it here.
-    if isinstance(email_data, EmailData):
-        email_data = asdict(email_data)
+    if isinstance(email_data, dict):
+        email_data = EmailData(**email_data)
 
     email_message = EmailMultiAlternatives(
-        subject=email_data["subject"],
-        body=email_data["text_content"],
+        subject=email_data.subject,
+        body=email_data.text_content,
         from_email=settings.DEFAULT_FROM_EMAIL,
-        alternatives=[(email_data["html_content"], "text/html")],
+        alternatives=[(email_data.html_content, "text/html")],
     )
-    for attachment in email_data["attachments"]:
+    for attachment in email_data.attachments:
         email_message.attach(**attachment)
 
-    for batch in batched(email_data["recipients"], settings.EMAIL_MAX_RECIPIENTS):
+    for batch in batched(email_data.recipients, settings.EMAIL_MAX_RECIPIENTS):
         email_message_copy = copy(email_message)
         email_message_copy.bcc = list(batch)
         email_message_copy.send(fail_silently=False)
