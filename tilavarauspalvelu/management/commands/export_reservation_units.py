1bf366f882122f50bf8108f6b6543b3a1681e125
Matti Lamppu <matti.lamppu@vincit.fi>
2024-12-18 13:01:58 +0200

    Fix export reservation units command

--- tilavarauspalvelu/management/commands/export_reservation_units.py
+++ tilavarauspalvelu/management/commands/export_reservation_units.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
+from pathlib import Path
 from typing import Any
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from tilavarauspalvelu.utils.reservation_units.export_data import ReservationUnitExporter
+from tilavarauspalvelu.services.csv_export import ReservationUnitExporter
 
 
 class Command(BaseCommand):
     help = "Exports reservation unit data from the database"
 
     def handle(self, *args: Any, **options: Any) -> None:
-        ReservationUnitExporter.export_reservation_unit_data()
+        string_io = ReservationUnitExporter().write()
+        string_io.seek(0)
+        Path(settings.BASE_DIR).write_bytes(string_io.getvalue().encode("utf-8"))
