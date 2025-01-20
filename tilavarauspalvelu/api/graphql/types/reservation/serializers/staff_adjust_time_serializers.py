617c31a9a0e9a22a89130552d21a5801389be03e
Matti Lamppu <matti.lamppu@vincit.fi>
2025-01-20 16:11:54 +0200

    Update staff adjust time serializer

--- tilavarauspalvelu/api/graphql/types/reservation/serializers/staff_adjust_time_serializers.py
+++ tilavarauspalvelu/api/graphql/types/reservation/serializers/staff_adjust_time_serializers.py
@@ -1,24 +1,24 @@
 from __future__ import annotations
 
-import datetime
 from typing import TYPE_CHECKING, Any
 
+from graphene_django_extensions import NestingModelSerializer
 from graphene_django_extensions.fields import EnumFriendlyChoiceField
+from rest_framework.fields import IntegerField
 
-from tilavarauspalvelu.api.graphql.extensions.serializers import OldPrimaryKeyUpdateSerializer
-from tilavarauspalvelu.api.graphql.extensions.validation_errors import ValidationErrorCodes, ValidationErrorWithCode
-from tilavarauspalvelu.api.graphql.types.reservation.serializers.mixins import ReservationSchedulingMixin
 from tilavarauspalvelu.enums import ReservationStateChoice
 from tilavarauspalvelu.integrations.email.main import EmailService
 from tilavarauspalvelu.models import Reservation
-from utils.date_utils import DEFAULT_TIMEZONE, local_datetime
+from utils.date_utils import DEFAULT_TIMEZONE
 
 if TYPE_CHECKING:
     from tilavarauspalvelu.models import ReservationUnit
 
 
-class StaffReservationAdjustTimeSerializer(OldPrimaryKeyUpdateSerializer, ReservationSchedulingMixin):
+class StaffReservationAdjustTimeSerializer(NestingModelSerializer):
     instance: Reservation
+
+    pk = IntegerField(required=True)
 
     state = EnumFriendlyChoiceField(
         choices=ReservationStateChoice.choices,
@@ -32,82 +32,41 @@ class StaffReservationAdjustTimeSerializer(OldPrimaryKeyUpdateSerializer, Reserv
             "pk",
             "begin",
             "end",
-            "state",
             "buffer_time_before",
             "buffer_time_after",
+            "state",
         ]
-        extra_kwargs = {
-            "buffer_time_before": {
-                "help_text": (
-                    "Can be a number of seconds or timespan in format HH:MM:SS. "
-                    "Null/undefined value means buffer from reservation unit is used."
-                )
-            },
-            "buffer_time_after": {
-                "help_text": (
-                    "Can be a number of seconds or timespan in format HH:MM:SS. "
-                    "Null/undefined value means buffer from reservation unit is used."
-                )
-            },
-        }
 
-    def save(self, **kwargs: Any) -> Reservation:
-        instance = super().save(**kwargs)
+    def validate(self, data: dict[str, Any]) -> dict[str, Any]:
+        begin = data["begin"].astimezone(DEFAULT_TIMEZONE)
+        end = data["end"].astimezone(DEFAULT_TIMEZONE)
+
+        self.instance.validator.validate_reservation_state_allows_rescheduling()
+        self.instance.validator.validate_single_reservation_unit()
+        self.instance.validator.validate_reservation_can_be_modified_by_staff()
+
+        reservation_unit: ReservationUnit = self.instance.reservation_units.first()
+
+        if reservation_unit.reservation_block_whole_day:
+            data["buffer_time_before"] = reservation_unit.actions.get_actual_before_buffer(begin)
+            data["buffer_time_after"] = reservation_unit.actions.get_actual_after_buffer(end)
+
+        reservation_unit.validator.validate_begin_before_end(begin=begin, end=end)
+        reservation_unit.validator.validate_reservation_begin_time_staff(begin=begin)
+        reservation_unit.validator.validate_no_overlapping_reservations(
+            begin=begin,
+            end=end,
+            new_buffer_time_before=data.get("buffer_time_before"),
+            new_buffer_time_after=data.get("buffer_time_after"),
+            ignore_ids=[self.instance.pk],
+        )
+
+        return data
+
+    def update(self, instance: Reservation, validated_data: dict[str, Any]) -> Reservation:
+        instance = super().update(instance=instance, validated_data=validated_data)
 
         EmailService.send_reservation_modified_email(reservation=instance)
         EmailService.send_staff_notification_reservation_requires_handling_email(reservation=instance)
 
         return instance
-
-    def validate(self, data: dict[str, Any]) -> dict[str, Any]:
-        data = super().validate(data)
-
-        if self.instance.state != ReservationStateChoice.CONFIRMED.value:
-            msg = "Reservation must be in confirmed state."
-            raise ValidationErrorWithCode(msg, ValidationErrorCodes.RESERVATION_MODIFICATION_NOT_ALLOWED)
-
-        begin = data["begin"].astimezone(DEFAULT_TIMEZONE)
-        end = data["end"].astimezone(DEFAULT_TIMEZONE)
-
-        new_buffer_before = data.get("buffer_time_before", None)
-        new_buffer_after = data.get("buffer_time_after", None)
-
-        reservation_unit: ReservationUnit
-        for reservation_unit in self.instance.reservation_units.all():
-            if reservation_unit.reservation_block_whole_day:
-                data["buffer_time_before"] = reservation_unit.actions.get_actual_before_buffer(begin)
-                data["buffer_time_after"] = reservation_unit.actions.get_actual_after_buffer(end)
-
-            self.check_reservation_overlap(reservation_unit, begin, end)
-            self.check_buffer_times(
-                reservation_unit,
-                begin,
-                end,
-                new_buffer_before=new_buffer_before,
-                new_buffer_after=new_buffer_after,
-            )
-            self.check_reservation_intervals_for_staff_reservation(reservation_unit, begin)
-
-        self.check_begin(begin, end)
-
-        return data
-
-    def check_begin(self, new_begin: datetime.datetime, new_end: datetime.datetime) -> None:
-        if new_begin > new_end:
-            msg = "End cannot be before begin"
-            raise ValidationErrorWithCode(msg, ValidationErrorCodes.RESERVATION_MODIFICATION_NOT_ALLOWED)
-
-        now = local_datetime()
-        min_allowed_date = now.date()
-
-        # For the first hour of the day, we allow reservations to be moved to the previous day
-        if now.hour == 0:
-            min_allowed_date -= datetime.timedelta(days=1)
-
-        if self.instance.end.astimezone(DEFAULT_TIMEZONE).date() < min_allowed_date:
-            msg = "Reservation time cannot be changed anymore."
-            raise ValidationErrorWithCode(msg, ValidationErrorCodes.RESERVATION_MODIFICATION_NOT_ALLOWED)
-
-        if new_begin.astimezone(DEFAULT_TIMEZONE).date() < min_allowed_date:
-            msg = "Reservation new begin date cannot be in the past."
-            raise ValidationErrorWithCode(msg, ValidationErrorCodes.RESERVATION_BEGIN_IN_PAST)
