c8bbbd9e0fba87a310c229ece712e0c66b071f37
Matti Lamppu <matti.lamppu@vincit.fi>
2025-01-20 16:10:12 +0200

    Update deny reservation serializer

--- tilavarauspalvelu/api/graphql/types/reservation/serializers/deny_serializers.py
+++ tilavarauspalvelu/api/graphql/types/reservation/serializers/deny_serializers.py
@@ -1,17 +1,20 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import TYPE_CHECKING
 
 from graphene_django_extensions import NestingModelSerializer
-from graphene_django_extensions.fields import EnumFriendlyChoiceField
-from rest_framework.exceptions import ValidationError
+from graphene_django_extensions.fields import EnumFriendlyChoiceField, IntegerPrimaryKeyField
+from rest_framework.fields import CharField, IntegerField
 
-from tilavarauspalvelu.api.graphql.extensions import error_codes
 from tilavarauspalvelu.enums import ReservationStateChoice
 from tilavarauspalvelu.integrations.email.main import EmailService
-from tilavarauspalvelu.models import Reservation
+from tilavarauspalvelu.models import Reservation, ReservationDenyReason
 from utils.date_utils import local_datetime
-from utils.utils import comma_sep_str
+
+if TYPE_CHECKING:
+    from django.db.models import Model
+
+    from tilavarauspalvelu.typing import ReservationDenyData
 
 __all__ = [
     "ReservationDenySerializer",
@@ -19,7 +22,14 @@ __all__ = [
 
 
 class ReservationDenySerializer(NestingModelSerializer):
+    """Deny a reservation during handling."""
+
     instance: Reservation
+
+    pk = IntegerField(required=True)
+
+    deny_reason = IntegerPrimaryKeyField(queryset=ReservationDenyReason.objects, required=True)
+    handling_details = CharField(required=True, allow_blank=True)
 
     state = EnumFriendlyChoiceField(
         choices=ReservationStateChoice.choices,
@@ -38,27 +48,15 @@ class ReservationDenySerializer(NestingModelSerializer):
         ]
         extra_kwargs = {
             "handled_at": {"read_only": True},
-            "deny_reason": {"required": True},
-            "handling_details": {"required": True},
         }
 
-    def validate(self, data: dict[str, Any]) -> dict[str, Any]:
-        if self.instance.state not in ReservationStateChoice.states_that_can_change_to_deny:
-            states = comma_sep_str(ReservationStateChoice.states_that_can_change_to_deny, last_sep="or", quote=True)
-            msg = f"Only reservations with states {states} can be denied."
-            raise ValidationError(msg, code=error_codes.RESERVATION_DENYING_NOT_ALLOWED)
-
-        if self.instance.state == ReservationStateChoice.CONFIRMED:
-            now = local_datetime()
-            if self.instance.end < now:
-                msg = "Reservation cannot be denied after it has ended."
-                raise ValidationError(msg, code=error_codes.RESERVATION_DENYING_NOT_ALLOWED)
-
+    def validate(self, data: ReservationDenyData) -> ReservationDenyData:
+        self.instance.validator.validate_reservation_state_allows_denying()
+        data["state"] = ReservationStateChoice.DENIED
+        data["handled_at"] = local_datetime()
         return data
 
-    def save(self, **kwargs: Any) -> Reservation:
-        kwargs["state"] = ReservationStateChoice.DENIED.value
-        kwargs["handled_at"] = local_datetime()
-        instance = super().save(**kwargs)
+    def update(self, instance: Model, validated_data: ReservationDenyData) -> Model:
+        instance = super().update(instance=instance, validated_data=validated_data)
         EmailService.send_reservation_rejected_email(reservation=instance)
         return instance
