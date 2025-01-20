bbd8a8fe240d613225a11cbe6e17d90bd78b9eed
Matti Lamppu <matti.lamppu@vincit.fi>
2025-01-20 16:17:13 +0200

    Update price calculation tests

--- tests/test_actions/test_pricing_calculation.py
+++ tests/test_actions/test_pricing_calculation.py
@@ -6,7 +6,6 @@ from decimal import Decimal
 import pytest
 from freezegun import freeze_time
 
-from tilavarauspalvelu.api.graphql.types.reservation.serializers.mixins import ReservationPriceMixin
 from tilavarauspalvelu.enums import PriceUnit
 from utils.date_utils import local_datetime
 
@@ -18,29 +17,33 @@ pytestmark = [
 ]
 
 
-def test_reservation__calculate_price__subsidised_price_is_equal_to_lowest_price__fixed_price():
+def test_calculate_reservation_price__subsidised_price_is_equal_to_lowest_price__fixed_price():
     begin = local_datetime()
     end = begin + datetime.timedelta(hours=2)
 
     pricing = ReservationUnitPricingFactory.create(price_unit=PriceUnit.PRICE_UNIT_FIXED)
-    prices = ReservationPriceMixin().calculate_price(begin, end, [pricing.reservation_unit])
 
-    assert prices.reservation_price == Decimal(10)
-    assert prices.subsidised_price == pricing.lowest_price
+    price = pricing.actions.calculate_reservation_price(duration=end - begin)
+    subsidised_price = pricing.actions.calculate_reservation_price(duration=end - begin, subsidised=True)
+
+    assert price == Decimal(10)
+    assert subsidised_price == pricing.lowest_price
 
 
-def test_reservation__calculate_price__subsidised_price_is_equal_to_lowest_price__hourly_price():
+def test_calculate_reservation_price__subsidised_price_is_equal_to_lowest_price__hourly_price():
     begin = local_datetime()
     end = begin + datetime.timedelta(hours=2)
 
     pricing = ReservationUnitPricingFactory.create(price_unit=PriceUnit.PRICE_UNIT_PER_HOUR)
-    prices = ReservationPriceMixin().calculate_price(begin, end, [pricing.reservation_unit])
 
-    assert prices.reservation_price == pricing.highest_price * 2
-    assert prices.subsidised_price == pricing.lowest_price * 2
+    price = pricing.actions.calculate_reservation_price(duration=end - begin)
+    subsidised_price = pricing.actions.calculate_reservation_price(duration=end - begin, subsidised=True)
+
+    assert price == pricing.highest_price * 2
+    assert subsidised_price == pricing.lowest_price * 2
 
 
-def test_reservation__calculate_price__pricing_is_calculated_per_15_min_with_pricing_unit_less_than_half_day():
+def test_calculate_reservation_price__pricing_is_calculated_per_15_min_with_pricing_unit_less_than_half_day():
     begin = local_datetime()
     end = begin + datetime.timedelta(hours=1, minutes=15)
 
@@ -50,12 +53,12 @@ def test_reservation__calculate_price__pricing_is_calculated_per_15_min_with_pri
         highest_price=Decimal("40.0"),
     )
 
-    prices = ReservationPriceMixin().calculate_price(begin, end, [pricing.reservation_unit])
+    price = pricing.actions.calculate_reservation_price(duration=end - begin)
 
-    assert prices.reservation_price == pricing.lowest_price * Decimal("1.25")
+    assert price == pricing.lowest_price * Decimal("1.25")
 
 
-def test_reservation__calculate_price__pricing_is_fixed_with_pricing_unit_more_than_half_day():
+def test_calculate_reservation_price__pricing_is_fixed_with_pricing_unit_more_than_half_day():
     begin = local_datetime()
     end = begin + datetime.timedelta(hours=14)
 
@@ -65,12 +68,12 @@ def test_reservation__calculate_price__pricing_is_fixed_with_pricing_unit_more_t
         highest_price=Decimal("100.0"),
     )
 
-    prices = ReservationPriceMixin().calculate_price(begin, end, [pricing.reservation_unit])
+    price = pricing.actions.calculate_reservation_price(duration=end - begin)
 
-    assert prices.reservation_price == pricing.lowest_price
+    assert price == pricing.lowest_price
 
 
-def test_reservation__calculate_price__pricing_is_fixed_even_when_duration_is_double_the_pricing_unit():
+def test_calculate_reservation_price__pricing_is_fixed_even_when_duration_is_double_the_pricing_unit():
     begin = local_datetime()
     end = begin + datetime.timedelta(days=1)
 
@@ -80,12 +83,12 @@ def test_reservation__calculate_price__pricing_is_fixed_even_when_duration_is_do
         highest_price=Decimal("100.0"),
     )
 
-    prices = ReservationPriceMixin().calculate_price(begin, end, [pricing.reservation_unit])
+    price = pricing.actions.calculate_reservation_price(duration=end - begin)
 
-    assert prices.reservation_price == pricing.lowest_price
+    assert price == pricing.lowest_price
 
 
-def test_reservation__calculate_price__future_has_same_tax_percentage():
+def test_get_active_pricing__future_has_same_tax_percentage():
     day_1 = local_datetime()
     day_2 = day_1 + datetime.timedelta(days=1)
 
@@ -104,14 +107,14 @@ def test_reservation__calculate_price__future_has_same_tax_percentage():
         highest_price=Decimal("100.0"),
     )
 
-    prices_1 = ReservationPriceMixin().calculate_price(day_1, day_1, [reservation_unit])
-    assert prices_1.tax_percentage_value == pricing_1.tax_percentage.value
+    pricing = reservation_unit.actions.get_active_pricing(by_date=day_1.date())
+    assert pricing.tax_percentage.value == pricing_1.tax_percentage.value
 
-    prices_2 = ReservationPriceMixin().calculate_price(day_2, day_2, [reservation_unit])
-    assert prices_2.tax_percentage_value == pricing_2.tax_percentage.value
+    pricing = reservation_unit.actions.get_active_pricing(by_date=day_2.date())
+    assert pricing.tax_percentage.value == pricing_2.tax_percentage.value
 
 
-def test_reservation__calculate_price__is_activated_on_begins():
+def test_get_active_pricing__is_activated_on_begins():
     day_1 = local_datetime()
     day_2 = day_1 + datetime.timedelta(days=1)
 
@@ -131,20 +134,20 @@ def test_reservation__calculate_price__is_activated_on_begins():
     )
 
     # Price for day 1 is used as expected
-    prices = ReservationPriceMixin().calculate_price(day_1, day_1, [reservation_unit])
-    assert prices.tax_percentage_value == pricing_1.tax_percentage.value
+    pricing = reservation_unit.actions.get_active_pricing(by_date=day_1.date())
+    assert pricing.tax_percentage.value == pricing_1.tax_percentage.value
 
     # Price for day 2 still uses the price from day 1, as the price for day 2 has different tax % and isn't ACTIVE yet
-    prices = ReservationPriceMixin().calculate_price(day_2, day_2, [reservation_unit])
-    assert prices.tax_percentage_value == pricing_1.tax_percentage.value
+    pricing = reservation_unit.actions.get_active_pricing(by_date=day_2.date())
+    assert pricing.tax_percentage.value == pricing_1.tax_percentage.value
 
     with freeze_time(day_2):
         # Price for day 2 is now used as expected
-        prices = ReservationPriceMixin().calculate_price(day_2, day_2, [reservation_unit])
-        assert prices.tax_percentage_value == pricing_2.tax_percentage.value
+        pricing = reservation_unit.actions.get_active_pricing(by_date=day_2.date())
+        assert pricing.tax_percentage.value == pricing_2.tax_percentage.value
 
 
-def test_reservation__calculate_price__active_is_free_future_is_paid_and_different_tax_percentage():
+def test_get_active_pricing__active_is_free_future_is_paid_and_different_tax_percentage():
     day_1 = local_datetime()
     day_2 = day_1 + datetime.timedelta(days=1)
 
@@ -161,14 +164,14 @@ def test_reservation__calculate_price__active_is_free_future_is_paid_and_differe
         price_unit=PriceUnit.PRICE_UNIT_FIXED,
     )
 
-    prices = ReservationPriceMixin().calculate_price(day_1, day_1, [reservation_unit])
-    assert prices.tax_percentage_value == Decimal(0)
+    pricing = reservation_unit.actions.get_active_pricing(by_date=day_1.date())
+    assert pricing.tax_percentage.value == Decimal(0)
 
-    prices = ReservationPriceMixin().calculate_price(day_2, day_2, [reservation_unit])
-    assert prices.tax_percentage_value == Decimal("25.5")
+    pricing = reservation_unit.actions.get_active_pricing(by_date=day_2.date())
+    assert pricing.tax_percentage.value == Decimal("25.5")
 
 
-def test_reservation__calculate_price__active_is_paid_future_is_free_and_different_tax_percentage():
+def test_get_active_pricing__active_is_paid_future_is_free_and_different_tax_percentage():
     day_1 = local_datetime()
     day_2 = day_1 + datetime.timedelta(days=1)
 
@@ -185,8 +188,8 @@ def test_reservation__calculate_price__active_is_paid_future_is_free_and_differe
         price_unit=PriceUnit.PRICE_UNIT_FIXED,
     )
 
-    prices = ReservationPriceMixin().calculate_price(day_1, day_1, [reservation_unit])
-    assert prices.tax_percentage_value == Decimal("25.5")
+    pricing = reservation_unit.actions.get_active_pricing(by_date=day_1.date())
+    assert pricing.tax_percentage.value == Decimal("25.5")
 
-    prices = ReservationPriceMixin().calculate_price(day_2, day_2, [reservation_unit])
-    assert prices.tax_percentage_value == Decimal(0)
+    pricing = reservation_unit.actions.get_active_pricing(by_date=day_2.date())
+    assert pricing.tax_percentage.value == Decimal(0)
