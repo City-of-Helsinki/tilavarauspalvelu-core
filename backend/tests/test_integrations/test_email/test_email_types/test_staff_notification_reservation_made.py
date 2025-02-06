52430e7f24b36054e5126f7da72cb38ac5e6523b
Eemeli Ranta <eemeli.ranta@vincit.fi>
2025-02-06 14:19:05 +0200

    Email: Split EmailType specific tests into separate files

---
+++ backend/tests/test_integrations/test_email/test_email_types/test_staff_notification_reservation_made.py
@@ -0,0 +1,331 @@
+# type: EmailType.STAFF_NOTIFICATION_RESERVATION_MADE
+
+from __future__ import annotations
+
+import datetime
+from inspect import cleandoc
+
+import pytest
+from django.test import override_settings
+from freezegun import freeze_time
+
+from tilavarauspalvelu.admin.email_template.utils import get_mock_data
+from tilavarauspalvelu.enums import EmailType, ReservationNotification, ReservationStateChoice
+from tilavarauspalvelu.integrations.email.main import EmailService
+from tilavarauspalvelu.integrations.email.rendering import render_html, render_text
+from tilavarauspalvelu.integrations.email.template_context import get_context_for_staff_notification_reservation_made
+from tilavarauspalvelu.integrations.sentry import SentryLogger
+
+from tests.factories import ReservationFactory, UnitFactory, UserFactory
+from tests.helpers import TranslationsFromPOFiles, patch_method
+from tests.test_integrations.test_email.helpers import (
+    BASE_TEMPLATE_CONTEXT_EN,
+    BASE_TEMPLATE_CONTEXT_FI,
+    BASE_TEMPLATE_CONTEXT_SV,
+    CLOSING_STAFF_CONTEXT_EN,
+    CLOSING_STAFF_CONTEXT_FI,
+    CLOSING_STAFF_CONTEXT_SV,
+    RESERVATION_BASIC_INFO_CONTEXT_EN,
+    RESERVATION_BASIC_INFO_CONTEXT_FI,
+    RESERVATION_BASIC_INFO_CONTEXT_SV,
+    html_email_to_text,
+)
+
+
+# CONTEXT ##############################################################################################################
+@pytest.mark.django_db
+@freeze_time("2024-01-01")
+def test_get_context__staff_notification_reservation_made__en(email_reservation):
+    with TranslationsFromPOFiles():
+        context = get_context_for_staff_notification_reservation_made(
+            reservee_name="[SÄHKÖPOSTIN VASTAANOTTAJAN NIMI]",
+            reservation_name="Test reservation",
+            reservation_unit_name="Test reservation unit",
+            unit_name="Test unit",
+            unit_location="Test Street, City",
+            begin_datetime=datetime.datetime(2024, 1, 1, 12),
+            end_datetime=datetime.datetime(2024, 1, 1, 14),
+            reservation_id=email_reservation.id,
+            language="en",
+        )
+
+    assert context == {
+        "email_recipient_name": None,
+        "reservation_name": "Test reservation",
+        "booking_number_label": "Booking number",
+        "reservation_id": f"{email_reservation.id}",
+        "reservee_name_label": "Reservee name",
+        "reservee_name": "[SÄHKÖPOSTIN VASTAANOTTAJAN NIMI]",
+        "staff_reservations_ext_link": f"https://fake.varaamo.hel.fi/kasittely/reservations/{email_reservation.id}",
+        "staff_reservations_ext_link_html": (
+            f'<a href="https://fake.varaamo.hel.fi/kasittely/reservations/{email_reservation.id}">'
+            f"https://fake.varaamo.hel.fi/kasittely/reservations/{email_reservation.id}</a>"
+        ),
+        "text_check_details": "You can view the booking at",
+        "text_staff_reservation_made": "A new booking has been confirmed for Test reservation unit",
+        "title": f"New booking {email_reservation.id} has been made for Test unit",
+        **BASE_TEMPLATE_CONTEXT_EN,
+        **RESERVATION_BASIC_INFO_CONTEXT_EN,
+        **CLOSING_STAFF_CONTEXT_EN,
+    }
+
+    with TranslationsFromPOFiles():
+        assert context == get_context_for_staff_notification_reservation_made(
+            reservation=email_reservation,
+            language="en",
+        )
+
+
+@freeze_time("2024-01-01")
+def test_get_context__staff_notification_reservation_made__fi():
+    with TranslationsFromPOFiles():
+        context = get_context_for_staff_notification_reservation_made(
+            reservee_name="Mikko Mallikas",
+            reservation_name="Test reservation",
+            reservation_unit_name="Test reservation unit",
+            unit_name="Test unit",
+            unit_location="Test Street, City",
+            begin_datetime=datetime.datetime(2024, 1, 1, 12),
+            end_datetime=datetime.datetime(2024, 1, 1, 14),
+            reservation_id=12,
+            language="fi",
+        )
+
+    assert context == {
+        "email_recipient_name": None,
+        "reservation_name": "Test reservation",
+        "booking_number_label": "Varausnumero",
+        "reservation_id": "12",
+        "reservee_name_label": "Varaajan nimi",
+        "reservee_name": "Mikko Mallikas",
+        "staff_reservations_ext_link": "https://fake.varaamo.hel.fi/kasittely/reservations/12",
+        "staff_reservations_ext_link_html": (
+            '<a href="https://fake.varaamo.hel.fi/kasittely/reservations/12">'
+            "https://fake.varaamo.hel.fi/kasittely/reservations/12</a>"
+        ),
+        "text_check_details": "Voit tarkistaa varauksen tiedot osoitteessa",
+        "text_staff_reservation_made": "Varausyksikköön Test reservation unit on tehty uusi hyväksytty varaus",
+        "title": "Toimipisteeseen Test unit on tehty uusi tilavaraus 12",
+        **BASE_TEMPLATE_CONTEXT_FI,
+        **RESERVATION_BASIC_INFO_CONTEXT_FI,
+        **CLOSING_STAFF_CONTEXT_FI,
+    }
+
+
+@freeze_time("2024-01-01")
+def test_get_context__staff_notification_reservation_made__sv():
+    with TranslationsFromPOFiles():
+        context = get_context_for_staff_notification_reservation_made(
+            reservee_name="Magnus Persson",
+            reservation_name="Test reservation",
+            reservation_unit_name="Test reservation unit",
+            unit_name="Test unit",
+            unit_location="Test Street, City",
+            begin_datetime=datetime.datetime(2024, 1, 1, 12),
+            end_datetime=datetime.datetime(2024, 1, 1, 14),
+            reservation_id=12,
+            language="sv",
+        )
+
+    assert context == {
+        "email_recipient_name": None,
+        "reservation_name": "Test reservation",
+        "booking_number_label": "Bokningsnummer",
+        "reservation_id": "12",
+        "reservee_name_label": "Bokare",
+        "reservee_name": "Magnus Persson",
+        "staff_reservations_ext_link": "https://fake.varaamo.hel.fi/kasittely/reservations/12",
+        "staff_reservations_ext_link_html": (
+            '<a href="https://fake.varaamo.hel.fi/kasittely/reservations/12">'
+            "https://fake.varaamo.hel.fi/kasittely/reservations/12</a>"
+        ),
+        "text_check_details": "Du kan se bokningen på",
+        "text_staff_reservation_made": "En ny bokningsförfrågan för Test reservation unit har bekräftats",
+        "title": "Ny bokning 12 har gjorts för Test unit",
+        **BASE_TEMPLATE_CONTEXT_SV,
+        **RESERVATION_BASIC_INFO_CONTEXT_SV,
+        **CLOSING_STAFF_CONTEXT_SV,
+    }
+
+
+# RENDER TEXT ##########################################################################################################
+
+
+@freeze_time("2024-01-01 12:00:00+02:00")
+def test_render_reservation_staff_notification_reservation_made__text():
+    context = get_mock_data(email_type=EmailType.STAFF_NOTIFICATION_RESERVATION_MADE, language="en")
+    text_content = render_text(email_type=EmailType.STAFF_NOTIFICATION_RESERVATION_MADE, context=context)
+
+    assert text_content == cleandoc(
+        """
+        Hi,
+
+        A new booking has been confirmed for [VARAUSYKSIKÖN NIMI]: [VARAUKSEN NIMI].
+
+        Reservee name: [VARAAJAN NIMI]
+        Booking number: 1234
+
+        [VARAUSYKSIKÖN NIMI]
+        [TOIMIPISTEEN NIMI]
+        [TOIMIPISTEEN OSOITE]
+
+        From: 1.1.2024 at 12:00
+        To: 2.1.2024 at 15:00
+
+        You can view the booking at:
+        https://fake.varaamo.hel.fi/kasittely/reservations/1234
+
+        Kind regards
+        Varaamo
+
+        This is an automated message, please do not reply.
+        """
+    )
+
+
+# RENDER HTML ##########################################################################################################
+
+
+@freeze_time("2024-01-01 12:00:00+02:00")
+def test_render_reservation_staff_notification_reservation_made__html():
+    context = get_mock_data(email_type=EmailType.STAFF_NOTIFICATION_RESERVATION_MADE, language="en")
+    html_content = render_html(email_type=EmailType.STAFF_NOTIFICATION_RESERVATION_MADE, context=context)
+    text_content = html_email_to_text(html_content)
+
+    assert text_content == cleandoc(
+        """
+        ![](https://makasiini.hel.ninja/helsinki-logos/helsinki-logo-black.png)
+
+        **Varaamo**
+
+        **Hi,**
+
+        A new booking has been confirmed for [VARAUSYKSIKÖN NIMI]: **[VARAUKSEN NIMI]**.
+
+        Reservee name: [VARAAJAN NIMI]
+        Booking number: 1234
+        **[VARAUSYKSIKÖN NIMI]**
+        [TOIMIPISTEEN NIMI]
+        [TOIMIPISTEEN OSOITE]
+        From: **1.1.2024** at **12:00**
+        To: **2.1.2024** at **15:00**
+
+        ## You can view the booking at
+
+        <https://fake.varaamo.hel.fi/kasittely/reservations/1234>
+
+        Kind regards
+        Varaamo
+        This is an automated message, please do not reply.
+
+        ![](https://makasiini.hel.ninja/helsinki-logos/helsinki-logo-black.png)
+
+        **Varaamo**
+
+        (C) City of Helsinki 2024
+        """
+    )
+
+
+# SEND EMAILS ##########################################################################################################
+
+
+@pytest.mark.django_db
+@override_settings(SEND_EMAILS=True)
+def test_email_service__send_staff_notification_reservation_made_email(outbox):
+    unit = UnitFactory.create(name_en="foo")
+
+    UserFactory.create_with_unit_role(
+        units=[unit],
+        email="admin@email.com",
+        reservation_notification=ReservationNotification.ALL,
+        preferred_language="en",
+    )
+
+    reservation = ReservationFactory.create(
+        state=ReservationStateChoice.CONFIRMED,
+        reservation_units__unit=unit,
+    )
+
+    EmailService.send_staff_notification_reservation_made_email(reservation)
+
+    assert len(outbox) == 1
+
+    assert outbox[0].subject == f"New booking {reservation.id} has been made for foo"
+    assert sorted(outbox[0].bcc) == ["admin@email.com"]
+
+
+@pytest.mark.django_db
+@override_settings(SEND_EMAILS=True)
+@patch_method(SentryLogger.log_message)
+def test_email_service__send_staff_notification_reservation_made_email__no_recipients(outbox):
+    unit = UnitFactory.create(name="foo")
+
+    reservation = ReservationFactory.create(
+        state=ReservationStateChoice.CONFIRMED,
+        reservation_units__unit=unit,
+    )
+
+    EmailService.send_staff_notification_reservation_made_email(reservation)
+
+    assert len(outbox) == 0
+
+    assert SentryLogger.log_message.call_count == 1
+    assert SentryLogger.log_message.call_args.args[0] == "No recipients for staff notification reservation made email"
+
+
+@pytest.mark.django_db
+@override_settings(SEND_EMAILS=True)
+def test_email_service__send_staff_notification_reservation_made_email__wrong_state(outbox):
+    unit = UnitFactory.create(name="foo")
+
+    UserFactory.create_with_unit_role(
+        units=[unit],
+        reservation_notification=ReservationNotification.ALL,
+        preferred_language="en",
+    )
+
+    reservation = ReservationFactory.create(
+        state=ReservationStateChoice.CANCELLED,
+        reservation_units__unit=unit,
+    )
+
+    EmailService.send_staff_notification_reservation_made_email(reservation)
+
+    assert len(outbox) == 0
+
+
+@pytest.mark.django_db
+@override_settings(SEND_EMAILS=True)
+def test_email_service__send_staff_notification_reservation_made_email__multiple_recipients(outbox):
+    unit = UnitFactory.create(name="foo", name_en="foo")
+
+    UserFactory.create_with_unit_role(
+        units=[unit],
+        email="admin1@email.com",
+        reservation_notification=ReservationNotification.ALL,
+        preferred_language="fi",
+    )
+
+    UserFactory.create_with_unit_role(
+        units=[unit],
+        email="admin2@email.com",
+        reservation_notification=ReservationNotification.ALL,
+        preferred_language="en",
+    )
+
+    reservation = ReservationFactory.create(
+        state=ReservationStateChoice.CONFIRMED,
+        reservation_units__unit=unit,
+    )
+
+    with TranslationsFromPOFiles():
+        EmailService.send_staff_notification_reservation_made_email(reservation)
+
+    assert len(outbox) == 2
+
+    assert outbox[0].subject == f"Toimipisteeseen foo on tehty uusi tilavaraus {reservation.id}"
+    assert sorted(outbox[0].bcc) == ["admin1@email.com"]
+
+    assert outbox[1].subject == f"New booking {reservation.id} has been made for foo"
+    assert sorted(outbox[1].bcc) == ["admin2@email.com"]
---
+++ backend/tests/test_integrations/test_email/test_email_types/test_user_anonymization.py
@@ -0,0 +1,335 @@
+# type: EmailType.USER_ANONYMIZATION
+
+from __future__ import annotations
+
+import datetime
+from inspect import cleandoc
+
+import pytest
+from django.test import override_settings
+from freezegun import freeze_time
+
+from tilavarauspalvelu.admin.email_template.utils import get_mock_data
+from tilavarauspalvelu.enums import EmailType, Language
+from tilavarauspalvelu.integrations.email.main import EmailService
+from tilavarauspalvelu.integrations.email.rendering import render_html, render_text
+from tilavarauspalvelu.integrations.email.template_context import get_context_for_user_anonymization
+from tilavarauspalvelu.models.user.actions import ANONYMIZED_FIRST_NAME, ANONYMIZED_LAST_NAME
+from utils.date_utils import local_datetime
+
+from tests.factories import UserFactory
+from tests.helpers import TranslationsFromPOFiles
+from tests.test_integrations.test_email.helpers import (
+    AUTOMATIC_REPLY_CONTEXT_EN,
+    AUTOMATIC_REPLY_CONTEXT_FI,
+    AUTOMATIC_REPLY_CONTEXT_SV,
+    BASE_TEMPLATE_CONTEXT_EN,
+    BASE_TEMPLATE_CONTEXT_FI,
+    BASE_TEMPLATE_CONTEXT_SV,
+    CLOSING_CONTEXT_EN,
+    CLOSING_CONTEXT_FI,
+    CLOSING_CONTEXT_SV,
+    html_email_to_text,
+)
+
+# CONTEXT ##############################################################################################################
+
+
+@freeze_time("2024-01-01")
+def test_get_context__user_anonymization__en():
+    with TranslationsFromPOFiles():
+        context = get_context_for_user_anonymization(language="en")
+
+    assert context == {
+        "email_recipient_name": None,
+        "title": "Your user account in the Varaamo service is expiring",
+        "text_user_anonymization": (
+            "Your user account in the Varaamo service will expire if you do not log in within two weeks. "
+            "The information will be permanently deleted if your account expires."
+        ),
+        "text_login_to_prevent": "You can extend the validity of your user account by logging into the service at",
+        "login_url": "https://fake.varaamo.hel.fi/en",
+        "login_url_html": '<a href="https://fake.varaamo.hel.fi/en">https://fake.varaamo.hel.fi/en</a>',
+        **BASE_TEMPLATE_CONTEXT_EN,
+        **CLOSING_CONTEXT_EN,
+        **AUTOMATIC_REPLY_CONTEXT_EN,
+    }
+
+
+@freeze_time("2024-01-01")
+def test_get_context__user_anonymization__fi():
+    with TranslationsFromPOFiles():
+        context = get_context_for_user_anonymization(language="fi")
+
+    assert context == {
+        "email_recipient_name": None,
+        "title": "Käyttäjätilisi Varaamo-palveluun on vanhentumassa",
+        "text_user_anonymization": (
+            "Käyttäjätilisi Varaamo-palvelussa vanhenee, jos et kirjaudu sisään palveluun kahden viikon kuluessa. "
+            "Tiedot poistetaan pysyvästi, jos tilisi vanhenee."
+        ),
+        "text_login_to_prevent": "Voit jatkaa käyttäjätilisi voimassaoloa kirjautumalla sisään palveluun osoitteessa",
+        "login_url": "https://fake.varaamo.hel.fi",
+        "login_url_html": '<a href="https://fake.varaamo.hel.fi">https://fake.varaamo.hel.fi</a>',
+        **BASE_TEMPLATE_CONTEXT_FI,
+        **CLOSING_CONTEXT_FI,
+        **AUTOMATIC_REPLY_CONTEXT_FI,
+    }
+
+
+@freeze_time("2024-01-01")
+def test_get_context__user_anonymization__sv():
+    with TranslationsFromPOFiles():
+        context = get_context_for_user_anonymization(language="sv")
+
+    assert context == {
+        "email_recipient_name": None,
+        "title": "Ditt användarkonto i Varaamo-tjänsten håller på att gå ut",
+        "text_user_anonymization": (
+            "Ditt användarkonto på tjänsten Varaamo kommer att upphöra om du inte loggar in inom två veckor. "
+            "Informationen kommer att raderas permanent om ditt konto upphör."
+        ),
+        "text_login_to_prevent": (
+            "Du kan förlänga giltigheten för ditt användarkonto genom att logga in på tjänsten på"
+        ),
+        "login_url": "https://fake.varaamo.hel.fi/sv",
+        "login_url_html": '<a href="https://fake.varaamo.hel.fi/sv">https://fake.varaamo.hel.fi/sv</a>',
+        **BASE_TEMPLATE_CONTEXT_SV,
+        **CLOSING_CONTEXT_SV,
+        **AUTOMATIC_REPLY_CONTEXT_SV,
+    }
+
+
+# RENDER TEXT ##########################################################################################################
+
+
+@freeze_time("2024-01-01 12:00:00+02:00")
+def test_render_user_anonymization__text():
+    context = get_mock_data(email_type=EmailType.USER_ANONYMIZATION, language="en")
+    text_content = render_text(email_type=EmailType.USER_ANONYMIZATION, context=context)
+
+    message = (
+        "Your user account in the Varaamo service will expire if you do not log in within two weeks. "
+        "The information will be permanently deleted if your account expires."
+    )
+
+    assert text_content == cleandoc(
+        f"""
+        Hi,
+
+        {message}
+
+        You can extend the validity of your user account by logging into the service at:
+        https://fake.varaamo.hel.fi/en
+
+        Kind regards
+        Varaamo
+
+        This is an automated message, please do not reply. Contact us: https://fake.varaamo.hel.fi/feedback?lang=en.
+
+        Book the city's premises and equipment for your use at https://fake.varaamo.hel.fi/en.
+        """
+    )
+
+
+# RENDER HTML ##########################################################################################################
+
+
+@freeze_time("2024-01-01 12:00:00+02:00")
+def test_render_user_anonymization__html():
+    context = get_mock_data(email_type=EmailType.USER_ANONYMIZATION, language="en")
+    html_content = render_html(email_type=EmailType.USER_ANONYMIZATION, context=context)
+    text_content = html_email_to_text(html_content)
+
+    message = (
+        "Your user account in the Varaamo service will expire if you do not log in within two weeks. "
+        "The information will be permanently deleted if your account expires."
+    )
+
+    assert text_content == cleandoc(
+        f"""
+        ![](https://makasiini.hel.ninja/helsinki-logos/helsinki-logo-black.png)
+
+        **Varaamo**
+
+        **Hi,**
+
+        {message}
+
+        You can extend the validity of your user account by logging into the service at:
+        <https://fake.varaamo.hel.fi/en>
+
+        Kind regards
+        Varaamo
+        This is an automated message, please do not reply.
+        [Contact us](https://fake.varaamo.hel.fi/feedback?lang=en).
+        Book the city's premises and equipment for your use at [varaamo.hel.fi](https://fake.varaamo.hel.fi/en).
+
+        ![](https://makasiini.hel.ninja/helsinki-logos/helsinki-logo-black.png)
+
+        **Varaamo**
+
+        (C) City of Helsinki 2024
+        """
+    )
+
+
+# SEND EMAILS ##########################################################################################################
+
+
+@pytest.mark.django_db
+@freeze_time("2024-01-01")
+@override_settings(
+    SEND_EMAILS=True,
+    ANONYMIZE_USER_IF_LAST_LOGIN_IS_OLDER_THAN_DAYS=10,
+    ANONYMIZATION_NOTIFICATION_BEFORE_DAYS=5,
+)
+def test_email_service__send_user_anonymization_emails(outbox):
+    UserFactory.create(
+        email="user@email.com",
+        preferred_language=Language.EN.value,
+        last_login=local_datetime() - datetime.timedelta(days=20),
+    )
+
+    EmailService.send_user_anonymization_emails()
+
+    assert len(outbox) == 1
+
+    assert outbox[0].subject == "Your user account in the Varaamo service is expiring"
+    assert sorted(outbox[0].bcc) == ["user@email.com"]
+
+
+@pytest.mark.django_db
+@freeze_time("2024-01-01")
+@override_settings(
+    SEND_EMAILS=True,
+    ANONYMIZE_USER_IF_LAST_LOGIN_IS_OLDER_THAN_DAYS=10,
+    ANONYMIZATION_NOTIFICATION_BEFORE_DAYS=5,
+)
+def test_email_service__send_user_anonymization_emails__no_email(outbox):
+    UserFactory.create(
+        # User has no email, so we can't notify them (but we should still anonymize)
+        email="",
+        preferred_language=Language.EN.value,
+        last_login=local_datetime() - datetime.timedelta(days=20),
+    )
+
+    EmailService.send_user_anonymization_emails()
+
+    assert len(outbox) == 0
+
+
+@pytest.mark.django_db
+@freeze_time("2024-01-01")
+@override_settings(
+    SEND_EMAILS=True,
+    ANONYMIZE_USER_IF_LAST_LOGIN_IS_OLDER_THAN_DAYS=10,
+    ANONYMIZATION_NOTIFICATION_BEFORE_DAYS=5,
+)
+def test_email_service__send_user_anonymization_emails__logged_in_recently(outbox):
+    UserFactory.create(
+        email="user@email.com",
+        preferred_language=Language.EN.value,
+        last_login=local_datetime() - datetime.timedelta(days=1),
+    )
+
+    EmailService.send_user_anonymization_emails()
+
+    assert len(outbox) == 0
+
+
+@pytest.mark.django_db
+@freeze_time("2024-01-01")
+@override_settings(
+    SEND_EMAILS=True,
+    ANONYMIZE_USER_IF_LAST_LOGIN_IS_OLDER_THAN_DAYS=10,
+    ANONYMIZATION_NOTIFICATION_BEFORE_DAYS=5,
+)
+def test_email_service__send_user_anonymization_emails__going_to_be_old(outbox):
+    UserFactory.create(
+        email="user@email.com",
+        preferred_language=Language.EN.value,
+        # Logged in within `ANONYMIZE_USER_IF_LAST_LOGIN_IS_OLDER_THAN_DAYS`,
+        # but after `ANONYMIZATION_NOTIFICATION_BEFORE_DAYS`
+        # user will be considered inactive, so we need to send email now.
+        last_login=local_datetime() - datetime.timedelta(days=6),
+    )
+
+    EmailService.send_user_anonymization_emails()
+
+    assert len(outbox) == 1
+
+    assert outbox[0].subject == "Your user account in the Varaamo service is expiring"
+    assert sorted(outbox[0].bcc) == ["user@email.com"]
+
+
+@pytest.mark.django_db
+@freeze_time("2024-01-01")
+@override_settings(
+    SEND_EMAILS=True,
+    ANONYMIZE_USER_IF_LAST_LOGIN_IS_OLDER_THAN_DAYS=10,
+    ANONYMIZATION_NOTIFICATION_BEFORE_DAYS=5,
+)
+def test_email_service__send_user_anonymization_emails__already_anonymized(outbox):
+    UserFactory.create(
+        first_name=ANONYMIZED_FIRST_NAME,
+        last_name=ANONYMIZED_LAST_NAME,
+        email="user@email.com",
+        preferred_language=Language.EN.value,
+        last_login=local_datetime() - datetime.timedelta(days=20),
+    )
+
+    EmailService.send_user_anonymization_emails()
+
+    assert len(outbox) == 0
+
+
+@pytest.mark.django_db
+@freeze_time("2024-01-01")
+@override_settings(
+    SEND_EMAILS=True,
+    ANONYMIZE_USER_IF_LAST_LOGIN_IS_OLDER_THAN_DAYS=10,
+    ANONYMIZATION_NOTIFICATION_BEFORE_DAYS=5,
+)
+def test_email_service__send_user_anonymization_emails__multiple_languages(outbox):
+    UserFactory.create(
+        email="user1@email.com",
+        preferred_language=Language.EN.value,
+        last_login=local_datetime() - datetime.timedelta(days=20),
+    )
+    UserFactory.create(
+        email="user2@email.com",
+        preferred_language=Language.FI.value,
+        last_login=local_datetime() - datetime.timedelta(days=25),
+    )
+
+    with TranslationsFromPOFiles():
+        EmailService.send_user_anonymization_emails()
+
+    assert len(outbox) == 2
+
+    assert outbox[0].subject == "Käyttäjätilisi Varaamo-palveluun on vanhentumassa"
+    assert sorted(outbox[0].bcc) == ["user2@email.com"]
+
+    assert outbox[1].subject == "Your user account in the Varaamo service is expiring"
+    assert sorted(outbox[1].bcc) == ["user1@email.com"]
+
+
+@pytest.mark.django_db
+@freeze_time("2024-01-01")
+@override_settings(
+    SEND_EMAILS=True,
+    ANONYMIZE_USER_IF_LAST_LOGIN_IS_OLDER_THAN_DAYS=10,
+    ANONYMIZATION_NOTIFICATION_BEFORE_DAYS=5,
+)
+def test_email_service__send_user_anonymization_emails__email_already_sent(outbox):
+    UserFactory.create(
+        email="user@email.com",
+        preferred_language=Language.EN.value,
+        last_login=local_datetime() - datetime.timedelta(days=20),
+        sent_email_about_anonymization=True,
+    )
+
+    EmailService.send_user_anonymization_emails()
+
+    assert len(outbox) == 0
