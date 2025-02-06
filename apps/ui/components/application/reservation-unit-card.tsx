City-of-Helsinki/tilavarauspalvelu-ui

fix: current step comparison and stepper handle click
- also fixes reservation-unit-card aria-hidden

apps/ui/components/application/reservation-unit-card.tsx
@@ -240,25 +240,25 @@ export function ReservationUnitCard({
         <OrderButtonContainer>
           <DeleteContainer>
             <DeleteButton
-              iconRight={undefined}
               variant="supplementary"
               size="small"
               onClick={() => {
                 onDelete(reservationUnit);
               }}
+              iconRight={undefined}
             >
               {t("reservationUnitList:buttonRemove")}
             </DeleteButton>
           </DeleteContainer>
           <UpButton
-            iconLeft={<IconArrowUp aria-hidden />}
+            iconLeft={<IconArrowUp aria-hidden="true" />}
             onClick={() => onMoveUp(reservationUnit)}
             disabled={first}
           >
             {t("reservationUnitList:buttonUp")}
           </UpButton>
           <DownButton
-            iconLeft={<IconArrowDown aria-hidden />}
+            iconLeft={<IconArrowDown aria-hidden="true" />}
             onClick={() => onMoveDown(reservationUnit)}
             disabled={last}
           >

City-of-Helsinki/tilavarauspalvelu-core

apps/ui/components/application/reservation-unit-card.tsx
@@ -245,12 +245,10 @@ export function ReservationUnitCard({
         <OrderButtonContainer>
           <DeleteContainer>
             <DeleteButton
-              iconEnd={undefined}
               variant={ButtonVariant.Supplementary}
               size={ButtonSize.Small}
-              onClick={() => {
-                onDelete(reservationUnit);
-              }}
+              iconEnd={undefined}
+              onClick={() => onDelete(reservationUnit)}
             >
               {t("reservationUnitList:buttonRemove")}
             </DeleteButton>