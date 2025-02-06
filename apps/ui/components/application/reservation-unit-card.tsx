City-of-Helsinki/tilavarauspalvelu-ui

fix: new cards to reservation unit order list, style fixes to the page

apps/ui/components/application/reservation-unit-card.tsx
@@ -1,20 +1,16 @@
-import {
-  IconArrowDown,
-  IconArrowUp,
-  IconGroup,
-  IconCross,
-  Notification,
-} from "hds-react";
+import { IconArrowDown, IconArrowUp, Notification, Button } from "hds-react";
 import React from "react";
 import { useTranslation } from "next-i18next";
 import styled from "styled-components";
-import { fontBold, H6 } from "common/src/common/typography";
+import { fontBold, fontRegular, H6 } from "common/src/common/typography";
 import { breakpoints } from "common/src/common/style";
 import type { ReservationUnitCardFieldsFragment } from "@gql/gql-types";
 import { getMainImage, getTranslation } from "@/modules/util";
 import { MediumButton } from "@/styles/util";
 import { getReservationUnitName } from "@/modules/reservationUnit";
 import { getImageSource } from "common/src/helpers";
+import Card from "common/src/components/Card";
+import { Flex } from "common/styles/util";
 
 type ReservationUnitType = ReservationUnitCardFieldsFragment;
 type Props = {
@@ -28,135 +24,171 @@ type Props = {
   invalid: boolean;
 };
 
-const NameCardContainer = styled.div``;
+const NameCardContainer = styled(Flex).attrs({ $gap: "0" })`
+  flex-direction: column;
+  @media (min-width: ${breakpoints.m}) {
+    flex-direction: row;
+  }
+`;
 
 const PreCardLabel = styled(H6).attrs({ as: "h3" })`
   margin-bottom: 0;
   margin-top: 0;
-`;
-
-const CardButtonContainer = styled.div`
-  display: grid;
-  grid-template-columns: 8fr 1fr;
-  margin-top: var(--spacing-2-xs);
-  align-items: center;
-  position: relative;
-
-  @media (max-width: ${breakpoints.m}) {
-    grid-template-columns: 1fr;
-  }
-`;
-
-const CardContainer = styled.div`
-  background-color: var(--tilavaraus-gray);
-  display: grid;
-  align-items: flex-start;
-  grid-template-columns: 1fr;
-
   @media (min-width: ${breakpoints.m}) {
-    grid-template-columns: 163px 6fr 2fr;
-    gap: var(--spacing-xs);
-  }
-`;
-
-const PaddedCell = styled.div`
-  padding: var(--spacing-m) 0;
-
-  @media (min-width: ${breakpoints.s}) {
     display: flex;
-    flex-direction: column;
-    gap: var(--spacing-xs);
+    align-items: center;
+    font-size: var(--fontsize-heading-l);
+    width: 3ch;
+    overflow: hidden; /* maybe overkill, but this makes sure index numbers > 99 won't end up breaking the layout */
+    ${fontRegular}
+    span {
+      display: none;
+    }
   }
 `;
 
-const ExtraPaddedCell = styled(PaddedCell)`
-  padding: var(--spacing-s);
-
-  @media (min-width: ${breakpoints.s}) {
-    gap: 0;
-  }
-`;
-
-const ImageCell = styled.div<{ $src?: string }>`
-  background-image: url(${(props) => props.$src});
-  width: 100%;
-  height: 150px;
-  background-size: cover;
-
+const OverlayContainer = styled(Flex)`
+  position: relative;
   @media (min-width: ${breakpoints.m}) {
-    height: 100%;
+    flex-direction: row;
+    width: 100%;
   }
 `;
 
-const Name = styled.div`
-  ${fontBold};
-  font-size: var(--fontsize-heading-s);
-  line-height: var(--lineheight-xl);
-  margin-bottom: var(--spacing-3-xs);
+const CardContainer = styled(Flex)`
+  @media (min-width: ${breakpoints.m}) {
+    width: calc(100% - 230px);
+    overflow: hidden;
+    [class*="Card__ImageWrapper"] {
+      max-width: 147px;
+      max-height: 99px !important;
+    }
+    [class*="Card__Header"],
+    [class*="Card__Text"] {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    [class*="Card__Header"] {
+      font-size: var(--fontsize-heading-xs);
+    }
+  }
 `;
 
-const UnitName = styled.div``;
-
-const MaxPersonsContainer = styled.div`
+const OrderButtonContainer = styled.div`
   display: flex;
-  align-items: center;
-  gap: var(--spacing-xs);
-  font-size: var(--fontsize-body-s);
-  margin-top: var(--spacing-xs);
-  padding-bottom: var(--spacing-2-xs);
+  margin-top: calc(var(--spacing-m) * -1);
+  background: var(--color-black-5);
+  padding: 0 var(--spacing-m) var(--spacing-m);
+  @media (min-width: ${breakpoints.m}) {
+    position: absolute;
+    top: 0;
+    right: 0;
+    display: grid;
+    grid-template-columns: 100px 1fr;
+    grid-template-rows: 1fr 1fr;
+    height: 100%;
+    width: 230px;
+    margin-top: 0;
+    padding: 0;
+    background: var(--color-black-20);
+    row-gap: 2px;
+  }
+`;
+
+const DeleteContainer = styled(Flex).attrs({ $justifyContent: "center" })`
+  background: var(--color-black-5);
+  order: 3;
+  @media (min-width: ${breakpoints.m}) {
+    grid-column: 1;
+    grid-row: 1 / span 2;
+    border-right: 2px solid var(--color-black-20);
+  }
 `;
 
 const DeleteButton = styled(MediumButton)`
   white-space: nowrap;
   margin: var(--spacing-s) var(--spacing-s) var(--spacing-s) 0;
-  place-self: flex-start;
+  color: var(--color-black-90) !important;
+  font-family: var(--font-bold), sans-serif !important;
   @media (min-width: ${breakpoints.m}) {
-    place-self: flex-end;
+    grid-column: 1;
+    grid-row: 1 / 2;
+    align-self: flex-end;
+  }
+  &:hover {
+    background-color: var(--color-black-10) !important;
   }
 `;
 
-const ArrowContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  position: absolute;
-  right: var(--spacing-m);
-  bottom: var(--spacing-m);
-
-  @media (min-width: ${breakpoints.m}) {
-    position: static;
-    flex-direction: column;
-    gap: var(--spacing-s);
-    justify-self: flex-end;
-  }
-`;
-
-const Circle = styled.div<{ passive: boolean }>`
-  margin-left: var(--spacing-xs);
-  height: var(--spacing-layout-m);
-  width: var(--spacing-layout-m);
-  background-color: ${(props) =>
-    props.passive ? "var(--color-black-10)" : "var(--color-white)"};
-  color: ${(props) =>
-    props.passive ? "var(--color-black-50)" : "var(--color-bus)"};
-  border-width: 2px;
-  border-style: solid;
-  border-color: ${(props) =>
-    props.passive ? "var(--color-black-10)" : "var(--color-bus)"};
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  button {
-    &:disabled {
-      color: var(--color-black-40);
-      cursor: default;
+const OrderButton = styled(Button)`
+  && {
+    position: relative;
+    z-index: 2;
+    color: var(--color-black-90) !important;
+    background-color: var(--color-white);
+    border-color: var(--color-black-90) !important;
+    ${fontBold}
+    &:hover,
+    &:focus:hover {
+      background-color: var(--color-black-5);
     }
 
-    border: 0;
-    background-color: transparent;
-    color: var(--color-bus);
-    cursor: pointer;
+    &:focus {
+      background-color: var(--color-white);
+    }
+
+    &:disabled {
+      background-color: var(--color-black-5);
+      border-color: var(--color-black-20) !important;
+      color: var(--color-black-20) !important;
+      z-index: 1;
+      ${fontRegular}
+    }
+    @media (min-width: ${breakpoints.m}) {
+      background-color: var(--color-black-5);
+      border-color: var(--color-black-5) !important;
+
+      &:hover,
+      &:focus:hover {
+        background-color: var(--color-black-10);
+      }
+
+      &:focus {
+        background-color: var(--color-black-5);
+      }
+
+      &:disabled {
+        background-color: var(--color-black-5);
+        color: var(--color-black-20) !important;
+        border-color: transparent !important;
+        ${fontRegular}
+      }
+    }
+    svg {
+      scale: 1.5;
+    }
+  }
+`;
+
+const UpButton = styled(OrderButton)`
+  @media (min-width: ${breakpoints.m}) {
+    grid-column: 2;
+    grid-row: 1;
+  }
+`;
+
+const DownButton = styled(OrderButton)`
+  margin-left: -2px !important;
+  margin-right: auto !important;
+  @media (min-width: ${breakpoints.m}) {
+    margin-left: 0 !important;
+    margin-right: 0 !important;
+    grid-column: 2;
+    grid-row: 2;
+    &&:disabled {
+      border-right: 0 !important;
+    }
   }
 `;
 
@@ -189,66 +221,51 @@ export function ReservationUnitCard({
   return (
     <NameCardContainer>
       <PreCardLabel>
-        {t("reservationUnitList:option")} {order + 1}.
+        <span>{t("reservationUnitList:option")} </span>
+        {order + 1}.
       </PreCardLabel>
       {invalid ? (
         <ErrorNotification
           label={t("application:validation.reservationUnitTooSmall")}
         />
       ) : null}
-      <CardButtonContainer>
+      <OverlayContainer>
         <CardContainer>
-          <ImageCell $src={imgSrc} />
-          <ExtraPaddedCell>
-            <Name>{getReservationUnitName(reservationUnit)}</Name>
-            <UnitName>{unitName}</UnitName>
-            <MaxPersonsContainer>
-              {reservationUnit.maxPersons && (
-                <>
-                  <IconGroup aria-hidden size="s" />
-                  {t("reservationUnitCard:maxPersons", {
-                    count: reservationUnit.maxPersons,
-                  })}
-                </>
-              )}
-            </MaxPersonsContainer>
-          </ExtraPaddedCell>
-          <DeleteButton
-            variant="supplementary"
-            iconLeft={<IconCross aria-hidden />}
-            size="small"
-            onClick={() => {
-              onDelete(reservationUnit);
-            }}
-          >
-            {t("reservationUnitList:buttonRemove")}
-          </DeleteButton>
+          <Card
+            heading={getReservationUnitName(reservationUnit) ?? ""}
+            text={unitName}
+            imageSrc={imgSrc}
+          />
         </CardContainer>
-        <ArrowContainer>
-          <Circle passive={first}>
-            <button
-              className="button-reset"
-              disabled={first}
-              type="button"
-              aria-label={t("reservationUnitList:buttonUp")}
-              onClick={() => onMoveUp(reservationUnit)}
+        <OrderButtonContainer>
+          <DeleteContainer>
+            <DeleteButton
+              iconRight={undefined}
+              variant="supplementary"
+              size="small"
+              onClick={() => {
+                onDelete(reservationUnit);
+              }}
             >
-              <IconArrowUp aria-hidden size="m" />
-            </button>
-          </Circle>
-          <Circle passive={last}>
-            <button
-              className="button-reset"
-              aria-label={t("reservationUnitList:buttonDown")}
-              type="button"
-              disabled={last}
-              onClick={() => onMoveDown(reservationUnit)}
-            >
-              <IconArrowDown aria-hidden size="m" />
-            </button>
-          </Circle>
-        </ArrowContainer>
-      </CardButtonContainer>
+              {t("reservationUnitList:buttonRemove")}
+            </DeleteButton>
+          </DeleteContainer>
+          <UpButton
+            iconLeft={<IconArrowUp aria-hidden />}
+            onClick={() => onMoveUp(reservationUnit)}
+            disabled={first}
+          >
+            {t("reservationUnitList:buttonUp")}
+          </UpButton>
+          <DownButton
+            iconLeft={<IconArrowDown aria-hidden />}
+            onClick={() => onMoveDown(reservationUnit)}
+            disabled={last}
+          >
+            {t("reservationUnitList:buttonDown")}
+          </DownButton>
+        </OrderButtonContainer>
+      </OverlayContainer>
     </NameCardContainer>
   );
 }

City-of-Helsinki/tilavarauspalvelu-core

apps/ui/components/application/reservation-unit-card.tsx
@@ -1,10 +1,7 @@
 import {
   IconArrowDown,
   IconArrowUp,
-  IconGroup,
-  IconCross,
   Notification,
-  IconSize,
   ButtonSize,
   ButtonVariant,
   Button,
@@ -12,12 +9,14 @@ import {
 import React from "react";
 import { useTranslation } from "next-i18next";
 import styled from "styled-components";
-import { fontBold, H6 } from "common/src/common/typography";
+import { fontBold, fontRegular, H6 } from "common/src/common/typography";
 import { breakpoints } from "common/src/common/style";
 import type { ReservationUnitCardFieldsFragment } from "@gql/gql-types";
 import { getMainImage, getTranslation } from "@/modules/util";
 import { getReservationUnitName } from "@/modules/reservationUnit";
 import { getImageSource } from "common/src/helpers";
+import Card from "common/src/components/Card";
+import { Flex } from "common/styles/util";
 
 type ReservationUnitType = ReservationUnitCardFieldsFragment;
 type Props = {
@@ -31,132 +30,170 @@ type Props = {
   invalid: boolean;
 };
 
-const NameCardContainer = styled.div``;
+const NameCardContainer = styled(Flex).attrs({ $gap: "0" })`
+  flex-direction: column;
+  @media (min-width: ${breakpoints.m}) {
+    flex-direction: row;
+  }
+`;
 
 const PreCardLabel = styled(H6).attrs({ as: "h3" })`
   margin-bottom: 0;
   margin-top: 0;
-`;
-
-const CardButtonContainer = styled.div`
-  display: grid;
-  grid-template-columns: 8fr 1fr;
-  margin-top: var(--spacing-2-xs);
-  align-items: center;
-  position: relative;
-
-  @media (max-width: ${breakpoints.m}) {
-    grid-template-columns: 1fr;
-  }
-`;
-
-const CardContainer = styled.div`
-  background-color: var(--tilavaraus-gray);
-  display: grid;
-  align-items: flex-start;
-  grid-template-columns: 1fr;
-
   @media (min-width: ${breakpoints.m}) {
-    grid-template-columns: 163px 6fr 2fr;
-    gap: var(--spacing-xs);
-  }
-`;
-
-const PaddedCell = styled.div`
-  padding: var(--spacing-m) 0;
-
-  @media (min-width: ${breakpoints.s}) {
     display: flex;
-    flex-direction: column;
-    gap: var(--spacing-xs);
+    align-items: center;
+    font-size: var(--fontsize-heading-l);
+    width: 3ch;
+    overflow: hidden; /* maybe overkill, but this makes sure index numbers > 99 won't end up breaking the layout */
+    ${fontRegular}
+    span {
+      display: none;
+    }
   }
 `;
 
-const ExtraPaddedCell = styled(PaddedCell)`
-  padding: var(--spacing-s);
-
-  @media (min-width: ${breakpoints.s}) {
-    gap: 0;
-  }
-`;
-
-const ImageCell = styled.div<{ $src?: string }>`
-  background-image: url(${(props) => props.$src});
-  width: 100%;
-  height: 150px;
-  background-size: cover;
-
+const OverlayContainer = styled(Flex)`
+  position: relative;
   @media (min-width: ${breakpoints.m}) {
-    height: 100%;
+    flex-direction: row;
+    width: 100%;
   }
 `;
 
-const Name = styled.div`
-  ${fontBold};
-  font-size: var(--fontsize-heading-s);
-  line-height: var(--lineheight-xl);
-  margin-bottom: var(--spacing-3-xs);
+const CardContainer = styled(Flex)`
+  @media (min-width: ${breakpoints.m}) {
+    width: calc(100% - 230px);
+    overflow: hidden;
+    [class*="Card__ImageWrapper"] {
+      max-width: 147px;
+      max-height: 99px !important;
+    }
+    [class*="Card__Header"],
+    [class*="Card__Text"] {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    [class*="Card__Header"] {
+      font-size: var(--fontsize-heading-xs);
+    }
+  }
 `;
 
-const MaxPersonsContainer = styled.div`
+const OrderButtonContainer = styled.div`
   display: flex;
-  align-items: center;
-  gap: var(--spacing-xs);
-  font-size: var(--fontsize-body-s);
-  margin-top: var(--spacing-xs);
-  padding-bottom: var(--spacing-2-xs);
+  margin-top: calc(var(--spacing-m) * -1);
+  background: var(--color-black-5);
+  padding: 0 var(--spacing-m) var(--spacing-m);
+  @media (min-width: ${breakpoints.m}) {
+    position: absolute;
+    top: 0;
+    right: 0;
+    display: grid;
+    grid-template-columns: 100px 1fr;
+    grid-template-rows: 1fr 1fr;
+    height: 100%;
+    width: 230px;
+    margin-top: 0;
+    padding: 0;
+    background: var(--color-black-20);
+    row-gap: 2px;
+  }
+`;
+
+const DeleteContainer = styled(Flex).attrs({ $justifyContent: "center" })`
+  background: var(--color-black-5);
+  order: 3;
+  @media (min-width: ${breakpoints.m}) {
+    grid-column: 1;
+    grid-row: 1 / span 2;
+    border-right: 2px solid var(--color-black-20);
+  }
 `;
 
 const DeleteButton = styled(Button)`
   margin: var(--spacing-s) var(--spacing-s) var(--spacing-s) 0;
-  place-self: flex-start;
+  color: var(--color-black-90) !important;
+  font-family: var(--font-bold), sans-serif !important;
   @media (min-width: ${breakpoints.m}) {
-    place-self: flex-end;
+    grid-column: 1;
+    grid-row: 1 / 2;
+    align-self: flex-end;
+  }
+  &:hover {
+    background-color: var(--color-black-10) !important;
   }
 `;
 
-const ArrowContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  position: absolute;
-  right: var(--spacing-m);
-  bottom: var(--spacing-m);
-
-  @media (min-width: ${breakpoints.m}) {
-    position: static;
-    flex-direction: column;
-    gap: var(--spacing-s);
-    justify-self: flex-end;
-  }
-`;
-
-const Circle = styled.div<{ passive: boolean }>`
-  margin-left: var(--spacing-xs);
-  height: var(--spacing-layout-m);
-  width: var(--spacing-layout-m);
-  background-color: ${(props) =>
-    props.passive ? "var(--color-black-10)" : "var(--color-white)"};
-  color: ${(props) =>
-    props.passive ? "var(--color-black-50)" : "var(--color-bus)"};
-  border-width: 2px;
-  border-style: solid;
-  border-color: ${(props) =>
-    props.passive ? "var(--color-black-10)" : "var(--color-bus)"};
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  button {
-    &:disabled {
-      color: var(--color-black-40);
-      cursor: default;
+const OrderButton = styled(Button)`
+  && {
+    position: relative;
+    z-index: 2;
+    color: var(--color-black-90) !important;
+    background-color: var(--color-white);
+    border-color: var(--color-black-90) !important;
+    ${fontBold}
+    &:hover,
+    &:focus:hover {
+      background-color: var(--color-black-5);
     }
 
-    border: 0;
-    background-color: transparent;
-    color: var(--color-bus);
-    cursor: pointer;
+    &:focus {
+      background-color: var(--color-white);
+    }
+
+    &:disabled {
+      background-color: var(--color-black-5);
+      border-color: var(--color-black-20) !important;
+      color: var(--color-black-20) !important;
+      z-index: 1;
+      ${fontRegular}
+    }
+    @media (min-width: ${breakpoints.m}) {
+      background-color: var(--color-black-5);
+      border-color: var(--color-black-5) !important;
+
+      &:hover,
+      &:focus:hover {
+        background-color: var(--color-black-10);
+      }
+
+      &:focus {
+        background-color: var(--color-black-5);
+      }
+
+      &:disabled {
+        background-color: var(--color-black-5);
+        color: var(--color-black-20) !important;
+        border-color: transparent !important;
+        ${fontRegular}
+      }
+    }
+    svg {
+      scale: 1.5;
+    }
+  }
+`;
+
+const UpButton = styled(OrderButton)`
+  @media (min-width: ${breakpoints.m}) {
+    grid-column: 2;
+    grid-row: 1;
+  }
+`;
+
+const DownButton = styled(OrderButton)`
+  margin-left: -2px !important;
+  margin-right: auto !important;
+  @media (min-width: ${breakpoints.m}) {
+    margin-left: 0 !important;
+    margin-right: 0 !important;
+    grid-column: 2;
+    grid-row: 2;
+    &&:disabled {
+      border-right: 0 !important;
+    }
   }
 `;
 
@@ -189,66 +226,51 @@ export function ReservationUnitCard({
   return (
     <NameCardContainer>
       <PreCardLabel>
-        {t("reservationUnitList:option")} {order + 1}.
+        <span>{t("reservationUnitList:option")} </span>
+        {order + 1}.
       </PreCardLabel>
       {invalid ? (
         <ErrorNotification
           label={t("application:validation.reservationUnitTooSmall")}
         />
       ) : null}
-      <CardButtonContainer>
+      <OverlayContainer>
         <CardContainer>
-          <ImageCell $src={imgSrc} />
-          <ExtraPaddedCell>
-            <Name>{getReservationUnitName(reservationUnit)}</Name>
-            <div>{unitName}</div>
-            <MaxPersonsContainer>
-              {reservationUnit.maxPersons && (
-                <>
-                  <IconGroup aria-hidden="true" size={IconSize.Small} />
-                  {t("reservationUnitCard:maxPersons", {
-                    count: reservationUnit.maxPersons,
-                  })}
-                </>
-              )}
-            </MaxPersonsContainer>
-          </ExtraPaddedCell>
-          <DeleteButton
-            variant={ButtonVariant.Supplementary}
-            iconStart={<IconCross aria-hidden="true" />}
-            size={ButtonSize.Small}
-            onClick={() => {
-              onDelete(reservationUnit);
-            }}
-          >
-            {t("reservationUnitList:buttonRemove")}
-          </DeleteButton>
+          <Card
+            heading={getReservationUnitName(reservationUnit) ?? ""}
+            text={unitName}
+            imageSrc={imgSrc}
+          />
         </CardContainer>
-        <ArrowContainer>
-          <Circle passive={first}>
-            <button
-              className="button-reset"
-              disabled={first}
-              type="button"
-              aria-label={t("reservationUnitList:buttonUp")}
-              onClick={() => onMoveUp(reservationUnit)}
+        <OrderButtonContainer>
+          <DeleteContainer>
+            <DeleteButton
+              iconEnd={undefined}
+              variant={ButtonVariant.Supplementary}
+              size={ButtonSize.Small}
+              onClick={() => {
+                onDelete(reservationUnit);
+              }}
             >
-              <IconArrowUp aria-hidden="true" size={IconSize.Small} />
-            </button>
-          </Circle>
-          <Circle passive={last}>
-            <button
-              className="button-reset"
-              aria-label={t("reservationUnitList:buttonDown")}
-              type="button"
-              disabled={last}
-              onClick={() => onMoveDown(reservationUnit)}
-            >
-              <IconArrowDown aria-hidden="true" size={IconSize.Small} />
-            </button>
-          </Circle>
-        </ArrowContainer>
-      </CardButtonContainer>
+              {t("reservationUnitList:buttonRemove")}
+            </DeleteButton>
+          </DeleteContainer>
+          <UpButton
+            iconStart={<IconArrowUp aria-hidden="true" />}
+            onClick={() => onMoveUp(reservationUnit)}
+            disabled={first}
+          >
+            {t("reservationUnitList:buttonUp")}
+          </UpButton>
+          <DownButton
+            iconStart={<IconArrowDown aria-hidden="true" />}
+            onClick={() => onMoveDown(reservationUnit)}
+            disabled={last}
+          >
+            {t("reservationUnitList:buttonDown")}
+          </DownButton>
+        </OrderButtonContainer>
+      </OverlayContainer>
     </NameCardContainer>
   );
 }