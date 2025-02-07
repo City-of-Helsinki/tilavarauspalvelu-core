044da16bad3a221eebef6420c801f415a33fb04a
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-02-07 17:21:54 +0200

    fix: validate page3 using unregister
    The schema is cleaner if we register / unregister the necessary fields.
    This allows us to use a single top level undefined for a field that
    should not be validate instead of writing custom validation logic.

---
+++ apps/ui/components/application/ApplicationFormTextInput.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { TextInput } from "hds-react";
+import { useTranslation } from "next-i18next";
+import { useFormContext } from "react-hook-form";
+import { type ApplicationPage3FormValues } from "./form";
+
+type TextFields =
+  | "organisation.name"
+  | "organisation.coreBusiness"
+  | "organisation.identifier"
+  | "organisation.address.streetAddress"
+  | "organisation.address.postCode"
+  | "organisation.address.city"
+  | "contactPerson.firstName"
+  | "contactPerson.lastName"
+  | "contactPerson.phoneNumber"
+  | "billingAddress.streetAddress"
+  | "billingAddress.postCode"
+  | "billingAddress.city"
+  | "additionalInformation";
+export function ApplicationFormTextInput({
+  name,
+  disabled,
+}: {
+  name: TextFields;
+  disabled?: boolean;
+}): JSX.Element {
+  const { t } = useTranslation();
+  const { register, getFieldState } =
+    useFormContext<ApplicationPage3FormValues>();
+
+  const translateError = (errorMsg?: string) =>
+    errorMsg ? t(`application:validation.${errorMsg}`) : "";
+  const state = getFieldState(name);
+
+  // avoid duplicating the translation key without polluting the props for a single case
+  const transformedLabel = name.replace(
+    "organisation.address.",
+    "billingAddress."
+  );
+
+  return (
+    <TextInput
+      {...register(name)}
+      label={t(`application:Page3.${transformedLabel}`)}
+      id={name}
+      required={!disabled}
+      disabled={disabled}
+      invalid={!!state.error?.message}
+      errorText={translateError(state.error?.message)}
+    />
+  );
+}
--- apps/ui/components/application/CompanyForm.tsx
+++ apps/ui/components/application/CompanyForm.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { TextInput } from "hds-react";
 import { useTranslation } from "next-i18next";
 import { useFormContext } from "react-hook-form";
 import { FormSubHeading } from "./styled";
 import { BillingAddress } from "./BillingAddress";
 import { type ApplicationPage3FormValues } from "./form";
 import { ControlledCheckbox } from "common/src/components/form/ControlledCheckbox";
+import { ApplicationFormTextInput } from "./ApplicationFormTextInput";
 
 export function CompanyForm(): JSX.Element {
   const { t } = useTranslation();
@@ -49,44 +49,5 @@ export function ContactPersonSection(): JSX.Element {
       <ApplicationFormTextInput name="contactPerson.lastName" />
       <ApplicationFormTextInput name="contactPerson.phoneNumber" />
     </>
-  );
-}
-
-type TextFields =
-  | "organisation.name"
-  | "organisation.coreBusiness"
-  | "organisation.identifier"
-  | "organisation.address.streetAddress"
-  | "organisation.address.postCode"
-  | "organisation.address.city"
-  | "contactPerson.firstName"
-  | "contactPerson.lastName"
-  | "contactPerson.phoneNumber"
-  | "additionalInformation";
-export function ApplicationFormTextInput({
-  name,
-  disabled,
-}: {
-  name: TextFields;
-  disabled?: boolean;
-}): JSX.Element {
-  const { t } = useTranslation();
-  const { register, getFieldState } =
-    useFormContext<ApplicationPage3FormValues>();
-
-  const translateError = (errorMsg?: string) =>
-    errorMsg ? t(`application:validation.${errorMsg}`) : "";
-  const state = getFieldState(name);
-
-  return (
-    <TextInput
-      {...register(name)}
-      label={t(`application:Page3.${name}`)}
-      id={name}
-      required={!disabled}
-      disabled={disabled}
-      invalid={!!state.error?.message}
-      errorText={translateError(state.error?.message)}
-    />
   );
 }
