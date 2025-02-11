871b488ccb0544080e4d69a940084567a17b0125
Eemeli Ranta <eemeli.ranta@vincit.fi>
2025-02-11 03:32:45 +0200

    Email: Refactor `EmailType` from Enum to classes
    - List required fields in the class
    - Use the list for creating tester forms and getting mock context data

--- backend/tilavarauspalvelu/integrations/email/rendering.py
+++ backend/tilavarauspalvelu/integrations/email/rendering.py
@@ -18,12 +18,12 @@ __all__ = [
 ]
 
 
-def render_text(*, email_type: EmailType, context: EmailContext) -> str:
+def render_text(*, email_type: type[EmailType], context: EmailContext) -> str:
     template_text: Template = get_template(email_type.text_path)
     return template_text.render(context).strip()
 
 
-def render_html(*, email_type: EmailType, context: EmailContext) -> str:
+def render_html(*, email_type: type[EmailType], context: EmailContext) -> str:
     # Assumes that all email HTML templates are written in MJML (as they should be to be compatible with Outlook)
     template_html: Template = get_template(email_type.html_path)
     mjml_content = template_html.render(context)
