49a49a0ae7c182ea9d100d98f1803ce05871b1ff
Matti Lamppu <matti.lamppu@vincit.fi>
2025-02-12 12:54:38 +0200

    Add `should_have_active_access_code` to series admin panel view

--- backend/tilavarauspalvelu/admin/recurring_reservation/admin.py
+++ backend/tilavarauspalvelu/admin/recurring_reservation/admin.py
@@ -1,16 +1,31 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from django.contrib import admin
 from django.contrib.admin import EmptyFieldListFilter
+from lookup_property import L
 
 from tilavarauspalvelu.admin.reservation.admin import ReservationInline
 from tilavarauspalvelu.models import RecurringReservation
+
+from .filters import ShouldHaveActiveAccessCodeFilter
+
+if TYPE_CHECKING:
+    from django.db import models
+
+    from tilavarauspalvelu.typing import WSGIRequest
+
+
+__all__ = [
+    "RecurringReservationAdmin",
+]
 
 
 @admin.register(RecurringReservation)
 class RecurringReservationAdmin(admin.ModelAdmin):
     # List
-    list_display = [
+    fields = [
         "ext_uuid",
         "name",
         "reservation_unit",
@@ -18,11 +33,35 @@ class RecurringReservationAdmin(admin.ModelAdmin):
         "begin_date",
         "end_date",
         "recurrence_in_days",
+        "should_have_active_access_code",
     ]
-    list_filter = [("allocated_time_slot", EmptyFieldListFilter)]
+    list_display = [
+        "name",
+        "reservation_unit",
+        "begin_date",
+        "end_date",
+    ]
+    list_filter = [
+        ("allocated_time_slot", EmptyFieldListFilter),
+        ShouldHaveActiveAccessCodeFilter,
+    ]
 
     # Form
     readonly_fields = [
         "ext_uuid",
+        "should_have_active_access_code",
     ]
     inlines = [ReservationInline]
+
+    def get_queryset(self, request: WSGIRequest) -> models.QuerySet:
+        return (
+            super()
+            .get_queryset(request)
+            .select_related(
+                "reservation_unit",
+                "allocated_time_slot",
+            )
+            .annotate(
+                should_have_active_access_code=L("should_have_active_access_code"),
+            )
+        )
