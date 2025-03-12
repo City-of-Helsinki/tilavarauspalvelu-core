053b1b072e6d024f4175761403ea628fd854f117
Matti Lamppu <matti.lamppu@vincit.fi>
2025-03-12 11:09:19 +0200

    Reschedule series and seasonal bookings when creating missing

--- backend/tests/test_integrations/test_keyless_entry/test_pindora_service/test_create_missing_access_codes.py
+++ backend/tests/test_integrations/test_keyless_entry/test_pindora_service/test_create_missing_access_codes.py
@@ -8,12 +8,8 @@ import pytest
 from tilavarauspalvelu.enums import AccessType, ReservationStateChoice, ReservationTypeChoice
 from tilavarauspalvelu.integrations.keyless_entry import PindoraService
 from tilavarauspalvelu.integrations.keyless_entry.exceptions import PindoraConflictError
-from tilavarauspalvelu.typing import (
-    PindoraReservationInfoData,
-    PindoraSectionInfoData,
-    PindoraSeriesInfoData,
-    PindoraValidityInfoData,
-)
+from tilavarauspalvelu.integrations.keyless_entry.typing import PindoraAccessCodeModifyResponse
+from tilavarauspalvelu.typing import PindoraReservationInfoData
 from utils.date_utils import local_datetime
 
 from tests.factories import ApplicationSectionFactory, RecurringReservationFactory, ReservationFactory, UserFactory
@@ -238,29 +234,18 @@ def test_create_missing_access_codes__series__already_generated():
 
 @patch_method(PindoraService.create_access_code, side_effect=PindoraConflictError("conflict"))
 @patch_method(
-    PindoraService.get_access_code,
-    return_value=PindoraSeriesInfoData(
-        access_code="1234",
+    PindoraService.reschedule_access_code,
+    return_value=PindoraAccessCodeModifyResponse(
         access_code_generated_at=local_datetime(2024, 1, 1),
         access_code_is_active=True,
-        access_code_keypad_url="1234",
-        access_code_phone_number="1234",
-        access_code_sms_number="1234",
-        access_code_sms_message="1234",
-        access_code_validity=[
-            PindoraValidityInfoData(
-                reservation_id=0,
-                reservation_series_id=0,
-                access_code_begins_at=local_datetime(2024, 1, 1, 12),
-                access_code_ends_at=local_datetime(2024, 1, 1, 13),
-            ),
-        ],
     ),
 )
+@patch_method(PindoraService.activate_access_code)
+@patch_method(PindoraService.deactivate_access_code)
 @freezegun.freeze_time(local_datetime(2024, 1, 1))
-def test_create_missing_access_codes__series__get_if_exists():
+def test_create_missing_access_codes__series__reschedule_if_exists():
     series = RecurringReservationFactory.create()
-    reservation = ReservationFactory.create(
+    ReservationFactory.create(
         reservation_units=[series.reservation_unit],
         recurring_reservation=series,
         begin=local_datetime(2024, 1, 1, 12),
@@ -275,12 +260,81 @@ def test_create_missing_access_codes__series__get_if_exists():
     PindoraService.create_missing_access_codes()
 
     assert PindoraService.create_access_code.called is True
-    assert PindoraService.get_access_code.called is True
-    assert PindoraService.get_access_code.call_args.kwargs["obj"] == series
+    assert PindoraService.reschedule_access_code.called is True
+    assert PindoraService.reschedule_access_code.call_args.kwargs["obj"] == series
 
-    reservation.refresh_from_db()
-    assert reservation.access_code_generated_at == local_datetime(2024, 1, 1)
-    assert reservation.access_code_is_active is True
+    assert PindoraService.activate_access_code.called is False
+    assert PindoraService.deactivate_access_code.called is False
+
+
+@patch_method(PindoraService.create_access_code, side_effect=PindoraConflictError("conflict"))
+@patch_method(
+    PindoraService.reschedule_access_code,
+    return_value=PindoraAccessCodeModifyResponse(
+        access_code_generated_at=local_datetime(2024, 1, 1),
+        access_code_is_active=False,
+    ),
+)
+@patch_method(PindoraService.activate_access_code)
+@patch_method(PindoraService.deactivate_access_code)
+@freezegun.freeze_time(local_datetime(2024, 1, 1))
+def test_create_missing_access_codes__series__reschedule_if_exists__activate():
+    series = RecurringReservationFactory.create()
+    ReservationFactory.create(
+        reservation_units=[series.reservation_unit],
+        recurring_reservation=series,
+        begin=local_datetime(2024, 1, 1, 12),
+        end=local_datetime(2024, 1, 1, 13),
+        access_type=AccessType.ACCESS_CODE,
+        state=ReservationStateChoice.CONFIRMED,
+        type=ReservationTypeChoice.NORMAL,
+        access_code_is_active=False,
+        access_code_generated_at=None,
+    )
+
+    PindoraService.create_missing_access_codes()
+
+    assert PindoraService.create_access_code.called is True
+    assert PindoraService.reschedule_access_code.called is True
+    assert PindoraService.reschedule_access_code.call_args.kwargs["obj"] == series
+
+    assert PindoraService.activate_access_code.called is True
+    assert PindoraService.deactivate_access_code.called is False
+
+
+@patch_method(PindoraService.create_access_code, side_effect=PindoraConflictError("conflict"))
+@patch_method(
+    PindoraService.reschedule_access_code,
+    return_value=PindoraAccessCodeModifyResponse(
+        access_code_generated_at=local_datetime(2024, 1, 1),
+        access_code_is_active=True,
+    ),
+)
+@patch_method(PindoraService.activate_access_code)
+@patch_method(PindoraService.deactivate_access_code)
+@freezegun.freeze_time(local_datetime(2024, 1, 1))
+def test_create_missing_access_codes__series__reschedule_if_exists__deactivate():
+    series = RecurringReservationFactory.create()
+    ReservationFactory.create(
+        reservation_units=[series.reservation_unit],
+        recurring_reservation=series,
+        begin=local_datetime(2024, 1, 1, 12),
+        end=local_datetime(2024, 1, 1, 13),
+        access_type=AccessType.ACCESS_CODE,
+        state=ReservationStateChoice.CONFIRMED,
+        type=ReservationTypeChoice.BLOCKED,
+        access_code_is_active=False,
+        access_code_generated_at=None,
+    )
+
+    PindoraService.create_missing_access_codes()
+
+    assert PindoraService.create_access_code.called is True
+    assert PindoraService.reschedule_access_code.called is True
+    assert PindoraService.reschedule_access_code.call_args.kwargs["obj"] == series
+
+    assert PindoraService.activate_access_code.called is False
+    assert PindoraService.deactivate_access_code.called is True
 
 
 @patch_method(PindoraService.create_access_code)
@@ -401,27 +455,16 @@ def test_create_missing_access_codes__seasonal_booking__already_generated():
 
 @patch_method(PindoraService.create_access_code, side_effect=PindoraConflictError("conflict"))
 @patch_method(
-    PindoraService.get_access_code,
-    return_value=PindoraSectionInfoData(
-        access_code="1234",
+    PindoraService.reschedule_access_code,
+    return_value=PindoraAccessCodeModifyResponse(
         access_code_generated_at=local_datetime(2024, 1, 1),
         access_code_is_active=True,
-        access_code_keypad_url="1234",
-        access_code_phone_number="1234",
-        access_code_sms_number="1234",
-        access_code_sms_message="1234",
-        access_code_validity=[
-            PindoraValidityInfoData(
-                reservation_id=0,
-                reservation_series_id=0,
-                access_code_begins_at=local_datetime(2024, 1, 1, 12),
-                access_code_ends_at=local_datetime(2024, 1, 1, 13),
-            ),
-        ],
     ),
 )
+@patch_method(PindoraService.activate_access_code)
+@patch_method(PindoraService.deactivate_access_code)
 @freezegun.freeze_time(local_datetime(2024, 1, 1))
-def test_create_missing_access_codes__seasonal_booking__get_if_exists():
+def test_create_missing_access_codes__seasonal_booking__reschedule_if_exists():
     user = UserFactory.create()
     section = ApplicationSectionFactory.create(
         application__user=user,
@@ -429,7 +472,7 @@ def test_create_missing_access_codes__seasonal_booking__get_if_exists():
     series = RecurringReservationFactory.create(
         allocated_time_slot__reservation_unit_option__application_section=section,
     )
-    reservation = ReservationFactory.create(
+    ReservationFactory.create(
         user=user,
         reservation_units=[series.reservation_unit],
         recurring_reservation=series,
@@ -445,9 +488,92 @@ def test_create_missing_access_codes__seasonal_booking__get_if_exists():
     PindoraService.create_missing_access_codes()
 
     assert PindoraService.create_access_code.called is True
-    assert PindoraService.get_access_code.called is True
-    assert PindoraService.get_access_code.call_args.kwargs["obj"] == section
+    assert PindoraService.reschedule_access_code.called is True
+    assert PindoraService.reschedule_access_code.call_args.kwargs["obj"] == section
 
-    reservation.refresh_from_db()
-    assert reservation.access_code_generated_at == local_datetime(2024, 1, 1)
-    assert reservation.access_code_is_active is True
+    assert PindoraService.activate_access_code.called is False
+    assert PindoraService.deactivate_access_code.called is False
+
+
+@patch_method(PindoraService.create_access_code, side_effect=PindoraConflictError("conflict"))
+@patch_method(
+    PindoraService.reschedule_access_code,
+    return_value=PindoraAccessCodeModifyResponse(
+        access_code_generated_at=local_datetime(2024, 1, 1),
+        access_code_is_active=False,
+    ),
+)
+@patch_method(PindoraService.activate_access_code)
+@patch_method(PindoraService.deactivate_access_code)
+@freezegun.freeze_time(local_datetime(2024, 1, 1))
+def test_create_missing_access_codes__seasonal_booking__reschedule_if_exists__activate():
+    user = UserFactory.create()
+    section = ApplicationSectionFactory.create(
+        application__user=user,
+    )
+    series = RecurringReservationFactory.create(
+        allocated_time_slot__reservation_unit_option__application_section=section,
+    )
+    ReservationFactory.create(
+        user=user,
+        reservation_units=[series.reservation_unit],
+        recurring_reservation=series,
+        begin=local_datetime(2024, 1, 1, 12),
+        end=local_datetime(2024, 1, 1, 13),
+        access_type=AccessType.ACCESS_CODE,
+        state=ReservationStateChoice.CONFIRMED,
+        type=ReservationTypeChoice.NORMAL,
+        access_code_is_active=False,
+        access_code_generated_at=None,
+    )
+
+    PindoraService.create_missing_access_codes()
+
+    assert PindoraService.create_access_code.called is True
+    assert PindoraService.reschedule_access_code.called is True
+    assert PindoraService.reschedule_access_code.call_args.kwargs["obj"] == section
+
+    assert PindoraService.activate_access_code.called is True
+    assert PindoraService.deactivate_access_code.called is False
+
+
+@patch_method(PindoraService.create_access_code, side_effect=PindoraConflictError("conflict"))
+@patch_method(
+    PindoraService.reschedule_access_code,
+    return_value=PindoraAccessCodeModifyResponse(
+        access_code_generated_at=local_datetime(2024, 1, 1),
+        access_code_is_active=True,
+    ),
+)
+@patch_method(PindoraService.activate_access_code)
+@patch_method(PindoraService.deactivate_access_code)
+@freezegun.freeze_time(local_datetime(2024, 1, 1))
+def test_create_missing_access_codes__seasonal_booking__reschedule_if_exists__deactivate():
+    user = UserFactory.create()
+    section = ApplicationSectionFactory.create(
+        application__user=user,
+    )
+    series = RecurringReservationFactory.create(
+        allocated_time_slot__reservation_unit_option__application_section=section,
+    )
+    ReservationFactory.create(
+        user=user,
+        reservation_units=[series.reservation_unit],
+        recurring_reservation=series,
+        begin=local_datetime(2024, 1, 1, 12),
+        end=local_datetime(2024, 1, 1, 13),
+        access_type=AccessType.ACCESS_CODE,
+        state=ReservationStateChoice.CONFIRMED,
+        type=ReservationTypeChoice.BLOCKED,
+        access_code_is_active=False,
+        access_code_generated_at=None,
+    )
+
+    PindoraService.create_missing_access_codes()
+
+    assert PindoraService.create_access_code.called is True
+    assert PindoraService.reschedule_access_code.called is True
+    assert PindoraService.reschedule_access_code.call_args.kwargs["obj"] == section
+
+    assert PindoraService.activate_access_code.called is False
+    assert PindoraService.deactivate_access_code.called is True
