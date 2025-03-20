d970cc54a1eba9e849c6d2c5d39dd5ae7b3c6f99
Eemeli Ranta <eemeli.ranta@vincit.fi>
2025-03-20 11:49:47 +0200

    Rewrite `HaukiResourceHashUpdater`
    This was done due to problems encountered on the test server, where for some reason the process was interrupted (most likely due to timeout), which led to deletion of existing future ReservableTimeSpans, but not creation of new ones since the operation was not atomic before. Now if there are issues with any one OriginHaukiResource, the data processed before it is updated.
    Also fixes a discovered bug, where new data was not updated from the Hauki API unless the hash was changed

--- backend/tilavarauspalvelu/integrations/opening_hours/hauki_resource_hash_updater.py
+++ backend/tilavarauspalvelu/integrations/opening_hours/hauki_resource_hash_updater.py
@@ -1,14 +1,15 @@
 from __future__ import annotations
 
-import datetime
 import logging
 from typing import TYPE_CHECKING
+
+from django.db.transaction import atomic
 
 from tilavarauspalvelu.exceptions import ReservableTimeSpanClientNothingToDoError, ReservableTimeSpanClientValueError
 from tilavarauspalvelu.integrations.opening_hours.hauki_api_client import HaukiAPIClient
 from tilavarauspalvelu.integrations.opening_hours.reservable_time_span_client import ReservableTimeSpanClient
 from tilavarauspalvelu.models import OriginHaukiResource
-from utils.date_utils import DEFAULT_TIMEZONE, local_date
+from utils.date_utils import local_date, local_start_of_day
 
 if TYPE_CHECKING:
     from tilavarauspalvelu.integrations.opening_hours.hauki_api_types import HaukiAPIResource
@@ -20,100 +21,91 @@ class HaukiResourceHashUpdater:
     # List of resource ids that should be updated
     hauki_resource_ids: list[int]
 
-    # List of resources fetched from Hauki API
-    fetched_hauki_resources: list[HaukiAPIResource]
-
     # Resources that have had their opening hours (hash) changed
     resources_updated: list[OriginHaukiResource]
+    total_time_spans_created: int
 
     def __init__(self, hauki_resource_ids: list[int] | None = None) -> None:
         self.hauki_resource_ids = hauki_resource_ids
+        self.resources_updated = []
+        self.total_time_spans_created = 0
 
+        # Initialise the list of resource ids if it is not provided
         if self.hauki_resource_ids is None:
             self.hauki_resource_ids = OriginHaukiResource.objects.values_list("id", flat=True)
 
-        if not self.hauki_resource_ids:
-            logger.info("No resources to update.")
-
-        self.fetched_hauki_resources = []
-        self.resources_updated = []
-
     def run(self, *, force_refetch: bool = False) -> None:
-        self._fetch_hauki_resources()
+        fetched_hauki_resources = HaukiAPIClient.get_resources_all_pages(hauki_resource_ids=self.hauki_resource_ids)
 
-        if not self.fetched_hauki_resources:
-            logger.info("No resources returned from the Hauki API.")
+        logger.info(f"Fetched {len(fetched_hauki_resources)} hauki resources for hash update.")
+        if not fetched_hauki_resources:
             return
 
-        self._update_origin_hauki_resource_hashes(force_refetch=force_refetch)
+        for resource in fetched_hauki_resources:
+            self._process_single_hauki_resource(resource, force_refetch=force_refetch)
 
+        self._log_results()
+
+    def _log_results(self) -> None:
         if not self.resources_updated:
             logger.info("There was no need to update any OriginHaukiResource hashes.")
             return
-
-        self._create_reservable_time_spans_for_reservation_units()
-
         logger.info(f"Updated hashes for {len(self.resources_updated)} origin hauki resources.")
-        logger.info("Done!")
 
-    def _fetch_hauki_resources(self) -> None:
-        """Fetch resources from Hauki API based on the given resource ids."""
-        self.fetched_hauki_resources = HaukiAPIClient.get_resources_all_pages(
-            hauki_resource_ids=self.hauki_resource_ids
-        )
-        logger.info(f"Fetched {len(self.fetched_hauki_resources)} hauki resources in total.")
-
-    def _update_origin_hauki_resource_hashes(self, *, force_refetch: bool = False) -> None:
-        """Update hashes for OriginHaukiResources that have had their opening hours changed."""
-        cutoff_date = local_date()
-
-        for resource in self.fetched_hauki_resources:
-            # Check if the resources hash has changed
-            origin_hauki_resource = OriginHaukiResource.objects.filter(id=resource["id"]).first()
-
-            if origin_hauki_resource is None:
-                logger.info(f"OriginHaukiResource with ID '{resource['id']}' was not found.")
-                continue
-
-            if origin_hauki_resource.opening_hours_hash == resource["date_periods_hash"]:
-                if not force_refetch:
-                    logger.info(f"OriginHaukiResource '{resource['id']}' date periods hash is unchanged, skipping.")
-                    continue
-            else:
-                logger.info(f"Updating 'Opening Hours Hash' for resource '{resource['id']}'.")
-
-            # Update the hash and set the latest fetched date to None
-            origin_hauki_resource.opening_hours_hash = resource["date_periods_hash"]
-            origin_hauki_resource.latest_fetched_date = None
-            origin_hauki_resource.save()
-
-            # Delete all new reservable time spans for the resource.
-            # Old time spans are not deleted, as they are kept for archival purposes.
-            origin_hauki_resource.reservable_time_spans.filter(start_datetime__gte=cutoff_date).delete()
-
-            # If there are any ReservableTimeSpans that overlap with the cutoff date, have them end at the cutoff date.
-            # This way we can keep all past data, and have the new data start from the cutoff date.
-            origin_hauki_resource.reservable_time_spans.filter(end_datetime__gte=cutoff_date).update(
-                end_datetime=datetime.datetime.combine(cutoff_date, datetime.time.min, tzinfo=DEFAULT_TIMEZONE)
-            )
-
-            self.resources_updated.append(origin_hauki_resource)
-
-    def _create_reservable_time_spans_for_reservation_units(self) -> int:
-        """Create reservable time spans for all updated OriginHaukiResources."""
-        total_reservable_time_spans_created = 0
-
-        for resource in self.resources_updated:
-            try:
-                client = ReservableTimeSpanClient(resource)
-            except (ReservableTimeSpanClientValueError, ReservableTimeSpanClientNothingToDoError):
-                continue
-            num_created_time_spans = len(client.run())
-            total_reservable_time_spans_created += num_created_time_spans
-            logger.info(f"Created {num_created_time_spans} reservable time spans for resource '{resource}'.")
-
-        if total_reservable_time_spans_created:
-            logger.info(f"Created {total_reservable_time_spans_created} new reservable time spans in total.")
+        if self.total_time_spans_created:
+            logger.info(f"Created {self.total_time_spans_created} new reservable time spans in total.")
         else:
             logger.info("No reservable time spans created.")
-        return total_reservable_time_spans_created
+
+    @atomic
+    def _process_single_hauki_resource(self, resource: HaukiAPIResource, *, force_refetch: bool = False) -> None:
+        origin_hauki_resource = OriginHaukiResource.objects.filter(id=resource["id"]).first()
+        if origin_hauki_resource is None:
+            logger.warning(f"OriginHaukiResource with ID '{resource['id']}' was not found.")
+            return
+
+        should_update_resource = (
+            force_refetch
+            or origin_hauki_resource.opening_hours_hash != resource["date_periods_hash"]
+            or origin_hauki_resource.should_update_opening_hours
+        )
+        if not should_update_resource:
+            return
+
+        logger.debug(f"Updating 'Opening Hours Hash' for resource '{resource['id']}'.")
+
+        # Only delete existing ReservableTimeSpans if the hash has changed
+        if origin_hauki_resource.opening_hours_hash != resource["date_periods_hash"]:
+            self._discard_old_reservable_time_spans(origin_hauki_resource)
+
+        self._update_resource_hash(origin_hauki_resource, resource)
+        self._create_reservable_time_spans(origin_hauki_resource)
+
+    def _update_resource_hash(self, origin_hauki_resource: OriginHaukiResource, resource: HaukiAPIResource) -> None:
+        origin_hauki_resource.opening_hours_hash = resource["date_periods_hash"]
+        origin_hauki_resource.latest_fetched_date = None  # Set to None, to clarify that the data is not up-to-date
+        origin_hauki_resource.save()
+        self.resources_updated.append(origin_hauki_resource)
+
+    def _discard_old_reservable_time_spans(self, origin_hauki_resource: OriginHaukiResource) -> None:
+        cutoff_date = local_date()  # Delete all new ReservableTimeSpans that start after this date.
+
+        # Old time spans are not deleted, as they are kept for archival purposes.
+        origin_hauki_resource.reservable_time_spans.filter(start_datetime__gte=cutoff_date).delete()
+
+        # If the cutoff_date is during a ReservableTimeSpan, end it at the cutoff date.
+        # This way we can keep past data intact, and have the new data start from the cutoff date.
+        origin_hauki_resource.reservable_time_spans.filter(end_datetime__gte=cutoff_date).update(
+            end_datetime=local_start_of_day()
+        )
+
+    def _create_reservable_time_spans(self, origin_hauki_resource: OriginHaukiResource) -> None:
+        try:
+            client = ReservableTimeSpanClient(origin_hauki_resource)
+        except (ReservableTimeSpanClientValueError, ReservableTimeSpanClientNothingToDoError):
+            return
+
+        num_created_time_spans = len(client.run())
+        logger.info(f"Created {num_created_time_spans} reservable time spans for resource {origin_hauki_resource.id}.")
+
+        self.total_time_spans_created += num_created_time_spans
