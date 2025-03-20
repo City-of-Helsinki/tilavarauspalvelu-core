f14d2af4bae6fed50ea60a0f1088a51987368e30
Eemeli Ranta <eemeli.ranta@vincit.fi>
2025-03-20 11:49:47 +0200

    Rewrite `HaukiResourceHashUpdater`
    This was done due to problems encountered on the test server, where for some reason the process was interrupted (most likely due to timeout), which led to deletion of existing future ReservableTimeSpans, but not creation of new ones since the operation was not atomic before. Now if there are issues with any one OriginHaukiResource, the data processed before it is updated.
    Also fixes a discovered bug, where new data was not updated from the Hauki API unless the hash was changed

--- backend/tilavarauspalvelu/models/origin_hauki_resource/model.py
+++ backend/tilavarauspalvelu/models/origin_hauki_resource/model.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
+import datetime
 from typing import TYPE_CHECKING, ClassVar
 
+from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from utils.date_utils import local_date
 from utils.lazy import LazyModelAttribute, LazyModelManager
 
 if TYPE_CHECKING:
-    import datetime
-
     from .actions import OriginHaukiResourceActions
     from .queryset import OriginHaukiResourceManager
     from .validators import OriginHaukiResourceValidator
@@ -25,7 +26,7 @@ class OriginHaukiResource(models.Model):
     id: int = models.IntegerField(unique=True, primary_key=True)
     # Hauki API hash for opening hours, which is used to determine if the opening hours have changed
     opening_hours_hash: str = models.CharField(max_length=64, blank=True)
-    # Latest date fetched from Hauki opening hours API
+    # Latest date fetched from Hauki opening hours API (NOT when the data was last updated)
     latest_fetched_date: datetime.datetime.date = models.DateField(blank=True, null=True)
 
     objects: ClassVar[OriginHaukiResourceManager] = LazyModelManager.new()
@@ -41,3 +42,12 @@ class OriginHaukiResource(models.Model):
 
     def __str__(self) -> str:
         return str(self.id)
+
+    @property
+    def should_update_opening_hours(self) -> bool:
+        """Return True if the opening hours should be updated from Hauki API."""
+        return (
+            not self.opening_hours_hash
+            or self.latest_fetched_date is None
+            or self.latest_fetched_date < (local_date() + datetime.timedelta(days=settings.HAUKI_DAYS_TO_FETCH))
+        )
