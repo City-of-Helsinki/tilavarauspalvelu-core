6ce7ee27343525fd89ca0ec75d44567d04b5c058
Matti Lamppu <matti.lamppu@vincit.fi>
2025-03-25 09:13:56 +0200

    Fix filename

--- backend/tilavarauspalvelu/api/graphql/types/recurring_reservation/serializers/__init__.py
+++ backend/tilavarauspalvelu/api/graphql/types/recurring_reservation/serializers/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from .add_serializer import ReservationSeriesAddReservationSerializer
 from .change_access_code import ReservationSeriesChangeAccessCodeSerializer
-from .create_seriealizer import ReservationSeriesCreateSerializer
+from .create_serializer import ReservationSeriesCreateSerializer
 from .deny_serializer import ReservationSeriesDenyInputSerializer, ReservationSeriesDenyOutputSerializer
 from .reschedule_serializer import ReservationSeriesRescheduleSerializer
 from .update_serializer import ReservationSeriesUpdateSerializer
