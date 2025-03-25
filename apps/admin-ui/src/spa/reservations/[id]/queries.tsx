cb8eaf0929ce5e410e8ac548be54349dc8a1f266
Eemeli Ranta <eemeli.ranta@vincit.fi>
2025-03-25 16:01:52 +0200

    add mutations for series change/repair access code

--- apps/admin-ui/src/spa/reservations/[id]/queries.tsx
+++ apps/admin-ui/src/spa/reservations/[id]/queries.tsx
@@ -17,27 +17,3 @@ export const REQUIRE_HANDLING_RESERVATION = gql`
     }
   }
 `;
-
-export const CHANGE_RESERVATION_ACCESS_CODE = gql`
-  mutation ChangeReservationAccessCode(
-    $input: ReservationStaffChangeAccessCodeMutationInput!
-  ) {
-    staffChangeReservationAccessCode(input: $input) {
-      pk
-      accessCodeIsActive
-      accessCodeGeneratedAt
-    }
-  }
-`;
-
-export const REPAIR_RESERVATION_ACCESS_CODE = gql`
-  mutation RepairReservationAccessCode(
-    $input: ReservationStaffRepairAccessCodeMutationInput!
-  ) {
-    staffRepairReservationAccessCode(input: $input) {
-      pk
-      accessCodeIsActive
-      accessCodeGeneratedAt
-    }
-  }
-`;
