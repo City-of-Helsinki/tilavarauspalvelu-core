ca13fd1ffa1d9c0fa7a5d8a485988570d0cf7b4f
Mats Eriksson <mats.eriksson@vincit.fi>
2025-03-25 12:29:19 +0200

    add: ErrorContainer component, statusCode content & test page
    - add testing page for easy layout checking (test?statusCode=404)
    - add images and content for status code errors
    - remove old status code error pages
    
    wip: non-admin
    
    wip: non-admin

---
+++ apps/ui/pages/test.tsx
@@ -0,0 +1,24 @@
+import CustomErrorComponent from "@/pages/_error";
+import type { GetServerSidePropsContext } from "next";
+import { getCommonServerSideProps } from "@/modules/serverUtils";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useSearchParams } from "next/navigation";
+
+function Page() {
+  const params = useSearchParams();
+  const statusCode = params.get("statusCode")
+    ? Number(params.get("statusCode"))
+    : 500;
+  return <CustomErrorComponent statusCode={statusCode} />;
+}
+
+export async function getStaticProps({ locale }: GetServerSidePropsContext) {
+  return {
+    props: {
+      ...getCommonServerSideProps(),
+      ...(await serverSideTranslations(locale ?? "fi")),
+    },
+  };
+}
+
+export default Page;
