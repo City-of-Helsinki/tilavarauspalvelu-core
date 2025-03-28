16da8ca2a8f6a094458b471a9e4f25eefcaa635d
Eemeli Ranta <eemeli.ranta@vincit.fi>
2025-03-29 00:46:15 +0200

    fix: Add missing semicolons to styled components
    Fixes errors highlighted in IDE, probably not a real css issue

--- apps/ui/components/application/OrderedReservationUnitCard.tsx
+++ apps/ui/components/application/OrderedReservationUnitCard.tsx
@@ -121,7 +121,7 @@ const DeleteContainer = styled(Flex).attrs({ $justifyContent: "center" })`
 const DeleteButton = styled(Button)`
   color: var(--color-black-90) !important;
   && {
-    ${fontBold}
+    ${fontBold};
     --padding-horizontal: var(--spacing-s);
     --padding-vertical: var(--spacing-xs);
   }
