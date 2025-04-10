fd52e7c96def3f65d7973c6787e7eb536aac4768
Matti Lamppu <matti.lamppu@vincit.fi>
2025-04-10 12:47:19 +0300

    Fix beat scheduler ref

--- backend/config/celery/health_checks.py
+++ backend/config/celery/health_checks.py
@@ -14,12 +14,10 @@ HEALTH_CHECK_FILE = Path("/tmp/celery_heartbeat")  # noqa: S108  # nosec  # NOSO
 
 def create_health_check_file(**kwargs: Any) -> None:
     # Create the file to indicate that the beat is alive.
-    print("Creating health check file")  # noqa: T201, RUF100
     HEALTH_CHECK_FILE.touch(exist_ok=True)
 
 
 # No signal exists for this beat shutdown
 def remove_health_check_file(**kwargs: Any) -> None:
     # Remove the file to indicate that the worker has crashed.
-    print("Removing health check file")  # noqa: T201, RUF100
     HEALTH_CHECK_FILE.unlink(missing_ok=True)
--- backend/docker/scheduler.sh
+++ backend/docker/scheduler.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 echo "Starting Celery beat task scheduler..."
-exec celery -A config beat --loglevel=INFO --scheduler=django-tvp
+exec celery -A config beat --loglevel=INFO --scheduler=config.celery.beat:VaraamoDatabaseScheduler
