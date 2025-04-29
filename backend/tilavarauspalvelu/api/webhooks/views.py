bfeb733d717703af0884f8b096d86e5299c2545b
Matti Lamppu <matti.lamppu@vincit.fi>
2025-04-29 12:32:15 +0300

    Refactor Webhooks & support invoicing

--- backend/tilavarauspalvelu/api/webhooks/views.py
+++ backend/tilavarauspalvelu/api/webhooks/views.py
@@ -8,8 +8,9 @@ from rest_framework.response import Response
 from tilavarauspalvelu.enums import OrderStatus
 from tilavarauspalvelu.integrations.sentry import SentryLogger
 from tilavarauspalvelu.integrations.verkkokauppa.order.exceptions import GetOrderError
+from tilavarauspalvelu.integrations.verkkokauppa.order.types import WebShopOrderStatus
 from tilavarauspalvelu.integrations.verkkokauppa.payment.exceptions import GetPaymentError, GetRefundStatusError
-from tilavarauspalvelu.integrations.verkkokauppa.payment.types import PaymentStatus, RefundStatus
+from tilavarauspalvelu.integrations.verkkokauppa.payment.types import WebShopRefundStatus
 from tilavarauspalvelu.integrations.verkkokauppa.verkkokauppa_api_client import VerkkokauppaAPIClient
 from tilavarauspalvelu.models import PaymentOrder
 from utils.date_utils import local_datetime
@@ -37,32 +38,38 @@ class WebhookOrderPaidViewSet(viewsets.GenericViewSet):
 
         payment_order: PaymentOrder | None = PaymentOrder.objects.filter(remote_id=order_id).first()
         if payment_order is None:
-            msg = f"Payment order '{order_id}' not found"
+            msg = "Payment order not found"
             SentryLogger.log_message(f"Verkkokauppa: {msg}", details=serializer.validated_data)
             return Response(data={"message": msg}, status=404)
 
-        if payment_order.status not in {OrderStatus.DRAFT, OrderStatus.EXPIRED, OrderStatus.CANCELLED}:
+        if payment_order.status not in OrderStatus.can_be_marked_paid_statuses:
             msg = "Order is already in a state where no updates are needed"
             return Response(data={"message": msg}, status=200)
 
         try:
-            payment = VerkkokauppaAPIClient.get_payment(order_uuid=order_id)
+            webshop_payment = VerkkokauppaAPIClient.get_payment(order_uuid=order_id)
         except GetPaymentError:
-            msg = f"Checking payment for order '{order_id}' failed"
+            msg = "Could not get payment from verkkokauppa"
             SentryLogger.log_message(f"Verkkokauppa: {msg}", details=serializer.validated_data)
             return Response(data={"message": msg}, status=500)
 
-        if payment is None:
-            msg = f"Payment '{order_id}' not found from verkkokauppa"
+        if webshop_payment is None:
+            msg = "Payment not found from verkkokauppa"
             SentryLogger.log_message(f"Verkkokauppa: {msg}", details=serializer.validated_data)
             return Response(data={"message": msg}, status=404)
 
-        if payment.status != PaymentStatus.PAID_ONLINE.value:
-            msg = f"Invalid payment status: '{payment.status}'"
-            SentryLogger.log_message(f"Verkkokauppa: {msg}", details=serializer.validated_data)
+        order_status = payment_order.actions.get_order_status_from_webshop_payment(webshop_payment)
+        if order_status not in OrderStatus.paid_in_webshop:
+            msg = "Payment order doesn't require update based webshop payment status"
+            details = {
+                **serializer.validated_data,
+                "webshop_payment_status": webshop_payment.status,
+                "payment_order_status": order_status.value,
+            }
+            SentryLogger.log_message(f"Verkkokauppa: {msg}", details=details)
             return Response(data={"message": msg}, status=400)
 
-        payment_order.actions.update_order_status(new_status=OrderStatus.PAID, payment_id=payment_id)
+        payment_order.actions.update_order_status(new_status=order_status, payment_id=payment_id)
 
         return Response(data={"message": "Order payment completed successfully"}, status=200)
 
@@ -80,29 +87,34 @@ class WebhookOrderCancelViewSet(viewsets.ViewSet):
 
         payment_order: PaymentOrder | None = PaymentOrder.objects.filter(remote_id=order_id).first()
         if payment_order is None:
-            msg = f"Payment order '{order_id}' not found"
+            msg = "Payment order not found"
             SentryLogger.log_message(f"Verkkokauppa: {msg}", details=serializer.validated_data)
             return Response(data={"message": msg}, status=404)
 
-        if payment_order.status != OrderStatus.DRAFT:
+        if payment_order.status not in OrderStatus.can_be_cancelled_statuses:
             msg = "Order is already in a state where no updates are needed"
             return Response(data={"message": msg}, status=200)
 
         try:
-            order = VerkkokauppaAPIClient.get_order(order_uuid=order_id)
+            webshop_order = VerkkokauppaAPIClient.get_order(order_uuid=order_id)
         except GetOrderError:
-            msg = f"Checking order '{order_id}' failed"
+            msg = "Could not get order from verkkokauppa"
             SentryLogger.log_message(f"Verkkokauppa: {msg}", details=serializer.validated_data)
             return Response(data={"message": msg}, status=500)
 
-        if order is None:
-            msg = f"Order '{order_id}' not found from verkkokauppa"
+        if webshop_order is None:
+            msg = "Order not found from verkkokauppa"
             SentryLogger.log_message(f"Verkkokauppa: {msg}", details=serializer.validated_data)
             return Response(data={"message": msg}, status=404)
 
-        if order.status != "cancelled":
-            msg = f"Invalid order status: '{order.status}'"
-            SentryLogger.log_message(f"Verkkokauppa: {msg}", details=serializer.validated_data)
+        if webshop_order.status != WebShopOrderStatus.CANCELLED:
+            msg = "Payment order cannot be cancelled based webshop order status"
+            details = {
+                **serializer.validated_data,
+                "webshop_order_status": webshop_order.status,
+                "payment_order_status": payment_order.status,
+            }
+            SentryLogger.log_message(f"Verkkokauppa: {msg}", details=details)
             return Response(data={"message": msg}, status=400)
 
         payment_order.actions.set_order_as_cancelled()
@@ -124,34 +136,42 @@ class WebhookRefundViewSet(viewsets.ViewSet):
 
         payment_order: PaymentOrder | None = PaymentOrder.objects.filter(remote_id=order_id).first()
         if payment_order is None:
-            msg = f"Payment order {order_id=!s} not found"
+            msg = "Payment order not found"
             SentryLogger.log_message(f"Verkkokauppa: {msg}", details=serializer.validated_data)
             return Response(data={"message": msg}, status=404)
 
         if payment_order.refund_id is not None and payment_order.refund_id != refund_id:
-            msg = f"Refund ID mismatch: expected {payment_order.refund_id}, got {refund_id}"
-            SentryLogger.log_message(f"Verkkokauppa: {msg}", details=serializer.validated_data)
+            msg = "Refund ID mismatch"
+            details = {
+                **serializer.validated_data,
+                "payment_order_refund_id": payment_order.refund_id,
+            }
+            SentryLogger.log_message(f"Verkkokauppa: {msg}", details=details)
             return Response(data={"message": msg}, status=400)
 
-        if payment_order.status != OrderStatus.PAID:
+        if payment_order.status not in OrderStatus.can_be_refunded_statuses:
             msg = f"Order '{order_id}' is already in a state where no updates are needed"
             return Response(data={"message": msg}, status=200)
 
         try:
             refund_result = VerkkokauppaAPIClient.get_refund_status(order_uuid=order_id)
         except GetRefundStatusError:
-            msg = f"Checking order '{order_id}' failed"
+            msg = "Could not get refund status from verkkokauppa"
             SentryLogger.log_message(f"Verkkokauppa: {msg}", details=serializer.validated_data)
             return Response(data={"message": msg}, status=500)
 
         if refund_result is None:
-            msg = f"Refund for order '{order_id}' not found from verkkokauppa"
+            msg = "Refund status not found from verkkokauppa"
             SentryLogger.log_message(f"Verkkokauppa: {msg}", details=serializer.validated_data)
             return Response(data={"message": msg}, status=404)
 
-        if refund_result.status != RefundStatus.PAID_ONLINE.value:
-            msg = f"Invalid refund status: '{refund_result.status}'"
-            SentryLogger.log_message(f"Verkkokauppa: {msg}", details=serializer.validated_data)
+        if refund_result.status != WebShopRefundStatus.PAID_ONLINE:
+            msg = "Payment order cannot be refunded based webshop refund status"
+            details = {
+                **serializer.validated_data,
+                "webshop_refund_status": refund_result.status,
+            }
+            SentryLogger.log_message(f"Verkkokauppa: {msg}", details=details)
             return Response(data={"message": msg}, status=400)
 
         payment_order.refund_id = refund_id
