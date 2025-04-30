a24f6d53dcd00983ad0ac96737a0089ad3922277
Matti Lamppu <matti.lamppu@vincit.fi>
2025-04-30 11:53:10 +0300

    Cancel invoice if reservation cancelled or denied

--- backend/tilavarauspalvelu/models/payment_order/validators.py
+++ backend/tilavarauspalvelu/models/payment_order/validators.py
@@ -3,6 +3,12 @@ from __future__ import annotations
 import dataclasses
 from typing import TYPE_CHECKING
 
+from rest_framework.exceptions import ValidationError
+
+from tilavarauspalvelu.api.graphql.extensions import error_codes
+from tilavarauspalvelu.enums import OrderStatus
+from utils.date_utils import DEFAULT_TIMEZONE, local_date
+
 if TYPE_CHECKING:
     from tilavarauspalvelu.models import PaymentOrder
 
@@ -15,3 +21,27 @@ __all__ = [
 @dataclasses.dataclass(slots=True, frozen=True)
 class PaymentOrderValidator:
     payment_order: PaymentOrder
+
+    def validate_order_can_be_refunded(self) -> None:
+        if self.payment_order.status == OrderStatus.REFUNDED or self.payment_order.refund_id is not None:
+            msg = "Order has already been refunded."
+            raise ValidationError(msg, code=error_codes.ORDER_REFUND_NOT_ALLOWED)
+
+        if self.payment_order.status != OrderStatus.PAID:
+            msg = "Only orders paid online can be refunded."
+            raise ValidationError(msg, code=error_codes.ORDER_REFUND_NOT_ALLOWED)
+
+    def validate_order_can_be_cancelled(self) -> None:
+        if self.payment_order.status == OrderStatus.CANCELLED:
+            msg = "Order has already been cancelled."
+            raise ValidationError(msg, code=error_codes.ORDER_CANCELLATION_NOT_ALLOWED)
+
+        if self.payment_order.status != OrderStatus.PAID_BY_INVOICE:
+            msg = "Only orders paid by invoice can be cancelled."
+            raise ValidationError(msg, code=error_codes.ORDER_CANCELLATION_NOT_ALLOWED)
+
+        begin_date = self.payment_order.reservation.begin.astimezone(DEFAULT_TIMEZONE).date()
+
+        if local_date() > begin_date:
+            msg = "Order cannot be cancelled after its reservation start date."
+            raise ValidationError(msg, code=error_codes.ORDER_CANCELLATION_NOT_ALLOWED)
