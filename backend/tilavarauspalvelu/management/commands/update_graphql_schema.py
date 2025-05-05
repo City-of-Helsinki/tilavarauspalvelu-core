5a88c75c4d0a27249d859e9c7ab681169a66daae
Eemeli Ranta <eemeli.ranta@vincit.fi>
2025-05-05 17:23:44 +0300

    Add Github Action to test for up-to-date `tilavaraus.graphql` schema

--- backend/tilavarauspalvelu/management/commands/update_graphql_schema.py
+++ backend/tilavarauspalvelu/management/commands/update_graphql_schema.py
@@ -1,12 +1,25 @@
+# ruff: noqa: E402
 from __future__ import annotations
 
+import os
 from pathlib import Path
 from typing import Any
 
 from django.conf import settings
 from django.core.management import BaseCommand
+from django.utils.translation import activate
+
+# Remove `graphiql_debug_toolbar` from INSTALLED_APPS to prevent `debug` from being included in the schema.
+if "graphiql_debug_toolbar" in settings.INSTALLED_APPS:
+    settings.INSTALLED_APPS.pop(settings.INSTALLED_APPS.index("graphiql_debug_toolbar"))
+
+# Must be called before importing schema to force translation to English
+activate("en")
 
 from tilavarauspalvelu.api.graphql.schema import schema
+
+
+class OutdatedSchemaError(Exception): ...
 
 
 class Command(BaseCommand):
@@ -14,5 +27,12 @@ class Command(BaseCommand):
 
     def handle(self, *args: Any, **options: Any) -> None:
         filepath = settings.BASE_DIR.parent / "tilavaraus.graphql"
-        with Path.open(filepath, "w", encoding="utf-8") as file:
-            file.write(str(schema))
+        new_schema = str(schema)
+
+        old_schema = Path(filepath).read_text(encoding="utf-8")
+
+        if os.getenv("CI") == "true" and old_schema != new_schema:
+            msg = "GraphQL schema has changed. Please run `python manage.py update_graphql_schema` to update it."
+            raise OutdatedSchemaError(msg)
+
+        Path(filepath).write_text(new_schema, encoding="utf-8")
