1ca1ecec8067ab7fb4806d19c9167e0987fd834b
Eemeli Ranta <eemeli.ranta@vincit.fi>
2025-05-06 21:58:01 +0300

    Clearer error message for missing Helsinki Profile permissions

--- backend/utils/external_service/errors.py
+++ backend/utils/external_service/errors.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import contextlib
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from rest_framework.exceptions import ValidationError
 
@@ -15,6 +15,10 @@ if TYPE_CHECKING:
 
 class ExternalServiceError(Exception):
     """Base class for exceptions related to external services."""
+
+    def __init__(self, message: str = "", *, extra_data: Any = None) -> None:
+        super().__init__(message)
+        self.extra_data = extra_data
 
 
 class ExternalServiceRequestError(ExternalServiceError):
