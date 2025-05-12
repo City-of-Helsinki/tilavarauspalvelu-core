ef64ff3cee510b3abbcc1dd39e83947abc4d264c
Mats Eriksson <mats.eriksson@vincit.fi>
2025-05-12 13:00:34 +0300

    add: paid by invoice status to relevant admin views

--- apps/admin-ui/src/spa/ReservationUnit/edit/components/PricingTypeView.tsx
+++ apps/admin-ui/src/spa/ReservationUnit/edit/components/PricingTypeView.tsx
@@ -1,32 +1,25 @@
-import { Controller, UseFormReturn } from "react-hook-form";
-import type { ReservationUnitEditFormValues } from "@/spa/ReservationUnit/edit/form";
-import { EditAccordion } from "@/spa/ReservationUnit/edit/components/styled";
-import { AutoGrid } from "common/styled";
-import { ControlledCheckbox } from "common/src/components/form/ControlledCheckbox";
-import {
-  ControlledDateInput,
-  ControlledNumberInput,
-  ControlledSelect,
-} from "common/src/components/form";
-import { getTranslatedError } from "@/common/util";
 import React from "react";
-import { FieldGroup } from "@/spa/ReservationUnit/edit/components/FieldGroup";
 import styled from "styled-components";
-import { PaymentType, PriceUnit } from "@gql/gql-types";
-import { addDays } from "date-fns";
-import { IconAlertCircleFill, RadioButton } from "hds-react";
 import { useTranslation } from "react-i18next";
+import { IconAlertCircleFill, RadioButton } from "hds-react";
+import { PriceUnit } from "@gql/gql-types";
+import { Controller, UseFormReturn } from "react-hook-form";
+import { addDays } from "date-fns";
+import { AutoGrid } from "common/styled";
+import { getTranslatedError } from "@/common/util";
+import { type ReservationUnitEditFormValues } from "../form";
+import { ControlledDateInput } from "common/src/components/form";
+import { ControlledSelect } from "common/src/components/form/ControlledSelect";
+import { ControlledNumberInput } from "common/src/components/form/ControlledNumberInput";
 
 const Error = styled.div`
   margin-top: var(--spacing-3-xs);
   color: var(--color-error);
   display: flex;
   gap: var(--spacing-2-xs);
-
   svg {
     flex-shrink: 0;
   }
-
   white-space: nowrap;
 `;
 
@@ -66,13 +59,8 @@ function PaidPricingPart({
   const { errors } = formState;
 
   const unitPriceOptions = Object.values(PriceUnit).map((choice) => ({
+    value: choice,
     label: t(`priceUnit.${choice}`),
-    value: choice,
-  }));
-
-  const paymentTypeOptions = Object.values(PaymentType).map((choice) => ({
-    label: t(`paymentType.${choice}`),
-    value: choice,
   }));
 
   const pricing = watch(`pricings.${index}`);
@@ -202,19 +190,6 @@ function PaidPricingPart({
           errors.pricings?.[index]?.highestPrice?.message
         )}
       />
-      <ControlledSelect
-        // This is not pricing type specific
-        name={`pricings.${index}.paymentType`}
-        control={control}
-        required
-        options={paymentTypeOptions}
-        label={t("ReservationUnitEditor.label.paymentTypes")}
-        tooltip={t("ReservationUnitEditor.tooltip.paymentTypes")}
-        error={getTranslatedError(
-          t,
-          errors.pricings?.[index]?.paymentType?.message
-        )}
-      />
     </>
   );
 }
@@ -293,107 +268,5 @@ export function PricingTypeView({
         />
       )}
     </AutoGrid>
-  );
-}
-
-function PricingControl({
-  pricing,
-  form,
-  taxPercentageOptions,
-}: {
-  form: UseFormReturn<ReservationUnitEditFormValues>;
-  pricing: ReservationUnitEditFormValues["pricings"][0];
-  taxPercentageOptions: TaxOption[];
-}) {
-  const { t } = useTranslation();
-  return (
-    <FieldGroup
-      key={`pricing-${pricing.pk}`}
-      heading={t("ReservationUnitEditor.label.pricingType")}
-      required
-      tooltip={t("ReservationUnitEditor.tooltip.pricingType")}
-      style={{ gridColumn: "1 / -1" }}
-    >
-      <PricingTypeView
-        pk={pricing.pk}
-        form={form}
-        taxPercentageOptions={taxPercentageOptions}
-      />
-    </FieldGroup>
-  );
-}
-
-export function PricingSection({
-  form,
-  taxPercentageOptions,
-  pricingTermsOptions,
-}: {
-  form: UseFormReturn<ReservationUnitEditFormValues>;
-  taxPercentageOptions: TaxOption[];
-  pricingTermsOptions: { value: string; label: string }[];
-}) {
-  const { t } = useTranslation();
-  const { control, watch, formState } = form;
-  const { errors } = formState;
-
-  const pricings = watch("pricings");
-  const isPaid = pricings.filter((p) => p.isPaid).length > 0;
-  const hasErrors = errors.pricings != null;
-
-  return (
-    <EditAccordion
-      open={hasErrors}
-      heading={t("ReservationUnitEditor.label.pricings")}
-    >
-      <AutoGrid>
-        {watch("pricings")
-          .filter((p) => !p.isFuture)
-          .map((pricing) => (
-            <PricingControl
-              key={pricing.pk}
-              form={form}
-              pricing={pricing}
-              taxPercentageOptions={taxPercentageOptions}
-            />
-          ))}
-        <ControlledCheckbox
-          control={control}
-          name="hasFuturePricing"
-          label={t("ReservationUnitEditor.label.hasFuturePrice")}
-          style={{ gridColumn: "1 / -1" }}
-        />
-        {watch("hasFuturePricing") &&
-          watch("pricings")
-            .filter((p) => p.isFuture)
-            .map((pricing) => (
-              <PricingControl
-                key={pricing.pk}
-                form={form}
-                pricing={pricing}
-                taxPercentageOptions={taxPercentageOptions}
-              />
-            ))}
-        {isPaid && (
-          <ControlledCheckbox
-            control={control}
-            name="canApplyFreeOfCharge"
-            label={t("ReservationUnitEditor.label.canApplyFreeOfCharge")}
-            tooltip={t("ReservationUnitEditor.tooltip.canApplyFreeOfCharge")}
-          />
-        )}
-        {watch("canApplyFreeOfCharge") && isPaid && (
-          <ControlledSelect
-            control={control}
-            name="pricingTerms"
-            label={t("ReservationUnitEditor.label.pricingTerms")}
-            required
-            clearable
-            options={pricingTermsOptions}
-            error={getTranslatedError(t, errors.pricingTerms?.message)}
-            tooltip={t("ReservationUnitEditor.tooltip.pricingTerms")}
-          />
-        )}
-      </AutoGrid>
-    </EditAccordion>
   );
 }
--- apps/admin-ui/src/spa/reservations/ReservationsDataLoader.tsx
+++ apps/admin-ui/src/spa/reservations/ReservationsDataLoader.tsx
@@ -24,6 +24,8 @@ function transformPaymentStatusSafe(t: string): OrderStatusWithFree | null {
       return OrderStatusWithFree.Paid;
     case OrderStatusWithFree.PaidManually:
       return OrderStatusWithFree.PaidManually;
+    case OrderStatusWithFree.PaidByInvoice:
+      return OrderStatusWithFree.PaidByInvoice;
     case OrderStatusWithFree.Draft:
       return OrderStatusWithFree.Draft;
     case OrderStatusWithFree.Expired:
