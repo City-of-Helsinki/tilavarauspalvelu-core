c98885d5bc8f9f197d6768d196b05f2cf0cee8ee
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-05-20 13:01:44 +0300

    Refactor application imports

--- apps/ui/components/application/BillingAddress.tsx
+++ apps/ui/components/application/BillingAddress.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
 import { FormSubHeading } from "./styled";
-import { ApplicationFormTextInput } from "./ApplicationFormTextInput";
+import { ApplicationFormTextInput } from ".";
 
 export function BillingAddress() {
   const { t } = useTranslation();
