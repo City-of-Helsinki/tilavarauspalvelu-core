674536b4e4ddbcc9eaf0c15d21ae389dd935220d
Matti Lamppu <matti.lamppu@vincit.fi>
2025-05-30 14:13:04 +0300

    Refactor `prune_reservation_statistics` tests

--- backend/tilavarauspalvelu/models/reservation_statistic/queryset.py
+++ backend/tilavarauspalvelu/models/reservation_statistic/queryset.py
@@ -20,9 +20,16 @@ class ReservationStatisticQuerySet(models.QuerySet):
         remove_older_than = local_datetime() - relativedelta(years=older_than_years)
         return self.filter(reservation_created_at__lte=remove_older_than)
 
+
+# Need to do this to get proper type hints in the manager methods, since
+# 'from_queryset' returns a subclass of Manager, but is not typed correctly...
+_BaseManager: type[models.Manager] = models.Manager.from_queryset(ReservationStatisticQuerySet)  # type: ignore[assignment]
+
+
+class ReservationStatisticManager(_BaseManager):
+    # Define to get type hints for queryset methods.
+    def all(self) -> ReservationStatisticQuerySet:
+        return super().all()  # type: ignore[return-value]
+
     def delete_expired_statistics(self) -> None:
-        """Deletes expired ReservationStatistics objects."""
-        self.expired_statistics().delete()
-
-
-class ReservationStatisticManager(models.Manager.from_queryset(ReservationStatisticQuerySet)): ...
+        self.all().expired_statistics().delete()
