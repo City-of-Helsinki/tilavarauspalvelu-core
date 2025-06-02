08b0db38db98bf79ab5846fcc60c1ad77d0922e5
Matti Lamppu <matti.lamppu@vincit.fi>
2025-06-02 11:54:37 +0300

    Remove BugReport, SQLLog & RequestLog

--- backend/tilavarauspalvelu/services/export/__init__.py
+++ backend/tilavarauspalvelu/services/export/__init__.py
@@ -3,11 +3,9 @@ from __future__ import annotations
 from .application_round_applications_exporter import ApplicationRoundApplicationsCSVExporter
 from .application_round_result_exporter import ApplicationRoundResultCSVExporter
 from .reservation_unit_exporter import ReservationUnitExporter
-from .sql_log_exporter import SQLLogCSVExporter
 
 __all__ = [
     "ApplicationRoundApplicationsCSVExporter",
     "ApplicationRoundResultCSVExporter",
     "ReservationUnitExporter",
-    "SQLLogCSVExporter",
 ]
