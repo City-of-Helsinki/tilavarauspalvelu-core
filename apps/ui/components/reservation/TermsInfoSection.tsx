58a58f98c5fd79bd44bd387157db2f99935c88b4
Mats Eriksson <mats.eriksson@vincit.fi>
2025-06-08 04:54:18 +0300

    add: reservation view payment, cancellation & pricing terms tests

---
+++ apps/ui/components/reservation/TermsInfoSection.tsx
@@ -0,0 +1,109 @@
+import { useTranslation } from "next-i18next";
+import React, { useMemo } from "react";
+import { isReservationUnitFreeOfCharge } from "@/modules/reservationUnit";
+import {
+  convertLanguageCode,
+  getTranslationSafe,
+} from "common/src/common/util";
+import { AccordionWithState as Accordion } from "@/components/Accordion";
+import { Sanitize } from "common/src/components/Sanitize";
+import type { ReservationPageQuery } from "@gql/gql-types";
+import { getServerSideProps } from "@/pages/reservations/[id]";
+
+type Props = Awaited<ReturnType<typeof getServerSideProps>>["props"];
+type PropsNarrowed = Exclude<Props, { notFound: boolean }>;
+type NodeT = NonNullable<ReservationPageQuery["reservation"]>;
+
+export function TermsInfoSection({
+  reservation,
+  termsOfUse,
+}: Readonly<{
+  reservation: Pick<
+    NodeT,
+    "reservationUnits" | "begin" | "applyingForFreeOfCharge"
+  >;
+  termsOfUse: PropsNarrowed["termsOfUse"];
+}>) {
+  const { t, i18n } = useTranslation();
+  const reservationUnit = reservation.reservationUnits.find(() => true);
+
+  const shouldDisplayPricingTerms: boolean = useMemo(() => {
+    if (!reservationUnit) {
+      return false;
+    }
+
+    const isFreeOfCharge = isReservationUnitFreeOfCharge(
+      reservationUnit.pricings,
+      new Date(reservation.begin)
+    );
+
+    return (
+      reservation.applyingForFreeOfCharge ||
+      (reservationUnit.canApplyFreeOfCharge && !isFreeOfCharge)
+    );
+  }, [reservation, reservationUnit]);
+
+  const lang = convertLanguageCode(i18n.language);
+  const paymentTermsContent =
+    reservationUnit?.paymentTerms != null
+      ? getTranslationSafe(reservationUnit.paymentTerms, "text", lang)
+      : undefined;
+  const cancellationTermsContent =
+    reservationUnit?.cancellationTerms != null
+      ? getTranslationSafe(reservationUnit.cancellationTerms, "text", lang)
+      : undefined;
+  const pricingTermsContent =
+    reservationUnit?.pricingTerms != null
+      ? getTranslationSafe(reservationUnit?.pricingTerms, "text", lang)
+      : undefined;
+  const serviceSpecificTermsContent =
+    reservationUnit?.serviceSpecificTerms != null
+      ? getTranslationSafe(reservationUnit.serviceSpecificTerms, "text", lang)
+      : undefined;
+
+  return (
+    <div>
+      {(paymentTermsContent || cancellationTermsContent) && (
+        <Accordion
+          heading={t(
+            `reservationUnit:${
+              paymentTermsContent
+                ? "paymentAndCancellationTerms"
+                : "cancellationTerms"
+            }`
+          )}
+          theme="thin"
+          data-testid="reservation__payment-and-cancellation-terms"
+        >
+          {paymentTermsContent && <Sanitize html={paymentTermsContent} />}
+          {cancellationTermsContent && (
+            <Sanitize html={cancellationTermsContent} />
+          )}
+        </Accordion>
+      )}
+      {shouldDisplayPricingTerms && pricingTermsContent && (
+        <Accordion
+          heading={t("reservationUnit:pricingTerms")}
+          theme="thin"
+          data-testid="reservation__pricing-terms"
+        >
+          <Sanitize html={pricingTermsContent} />
+        </Accordion>
+      )}
+      <Accordion
+        heading={t("reservationUnit:termsOfUse")}
+        theme="thin"
+        data-testid="reservation__terms-of-use"
+      >
+        {serviceSpecificTermsContent && (
+          <Sanitize html={serviceSpecificTermsContent} />
+        )}
+        {termsOfUse?.genericTerms != null && (
+          <Sanitize
+            html={getTranslationSafe(termsOfUse.genericTerms, "text", lang)}
+          />
+        )}
+      </Accordion>
+    </div>
+  );
+}
