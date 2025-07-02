380f4902ec36d9d07d956233e2e3704c1641b1b0
Eemeli Ranta <eemeli.ranta@vincit.fi>
2025-07-02 14:22:49 +0300

    Fix circular dependency issue
    refs. https://github.com/styled-components/styled-components/issues/1449

--- apps/ui/components/reservation-unit/index.ts
+++ apps/ui/components/reservation-unit/index.ts
@@ -1,4 +1,4 @@
-export { QuickReservation } from "./QuickReservation";
+// export { QuickReservation } from "./QuickReservation"; Don't export due to circular dependency issues
 export { ReservationInfoSection } from "./ReservationInfoSection";
 export { ReservationUnitCalendarSection } from "./ReservationUnitCalendarSection";
 export { Head } from "./Head";
