767a3c1a0fc442207be03ab6a5fc5126b9496763
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-07-14 16:25:21 +0300

    Fix allocation selection reload on drag
    Replace query params with context, because query params cause gql queries to be refetched.

---
+++ apps/admin-ui/src/pages/application-rounds/[id]/allocation/SelectedSlotsContext.tsx
@@ -0,0 +1,38 @@
+import { DayT } from "common/src/const";
+import React, { createContext, useContext, useMemo, useState } from "react";
+
+type SelectedSlotsContextProps = {
+  selection: TimeSlotRange | null;
+  setSelection: (newSelection: TimeSlotRange | null) => void;
+};
+
+export type TimeSlotRange = {
+  day: DayT;
+  begins: number;
+  ends: number;
+};
+
+const SelectedSlotsContext = createContext<SelectedSlotsContextProps>({
+  selection: null,
+  setSelection: () => undefined,
+});
+
+export const useSelectedSlots = (): SelectedSlotsContextProps => useContext(SelectedSlotsContext);
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const SelectedSlotsContextProvider: React.FC<Props> = ({ children }: Props) => {
+  const [selection, setSelection] = useState<TimeSlotRange | null>(null);
+
+  const value = useMemo(
+    () => ({
+      selection,
+      setSelection: (newSelection: TimeSlotRange | null) => setSelection(newSelection),
+    }),
+    [selection]
+  );
+
+  return <SelectedSlotsContext.Provider value={value}>{children}</SelectedSlotsContext.Provider>;
+};
