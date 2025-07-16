d612c269648a736377f742c970b609854f42dc87
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-07-16 16:23:24 +0300

    Move units components

---
+++ apps/admin-ui/src/pages/units/[id]/_components/index.ts
@@ -0,0 +1,9 @@
+export { ResourcesTable } from "./ResourcesTable";
+export { SpacesTable } from "./SpacesTable";
+export { SubPageHead } from "./SubPageHead";
+export { NewSpaceModal } from "./new-space-modal/NewSpaceModal";
+export { NewResourceModal } from "./NewResourceModal";
+export { ResourceEditorFields } from "./ResourceEditForm";
+export { Editor, ResourceUpdateSchema, type ResourceUpdateForm } from "./modules/resourceEditor";
+export { ParentSelector } from "./ParentSelector";
+export { type SpaceUpdateForm, SpaceForm, SpaceUpdateSchema } from "./SpaceForm";
