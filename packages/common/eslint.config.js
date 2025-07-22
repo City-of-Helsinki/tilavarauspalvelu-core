822bf61ed6375878c947164ae6141439ec604de7
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-07-22 14:41:52 +0300

    Remove eslint

--- packages/common/eslint.config.js
+++
@@ -1,6 +0,0 @@
-import baseConfig, { constructGQLConfig } from "eslint-config-custom/react.mjs";
-
-const appPath = "../../packages/common";
-const config = [...baseConfig, constructGQLConfig(appPath, true)];
-
-export default config;
