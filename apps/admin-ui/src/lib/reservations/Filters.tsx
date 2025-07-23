c03192403d716ade761253bebafa9cce75bfb1ba
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-07-23 15:11:59 +0300

    Replace `admin` query param search filters with controlled

--- apps/admin-ui/src/lib/reservations/Filters.tsx
+++ apps/admin-ui/src/lib/reservations/Filters.tsx
@@ -1,26 +1,164 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useTranslation } from "next-i18next";
 import styled from "styled-components";
 import { ShowAllContainer } from "common/src/components";
 import { useReservationUnitTypes, useUnitOptions, useReservationUnitOptions } from "@/hooks";
 import { Flex } from "common/styled";
 import {
-  CheckboxFilter,
-  DateRangeFilter,
-  MultiSelectFilter,
-  RangeNumberFilter,
-  SearchFilter,
-  SelectFilter,
+  ControlledCheckboxFilter,
+  ControlledMultiSelectFilter,
+  ControlledSearchFilter,
+  ControlledSelectFilter,
+  ControlledDateRangeFilter,
+  ControlledRangeNumberFilter,
 } from "@/component/QueryParamFilters";
 import { SearchTags } from "@/component/SearchTags";
 import { OrderStatusWithFree, ReservationTypeChoice, ReservationStateChoice } from "@gql/gql-types";
 import { type TagOptionsList, translateTag } from "@/modules/search";
+import { useForm } from "react-hook-form";
+import { useSetSearchParams } from "@/hooks/useSetSearchParams";
+import { SearchButton, SearchButtonContainer } from "@/component/SearchButton";
+import { useSearchParams } from "next/navigation";
+import { transformReservationState, transformReservationType } from "common/src/conversion";
+import { filterNonNullable } from "common/src/helpers";
 
 const MoreWrapper = styled(ShowAllContainer)`
   .ShowAllContainer__ToggleButton {
     color: var(--color-bus);
   }
 `;
+
+type SearchFormValues = {
+  reservationType: ReservationTypeChoice[];
+  state: ReservationStateChoice[];
+  reservationUnit: number[];
+  search?: string;
+  dateGte?: string;
+  dateLte?: string;
+  unit: number[];
+  reservationUnitType: number[];
+  minPrice?: number;
+  maxPrice?: number;
+  orderStatus: OrderStatusWithFree[];
+  createdAtGte?: string;
+  createdAtLte?: string;
+  recurring?: "only" | "onlyNot" | undefined;
+  freeOfCharge: boolean;
+};
+
+// TODO the reverse of this (from search params to form values)
+// TODO might just be better make this generic and copy all form values to search params (not specific keys)
+function mapFormToSearchParams(data: SearchFormValues): URLSearchParams {
+  const params = new URLSearchParams();
+  if (data.search) {
+    params.set("search", data.search);
+  }
+  if (data.dateGte) {
+    params.set("dateGte", data.dateGte);
+  }
+  if (data.dateLte) {
+    params.set("dateLte", data.dateLte);
+  }
+  if (data.unit.length > 0) {
+    data.unit.forEach((unit) => params.append("unit", unit.toString()));
+  }
+  if (data.reservationUnitType.length > 0) {
+    data.reservationUnitType.forEach((type) => params.append("reservationUnitType", type.toString()));
+  }
+  if (data.minPrice !== undefined) {
+    params.set("minPrice", data.minPrice.toString());
+  }
+  if (data.maxPrice !== undefined) {
+    params.set("maxPrice", data.maxPrice.toString());
+  }
+  if (data.orderStatus.length > 0) {
+    data.orderStatus.forEach((status) => params.append("orderStatus", status));
+  }
+  if (data.createdAtGte) {
+    params.set("createdAtGte", data.createdAtGte);
+  }
+  if (data.createdAtLte) {
+    params.set("createdAtLte", data.createdAtLte);
+  }
+  if (data.recurring !== undefined) {
+    params.set("recurring", data.recurring.toString());
+  }
+  if (data.freeOfCharge) {
+    params.set("freeOfCharge", "true");
+  }
+  if (data.reservationType.length > 0) {
+    data.reservationType.forEach((type) => params.append("reservationType", type));
+  }
+  if (data.state.length > 0) {
+    data.state.forEach((state) => params.append("state", state));
+  }
+  if (data.reservationUnit.length > 0) {
+    data.reservationUnit.forEach((unit) => params.append("reservationUnit", unit.toString()));
+  }
+  return params;
+}
+
+function useOptions() {
+  const { t } = useTranslation("filters");
+  const { options: reservationUnitTypes } = useReservationUnitTypes();
+  const { options: units } = useUnitOptions();
+  const { options: reservationUnits } = useReservationUnitOptions();
+
+  const states = Object.values(ReservationStateChoice)
+    .filter((s) => s !== ReservationStateChoice.Created)
+    .map((s) => ({
+      value: s,
+      label: t(`reservation:state.${s}`),
+    }));
+
+  const orderStatus = Object.values(OrderStatusWithFree).map((s) => ({
+    value: s,
+    label: t(`translation:orderStatus.${s}`),
+  }));
+
+  const reservationTypes = Object.values(ReservationTypeChoice).map((s) => ({
+    value: s,
+    label: t(`filters:reservationTypeChoice.${s}`),
+  }));
+
+  const recurring = [
+    { value: "only", label: t("filters:label.onlyRecurring") },
+    { value: "onlyNot", label: t("filters:label.onlyNotRecurring") },
+  ];
+
+  return {
+    reservationUnitTypes,
+    units,
+    reservationUnits,
+    states,
+    orderStatus,
+    reservationTypes,
+    recurring,
+  };
+}
+
+// TODO replace with safer version that checks for valid values
+// also a generice would be nice
+function mapParamsToForm(params: URLSearchParams): SearchFormValues {
+  return {
+    reservationType: filterNonNullable(params.getAll("reservationType").map(transformReservationType)),
+    state: filterNonNullable(params.getAll("state").map(transformReservationState)),
+    reservationUnit: params.getAll("reservationUnit").map(Number),
+    search: params.get("search") ?? undefined,
+    dateGte: params.get("dateGte") ?? undefined,
+    dateLte: params.get("dateLte") ?? undefined,
+    unit: params.getAll("unit").map(Number),
+    reservationUnitType: params.getAll("reservationUnitType").map(Number),
+    minPrice: params.has("minPrice") ? Number(params.get("minPrice")) : undefined,
+    maxPrice: params.has("maxPrice") ? Number(params.get("maxPrice")) : undefined,
+    orderStatus: params.getAll("orderStatus") as OrderStatusWithFree[],
+    createdAtGte: params.get("createdAtGte") ?? undefined,
+    createdAtLte: params.get("createdAtLte") ?? undefined,
+    recurring:
+      params.get("recurring") === "only" ? "only" : params.get("recurring") === "onlyNot" ? "onlyNot" : undefined,
+    freeOfCharge: params.get("freeOfCharge") === "true",
+  };
+}
 
 export function Filters({
   defaultFilters = [],
@@ -32,40 +170,15 @@ export function Filters({
   clearButtonAriaLabel?: string;
 }>): JSX.Element {
   const { t } = useTranslation();
+  const setSearchParams = useSetSearchParams();
+  const searchParams = useSearchParams();
 
-  const { options: reservationUnitTypeOptions } = useReservationUnitTypes();
-
-  const stateOptions = Object.values(ReservationStateChoice)
-    .filter((s) => s !== ReservationStateChoice.Created)
-    .map((s) => ({
-      value: s,
-      label: t(`reservation:state.${s}`),
-    }));
-
-  const paymentStatusOptions = Object.values(OrderStatusWithFree).map((s) => ({
-    value: s,
-    label: t(`translation:orderStatus.${s}`),
-  }));
-
-  const reservationTypeOptions = Object.values(ReservationTypeChoice).map((s) => ({
-    value: s,
-    label: t(`filters:reservationTypeChoice.${s}`),
-  }));
-
-  const { options: unitOptions } = useUnitOptions();
-
-  const { options: reservationUnitOptions } = useReservationUnitOptions();
-
-  const recurringOptions = [
-    { value: "only", label: t("filters:label.onlyRecurring") },
-    { value: "onlyNot", label: t("filters:label.onlyNotRecurring") },
-  ];
-
-  const options: TagOptionsList = {
-    reservationUnitTypes: reservationUnitTypeOptions,
-    stateChoices: stateOptions,
-    reservationUnits: reservationUnitOptions,
-    units: unitOptions,
+  const options = useOptions();
+  const tagOptions: TagOptionsList = {
+    reservationUnitTypes: options.reservationUnitTypes,
+    stateChoices: options.states,
+    reservationUnits: options.reservationUnits,
+    units: options.units,
     // Not needed on this page
     orderChoices: [],
     priorityChoices: [],
@@ -77,28 +190,56 @@ export function Filters({
     municipalities: [],
   };
 
+  const defaultValues = mapParamsToForm(searchParams);
+  const form = useForm<SearchFormValues>({
+    defaultValues,
+  });
+  const { handleSubmit, control, reset } = form;
+  useEffect(() => {
+    reset(mapParamsToForm(searchParams));
+  }, [searchParams, reset]);
+
+  const onSubmit = (data: SearchFormValues) => {
+    const searchParams = mapFormToSearchParams(data);
+    setSearchParams(searchParams);
+  };
+
   return (
-    <Flex>
+    <Flex as="form" noValidate onSubmit={handleSubmit(onSubmit)} $direction="column" $gap="s">
       <MoreWrapper showAllLabel={t("filters:moreFilters")} showLessLabel={t("filters:lessFilters")} maximumNumber={4}>
-        <MultiSelectFilter options={reservationTypeOptions} name="reservationType" />
-        <MultiSelectFilter options={stateOptions} name="state" />
-        <MultiSelectFilter options={reservationUnitOptions} name="reservationUnit" />
-        <SearchFilter name="search" labelKey="searchReservation" />
-        <DateRangeFilter name="date" />
-        <MultiSelectFilter options={unitOptions} name="unit" />
-        <MultiSelectFilter options={reservationUnitTypeOptions} name="reservationUnitType" />
-        <RangeNumberFilter label={t("filters:label.price")} minName="minPrice" maxName="maxPrice" />
-        <MultiSelectFilter name="orderStatus" options={paymentStatusOptions} />
-        <DateRangeFilter name="createdAt" />
-        <SelectFilter name="recurring" options={recurringOptions} clearable />
-        <CheckboxFilter name="freeOfCharge" />
+        <ControlledMultiSelectFilter control={control} options={options.reservationTypes} name="reservationType" />
+        <ControlledMultiSelectFilter control={control} options={options.states} name="state" />
+        <ControlledMultiSelectFilter control={control} options={options.reservationUnits} name="reservationUnit" />
+        <ControlledSearchFilter control={control} name="search" labelKey="searchReservation" />
+        <ControlledDateRangeFilter control={control} nameBegin="dateGte" nameEnd="dateLte" />
+        <ControlledMultiSelectFilter control={control} options={options.units} name="unit" />
+        <ControlledMultiSelectFilter
+          control={control}
+          options={options.reservationUnitTypes}
+          name="reservationUnitType"
+        />
+        <ControlledRangeNumberFilter
+          control={control}
+          label={t("filters:label.price")}
+          minName="minPrice"
+          maxName="maxPrice"
+        />
+        {/* FIXME doesn't work properly */}
+        <ControlledMultiSelectFilter control={control} name="orderStatus" options={options.orderStatus} />
+        <ControlledDateRangeFilter control={control} nameBegin="createdAtGte" nameEnd="createdAtLte" />
+        {/* FIXME doesn't work properly */}
+        <ControlledSelectFilter control={control} name="recurring" options={options.recurring} clearable />
+        <ControlledCheckboxFilter control={control} name="freeOfCharge" />
       </MoreWrapper>
-      <SearchTags
-        translateTag={translateTag(t, options)}
-        defaultTags={defaultFilters}
-        clearButtonLabel={clearButtonLabel}
-        clearButtonAriaLabel={clearButtonAriaLabel}
-      />
+      <SearchButtonContainer>
+        <SearchTags
+          translateTag={translateTag(t, tagOptions)}
+          defaultTags={defaultFilters}
+          clearButtonLabel={clearButtonLabel}
+          clearButtonAriaLabel={clearButtonAriaLabel}
+        />
+        <SearchButton />
+      </SearchButtonContainer>
     </Flex>
   );
 }
