cc70703cc6f22ac3f758727c62922dde1486ce23
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-07-24 18:18:04 +0300

    WIP: testing methods to not refetch queries on searchParam changes

--- apps/admin-ui/src/hooks/auth/index.ts
+++ apps/admin-ui/src/hooks/auth/index.ts
@@ -1,10 +1,26 @@
 import { useCurrentUserQuery } from "@gql/gql-types";
+import { useEffect } from "react";
 
+const POLLING_INTERVAL = 60 * 1000; // 1 minute
+
+// Testing use of manual polling / refetching with current user query.
+// problem with Apollo hooks is that nextjs reruns all hooks on query param change.
 export function useSession() {
-  const { data, error } = useCurrentUserQuery({
-    fetchPolicy: "cache-and-network",
-    nextFetchPolicy: "cache-first",
+  const { data, error, refetch } = useCurrentUserQuery({
+    // SSR query is written to cache
+    fetchPolicy: "cache-only",
+    // Disable updates on hook remounts
+    nextFetchPolicy: "standby",
   });
+
+  // setup polling
+  useEffect(() => {
+    const id = setInterval(() => {
+      refetch();
+    }, POLLING_INTERVAL);
+    return () => clearTimeout(id);
+  }, [refetch]);
+
   const user = data?.currentUser ?? null;
 
   return { isAuthenticated: user != null, user, error };
