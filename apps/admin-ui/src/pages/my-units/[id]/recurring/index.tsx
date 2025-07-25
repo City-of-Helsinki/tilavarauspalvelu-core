49e1d05dd18be91b528ee6726fac78955bee51da
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-07-25 14:42:01 +0300

    Refactor use SSR queries for series pages

--- apps/admin-ui/src/pages/my-units/[id]/recurring/index.tsx
+++ apps/admin-ui/src/pages/my-units/[id]/recurring/index.tsx
@@ -1,30 +1,38 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
 import { ReservationSeriesForm } from "@lib/my-units/[id]/recurring/ReservationSeriesForm";
-import { useSeriesReservationsUnits } from "@/hooks";
 import { LinkPrev } from "@/component/LinkPrev";
-import { CenterSpinner, H1 } from "common/styled";
-import { ignoreMaybeArray, toNumber } from "common/src/helpers";
+import { H1 } from "common/styled";
+import { base64encode, ignoreMaybeArray, toNumber } from "common/src/helpers";
 import { AuthorizationChecker } from "@/component/AuthorizationChecker";
 import { getCommonServerSideProps } from "@/modules/serverUtils";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { type GetServerSidePropsContext } from "next";
 import { NOT_FOUND_SSR_VALUE } from "@/common/const";
+import {
+  SeriesReservationUnitDocument,
+  type SeriesReservationUnitQuery,
+  type SeriesReservationUnitQueryVariables,
+} from "@gql/gql-types";
+import { createClient } from "@/common/apolloClient";
+import { gql } from "@apollo/client";
 
 type PageProps = Awaited<ReturnType<typeof getServerSideProps>>["props"];
 type PropsNarrowed = Exclude<PageProps, { notFound: boolean }>;
-export default function Page({ apiBaseUrl, unitPk }: PropsNarrowed): JSX.Element {
+export default function Page({ apiBaseUrl, unitPk, reservationUnits }: PropsNarrowed): JSX.Element {
   const { t } = useTranslation();
-  const { loading, reservationUnits } = useSeriesReservationsUnits(unitPk);
+
+  const reservationUnitOptions = reservationUnits.map((unit) => ({
+    label: unit?.nameFi ?? "",
+    value: unit?.pk ?? 0,
+  }));
 
   return (
     <AuthorizationChecker apiUrl={apiBaseUrl}>
       <LinkPrev />
       <H1 $noMargin>{t("myUnits:ReservationSeries.pageTitle")}</H1>
-      {loading ? (
-        <CenterSpinner />
-      ) : reservationUnits.length > 0 ? (
-        <ReservationSeriesForm reservationUnits={reservationUnits} unitPk={unitPk} />
+      {reservationUnitOptions.length > 0 ? (
+        <ReservationSeriesForm reservationUnitOptions={reservationUnitOptions} unitPk={unitPk} />
       ) : (
         <p>{t("myUnits:ReservationSeries.error.notPossibleForThisUnit")}</p>
       )}
@@ -32,16 +40,42 @@ export default function Page({ apiBaseUrl, unitPk }: PropsNarrowed): JSX.Element
   );
 }
 
-export async function getServerSideProps({ query, locale }: GetServerSidePropsContext) {
+export async function getServerSideProps({ query, locale, req }: GetServerSidePropsContext) {
   const unitPk = toNumber(ignoreMaybeArray(query.id)) ?? 0;
   if (unitPk <= 0) {
     return NOT_FOUND_SSR_VALUE;
   }
+  const commonProps = await getCommonServerSideProps();
+  const apolloClient = createClient(commonProps.apiBaseUrl, req);
+  const unitData = await apolloClient.query<SeriesReservationUnitQuery, SeriesReservationUnitQueryVariables>({
+    query: SeriesReservationUnitDocument,
+    variables: { id: base64encode(`UnitNode:${unitPk}`) },
+  });
+  const reservationUnits = unitData.data.unit?.reservationUnits ?? [];
   return {
     props: {
       unitPk,
-      ...(await getCommonServerSideProps()),
+      reservationUnits,
+      ...commonProps,
       ...(await serverSideTranslations(locale ?? "fi")),
     },
   };
 }
+
+export const SERIES_RESERVATION_UNIT_QUERY = gql`
+  query SeriesReservationUnit($id: ID!) {
+    unit(id: $id) {
+      id
+      nameFi
+      pk
+      reservationUnits {
+        id
+        pk
+        nameFi
+        reservationStartInterval
+        bufferTimeBefore
+        bufferTimeAfter
+      }
+    }
+  }
+`;
