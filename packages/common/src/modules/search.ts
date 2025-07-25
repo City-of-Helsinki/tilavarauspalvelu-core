41e71c2ad122c1514da315ccf53e511959251c00
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-07-25 15:38:39 +0300

    Refactor generic form -> query params conversion

--- packages/common/src/modules/search.ts
+++ packages/common/src/modules/search.ts
@@ -9,3 +9,25 @@ export type OptionsListT = Readonly<{
   ageGroups: Readonly<OptionT[]>;
   municipalities: Readonly<{ value: MunicipalityChoice; label: string }[]>;
 }>;
+
+export function mapFormToSearchParams<T extends object>(data: T): URLSearchParams {
+  const params = new URLSearchParams();
+  for (const [k, v] of Object.entries(data)) {
+    if (v == null) {
+      continue;
+    }
+    if (Array.isArray(v) && v.length === 0) {
+      continue;
+    }
+    if (Array.isArray(v)) {
+      for (const item of v) {
+        if (item != null) {
+          params.append(k, item.toString());
+        }
+      }
+    } else {
+      params.set(k, v.toString());
+    }
+  }
+  return params;
+}
