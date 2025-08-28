dda7d23ca9beecc2fbeb534311aa05a9bdef92be
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-08-28 11:37:47 +0300

    Undine: Fix filter invalid type in options query

--- apps/ui/modules/queryOptions.tsx
+++ apps/ui/modules/queryOptions.tsx
@@ -1,7 +1,7 @@
 import { type ApolloClient } from "@apollo/client";
 import { OptionsDocument, type OptionsQueryVariables, type OptionsQuery } from "@gql/gql-types";
 import { getTranslationSafe } from "common/src/common/util";
-import { filterNonNullable, getLocalizationLang } from "common/src/helpers";
+import { filterNonNullable, getLocalizationLang, sort } from "common/src/helpers";
 
 export async function queryOptions(apolloClient: ApolloClient<unknown>, locale: string) {
   const { data } = await apolloClient.query<OptionsQuery, OptionsQueryVariables>({
@@ -11,13 +11,13 @@ export async function queryOptions(apolloClient: ApolloClient<unknown>, locale: 
 
   const lang = getLocalizationLang(locale);
 
-  const reservationPurposeOptions = data.allReservationPurposes.map((purpose) => ({
+  // incorrect non-nullable types for data
+  const reservationPurposeOptions = filterNonNullable(data?.allReservationPurposes).map((purpose) => ({
     label: getTranslationSafe(purpose, "name", lang),
     value: purpose.pk ?? 0,
   }));
 
-  const ageGroups = filterNonNullable(data.allAgeGroups);
-  const sortedAgeGroups = ageGroups.sort((a, b) => a.minimum - b.minimum);
+  const sortedAgeGroups = sort(filterNonNullable(data?.allAgeGroups), (a, b) => a.minimum - b.minimum);
   const ageGroupOptions = [
     // the sortedAgeGroups array has "1 - 99" as the first element, so let's move it to the end for correct order
     ...sortedAgeGroups.slice(1),
