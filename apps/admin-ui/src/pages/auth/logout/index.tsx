847547c810245b2503ce0af639b47f7b3e0bb17c
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-09-01 14:47:48 +0300

    fix: Cleaner logout redirect without hacks

--- apps/admin-ui/src/pages/auth/logout/index.tsx
+++ apps/admin-ui/src/pages/auth/logout/index.tsx
@@ -1,30 +1,13 @@
-import { useEffect } from "react";
-import { getVersion } from "@/modules/baseUtils.mjs";
-import { useRouter } from "next/router";
-import { GetServerSidePropsContext } from "next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-
-type Props = Awaited<ReturnType<typeof getServerSideProps>>["props"];
-
-/* Page to redirect to the front page after a successful logout
- * TODO might be able to replace this with middleware
- * */
-export default function LogoutPage({ redirectUrl }: Props) {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace(redirectUrl);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+export default function LogoutPage() {
   return null;
 }
 
-export async function getServerSideProps({ locale }: GetServerSidePropsContext) {
+// eslint-disable-next-line require-await
+export async function getServerSideProps() {
   return {
-    props: {
-      redirectUrl: "/",
-      version: getVersion(),
-      ...(await serverSideTranslations(locale ?? "fi")),
+    redirect: {
+      destination: "/",
+      permanent: false,
     },
   };
 }
