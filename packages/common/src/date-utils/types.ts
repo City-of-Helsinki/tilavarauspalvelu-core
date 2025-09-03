9f445bd8c738ffc6807f201f62d9e9b1d6fcd59d
Mats Eriksson <mats.eriksson@vincit.fi>
2025-09-03 18:12:11 +0300

    add date-utils to common

---
+++ packages/common/src/date-utils/types.ts
@@ -0,0 +1,42 @@
+import type { TFunction } from "next-i18next";
+import type { LocalizationLanguages } from "../urlBuilder";
+
+export type TimeStruct = {
+  hours?: number;
+  minutes?: number;
+  seconds?: number;
+};
+
+export interface CommonDateOptions {
+  t?: TFunction;
+  locale?: LocalizationLanguages | "fi" | "sv" | "en";
+  formatString?: string;
+}
+
+export interface FormatTimeOptions extends CommonDateOptions {
+  includeTimeSeparator?: boolean;
+}
+
+export interface FormatDateOptions extends CommonDateOptions {
+  includeWeekday?: boolean;
+}
+
+export interface FormatDateTimeOptions extends CommonDateOptions {
+  includeWeekday?: boolean;
+  includeTimeSeparator?: boolean;
+}
+
+export interface FormatDateRangeOptions extends FormatDateOptions {
+  showEndDate?: boolean;
+}
+
+export interface FormatDateTimeRangeOptions extends FormatDateTimeOptions {
+  showEndDate?: boolean;
+}
+
+export interface ReservationDateTime {
+  date: Date;
+  time: string;
+  dayOfWeek: string;
+  isModified: boolean;
+}
