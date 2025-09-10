0896695aa3febe36a7444e0ac9260b99632aa797
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-09-10 10:35:08 +0300

    refactor reservation form css

--- packages/common/src/reservation-form/CustomerTypeSelector.tsx
+++ packages/common/src/reservation-form/CustomerTypeSelector.tsx
@@ -6,16 +6,21 @@ import { IconGroup, IconUser } from "hds-react";
 import IconPremises from "../icons/IconPremises";
 import { ReserveeType } from "../../gql/gql-types";
 import { RadioButtonWithImage } from "./RadioButtonWithImage";
+import { Flex, FullRow } from "../../styled";
 
-const CustomerTypeSelectContainer = styled.div`
-  display: flex;
-  margin-bottom: var(--spacing-3-xl);
-  width: 100%;
-  gap: var(--spacing-xs);
-  flex-wrap: wrap;
+const CustomerTypeSelectContainer = styled(Flex).attrs({
+  $wrap: "wrap",
+  $gap: "m",
+  $direction: "row",
+})`
+  grid-column: 1 / -1;
 `;
 
-const reserveeOptions = [
+const TypeLabel = styled(FullRow).attrs({ as: "p" })`
+  margin: 0;
+`;
+
+const RESERVEE_OPTIONS = [
   {
     id: ReserveeType.Individual,
     icon: <IconUser />,
@@ -35,28 +40,26 @@ export function CustomerTypeSelector() {
 
   return (
     <>
-      <p id="reserveeType-label">{t("reservationApplication:reserveeTypePrefix")}</p>
+      <TypeLabel id="reserveeType-label">{t("reservationApplication:reserveeTypePrefix")}</TypeLabel>
       <CustomerTypeSelectContainer data-testid="reservation__checkbox--reservee-type">
         <Controller
           name="reserveeType"
           render={({ field: { value, onChange } }) => (
             <>
-              {reserveeOptions
-                .map(({ id, icon }) => ({
-                  choice: id,
-                  icon,
-                  name: id,
-                }))
-                .map(({ choice, icon, name }) => (
-                  <RadioButtonWithImage
-                    key={choice}
-                    id={`reserveeType__${name}`}
-                    label={t(`reservationApplication:reserveeTypes.labels.${name}`)}
-                    onClick={() => onChange(choice)}
-                    icon={icon}
-                    checked={value === choice}
-                  />
-                ))}
+              {RESERVEE_OPTIONS.map(({ id, icon }) => ({
+                choice: id,
+                icon,
+                name: id,
+              })).map(({ choice, icon, name }) => (
+                <RadioButtonWithImage
+                  key={choice}
+                  id={`reserveeType__${name}`}
+                  label={t(`reservationApplication:reserveeTypes.labels.${name}`)}
+                  onClick={() => onChange(choice)}
+                  icon={icon}
+                  checked={value === choice}
+                />
+              ))}
             </>
           )}
         />
