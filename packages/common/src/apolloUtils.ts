0fa237d9a01d4bf321139810528a0cbdec593eeb
Eemeli Ranta <eemeli.ranta@vincit.fi>
2025-09-15 19:39:54 +0300

    oxc: Disable `unicorn/prefer-node-protocol`
    Automatic fixing causes this error on build:
    `Module build failed: UnhandledSchemeError: Reading from "node:querystring" is not handled by plugins (Unhandled scheme).`

--- packages/common/src/apolloUtils.ts
+++ packages/common/src/apolloUtils.ts
@@ -5,7 +5,7 @@ import { onError } from "@apollo/client/link/error";
 import toast from "./components/toast";
 import { isBrowser } from "./helpers";
 import type { IncomingMessage, IncomingHttpHeaders } from "node:http";
-import qs from "node:querystring";
+import qs from "querystring";
 import { getCookie } from "typescript-cookie";
 
 // TODO narrow down the error codes and transform unknowns to catch all
