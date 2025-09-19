1f59a60ce57145e2eebfb4309604a6f81f1ffc8b
Matti Lamppu <matti.lamppu@vincit.fi>
2025-09-19 09:17:45 +0300

    Setup locale in wsgi.py if not set

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,8 +1,18 @@
 from __future__ import annotations
 
+import locale
+import logging
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+logger = logging.getLogger(__name__)
+
+# Setup locale is not set in the environment.
+lang_code, encoding = locale.getlocale()
+if lang_code is None:
+    logger.info("Locale not set up. Defaulting to 'C.UTF-8'.")
+    locale.setlocale(locale.LC_ALL, "")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
