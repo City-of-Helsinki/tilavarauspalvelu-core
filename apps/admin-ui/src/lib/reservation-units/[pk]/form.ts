ab3d10d873f500a2cab07562355c99154961ae2f
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-09-19 14:14:57 +0300

    Refactor remove unnecessary duplicate enum

--- apps/admin-ui/src/lib/reservation-units/[pk]/form.ts
+++ apps/admin-ui/src/lib/reservation-units/[pk]/form.ts
@@ -23,8 +23,6 @@ import { WEEKDAYS_SORTED } from "common/src/const";
 import { type TaxOption } from "./PricingSection";
 import sanitizeHtml from "sanitize-html";
 
-export const AccessTypes = ["ACCESS_CODE", "OPENED_BY_STAFF", "PHYSICAL_KEY", "UNRESTRICTED"] as const;
-
 type QueryData = ReservationUnitEditQuery["reservationUnit"];
 type Node = NonNullable<QueryData>;
 
