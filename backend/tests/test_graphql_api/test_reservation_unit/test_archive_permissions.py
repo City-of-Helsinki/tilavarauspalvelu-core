53d4d49467371987fab858762dbfb71768bd8f8c
Matti Lamppu <matti.lamppu@vincit.fi>
2025-10-01 10:00:13 +0300

    Add separate reservation unit archival endpoint

---
+++ backend/tests/test_graphql_api/test_reservation_unit/test_archive_permissions.py
@@ -0,0 +1,75 @@
+from __future__ import annotations
+
+import pytest
+
+from tests.factories import ReservationUnitFactory, SpaceFactory, UnitFactory, UserFactory
+
+from .helpers import ARCHIVE_MUTATION
+
+# Applied to all tests
+pytestmark = [
+    pytest.mark.django_db,
+]
+
+
+def test_reservation_unit__archive__general_admin(graphql):
+    space = SpaceFactory.create()
+    reservation_unit = ReservationUnitFactory.create(is_draft=False, spaces=[space])
+
+    user = UserFactory.create_with_general_role()
+    graphql.force_login(user)
+
+    data = {"pk": reservation_unit.pk}
+
+    response = graphql(ARCHIVE_MUTATION, variables={"input": data})
+    assert response.has_errors is False, response
+
+
+def test_reservation_unit__archive__unit_admin(graphql):
+    space = SpaceFactory.create()
+    reservation_unit = ReservationUnitFactory.create(is_draft=False, spaces=[space])
+
+    user = UserFactory.create_with_unit_role(units=[reservation_unit.unit])
+    graphql.force_login(user)
+
+    data = {"pk": reservation_unit.pk}
+
+    response = graphql(ARCHIVE_MUTATION, variables={"input": data})
+    assert response.has_errors is False, response
+
+
+def test_reservation_unit__archive__unit_admin__different_unit(graphql):
+    space = SpaceFactory.create()
+    reservation_unit = ReservationUnitFactory.create(is_draft=False, spaces=[space])
+
+    unit = UnitFactory.create()
+
+    user = UserFactory.create_with_unit_role(units=[unit])
+    graphql.force_login(user)
+
+    data = {"pk": reservation_unit.pk}
+
+    response = graphql(ARCHIVE_MUTATION, variables={"input": data})
+    assert response.error_message() == "No permission to update."
+
+
+def test_reservation_unit__archive__regular_user(graphql):
+    space = SpaceFactory.create()
+    reservation_unit = ReservationUnitFactory.create(is_draft=False, spaces=[space])
+
+    graphql.login_with_regular_user()
+
+    data = {"pk": reservation_unit.pk}
+
+    response = graphql(ARCHIVE_MUTATION, variables={"input": data})
+    assert response.error_message() == "No permission to update."
+
+
+def test_reservation_unit__archive__anonymous_user(graphql):
+    space = SpaceFactory.create()
+    reservation_unit = ReservationUnitFactory.create(is_draft=False, spaces=[space])
+
+    data = {"pk": reservation_unit.pk}
+
+    response = graphql(ARCHIVE_MUTATION, variables={"input": data})
+    assert response.error_message() == "No permission to update."
