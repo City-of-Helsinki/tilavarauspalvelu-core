97f6c3182527ec56ce8a6b69eeadecf81756aefa
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-10-29 12:06:10 +0200

    Refactor move ReservationUnit status labels to common

--- frontend/apps/staff/src/lib/reservation-units/[pk]/DisplayUnit.tsx
+++ frontend/apps/staff/src/lib/reservation-units/[pk]/DisplayUnit.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { IconCheck, IconClock, IconEye, IconEyeCrossed, IconLock, IconPen, IconQuestionCircle } from "hds-react";
-import { useTranslation } from "next-i18next";
 import styled from "styled-components";
-import { Flex, fontBold, H1, NoWrap, TitleSection } from "ui/src/styled";
+import {
+  ReservationUnitPublishingStatusLabel,
+  ReservationUnitReservationStatusLabel,
+} from "ui/src/components/statuses";
+import { Flex, fontBold, H1, TitleSection } from "ui/src/styled";
 import { formatAddress } from "@/modules/util";
 import {
   ReservationUnitPublishingState,
   ReservationUnitReservationState,
   UnitSubpageHeadFragment,
 } from "@gql/gql-types";
-import { ReservationUnitPublishingStatusLabel, ReservationUnitReservationStatusLabel } from "./tags";
 
 const UnitInformationWrapper = styled.div`
   font-size: var(--fontsize-heading-s);
