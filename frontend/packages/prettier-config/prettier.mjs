44a9edd0e6ca6fd21c932d12a8d7bc92dfcb153a
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-11-03 14:29:36 +0200

    Add prettier plugin to sort imports

--- frontend/packages/prettier-config/prettier.mjs
+++ frontend/packages/prettier-config/prettier.mjs
@@ -1,7 +1,21 @@
+// NOTE invalid plugins will break all plugins (not just the invalid one)
+// e.g. for "@prettier/plugin-oxc" to work we'd need to setup parsers properly
+// otherwise all plugins are broken
+
 /** @type {import('prettier').Options} */
 export default {
   singleQuote: false,
-  plugins: ["@prettier/plugin-oxc"],
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
   trailingComma: "es5",
   printWidth: 120,
+  importOrder: [
+    "^react",
+    "<BUILTIN_MODULES>",
+    "<THIRD_PARTY_MODULES>",
+    "^ui/(.*)$",
+    "^@ui/(.*)$",
+    "^@/(.*)$",
+    "^@gql/(.*)$",
+    "^[./]",
+  ],
 };
