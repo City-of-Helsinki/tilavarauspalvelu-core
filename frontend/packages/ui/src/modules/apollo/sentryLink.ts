50665536969f886d2a68c23017a77abc8478085e
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2025-11-25 11:08:03 +0200

    Add graphql query names to sentry tracing as spans

--- frontend/packages/ui/src/modules/apollo/sentryLink.ts
+++ frontend/packages/ui/src/modules/apollo/sentryLink.ts
@@ -1,16 +1,37 @@
-import type { FetchResult, NextLink, Observable, Operation } from "@apollo/client";
-import { ApolloLink } from "@apollo/client";
+import type { FetchResult, NextLink, Operation } from "@apollo/client";
+import { ApolloLink, Observable } from "@apollo/client";
 import * as Sentry from "@sentry/nextjs";
 
 export class SentryContextLink extends ApolloLink {
   request(operation: Operation, forward?: NextLink): Observable<FetchResult> | null {
     const span = Sentry.getActiveSpan();
+    const name = `GRAPHQL: ${operation.operationName}`;
+    let gqlSpan = null;
     if (span) {
+      gqlSpan = Sentry.startInactiveSpan({ name, op: "graphql" });
       span.setAttributes({
         operation_name: operation.operationName,
       });
     }
 
-    return forward?.(operation) ?? null;
+    if (forward == null) {
+      return null;
+    }
+    return new Observable((observer) => {
+      const observable = forward(operation);
+      const subscription = observable.subscribe({
+        next(value) {
+          observer.next(value);
+        },
+        error(networkError) {
+          observer.error(networkError);
+        },
+        complete() {
+          gqlSpan?.end();
+          observer.complete();
+        },
+      });
+      return () => subscription.unsubscribe();
+    });
   }
 }
