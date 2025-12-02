392986863f622bda0bf9f67173dcc53b6c66db8f
Eemeli Ranta <eemeli.ranta@vincit.fi>
2025-12-02 18:49:10 +0200

    ReservationMetadata: Remove actions, validators, querysets, managers

--- backend/tilavarauspalvelu/models/reservation_metadata_field/model.py
+++ backend/tilavarauspalvelu/models/reservation_metadata_field/model.py
@@ -1,20 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, ClassVar
-
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from lazy_managers import LazyModelAttribute, LazyModelManager
-
-if TYPE_CHECKING:
-    from tilavarauspalvelu.models import ReservationMetadataSet
-    from tilavarauspalvelu.models._base import ManyToManyRelatedManager
-    from tilavarauspalvelu.models.reservation_metadata_set.queryset import ReservationMetadataSetQuerySet
-
-    from .actions import ReservationMetadataFieldActions
-    from .queryset import ReservationMetadataFieldManager
-    from .validators import ReservationMetadataFieldValidator
-
 
 __all__ = [
     "ReservationMetadataField",
@@ -23,13 +10,6 @@ __all__ = [
 
 class ReservationMetadataField(models.Model):
     field_name: str = models.CharField(max_length=100, unique=True)
-
-    objects: ClassVar[ReservationMetadataFieldManager] = LazyModelManager.new()
-    actions: ReservationMetadataFieldActions = LazyModelAttribute.new()
-    validators: ReservationMetadataFieldValidator = LazyModelAttribute.new()
-
-    metadata_sets_supported: ManyToManyRelatedManager[ReservationMetadataSet, ReservationMetadataSetQuerySet]
-    metadata_sets_required: ManyToManyRelatedManager[ReservationMetadataSet, ReservationMetadataSetQuerySet]
 
     class Meta:
         db_table = "reservation_metadata_field"
--- backend/tilavarauspalvelu/models/reservation_metadata_set/model.py
+++ backend/tilavarauspalvelu/models/reservation_metadata_set/model.py
@@ -1,20 +1,14 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, ClassVar
+from typing import TYPE_CHECKING
 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from lazy_managers import LazyModelAttribute, LazyModelManager
 
 if TYPE_CHECKING:
-    from tilavarauspalvelu.models import ReservationMetadataField, ReservationUnit
-    from tilavarauspalvelu.models._base import ManyToManyRelatedManager, OneToManyRelatedManager
-    from tilavarauspalvelu.models.reservation_metadata_field.queryset import ReservationMetadataFieldQuerySet
+    from tilavarauspalvelu.models import ReservationUnit
+    from tilavarauspalvelu.models._base import OneToManyRelatedManager
     from tilavarauspalvelu.models.reservation_unit.queryset import ReservationUnitQuerySet
-
-    from .actions import ReservationMetadataSetActions
-    from .queryset import ReservationMetadataSetManager
-    from .validators import ReservationMetadataSetValidator
 
 
 __all__ = [
@@ -25,22 +19,16 @@ __all__ = [
 class ReservationMetadataSet(models.Model):
     name: str = models.CharField(max_length=100, unique=True)
 
-    supported_fields: ManyToManyRelatedManager[ReservationMetadataField, ReservationMetadataFieldQuerySet]
     supported_fields = models.ManyToManyField(
         "tilavarauspalvelu.ReservationMetadataField",
         related_name="metadata_sets_supported",
     )
 
-    required_fields: ManyToManyRelatedManager[ReservationMetadataField, ReservationMetadataFieldQuerySet]
     required_fields = models.ManyToManyField(
         "tilavarauspalvelu.ReservationMetadataField",
         related_name="metadata_sets_required",
         blank=True,
     )
-
-    objects: ClassVar[ReservationMetadataSetManager] = LazyModelManager.new()
-    actions: ReservationMetadataSetActions = LazyModelAttribute.new()
-    validators: ReservationMetadataSetValidator = LazyModelAttribute.new()
 
     reservation_units: OneToManyRelatedManager[ReservationUnit, ReservationUnitQuerySet]
 
